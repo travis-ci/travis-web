ecdf0b9c3aa0026d3285bd815c97cfdd90ceb73f
Sven Fuchs <me@svenfuchs.com>
2012-10-10 18:51:05 +0200

    fix negation

--- lib/travis/web/app/assets.rb
+++ lib/travis/web/app/assets.rb
@@ -11,17 +11,14 @@ class Travis::Web::App
 
     def call(env)
       path = env['PATH_INFO']
-      if !asset?(path)
-        app.call(map_env(env, config.version))
-      else
-        app.call(env)
-      end
+      env = map_env(env, config.version) if asset?(path)
+      app.call(env)
     end
 
     private
 
       def asset?(path)
-        path !~ Travis::Web::App::ASSET_DIRS
+        path =~ Travis::Web::App::ASSET_DIRS
       end
   end
 end
