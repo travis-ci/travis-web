e5b0869201e2086d3af1c3b9802d5a41a1b7fe76
Randy Morgan <digital.ipseity@gmail.com>
2012-12-04 00:18:17 +0900

    change ENV variable name for localeapp api key to LOCALEAPP_API_KEY

--- .localeapp/config.rb
+++ .localeapp/config.rb
@@ -1,5 +1,5 @@
 Localeapp.configure do |config|
-  config.api_key                    = ENV['LOCALEAPP']
+  config.api_key                    = ENV['LOCALEAPP_API_KEY']
   config.translation_data_directory = 'locales'
   config.synchronization_data_file  = '.localeapp/log.yml'
   config.daemon_pid_file            = '.localeapp/localeapp.pid'
