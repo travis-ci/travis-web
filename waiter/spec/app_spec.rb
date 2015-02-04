89e604fdc97d1d54a3ed08ccc53f47a78c5085ca
Piotr Sarnacki <drogus@gmail.com>
2015-02-04 19:55:34 +0100

    Remove specs for unused features in ruby part

--- waiter/spec/app_spec.rb
+++ waiter/spec/app_spec.rb
@@ -22,33 +22,4 @@ describe Travis::Web::App do
     example { headers['Cache-Control'].should include('public') }
     example { headers['Vary'].split(',').should_not include('Accept') }
   end
-
-  describe 'version' do
-    before  { get('/version') }
-    example { last_response.should be_ok }
-    example { headers['Content-Location'].should be == '/version' }
-    example { headers['Cache-Control'].should be == 'no-cache' }
-    example { headers['Vary'].split(',').should_not include('Accept') }
-  end
-
-  describe 'alternate asset versions' do
-    context 'not passing an alt param' do
-      before  { get('/') }
-      example { headers['Set-Cookie'].should be_nil }
-    end
-
-    context 'passing an alt param' do
-      before  { get('/?alt=foo') }
-      example { last_response.should be_ok }
-      example { last_response.body.should include('/assets/foo/styles/app.css') }
-      example { last_response.body.should include('/assets/foo/scripts/app.js') }
-      example { headers['Set-Cookie'].should == 'alt=foo; path=/; max-age=86400' }
-    end
-
-    context 'passing default as an alt param' do
-      before  { get('/?alt=default') }
-      example { last_response.body.should_not =~ /\/assets\/[^\/]+\/scripts\/app.js/ }
-      example { headers['Set-Cookie'].should == 'alt=; path=/; max-age=0' }
-    end
-  end
 end
