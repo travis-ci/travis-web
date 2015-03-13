e61846a43c492bad4a480394df742084cc65d173
Piotr Sarnacki <drogus@gmail.com>
2015-03-13 12:45:16 +0100

    Provide proper sourceEndpoint config

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,7 @@ module.exports = function(environment) {
 
     // defaults for running travis-web
     apiEndpoint: 'https://api.travis-ci.org',
-    source_endpoint: 'https://github.com',
+    sourceEndpoint: 'https://github.com',
     pusher: {
       key: '5df8ac576dcccf4fd076',
       host: 'ws.pusherapp.com'
