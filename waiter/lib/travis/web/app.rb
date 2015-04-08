23ede2715309e1bf563bd77c23423f91a3af4039
Piotr Sarnacki <drogus@gmail.com>
2015-04-08 10:15:56 +0200

    Update default page title

--- waiter/lib/travis/web/app.rb
+++ waiter/lib/travis/web/app.rb
@@ -169,7 +169,7 @@ class Travis::Web::App
     end
 
     def title
-      default_title = "Travis CI - Test and deploy your code in confidence"
+      default_title = "Travis CI - Test and deploy your code with confidence"
       ENV['SITE_TITLE'] || default_title
     end
 
