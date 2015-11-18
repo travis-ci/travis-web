df0f883ad0689b82ab76b76ea4ebdc4d38da511d
Piotr Sarnacki <drogus@gmail.com>
2015-11-18 11:48:59 +0100

    app/serializers/repo.coffee -> app/serializers/repo.js

---
+++ app/serializers/repo.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import V2FallbackSerializer from 'travis/serializers/v2_fallback';
+
+var Serializer = V2FallbackSerializer.extend({
+  isNewSerializerAPI: true
+});
+
+export default Serializer;
