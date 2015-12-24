daed6a6d8af9c48256aba617c8b94cfd8468e0f0
Piotr Sarnacki <drogus@gmail.com>
2015-12-24 11:05:31 +0100

    Comment preparing testem.json for now

--- ci/prepare_testem.rb
+++ ci/prepare_testem.rb
@@ -1,10 +1,10 @@
 require 'json'
 
-pull_request = ENV['TRAVIS_PULL_REQUEST'] != 'false'
+#pull_request = ENV['TRAVIS_PULL_REQUEST'] != 'false'
 
-testem = JSON.parse(File.read('testem.json'))
+#testem = JSON.parse(File.read('testem.json'))
 
-testem['launch_in_ci'] = ['PhantomJS']
-testem['launch_in_ci'] = ['SL_chrome', 'SL_firefox'] unless pull_request
+#testem['launch_in_ci'] = ['PhantomJS']
+#testem['launch_in_ci'] = ['SL_chrome', 'SL_firefox'] unless pull_request
 
-File.open('testem.json', 'w') { |f| f.write testem.to_json }
+#File.open('testem.json', 'w') { |f| f.write testem.to_json }
