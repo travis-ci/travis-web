069f7ddbeb5908b1a179ac99aa11a7d2f9a8ad8d
Piotr Sarnacki <drogus@gmail.com>
2016-01-21 16:33:08 +0100

    Fix displaying no-builds and not-active errors
    This commit unifies the behaviour of displaying no-builds and not-active
    errors for repo and fixes the error when user can't access repo settings
    when the project is not active.

--- app/routes/repo/index.js
+++ app/routes/repo/index.js
@@ -7,14 +7,41 @@ export default TravisRoute.extend({
     return this.controllerFor('repo').activate('current');
   },
 
-  renderTemplate() {
-    return this.render('build');
-  },
   deactivate() {
     var repo;
     repo = this.controllerFor('repo');
     this.controllerFor('build').set('build', null);
     this.controllerFor('job').set('job', null);
+    this.stopObservingRepoStatus();
     return this._super.apply(this, arguments);
+  },
+
+  activate() {
+    this.observeRepoStatus();
+    return this._super.apply(this, arguments);
+  },
+
+  observeRepoStatus() {
+    let controller = this.controllerFor('repo');
+    controller.addObserver('repo.active', this, 'renderTemplate');
+    controller.addObserver('repo.lastBuildId', this, 'renderTemplate');
+  },
+
+  stopObservingRepoStatus() {
+    let controller = this.controllerFor('repo');
+    controller.removeObserver('repo.active', this, 'renderTemplate');
+    controller.removeObserver('repo.lastBuildId', this, 'renderTemplate');
+  },
+
+  renderTemplate() {
+    let controller = this.controllerFor('repo');
+
+    if(!controller.get('repo.active')) {
+      this.render('repo/not-active');
+    } else if(!controller.get('repo.lastBuildId')) {
+      this.render('repo/no-build');
+    } else {
+      this.render('build');
+    }
   }
 });
