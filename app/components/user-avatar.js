d82ff4935956703cef56283646a8fd139425441f
Lisa P <mail@lislis.de>
2016-01-26 14:50:58 +0100

    change component implementation

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -7,25 +7,8 @@ export default Ember.Component.extend({
   tagName: 'span',
   classNames: ['avatar'],
 
-  gravatarUrl: function() {
-    if (!this.get('isAuthor') && !this.get('isComitter')) {
-      if (this.get('user.type') === 'organization') {
-        return this.get('user.avatarUrl');
-      } else {
-          return gravatarImage(this.get('user.email'), 36);
-      }
-    } else {
-        if (this.get('isAuthor')) {
-            return this.user.get('authorAvatarUrlOrGravatar');
-        } else if (this.get('isCommitter')) {
-            return this.user.get('committerAvatarUrlorGravatar');
-       }
-    }
-  }.property('gravatarUrl'),
-
   userInitials: function() {
-    var name = this.get('user.name') || this.get('user.login') ||
-              this.get('user.authorName') || this.get('user.committerName');
+    var name = this.get('name');
     var arr = name.split(' ');
     var initials;
 
