b5c50ddce743969ab9ee1aa95dd0fa2fcb997e66
Lisa P <mail@lislis.de>
2016-01-26 15:56:21 +0100

    hide some UI elements for enterprise

--- app/routes/home-pro.js
+++ app/routes/home-pro.js
@@ -1,5 +1,12 @@
 import TravisRoute from 'travis/routes/basic';
+import config from 'travis/config/environment';
 
 export default TravisRoute.extend({
-  needsAuth: false
+  needsAuth: false,
+
+  activate: function() {
+    if (config.enterprise) {
+      return  this.transitionTo('auth');
+    }
+  }
 });
