1c4f748b2e289ee790cd7e89c19b7b61949a15d2
Lisa P <mail@lislis.de>
2016-02-01 12:59:35 +0100

    fix tests, refactor repo-actions component

--- app/components/repo-actions.js
+++ app/components/repo-actions.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({});
+export default Ember.Component.extend({
+
+  classNames: ['repo-main-tools']
+
+});
--- tests/unit/components/repo-actions-test.js
+++ tests/unit/components/repo-actions-test.js
@@ -5,28 +5,11 @@ moduleForComponent('repo-actions', 'RepoActionsComponent', {
   needs: ['component:build-repo-actions', 'component:job-repo-actions']
 });
 
-test('it displays code climate if the repo language is ruby', function() {
-  var component, repo;
-  repo = Ember.Object.create({
-    githubLanguage: 'Ruby'
-  });
-  component = this.subject({
-    repo: repo
-  });
-  this.append();
-  ok(component.get('displayCodeClimate'), 'component should try to display code climate');
-  return ok(component.$('a[name=code-climate]').length, 'component should render a code climate button');
-});
+test('it renders', function() {
 
-test('it doesn\'t display code climate for other languages', function() {
-  var component, repo;
-  repo = Ember.Object.create({
-    githubLanguage: 'Go'
-  });
-  component = this.subject({
-    repo: repo
-  });
+  var component;
+  component = this.subject({});
   this.append();
-  ok(!component.get('displayCodeClimate'), 'component should not try to display code climate');
-  return ok(!component.$('a[name=code-climate]').length, 'component should not render a code climate button');
+  return ok(component.$().hasClass('repo-main-tools'), 'component has class');
+
 });
