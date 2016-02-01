cd7ab9b71966252298a53b9e08a6a15def67b14c
Lisa P <mail@lislis.de>
2016-02-01 12:08:07 +0100

    remove code climate
    remove code climate popup function

--- app/components/repo-actions.js
+++ app/components/repo-actions.js
@@ -1,15 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  displayCodeClimate: function() {
-    return this.get('repo.githubLanguage') === 'Ruby';
-  }.property('repo.githubLanguage'),
-
-  actions: {
-    codeClimatePopup() {
-      $('.popup').removeClass('display');
-      $('#code-climate').addClass('display');
-      return false;
-    }
-  }
-});
+export default Ember.Component.extend({});
