878f831798a7e70d986941d36e00fbcf3df5ebfd
Steffen Kötte <steffen.koette@gmail.com>
2016-02-22 09:34:11 +0100

    show existing crons

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,7 +1,26 @@
 import Ember from 'ember';
+import config from 'travis/config/environment';
 
 export default Ember.Controller.extend({
   envVars: Ember.computed.filterBy('model.envVars', 'isNew', false),
+
+  crons: function() {
+    var result, apiEndpoint, options, repoId;
+    apiEndpoint = config.apiEndpoint;
+    repoId = this.get('repo.id');
+    result = Ember.ArrayProxy.create();
+    options = {};
+    if (this.get('auth.signedIn')) {
+      options.headers = {
+        Authorization: "token " + (this.auth.token())
+      };
+    }
+    $.ajax(apiEndpoint + "/v3/repo/" + repoId + "/crons", options).then(function(response) {
+      return result.set('content', response.crons);
+    });
+    return result;
+  }.property('repo'),
+
   actions: {
     sshKeyAdded(sshKey) {
       return this.set('model.customSshKey', sshKey);
