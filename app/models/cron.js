52f053462f6e2a13779efb9012c02d35c68523d5
Steffen Kötte <steffen.koette@gmail.com>
2016-02-23 16:33:04 +0100

    add next_enqueuing to cron model

--- app/models/cron.js
+++ app/models/cron.js
@@ -6,5 +6,6 @@ import { hasMany, belongsTo } from 'ember-data/relationships';
 export default Model.extend({
   branch: belongsTo('branch', { async: true }),
   interval: attr('string'),
-  disable_by_build: attr('boolean')
+  disable_by_build: attr('boolean'),
+  next_enqueuing: attr('string')
 });
