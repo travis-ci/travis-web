96a7edcd05a1ca9af1eb91a89f251458d9e26ebf
Lisa P <mail@lislis.de>
2016-02-26 17:02:34 +0100

    use the logo
    make path toggle

--- app/controllers/top.js
+++ app/controllers/top.js
@@ -11,6 +11,8 @@ export default Ember.Controller.extend({
     return this.get('user.name') || this.get('user.login');
   }.property('user.login', 'user.name'),
 
+  isDashboard: false,
+
   defineTowerColor(broadcastArray) {
     if (!broadcastArray) {
       return '';
@@ -68,6 +70,12 @@ export default Ember.Controller.extend({
   }.property('broadcasts'),
 
   actions: {
+
+    cheatcode() {
+      $('body').toggleClass('cheatcode');
+      this.toggleProperty('isDashboard');
+    },
+
     toggleBurgerMenu() {
       this.toggleProperty('is-open');
       return false;
