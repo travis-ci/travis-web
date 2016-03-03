adb8753b605ecf5639294c6962c4102adc98f765
Lisa P <mail@lislis.de>
2016-03-03 16:41:14 +0100

    fix uppercase error for avatars

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -8,14 +8,14 @@ export default Ember.Component.extend({
   userInitials: function() {
     var name = this.get('name');
     var arr = name.split(' ');
-    var initials;
+    var initials = '';
 
     if (arr.length >= 2) {
       initials = arr[0].split('')[0] + arr[1].split('')[0];
     } else {
       initials = arr[0].split('')[0];
     }
-      return initials.toUpperCase();
+      return initials;
   }.property('userInitials')
 
 });
