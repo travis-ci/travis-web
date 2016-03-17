fa62539a401463c9e58b3eab4abb28b98ba0cf9a
Lisa P <mail@lislis.de>
2016-03-17 12:10:49 +0100

    add more tests

--- tests/unit/utils/permission-test.js
+++ tests/unit/utils/permission-test.js
@@ -4,12 +4,20 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | permission');
 
-
 test('it checks permissions', function(assert) {
-
   let user = Ember.Object.create({permissions: [1]});
-
   assert.ok(hasPermission(user, 1));
   assert.ok(!hasPermission(user, 2));
+});
 
+test('it checks push permissions', function(assert) {
+  let user = Ember.Object.create({pushPermissions: [1]});
+  assert.ok(hasPushPermission(user, 1));
+  assert.ok(!hasPushPermission(user, 2));
+});
+
+test('it checks admin permissions', function(assert) {
+  let user = Ember.Object.create({adminPermissions: [1]});
+  assert.ok(hasAdminPermission(user, 1));
+  assert.ok(!hasAdminPermission(user, 2));
 });
