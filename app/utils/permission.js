e66efc1cbce53eb0df8636115bbcdbf333884f1a
lislis <mail@lislis.de>
2016-03-22 12:24:44 +0100

    check for currentUser in permissions

--- app/utils/permission.js
+++ app/utils/permission.js
@@ -2,24 +2,30 @@
 var hasPermission = function(currentUser, repoId) {
   var id = parseInt(repoId);
   var permissions;
-  if (permissions = currentUser.get('permissions')) {
-    return permissions.contains(id);
+  if (currentUser) {
+    if (permissions = currentUser.get('permissions')) {
+      return permissions.contains(id);
+    }
   }
 };
 
 var hasPushPermission = function(currentUser, repoId) {
   var id = parseInt(repoId);
   var permissions;
-  if (permissions = currentUser.get('pushPermissions')) {
-    return permissions.contains(id);
+  if (currentUser) {
+    if (permissions = currentUser.get('pushPermissions')) {
+      return permissions.contains(id);
+    }
   }
 };
 
 var hasAdminPermission = function(currentUser, repoId) {
   var id = parseInt(repoId);
   var permissions;
-  if (permissions = currentUser.get('adminPermissions')) {
-    return permissions.contains(id);
+  if (currentUser) {
+    if (permissions = currentUser.get('adminPermissions')) {
+      return permissions.contains(id);
+    }
   }
 };
 
