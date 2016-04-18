16e279b05a5446ded154ca012bb7f15eb6501cc6
Buck Doyle <buck@travis-ci.org>
2016-04-18 15:19:28 +0200

    Extract GitHub status update to a script

---
+++ update-github-status.sh
@@ -0,0 +1,1 @@
+curl -X POST --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.org\", \"description\": \"Visit a deployment for this commit\", \"context\": \"deployment\"}" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_COMMIT
