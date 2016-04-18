5e50a8549df23dffcced0d2d0e1f67fbd4b5ec29
Buck Doyle <buck@travis-ci.org>
2016-04-18 16:43:23 +0200

    Add logging of commit information

--- deploy-pull-request.sh
+++ deploy-pull-request.sh
@@ -1,3 +1,6 @@
+curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST
+echo "commit:"
+echo $TRAVIS_COMMIT
 export TRAVIS_PULL_REQUEST_BRANCH=`curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST | grep -A 2 "\"head\"" | grep ref | cut -d '"' -f4`
 echo $TRAVIS_PULL_REQUEST_BRANCH
 
