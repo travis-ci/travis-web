912ee62f119d790fc55899acb59fbfd4663a28a8
Buck Doyle <buck@travis-ci.org>
2016-04-18 16:32:57 +0200

    Add logging

--- update-github-status.sh
+++ update-github-status.sh
@@ -1,1 +1,3 @@
+echo "trying to curl with this:"
+echo $TRAVIS_PULL_REQUEST_BRANCH
 curl -X POST --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.org\", \"description\": \"Visit a deployment for this commit\", \"context\": \"deployment\"}" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_COMMIT
