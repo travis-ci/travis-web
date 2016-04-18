67908d30594796f9df6cf6a7622709b88fa6bd1b
Buck Doyle <buck@travis-ci.org>
2016-04-18 15:40:51 +0200

    Extract deployment script

---
+++ deploy-pull-request.sh
@@ -0,0 +1,5 @@
+export TRAVIS_PULL_REQUEST_BRANCH=`curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST | grep -A 2 "\"head\"" | grep ref | cut -d '"' -f4`
+echo $TRAVIS_PULL_REQUEST_BRANCH
+
+ember deploy pull-request --activate --verbose
+./update-github-status.sh
