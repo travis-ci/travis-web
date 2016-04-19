94291dd115e1e728c7e14cc7950277da81d5bd64
Buck Doyle <buck@travis-ci.org>
2016-04-19 12:19:05 +0200

    Add sanitising of branch names
    Thanks to @clekstro for reminding me of this.

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,4 +1,4 @@
 curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST > pull-request.json
-export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json`
+export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
 ember deploy pull-request --activate --verbose
 ./config/deployment/update-github-status.sh
