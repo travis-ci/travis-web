2dc0bafa56021ec705c1d26d8c737287d373ea2d
Buck Doyle <buck@travis-ci.org>
2016-04-19 11:45:47 +0200

    Remove unused deployment targets
    We can add deployment targets easily.

--- config/deploy.js
+++ config/deploy.js
@@ -1,7 +1,5 @@
-var VALID_DEPLOY_TARGETS = [ //update these to match what you call your deployment targets
-  'dev',
+var VALID_DEPLOY_TARGETS = [
   'qa',
-  'pull-request'
 ];
 
 module.exports = function(deployTarget) {
@@ -20,23 +18,11 @@ module.exports = function(deployTarget) {
     throw new Error('Invalid deployTarget ' + deployTarget);
   }
 
-  if (deployTarget === 'dev') {
-    ENV.build.environment = 'development';
-    ENV.redis.url = process.env.REDIS_URL;
-    ENV.plugins = ['build', 'redis']; // only care about deploying index.html into redis in dev
-  }
-
-  if (deployTarget === 'qa' || deployTarget === 'pull-request') {
+  if (deployTarget === 'pull-request') {
     ENV.build.environment = 'production';
     ENV.s3.accessKeyId = process.env.AWS_KEY;
     ENV.s3.secretAccessKey = process.env.AWS_SECRET;
-  }
 
-  if (deployTarget === 'qa') {
-    ENV.redis.url = process.env.QA_REDIS_URL;
-  }
-
-  if (deployTarget === 'pull-request') {
     ENV.redis.keyPrefix = process.env.TRAVIS_PULL_REQUEST_BRANCH;
     ENV.redis.url = process.env.REDIS_URL;
 
@@ -52,24 +38,4 @@ module.exports = function(deployTarget) {
   }
 
   return ENV;
-
-  /* Note: a synchronous return is shown above, but ember-cli-deploy
-   * does support returning a promise, in case you need to get any of
-   * your configuration asynchronously. e.g.
-   *
-   *    var Promise = require('ember-cli/lib/ext/promise');
-   *    return new Promise(function(resolve, reject){
-   *      var exec = require('child_process').exec;
-   *      var command = 'heroku config:get REDISTOGO_URL --app my-app-' + deployTarget;
-   *      exec(command, function (error, stdout, stderr) {
-   *        ENV.redis.url = stdout.replace(/\n/, '').replace(/\/\/redistogo:/, '//:');
-   *        if (error) {
-   *          reject(error);
-   *        } else {
-   *          resolve(ENV);
-   *        }
-   *      });
-   *    });
-   *
-   */
 }
