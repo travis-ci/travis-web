3862189719ffd5069dfb54d24dfeec6001445aad
Buck Doyle <buck@travis-ci.org>
2016-04-19 15:50:01 +0200

    Extract GitHub API call

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,4 +1,4 @@
-curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST > pull-request.json
+./config/deployment/store-pull-request-data.sh
 export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
 ember deploy pull-request --activate --verbose
 ./config/deployment/update-github-status.sh
