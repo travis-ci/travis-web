0a2a52e30fbd97edc388ad42a69cbd2aa1325433
Buck Doyle <buck@travis-ci.org>
2016-04-19 11:07:34 +0200

    Use jq to parse pull request API response

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,7 +1,7 @@
-curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST
+curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST > pull-request.json
 echo "commit:"
 echo $TRAVIS_COMMIT
-export TRAVIS_PULL_REQUEST_BRANCH=`curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/travis-ci/travis-web/pulls/$TRAVIS_PULL_REQUEST | grep -A 2 "\"head\"" | grep ref | cut -d '"' -f4`
+export TRAVIS_PULL_REQUEST_BRANCH=`jq '.head.ref' pull-request.json`
 echo $TRAVIS_PULL_REQUEST_BRANCH
 
 ember deploy pull-request --activate --verbose
