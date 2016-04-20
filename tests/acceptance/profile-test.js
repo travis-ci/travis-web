ab649f6af15740845db0dc201d6e35aa0d8e4187
Buck Doyle <buck@travis-ci.org>
2016-04-20 22:40:52 +0200

    Add assertions for hook listing

--- tests/acceptance/profile-test.js
+++ tests/acceptance/profile-test.js
@@ -9,6 +9,24 @@ moduleForAcceptance('Acceptance | profile', {
       login: 'feministkilljoy'
     });
 
+    const activeHook = server.create('hook', {
+      name: 'living-a-feminist-life',
+      owner_name: 'feministkilljoy',
+      active: true
+    });
+
+    const inactiveHook = server.create('hook', {
+      name: 'willful-subjects',
+      owner_name: 'feministkilljoy',
+      active: false
+    });
+
+    const otherHook = server.create('hook', {
+      name: 'feminism-is-for-everybody',
+      owner_name: 'bellhooks',
+      active: false
+    });
+
     let localStorageUser = JSON.parse(JSON.stringify(currentUser.attrs));
     localStorageUser.token = "abc123";
     window.localStorage.setItem('travis.token', 'testUserToken');
@@ -26,5 +44,9 @@ test('view profile', function(assert) {
 
   andThen(function() {
     assert.equal(profilePage.name, 'Sara Ahmed');
+
+    assert.equal(profilePage.hooks().count, 2, 'expected two hooks');
+    assert.equal(profilePage.hooks(0).name, 'feministkilljoy/living-a-feminist-life');
+    assert.equal(profilePage.hooks(1).name, 'feministkilljoy/willful-subjects');
   });
 });
