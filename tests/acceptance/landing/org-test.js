39b4f5f3b2a124792ad9786295ae5008db6315d1
Curtis Ekstrom <code@fauxton.io>
2016-04-21 14:58:22 +0200

    Add header assertions to org landing page test

--- tests/acceptance/landing/org-test.js
+++ tests/acceptance/landing/org-test.js
@@ -4,11 +4,14 @@ import orgLandingPage from 'travis/tests/pages/landing-org';
 
 moduleForAcceptance('Acceptance | org landing page test');
 
-test('view langding page when unauthenticated', function(assert) {
+test('view landing page when unauthenticated', function(assert) {
   orgLandingPage
     .visit();
 
   andThen(function() {
+    assert.equal(orgLandingPage.headerLinks(0).linkText, 'Blog', 'displays blog link');
+    assert.equal(orgLandingPage.headerLinks(1).linkText, 'Docs', 'displays docs link');
+    assert.equal(orgLandingPage.helpLink, 'Help', 'displays help dropdown');
     assert.equal(orgLandingPage.heroText, 'Test and Deploy with Confidence');
   });
 });
--- tests/pages/landing-org.js
+++ tests/pages/landing-org.js
@@ -2,10 +2,18 @@ import PageObject from 'travis/tests/page-object';
 
 let {
   visitable,
-  text
+  text,
+  collection
 } = PageObject;
 
 export default PageObject.create({
   visit: visitable('/'),
-  heroText: text('#hero-copy h1')
+  heroText: text('#hero-copy h1'),
+  headerLinks: collection({
+    itemScope: 'nav#navigation ul',
+    item: {
+      linkText: text('li', { at: 0 }),
+    }
+  }),
+  helpLink: text('nav#navigation ul li span')
 });
