5709b2cfae451564405119fb2424ea0a77771dfc
Curtis Ekstrom <code@fauxton.io>
2016-05-10 11:59:32 +0200

    Fix Sentry release versions in production environments
    Heroku does not appear to build the app within the context of a Git
    repository, meaning that the version SHA's are not currently present.
    
    This is being used as part of unique release key for each deploy, which
    leads to exceptions not being mapped to releases. This should now be
    resolved.

--- app/instance-initializers/raven-release.js
+++ app/instance-initializers/raven-release.js
@@ -1,9 +1,16 @@
 import config from 'travis/config/environment';
 
-export function initialize(/* appInstance */) {
+export function initialize(appInstance) {
+  let sha;
+  // this is Heroku-specific, will not work in other environments
+  if (config.environment === 'production') {
+    sha = config.release.slice(0,7);
+  } else {
+    sha = appInstance.application.version.slice(6, -1);
+  }
+
   let env = window.location.href;
-  let sha = config.APP.version.slice(6, -1);
-  let domain = env === 'https://travis-ci.org' ? 'org' : 'com';
+  let domain = env === 'https://travis-ci.org/' ? 'org' : 'com';
   let release = `${domain}-${sha}`;
 
   window.Raven.setRelease(release);
