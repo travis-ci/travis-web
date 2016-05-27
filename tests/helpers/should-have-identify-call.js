39a1c7018dc213f0ee1a51565da020161b75e09f
Buck Doyle <buck@travis-ci.org>
2016-05-27 11:11:48 -0500

    Add assertion failure message

--- tests/helpers/should-have-identify-call.js
+++ tests/helpers/should-have-identify-call.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Test.registerHelper('shouldHaveIdentifyCall', function(app, assert, options) {
-  assert.deepEqual(app.__container__.lookup('service:metrics').get('identifications.firstObject'), options);
+  assert.deepEqual(app.__container__.lookup('service:metrics').get('identifications.firstObject'), options, 'expected a matching identify event');
 });
