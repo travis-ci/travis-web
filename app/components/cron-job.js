71599bed02456e46ca1b4b772b16f1d26d882401
Christopher Weyand <christopher.weyand@student.hpi.de>
2016-05-30 11:04:28 +0200

    cron time UI

--- app/components/cron-job.js
+++ app/components/cron-job.js
@@ -10,20 +10,25 @@ export default Ember.Component.extend({
 
   enqueuingText: function(key) {
     var interval = this.get('cron.interval');
-    var creationTime = Date.parse(this.get('cron.created_at'));
+    var creationTime = new Date(this.get('cron.created_at'));
+    var hours = creationTime.getHours();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours !== 0 ? hours : 12;
+    var time = hours + ' ' + ampm;
     var text = "";
-    switch interval {
+    switch (interval) {
       case 'monthly':
         var post = 'th';
         var day = creationTime.getDate();
-        if day = 1 {
+        if ( day === 1 ){
           post = 'st';
-        } else if day = 2 {
+        } else if ( day === 2 ){
           post = 'nd';
-        } else if day = 3 {
+        } else if ( day === 3 ){
           post = 'rd';
         }
-        text = 'Every ' + day + post + 'of the month at ' + creationTime.getHours();
+        text = 'Every ' + day + post + ' of the month at ' + time;
         break;
       case 'weekly':
         var weekday = new Array(7);
@@ -34,14 +39,14 @@ export default Ember.Component.extend({
         weekday[4] = "Thursday";
         weekday[5] = "Friday";
         weekday[6] = "Saturday";
-        text = 'Every ' + weekday[creationTime.getDay()] + ' at ' + creationTime.getHours();
+        text = 'Every ' + weekday[creationTime.getDay()] + ' at ' + time;
         break;
       case 'daily':
-        text = 'Every day at ' + creationTime.getHours();
+        text = 'Every day at ' + time;
         break;
     }
     return text;
-  }.property('cron.next_enqueuing'),
+  }.property('cron.created_at'),
 
   disableByBuild: function(key) {
     var value = '';
