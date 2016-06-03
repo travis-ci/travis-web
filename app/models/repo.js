00678293c42b2f2d245cbdf9f26e136b7d638e00
Curtis Ekstrom <code@fauxton.io>
2016-06-03 14:26:04 +0200

    Pass current_build instead of repository.current_build

--- app/models/repo.js
+++ app/models/repo.js
@@ -247,7 +247,7 @@ Repo.reopenClass({
       promise = new Ember.RSVP.Promise(function(resolve, reject) {
         return store.query('repo', {
           'repository.active': 'true',
-          sort_by: 'repository.current_build:desc',
+          sort_by: 'current_build:desc',
           limit: 30
         }).then(function() {
           return resolve(repos);
