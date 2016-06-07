9b8dbabd4815cc36b2bafcfdd1faaed37a328d45
Steffen <steffen.koette@gmail.com>
2016-06-07 09:09:47 +0200

    Clarify cron time UI (#552)
    * clarifiy cron time UI
    
    * shorten cron ui texts
    
    * change default option

--- app/components/cron-job.js
+++ app/components/cron-job.js
@@ -8,14 +8,66 @@ export default Ember.Component.extend({
   actionType: 'Save',
   store: service(),
 
-  disableByBuild: function(key) {
-    var value = '';
-    if (this.get('cron.disable_by_build')) {
-      value = 'Only ';
-    } else {
-      value = 'Even ';
+  intervalText: function() {
+    function timeOfDay(creationTime) {
+      var hours = creationTime.getHours();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours !== 0 ? hours : 12;
+      return hours + ' ' + ampm;
     }
-    return value + 'if no new commit after last cron build';
+
+    function dayOfWeek(creationTime) {
+      var weekday = new Array(7);
+      weekday[0] = "Sunday";
+      weekday[1] = "Monday";
+      weekday[2] = "Tuesday";
+      weekday[3] = "Wednesday";
+      weekday[4] = "Thursday";
+      weekday[5] = "Friday";
+      weekday[6] = "Saturday";
+      return weekday[creationTime.getDay()];
+    }
+
+    function dayOfMonth(creationTime) {
+      var post = 'th';
+      var day = creationTime.getDate();
+      if ( day === 1 ){
+        post = 'st';
+      } else if ( day === 2 ){
+        post = 'nd';
+      } else if ( day === 3 ){
+        post = 'rd';
+      }
+      return day + post;
+    }
+
+    var interval = this.get('cron.interval');
+    var creationTime = new Date(this.get('cron.created_at'));
+    var text = "";
+    var time = timeOfDay(creationTime);
+
+    switch (interval) {
+      case 'monthly':
+        text = 'Enqueues the ' + dayOfMonth(creationTime) + ' of every month after ' + time;
+        break;
+      case 'weekly':
+        text = 'Enqueues each ' + dayOfWeek(creationTime) + ' after ' + time;
+        break;
+      case 'daily':
+        text = 'Enqueues each day after ' + time;
+        break;
+    }
+    return text;
+  }.property('cron.created_at'),
+
+
+  disableByBuild: function(key) {
+    if (this.get('cron.disable_by_build')) {
+      return 'Only if no new commit';
+    } else {
+      return 'Always run';
+    }
   }.property('cron.disable_by_build'),
 
   actions: {
