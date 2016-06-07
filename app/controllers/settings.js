62b6c83ebf1ae127ef8d26bdcae75e9850e0c1f8
Christopher Weyand <christopher.weyand@student.hpi.de>
2016-06-07 15:21:32 +0200

    add cron only displays cron-free branches

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   envVars: Ember.computed.filterBy('model.envVars', 'isNew', false),
-  cronJobs: Ember.computed.sort('model.cronJobs', function(a, b) {
-    if(a.get('branch.defaultBranch')) {
-      return -1;
-    } else if(b.get('branch.defaultBranch')) {
-      return 1;
-    } else {
-      return a.get('branch.name') > b.get('branch.name');
-    }
-  }),
-  branches: Ember.computed.sort('model.branches', function(a, b) {
+  freeBranches: function() {
+    var cronJobs = this.get('model.cronJobs.jobs');
+      return this.get('model.branches').filter(function(branch) {
+        return ! cronJobs.any(function(cron) {
+        return branch.get('name') === cron.get('branch').get('name');
+      });
+    });
+  }.property('model.cronJobs','cronJobs','this.model.cronJobs'),
+
+  freeSortedBranches: Ember.computed.sort('freeBranches', function(a, b) {
     if(a.get('defaultBranch')) {
       return -1;
     } else if(b.get('defaultBranch')) {
