0e6dc8db4ef673e4716a86244008c2feebec08a9
Christopher Weyand <christopher.weyand@student.hpi.de>
2016-06-20 10:26:52 +0200

    rename freeBranches to brachesWithoutCron, change computed property to Ember.computed, shorten any-function

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -2,19 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   envVars: Ember.computed.filterBy('model.envVars', 'isNew', false),
-  freeBranches: function() {
+
+  branchesWithoutCron: Ember.computed('model.cronJobs.jobs.@each', function() {
     var cronJobs = this.get('model.cronJobs.jobs');
     var branches = this.get('model.branches').filter(function(branch) {
       return branch.get('exists_on_github');
     });
     return branches.filter(function(branch) {
-      return ! cronJobs.any(function(cron) {
-        return branch.get('name') === cron.get('branch').get('name');
-      });
+      return ! cronJobs.any(cron => branch.get('name') === cron.get('branch.name'));
     });
-  }.property('model.cronJobs.jobs.@each'),
+  }),
 
-  freeSortedBranches: Ember.computed.sort('freeBranches', function(a, b) {
+  sortedBranchesWithoutCron: Ember.computed.sort('branchesWithoutCron', function(a, b) {
     if(a.get('defaultBranch')) {
       return -1;
     } else if(b.get('defaultBranch')) {
@@ -23,6 +22,7 @@ export default Ember.Controller.extend({
       return a.get('name') > b.get('name');
     }
   }),
+
   actions: {
     sshKeyAdded(sshKey) {
       return this.set('model.customSshKey', sshKey);
