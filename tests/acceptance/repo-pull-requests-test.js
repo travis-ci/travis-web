fc80bc5dd39ed2e5200eeb8024d3e810ec397b2f
Buck Doyle <buck@travis-ci.org>
2016-06-20 11:44:13 -0400

    Add assertions for other pull request statuses

--- tests/acceptance/repo-pull-requests-test.js
+++ tests/acceptance/repo-pull-requests-test.js
@@ -45,11 +45,40 @@ moduleForAcceptance('Acceptance | repo pull requests', {
       pull_request_title: 'A pull request'
     });
 
-    lastBuild.createCommit({
+    const commitAttributes = {
       sha: '1234567890',
-      committer: currentUser,
       author_name: currentUser.name
+    };
+
+    lastBuild.createCommit(commitAttributes);
+
+    const failedPullRequest = server.create('branch', {
+      name: 'failed',
+      id: '/v3/repos/${repoId}/branches/failed'
     });
+
+    const failedBuild = failedPullRequest.createBuild({
+      state: 'failed',
+      event_type: 'pull_request',
+      pull_request: true,
+      repository_id: repoId
+    });
+
+    failedBuild.createCommit(commitAttributes);
+
+    const erroredPullRequest = server.create('branch', {
+      name: 'errored',
+      id: '/v3/repos/${repoId}/branches/errored'
+    });
+
+    const erroredBuild = erroredPullRequest.createBuild({
+      state: 'errored',
+      event_type: 'pull_request',
+      pull_request: true,
+      repository_id: repoId
+    });
+
+    erroredBuild.createCommit(commitAttributes);
   }
 });
 
@@ -57,14 +86,17 @@ test('view pull requests', function(assert) {
   pullRequestsPage.visit({organization: 'killjoys', repo: 'living-a-feminist-life'});
 
   andThen(() => {
-    assert.equal(pullRequestsPage.pullRequests.length, 1, 'expected one pull request');
+    assert.equal(pullRequestsPage.pullRequests().count, 3, 'expected three pull requests');
 
     const pullRequest = pullRequestsPage.pullRequests(0);
 
-    assert.ok(pullRequest.passed, 'expected pull request to have passed');
+    assert.ok(pullRequest.passed, 'expected the first pull request to have passed');
     assert.equal(pullRequest.name, 'PR #2010 A pull request');
     assert.equal(pullRequest.committer, 'Sara Ahmed');
     assert.equal(pullRequest.commitSha, '1234567');
     assert.equal(pullRequest.commitDate, 'about a year ago');
+
+    assert.ok(pullRequestsPage.pullRequests(1).failed, 'expected the second pull request to have failed');
+    assert.ok(pullRequestsPage.pullRequests(2).errored, 'expected the third pull request to have errored');
   });
 });
