61b631d484ed904ebf566d06dbd08481ed5ea77f
Christopher Weyand <christopher.weyand@student.hpi.de>
2016-06-20 13:30:24 +0200

    hide branches that already have cron from form select (#559)

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -2,16 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   envVars: Ember.computed.filterBy('model.envVars', 'isNew', false),
-  cronJobs: Ember.computed.sort('model.cronJobs', function(a, b) {
-    if(a.get('branch.defaultBranch')) {
-      return -1;
-    } else if(b.get('branch.defaultBranch')) {
-      return 1;
-    } else {
-      return a.get('branch.name') > b.get('branch.name');
-    }
+
+  branchesWithoutCron: Ember.computed('model.cronJobs.jobs.@each', function() {
+    var cronJobs = this.get('model.cronJobs.jobs');
+    var branches = this.get('model.branches').filter(function(branch) {
+      return branch.get('exists_on_github');
+    });
+    return branches.filter(function(branch) {
+      return ! cronJobs.any(cron => branch.get('name') === cron.get('branch.name'));
+    });
   }),
-  branches: Ember.computed.sort('model.branches', function(a, b) {
+
+  sortedBranchesWithoutCron: Ember.computed.sort('branchesWithoutCron', function(a, b) {
     if(a.get('defaultBranch')) {
       return -1;
     } else if(b.get('defaultBranch')) {
@@ -20,6 +22,7 @@ export default Ember.Controller.extend({
       return a.get('name') > b.get('name');
     }
   }),
+
   actions: {
     sshKeyAdded(sshKey) {
       return this.set('model.customSshKey', sshKey);
