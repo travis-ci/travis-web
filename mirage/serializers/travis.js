a7db569a4aaa4fb1809fc601aff5ff56590ffc59
Buck Doyle <buck@travis-ci.org>
2016-06-21 09:14:26 -0400

    Move serialiser to its own file

---
+++ mirage/serializers/travis.js
@@ -0,0 +1,48 @@
+import Serializer from 'ember-cli-mirage/serializer';
+
+import { hasMany, belongsTo } from 'ember-cli-mirage';
+
+import Ember from 'ember';
+
+export default Serializer.extend({
+  serialize(response, request) {
+    if (this.isModel(response)) {
+      // FIXME is there a way to call Serializer.serialize without this.super?
+      let result = this._serializeModel(response, request);
+
+      if (this._requestIsForV3(request)) {
+        result['@type'] = response.modelName;
+        return result;
+      } else {
+        const wrappedResult = {};
+        wrappedResult[response.modelName] = result;
+        return wrappedResult;
+      }
+    } else {
+      const results = response.models.map(model => this._serializeModel(model, request));
+      const pluralType = Ember.String.pluralize(response.modelName);
+      const result = {
+        '@type': pluralType
+      };
+
+      result[pluralType] = results;
+      return result;
+    }
+  },
+
+  _requestIsForV3(request) {
+    if (!request) {
+      return false;
+    }
+
+    return this._requestHasV3Header(request) || this._requestHasV3Path(request);
+  },
+
+  _requestHasV3Header(request) {
+    return request.requestHeaders && request.requestHeaders['Travis-API-Version'] === '3';
+  },
+
+  _requestHasV3Path(request) {
+    return request.url && request.url.indexOf('/v3') === 0;
+  }
+});
--- tests/integration/travis-serializer-test.js
+++ tests/integration/travis-serializer-test.js
@@ -1,57 +1,11 @@
-// TODO import the true serializer
-import Serializer from 'ember-cli-mirage/serializer';
+import TravisSerializer from 'travis/mirage/serializers/travis';
 
-import { hasMany, belongsTo } from 'ember-cli-mirage';
 import Schema from 'ember-cli-mirage/orm/schema';
 import Model from 'ember-cli-mirage/orm/model';
 import Db from 'ember-cli-mirage/db';
 import SerializerRegistry from 'ember-cli-mirage/serializer-registry';
+
 import { module, test } from 'qunit';
-
-import Ember from 'ember';
-
-const TravisSerializer = Serializer.extend({
-  serialize(response, request) {
-    if (this.isModel(response)) {
-      // FIXME is there a way to call Serializer.serialize without this.super?
-      let result = this._serializeModel(response, request);
-
-      if (this._requestIsForV3(request)) {
-        result['@type'] = response.modelName;
-        return result;
-      } else {
-        const wrappedResult = {};
-        wrappedResult[response.modelName] = result;
-        return wrappedResult;
-      }
-    } else {
-      const results = response.models.map(model => this._serializeModel(model, request));
-      const pluralType = Ember.String.pluralize(response.modelName);
-      const result = {
-        '@type': pluralType
-      };
-
-      result[pluralType] = results;
-      return result;
-    }
-  },
-
-  _requestIsForV3(request) {
-    if (!request) {
-      return false;
-    }
-
-    return this._requestHasV3Header(request) || this._requestHasV3Path(request);
-  },
-
-  _requestHasV3Header(request) {
-    return request.requestHeaders && request.requestHeaders['Travis-API-Version'] === '3';
-  },
-
-  _requestHasV3Path(request) {
-    return request.url && request.url.indexOf('/v3') === 0;
-  }
-});
 
 module('Integration | Serializer | TravisSerializer', {
   beforeEach() {
