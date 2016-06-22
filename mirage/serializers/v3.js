6af4c93376a02bf87e78a4bbd8627b7d49d62400
Buck Doyle <buck@travis-ci.org>
2016-06-22 12:11:06 -0400

    Add embedded resources to V3 serialiser

--- mirage/serializers/v3.js
+++ mirage/serializers/v3.js
@@ -3,15 +3,19 @@ import { ActiveModelSerializer } from 'ember-cli-mirage';
 export default ActiveModelSerializer.extend({
   serialize(object, request) {
     // TODO is there any way to avoid this nightmare?
+
+    ActiveModelSerializer.prototype.embed = true;
+
     if (this.isModel(object)) {
       ActiveModelSerializer.prototype.root = false;
-      ActiveModelSerializer.prototype.embed = true;
     } else {
       ActiveModelSerializer.prototype.root = true;
     }
 
     const json = ActiveModelSerializer.prototype.serialize.apply(this, arguments);
     json['@type'] = this._keyForModelOrCollection(object);
+
+    ActiveModelSerializer.prototype.embed = false;
 
     return json;
   }
--- tests/integration/v3-serializer-test.js
+++ tests/integration/v3-serializer-test.js
@@ -28,7 +28,10 @@ module('Integration | Mirage Serializer | V3Serializer', {
     author.createBook({title: 'On Being Included'});
 
     this.registry = new SerializerRegistry(this.schema, {
-      application: V3Serializer
+      application: V3Serializer,
+      book: V3Serializer.extend({
+        include: ['author']
+      })
     });
   },
 
@@ -46,23 +49,42 @@ test('it serialises a collection with underscored property keys', function(asser
     books: [{
       id: '1',
       author_id: '1',
-      title: 'Willful Subjects'
+      title: 'Willful Subjects',
+      author: {
+        id: '1',
+        name: 'Sara Ahmed'
+      }
     }, {
       id: '2',
       author_id: '1',
-      title: 'On Being Included'
+      title: 'On Being Included',
+      author: {
+        id: '1',
+        name: 'Sara Ahmed'
+      }
     }]
   });
 });
 
-test('it serialises a single resource with its properties included directly', function(assert) {
+test('it serialises a single resource with its properties included directly and a related resource embedded', function(assert) {
   const book = this.schema.books.find(1);
-  const result = this.registry.serialize(book);
+  const registryWithInclusion = new SerializerRegistry(this.schema, {
+    application: V3Serializer,
+    book: V3Serializer.extend({
+      include: ['author']
+    })
+  });
+
+  const result = registryWithInclusion.serialize(book);
 
   assert.deepEqual(result, {
     '@type': 'book',
     id: '1',
     author_id: '1',
-    title: 'Willful Subjects'
+    title: 'Willful Subjects',
+    author: {
+      id: '1',
+      name: 'Sara Ahmed'
+    }
   });
 });
