08f226862b7b6405c4c54b1db0c5eb3b9f9efe90
Piotr Sarnacki <drogus@gmail.com>
2016-07-05 15:07:38 +0200

    Fix owner page
    We use plain JSON data there, so we need to use underscore instead of
    camelcase

--- app/components/owner-repo-tile.js
+++ app/components/owner-repo-tile.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['owner-tile', 'row-li'],
-  classNameBindings: ['repo.currentBuild.state'],
+  classNameBindings: ['repo.current_build.state'],
 
   ownerName: function() {
     return this.get('repo.slug').split(/\//)[0];
@@ -15,10 +15,10 @@ export default Ember.Component.extend({
 
   isAnimating: function() {
     var animationStates, state;
-    state = this.get('repo.currentBuild.state');
+    state = this.get('repo.current_build.state');
     animationStates = ['received', 'queued', 'started', 'booting'];
     if (animationStates.indexOf(state) !== -1) {
       return true;
     }
-  }.property('repo.currentBuild.state')
+  }.property('repo.current_build.state')
 });
