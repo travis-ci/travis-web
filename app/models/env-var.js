7ce0153817274c89b843445689dcf4d067622e0f
Curtis Ekstrom <code@fauxton.io>
2016-07-12 21:05:27 +0200

    Fix warnings for app/models/env-var.js

--- app/models/env-var.js
+++ app/models/env-var.js
@@ -1,7 +1,6 @@
-import Ember from 'ember';
 import Model from 'travis/models/model';
 import attr from 'ember-data/attr';
-import { hasMany, belongsTo } from 'ember-data/relationships';
+import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr(),
