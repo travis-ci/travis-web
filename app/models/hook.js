52c38610d2ce7a3ff2704dd673fc883b8c951d3d
Curtis Ekstrom <code@fauxton.io>
2016-07-12 21:06:09 +0200

    Fix warnings for app/models/hook.js

--- app/models/hook.js
+++ app/models/hook.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Model from 'travis/models/model';
 import config from 'travis/config/environment';
 import attr from 'ember-data/attr';
-import { hasMany, belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr(),
