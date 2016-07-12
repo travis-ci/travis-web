8152c44304dab43843ecf89bec52f6468a07324b
Curtis Ekstrom <code@fauxton.io>
2016-07-12 21:24:05 +0200

    Fix warnings for app/models/request.js

--- app/models/request.js
+++ app/models/request.js
@@ -1,7 +1,6 @@
-import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { hasMany, belongsTo } from 'ember-data/relationships';
+import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   created_at: attr(),
