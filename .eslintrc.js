3a45897d038cfbc0f2d7e1fe264a3184c29e59e2
Curtis Ekstrom <code@fauxton.io>
2016-07-13 16:14:19 +0200

    Prefer local eslint config over packaged version
    This makes it more transparent in terms of where rules are coming from, though it does mean that we will need to stay up to date with which new rules are offered (instead of just prepackaged by someone else).

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 var path = require('path');
 
 module.exports = {
-  extends: [
-    require.resolve('ember-cli-eslint/coding-standard/ember-application.js')
-  ]
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  parser: 'babel-eslint',
+  extends: 'eslint:recommended',
+  env: {
+    'browser': true
+  },
+  rules: {
+  },
+  globals: {
+  }
 };
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: '../node_modules/ember-cli-eslint/coding-standard/ember-testing.js'
+  env: {
+    'browser': true,
+    'embertest': true,
+    'qunit': true
+  }
 };
