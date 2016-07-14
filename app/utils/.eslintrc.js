9db714a0ac44feb8985d7ad7c5ca6fe793b5e7b4
Curtis Ekstrom <code@fauxton.io>
2016-07-14 10:09:28 +0200

    Disable func-names for app/utils
    This is only temporary until we figure out how best to handle these classes. They should probably just be converted to ES6 classes

---
+++ app/utils/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    // disable func-names for utils as they are mainly a collection
+    // of vanilla JS functions. Not sure how to fix yet.
+    'func-names': 0,
+  }
+}
