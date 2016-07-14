1441490b5aec2b9ee0f03c957caffc817889eb12
Buck Doyle <buck@travis-ci.org>
2016-07-14 17:34:50 -0500

    Change deployment status keys to be conventional
    This more closely mirrors the Travis CI updates:
    continuous-integration/travis-ci/push etc

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -1,9 +1,9 @@
 export TRAVIS_PULL_REQUEST_COMMIT=`jq -r '.head.sha' pull-request.json`
 curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.org\", \"description\": \"Visit a org-production deployment for this commit\", \"context\": \"deployment-org\"}" \
+     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.org\", \"description\": \"Visit a org-production deployment for this commit\", \"context\": \"deployments/org-production\"}" \
      -H "Authorization: token $GITHUB_TOKEN" \
      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_COMMIT
 curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.com\", \"description\": \"Visit a com-production deployment for this commit\", \"context\": \"deployment-com\"}" \
+     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.com\", \"description\": \"Visit a com-production deployment for this commit\", \"context\": \"deployments/com-production\"}" \
      -H "Authorization: token $GITHUB_TOKEN" \
      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_COMMIT
