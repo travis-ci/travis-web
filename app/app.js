7f04862cda7eba7b867dc85eddb93e22a2315607
Curtis Ekstrom <curtis@travis-ci.org>
2016-07-19 14:13:08 +0200

    Fix errors in app/app.js

--- app/app.js
+++ app/app.js
@@ -12,26 +12,20 @@ Ember.LinkComponent.reopen({
   attributeBindings: ['alt']
 });
 
-var App = Ember.Application.extend(Ember.Evented, {
+const App = Ember.Application.extend(Ember.Evented, {
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver,
+  Resolver,
 
   ready() {
     if (location.hash.slice(0, 2) === '#!') {
       location.href = location.href.replace('#!/', '');
     }
 
-    this.on('user:signed_in', function (user) {
-      return Travis.onUserUpdate(user);
-    });
-    this.on('user:refreshed', function (user) {
-      return Travis.onUserUpdate(user);
-    });
-    this.on('user:synced', function (user) {
-      return Travis.onUserUpdate(user);
-    });
-    return this.on('user:signed_out', function () {
+    this.on('user:signed_in', user => Travis.onUserUpdate(user));
+    this.on('user:refreshed', user => Travis.onUserUpdate(user));
+    this.on('user:synced', user => Travis.onUserUpdate(user));
+    return this.on('user:signed_out', () => {
       if (config.beacon) {
         return Travis.destroyBeacon();
       }
@@ -54,9 +48,7 @@ var App = Ember.Application.extend(Ember.Evented, {
   },
 
   destroyBeacon() {
-    HS.beacon.ready(function () {
-      return HS.beacon.destroy();
-    });
+    HS.beacon.ready(() => HS.beacon.destroy());
   },
 
   setupBeacon() {
@@ -66,17 +58,17 @@ var App = Ember.Application.extend(Ember.Evented, {
   },
 
   subscribePusher(user) {
-    var channels;
+    let channels;
     if (!user.channels) {
       return;
     }
     channels = user.channels;
     if (config.pro) {
-      channels = channels.map(function (channel) {
+      channels = channels.map(channel => {
         if (channel.match(/^private-/)) {
           return channel;
         } else {
-          return 'private-' + channel;
+          return `private-${channel}`;
         }
       });
     }
@@ -85,14 +77,12 @@ var App = Ember.Application.extend(Ember.Evented, {
 
   identifyHSBeacon(user) {
     if (HS && HS.beacon) {
-      HS.beacon.ready(function () {
-        return HS.beacon.identify({
-          name: user.name,
-          email: user.email,
-          login: user.login,
-          last_synced_at: user.synced_at
-        });
-      });
+      HS.beacon.ready(() => HS.beacon.identify({
+        name: user.name,
+        email: user.email,
+        login: user.login,
+        last_synced_at: user.synced_at
+      }));
     }
   },
 
