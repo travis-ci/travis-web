b705cab1d7d30f7d3c3dbaaea4cea5eb1e3abce5
Curtis Ekstrom <curtis@travis-ci.org>
2016-07-20 11:21:10 +0200

    Restore behavior using $.ajax over $.get
    This was previously added, but got lost in the noise of a recent merge

--- app/routes/owner/repositories.js
+++ app/routes/owner/repositories.js
@@ -24,6 +24,6 @@ export default TravisRoute.extend({
     let includes = `?include=owner.repositories,repository.default_branch,build.commit,repository.current_build`;
     let { owner } = transition.params.owner;
     let url = `${config.apiEndpoint}/v3/owner/${owner}${includes}`;
-    return Ember.$.get(url, options);
+    return Ember.$.ajax(url, options);
   }
 });
