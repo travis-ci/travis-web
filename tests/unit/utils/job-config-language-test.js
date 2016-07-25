57b89968f954922db1c13b582c6126cb53ef29ad
Buck Doyle <buck@travis-ci.org>
2016-07-25 12:23:02 -0700

    Add assertion for multi-language job

--- tests/unit/utils/job-config-language-test.js
+++ tests/unit/utils/job-config-language-test.js
@@ -13,3 +13,7 @@ test('a job with an RVM value returns Ruby with its version', (assert) => {
 test('a Ruby job with no RVM value returns Ruby alone', (assert) => {
   assert.equal(jobConfigLanguage({ language: 'ruby' }), 'Ruby');
 });
+
+test('a job with two language-related keys returns both languages', (assert) => {
+  assert.equal(jobConfigLanguage({ rvm: '2.1', node_js: '5.3' }), 'Node.js: 5.3 Ruby: 2.1');
+});
