65b4f2486de55820d45f9b2364b6042c283aa27f
Buck Doyle <buck@travis-ci.org>
2016-07-25 12:29:17 -0700

    Add assertions for Gemfile display

--- tests/unit/utils/job-config-language-test.js
+++ tests/unit/utils/job-config-language-test.js
@@ -17,3 +17,15 @@ test('a Ruby job with no RVM value returns Ruby alone', (assert) => {
 test('a job with two language-related keys returns both languages', (assert) => {
   assert.equal(jobConfigLanguage({ rvm: '2.1', node_js: '5.3' }), 'Node.js: 5.3 Ruby: 2.1');
 });
+
+test('a job with a Gemfile and an env shows the Gemfile', (assert) => {
+  assert.equal(jobConfigLanguage({ gemfile: 'Hellofile', env: 'yes' }), 'Gemfile: Hellofile');
+});
+
+test('the Gemfile is shown after the languages', (assert) => {
+  assert.equal(jobConfigLanguage({ gemfile: 'Hellofile', env: 'yes', node_js: '6.0' }), 'Node.js: 6.0 Gemfile: Hellofile');
+});
+
+test('a job with a Gemfile without an env does not show the Gemfile', (assert) => {
+  assert.equal(jobConfigLanguage({ gemfile: 'Nofile' }), '', 'expected a Gemfile-only config to return an empty string');
+});
