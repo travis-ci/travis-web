80d0feebf8b8276dbaef11fa860c8ad2b2043bc3
Curtis Ekstrom <curtis@travis-ci.org>
2016-08-03 07:49:15 +0200

    Extract home page tests to home/

--- tests/acceptance/home/with-repositories-test.js
+++ tests/acceptance/home/with-repositories-test.js
@@ -2,22 +2,7 @@ import { test } from 'qunit';
 import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
 import dashboardPage from 'travis/tests/pages/dashboard';
 
-moduleForAcceptance('Acceptance | home page for user with no repositories', {
-  beforeEach() {
-    const currentUser = server.create('user');
-    signInUser(currentUser);
-  }
-});
-
-test('signed in but without repositories', function (assert) {
-  dashboardPage.visit();
-
-  andThen(function () {
-    assert.equal(currentURL(), '/getting_started');
-  });
-});
-
-moduleForAcceptance('Acceptance | home page for user with repositories', {
+moduleForAcceptance('Acceptance | home/with repositories', {
   beforeEach() {
     const currentUser = server.create('user', {
       name: 'Sara Ahmed',
---
+++ tests/acceptance/home/without-repositories-test.js
@@ -0,0 +1,18 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import dashboardPage from 'travis/tests/pages/dashboard';
+
+moduleForAcceptance('Acceptance | home/without repositories', {
+  beforeEach() {
+    const currentUser = server.create('user');
+    signInUser(currentUser);
+  }
+});
+
+test('signed in but without repositories', function (assert) {
+  dashboardPage.visit();
+
+  andThen(function () {
+    assert.equal(currentURL(), '/getting_started');
+  });
+});
