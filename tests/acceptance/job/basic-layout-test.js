3ed571bcbfb974b9f1ae91e223968f7885b9fb6d
Curtis Ekstrom <curtis@travis-ci.org>
2016-08-03 07:43:20 +0200

    Extract/move job tests to job/ folder

---
+++ tests/acceptance/job/basic-layout-test.js
@@ -0,0 +1,33 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import jobPage from 'travis/tests/pages/job';
+
+moduleForAcceptance('Acceptance | job/basic layout');
+
+test('visiting job-view', function (assert) {
+  let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
+  // create branch
+  server.create('branch', {});
+  let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+  let build = server.create('build', { repository_id: repo.id, state: 'passed', commit_id: commit.id, commit });
+  let job = server.create('job', { number: '1234.1', repository_id: repo.id, state: 'passed', build_id: build.id, commit, build });
+  commit.job = job;
+
+  job.save();
+  commit.save();
+
+  // create log
+  server.create('log', { id: job.id });
+
+  visit('/travis-ci/travis-web/jobs/' + job.id);
+
+  andThen(function () {
+    assert.equal(jobPage.branch, 'acceptance-tests');
+    assert.equal(jobPage.message, 'acceptance-tests This is a message');
+    assert.equal(jobPage.state, '#1234.1 passed');
+    assert.equal(jobPage.author, 'Mr T authored and committed');
+
+    assert.equal(jobPage.log, 'Hello log');
+  });
+});
+
--- tests/acceptance/job/build-matrix-test.js
+++ tests/acceptance/job/build-matrix-test.js
@@ -1,36 +1,8 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
-import jobPage from 'travis/tests/pages/job';
 import buildPage from 'travis/tests/pages/build';
 
-moduleForAcceptance('Acceptance | job view');
-
-test('visiting job-view', function (assert) {
-  let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
-  // create branch
-  server.create('branch', {});
-  let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { repository_id: repo.id, state: 'passed', commit_id: commit.id, commit });
-  let job = server.create('job', { number: '1234.1', repository_id: repo.id, state: 'passed', build_id: build.id, commit, build });
-  commit.job = job;
-
-  job.save();
-  commit.save();
-
-  // create log
-  server.create('log', { id: job.id });
-
-  visit('/travis-ci/travis-web/jobs/' + job.id);
-
-  andThen(function () {
-    assert.equal(jobPage.branch, 'acceptance-tests');
-    assert.equal(jobPage.message, 'acceptance-tests This is a message');
-    assert.equal(jobPage.state, '#1234.1 passed');
-    assert.equal(jobPage.author, 'Mr T authored and committed');
-
-    assert.equal(jobPage.log, 'Hello log');
-  });
-});
+moduleForAcceptance('Acceptance | job/build matrix');
 
 test('visiting build matrix', function (assert) {
   let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
@@ -72,31 +44,5 @@ test('visiting build matrix', function (assert) {
     const failedJobRow = buildPage.allowedFailureJobs(0);
     assert.ok(failedJobRow.state.isFailed, 'expected the allowed failure job to have failed');
     assert.equal(failedJobRow.language, 'Ruby');
-  });
-});
-
-
-test('handling log error', function (assert) {
-  let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
-  // create branch
-  server.create('branch', {});
-  let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { repository_id: repo.id, state: 'passed', commit_id: commit.id, commit });
-  let job = server.create('job', { number: '1234.1', reposiptoy_id: repo.id, state: 'passed', build_id: build.id, commit, build });
-
-  commit.job = job;
-
-  job.save();
-  commit.save();
-
-  visit('/travis-ci/travis-web/jobs/' + job.id);
-
-  andThen(function () {
-    assert.equal(jobPage.branch, 'acceptance-tests');
-    assert.equal(jobPage.message, 'acceptance-tests This is a message');
-    assert.equal(jobPage.state, '#1234.1 passed');
-    assert.equal(jobPage.author, 'Mr T authored and committed');
-
-    assert.equal(jobPage.logError, 'There was an error while trying to fetch the log.');
   });
 });
