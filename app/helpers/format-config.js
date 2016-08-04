72064ce6eda49e7a8695960bf22272ef52440839
lislis <mail@lislis.de>
2016-08-04 16:09:15 +0200

    filter properties form config object

--- app/helpers/format-config.js
+++ app/helpers/format-config.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export function safeFormatConfig(config) {
-  return JSON.stringify(config, null, 2);
+  const rejectKeys = ['.result', 'notifications'];
+  let result = {};
+  Object.keys(config[0]).forEach(function (item) {
+    if (rejectKeys.indexOf(item) === -1) {
+      result[item] = config[0][item];
+    }
+  });
+  return JSON.stringify(result, null, 2);
 }
 
 export default Ember.Helper.helper(safeFormatConfig);
