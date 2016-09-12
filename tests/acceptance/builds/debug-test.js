9ab4929087ac93d0e43a31dd86698ca131bca41e
Buck Doyle <buck@travis-ci.org>
2016-09-12 09:47:21 -0400

    Remove redundant comments
    This aligns with the changes in #679.

--- tests/acceptance/builds/debug-test.js
+++ tests/acceptance/builds/debug-test.js
@@ -13,12 +13,11 @@ test('debugging single-job build', function (assert) {
   withFeature('pro-version');
 
   let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
-  // create branch
   server.create('branch', {});
+
   let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
   let build = server.create('build', { number: '5', repository_id: repository.id, state: 'passed', commit_id: commit.id, commit });
   let job = server.create('job', { number: '1234.1', repository_id: repository.id, state: 'passed', build, commit_id: commit.id });
-  // create log
   server.create('log', { id: job.id });
 
   const requestBodies = [];
@@ -42,8 +41,8 @@ test('multi-job builds cannot be debugged', function (assert) {
   withFeature('pro-version');
 
   let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
-  // create branch
   server.create('branch', {});
+
   let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
   let build = server.create('build', { number: '5', repository_id: repository.id, state: 'passed', commit_id: commit.id, commit });
   server.create('job', { number: '1234.1', repository_id: repository.id, state: 'passed', build, commit_id: commit.id });
--- tests/acceptance/job/debug-test.js
+++ tests/acceptance/job/debug-test.js
@@ -13,8 +13,8 @@ test('debugging job', function (assert) {
   withFeature('pro-version');
 
   let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
-  // create branch
   server.create('branch', {});
+
   let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
   let build = server.create('build', { repository_id: repo.id, state: 'failed', commit_id: commit.id, commit });
   let job = server.create('job', { number: '1234.1', repository_id: repo.id, state: 'failed', build_id: build.id, commit, build });
@@ -23,7 +23,6 @@ test('debugging job', function (assert) {
   job.save();
   commit.save();
 
-  // create log
   server.create('log', { id: job.id });
 
   const requestBodies = [];
