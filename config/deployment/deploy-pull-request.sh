89fb75e1ce25aa7104a5e53148e6828a39f76d31
Curtis Ekstrom <curtis@travis-ci.org>
2016-09-12 14:24:29 +0200

    Remove ember-cli-pretender
    It seems like this might not be compatible with running the app in production mode?

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,5 +1,6 @@
 ./config/deployment/store-pull-request-data.sh
 export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
+rm -rf /tmp
 ember deploy org-production-pull-request --activate --verbose
 ember deploy com-production-pull-request --activate --verbose
 ./config/deployment/update-github-status.sh
