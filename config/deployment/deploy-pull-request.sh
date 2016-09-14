7b3e551d032f566da1815f8177aa377863ed56c7
Buck Doyle <buck@travis-ci.org>
2016-09-14 09:38:48 -0700

    Add org-staging PR deployments

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,5 +1,6 @@
 ./config/deployment/store-pull-request-data.sh
 export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
 ember deploy org-production-pull-request --activate --verbose
+API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate --verbose
 TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
 ./config/deployment/update-github-status.sh
