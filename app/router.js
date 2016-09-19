0f56dd5e18e2867baa1c19b6ab7565e0f899ca63
Curtis Ekstrom <curtis@travis-ci.org>
2016-09-19 15:18:27 +0200

    Use auto location type
    This means we no longer invoke the auth-dependent locations/url rewrites

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ var Router = Ember.Router.extend({
   //
   // we should probably think about a more general way to
   // do this, location should not know about auth status
-  location: Ember.testing ? 'none' : 'auth-dependent',
+  location: config.locationType,
 
   generate() {
     var url;
