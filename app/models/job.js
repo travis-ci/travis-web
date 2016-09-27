d18932b2f36dacc2ba7c43115859e8df015efdb7
Buck Doyle <buck@travis-ci.org>
2016-09-27 11:15:12 -0400

    Add postV3 method for Ajax service
    This will make it easier when the cancel and restart events
    move to V3 without needing the unsightly /v3 path prefix.

--- app/models/job.js
+++ app/models/job.js
@@ -128,13 +128,8 @@ export default Model.extend(DurationCalculations, {
   },
 
   debug() {
-    return this.get('ajax').ajax(`/job/${this.get('id')}/debug`, 'POST', {
-      data: {
-        quiet: true
-      },
-      headers: {
-        'Travis-API-Version': '3'
-      }
+    return this.get('ajax').postV3(`/job/${this.get('id')}/debug`, {
+      quiet: true
     });
   },
 
