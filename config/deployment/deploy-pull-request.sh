608ce51b82857fde1bd2302435bd4f02ce240de4
Buck Doyle <buck@travis-ci.org>
2016-09-27 08:51:11 -0400

    Add note when skipping staging PR deployments

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -9,6 +9,8 @@ if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
 then
   TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
   API_ENDPOINT=https://api-staging.travis-ci.com TRAVIS_PRO=true ember deploy com-staging-pull-request --activate --verbose
+else
+  echo "Skipping com- and org-staging PR deployments: no 'staging' in branch name."
 fi
 
 ./config/deployment/update-github-status.sh
