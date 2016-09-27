7e8adfc806914cb0b7c4cca357882c920b1031be
Buck Doyle <b@chromatin.ca>
2016-09-27 08:14:16 -0400

    Add com-staging PR deployment

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -11,3 +11,7 @@ curl -X POST \
      --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.com\", \"description\": \"Visit a com-production deployment for this commit\", \"context\": \"deployments/com-production\"}" \
      -H "Authorization: token $GITHUB_TOKEN" \
      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_COMMIT
+ curl -X POST \
+      --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`-staging.test-deployments.travis-ci.com\", \"description\": \"Visit a com-staging deployment for this commit\", \"context\": \"deployments/com-staging\"}" \
+      -H "Authorization: token $GITHUB_TOKEN" \
+      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_COMMIT
