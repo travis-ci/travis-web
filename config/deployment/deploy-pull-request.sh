b8942594fc89acc2cc271af27d96f65a573a2034
Buck Doyle <buck@travis-ci.org>
2016-09-27 08:48:27 -0400

    Add filter for deploying staging
    This is hackish, but will prevent org- and com-staging deployments
    unless the branch name contains “staging”. This should speed up
    PR deployments because these are unnecessary in the majority
    of pull requests.

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,7 +1,14 @@
 ./config/deployment/store-pull-request-data.sh
+
 export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
+
 ember deploy org-production-pull-request --activate --verbose
 API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate --verbose
-TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
-API_ENDPOINT=https://api-staging.travis-ci.com TRAVIS_PRO=true ember deploy com-staging-pull-request --activate --verbose
+
+if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
+then
+  TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
+  API_ENDPOINT=https://api-staging.travis-ci.com TRAVIS_PRO=true ember deploy com-staging-pull-request --activate --verbose
+fi
+
 ./config/deployment/update-github-status.sh
