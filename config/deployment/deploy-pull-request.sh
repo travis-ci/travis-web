277b153c7bfd4c1d2a09911f877dcfa87cec17db
Buck Doyle <buck@travis-ci.org>
2016-09-28 17:15:38 -0400

    Fix misplaced deployment commands

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -3,11 +3,11 @@
 export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
 
 ember deploy org-production-pull-request --activate --verbose
-API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate --verbose
+TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
 
 if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
 then
-  TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
+  API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate --verbose
   API_ENDPOINT=https://api-staging.travis-ci.com TRAVIS_PRO=true ember deploy com-staging-pull-request --activate --verbose
 else
   echo "Skipping com- and org-staging PR deployments: no 'staging' in branch name."
