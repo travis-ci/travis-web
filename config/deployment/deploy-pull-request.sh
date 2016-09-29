7736c9d48ec26f608df387b96ab4df7192da0b51
Buck Doyle <b@chromatin.ca>
2016-09-29 13:39:09 -0400

    Remove API-fetched deployment variables (#703)
    Thanks to @BanzaiMan’s travis-ci/travis-scheduler#38 and
    travis-ci/travis-build#848, these are now present as
    environment variables rather than needing to be fetched
    via the GitHub API.

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,5 +1,3 @@
-./config/deployment/store-pull-request-data.sh
-export TRAVIS_PULL_REQUEST_BRANCH=`jq -r '.head.ref' pull-request.json | tr '.' '-'`
 ember deploy org-production-pull-request --activate --verbose
 TRAVIS_PRO=true ember deploy com-production-pull-request --activate --verbose
 ./config/deployment/update-github-status.sh
