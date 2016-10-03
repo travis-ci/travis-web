c7a20fd2f93b569e7231037f728c405e5c2202ff
Buck Doyle <buck@travis-ci.org>
2016-10-03 15:34:04 -0400

    Restore staging PR deployments
    😱 I’ve been mixing up directories and branches apparently

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -3,14 +3,18 @@ curl -X POST \
      -H "Authorization: token $GITHUB_TOKEN" \
      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
 curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`-staging.test-deployments.travis-ci.org\", \"description\": \"Visit a org-staging deployment for this commit\", \"context\": \"deployments/org-staging\"}" \
-     -H "Authorization: token $GITHUB_TOKEN" \
-     https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
-curl -X POST \
      --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`.test-deployments.travis-ci.com\", \"description\": \"Visit a com-production deployment for this commit\", \"context\": \"deployments/com-production\"}" \
      -H "Authorization: token $GITHUB_TOKEN" \
      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
- curl -X POST \
-      --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`-staging.test-deployments.travis-ci.com\", \"description\": \"Visit a com-staging deployment for this commit\", \"context\": \"deployments/com-staging\"}" \
+
+if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
+then
+  curl -X POST \
+      --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`-staging.test-deployments.travis-ci.org\", \"description\": \"Visit a org-staging deployment for this commit\", \"context\": \"deployments/org-staging\"}" \
       -H "Authorization: token $GITHUB_TOKEN" \
       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+  curl -X POST \
+       --data "{\"state\": \"success\", \"target_url\": \"https://`echo $TRAVIS_PULL_REQUEST_BRANCH`-staging.test-deployments.travis-ci.com\", \"description\": \"Visit a com-staging deployment for this commit\", \"context\": \"deployments/com-staging\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+fi
