739a677754ad17a8461ed6ed57ea4cc9f2452b22
Buck Doyle <buck@travis-ci.org>
2016-10-04 08:21:52 -0400

    Move status updates to after deployments
    This should give access to deployments immediately upon
    their creation.

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -1,20 +1,13 @@
-curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $CLEANED_BRANCH_SUBDOMAIN`.test-deployments.travis-ci.org\", \"description\": \"Visit a org-production deployment for this commit\", \"context\": \"deployments/org-production\"}" \
-     -H "Authorization: token $GITHUB_TOKEN" \
-     https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
-curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $CLEANED_BRANCH_SUBDOMAIN`.test-deployments.travis-ci.com\", \"description\": \"Visit a com-production deployment for this commit\", \"context\": \"deployments/com-production\"}" \
-     -H "Authorization: token $GITHUB_TOKEN" \
-     https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
-
-if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
+if [[ "$ENVIRONMENT" = "staging" ]]
 then
-  curl -X POST \
-      --data "{\"state\": \"success\", \"target_url\": \"https://`echo $CLEANED_BRANCH_SUBDOMAIN`-staging.test-deployments.travis-ci.org\", \"description\": \"Visit a org-staging deployment for this commit\", \"context\": \"deployments/org-staging\"}" \
-      -H "Authorization: token $GITHUB_TOKEN" \
-      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
-  curl -X POST \
-       --data "{\"state\": \"success\", \"target_url\": \"https://`echo $CLEANED_BRANCH_SUBDOMAIN`-staging.test-deployments.travis-ci.com\", \"description\": \"Visit a com-staging deployment for this commit\", \"context\": \"deployments/com-staging\"}" \
-       -H "Authorization: token $GITHUB_TOKEN" \
-       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+  SUFFIX="-staging"
+else
+  SUFFIX=""
 fi
+
+FULL_ENVIRONMENT=`echo $TLD`-`echo $ENVIRONMENT`
+
+curl -X POST \
+     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments.travis-ci.`echo $TLD`\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+     -H "Authorization: token $GITHUB_TOKEN" \
+     https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
