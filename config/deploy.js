1a74fbebdf0d2ad501dd0b651fbcce4f2accb9fd
Buck Doyle <buck@travis-ci.org>
2016-10-05 15:09:51 -0400

    Add Ember beta and canary deployment targets
    These will behave similarly to PR deployments.

--- config/deploy.js
+++ config/deploy.js
@@ -2,7 +2,11 @@ var VALID_DEPLOY_TARGETS = [
   'org-staging-pull-request',
   'org-production-pull-request',
   'com-staging-pull-request',
-  'com-production-pull-request'
+  'com-production-pull-request',
+  'org-beta',
+  'com-beta',
+  'org-canary',
+  'com-canary'
 ];
 
 module.exports = function(deployTarget) {
@@ -25,7 +29,9 @@ module.exports = function(deployTarget) {
     throw new Error('Invalid deployTarget ' + deployTarget);
   }
 
-  if (deployTarget === 'org-production-pull-request') {
+  if (deployTarget === 'org-production-pull-request' ||
+      deployTarget === 'org-canary' ||
+      deployTarget === 'org-beta') {
     ENV.s3.bucket = 'travis-web-production-next';
     ENV.redis.url = process.env.ORG_PRODUCTION_REDIS_URL;
   }
@@ -36,7 +42,9 @@ module.exports = function(deployTarget) {
     ENV.redis.keyPrefix = `${process.env.CLEANED_BRANCH_SUBDOMAIN}-staging`;
   }
 
-  if (deployTarget === 'com-production-pull-request') {
+  if (deployTarget === 'com-production-pull-request' ||
+      deployTarget === 'com-canary' ||
+      deployTarget === 'com-beta') {
     ENV.s3.bucket = 'travis-pro-web-production-next';
     ENV.redis.url = process.env.COM_PRODUCTION_REDIS_URL;
   }
