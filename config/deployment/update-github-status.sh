144332cee2c6bea87f2deb0695553700e9b0b394
Buck Doyle <buck@travis-ci.org>
2016-10-05 12:17:53 -0400

    Add environment variable logging again
    😩

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -8,6 +8,8 @@ fi
 FULL_ENVIRONMENT=`echo $TLD`-`echo $ENVIRONMENT`
 FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments.travis-ci.`echo $TLD`
 
+echo deployment url: $FULL_URL
+
 curl -X POST \
      --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
      -H "Authorization: token $GITHUB_TOKEN" \
