b85d925d135b326c10381154cf4472893cb46fee
Buck Doyle <buck@travis-ci.org>
2016-10-05 12:16:32 -0400

    Extract PR deployment URL

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -6,8 +6,9 @@ else
 fi
 
 FULL_ENVIRONMENT=`echo $TLD`-`echo $ENVIRONMENT`
+FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments.travis-ci.`echo $TLD`
 
 curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments.travis-ci.`echo $TLD`\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+     --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
      -H "Authorization: token $GITHUB_TOKEN" \
      https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
