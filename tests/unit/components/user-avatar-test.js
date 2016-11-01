68876d71ab99822dd34ea706a4c64748ce81ce3c
Buck Doyle <buck@travis-ci.org>
2016-11-01 16:14:50 +0100

    Change avatar URL to locally-present image

--- tests/unit/components/user-avatar-test.js
+++ tests/unit/components/user-avatar-test.js
@@ -7,12 +7,12 @@ moduleForComponent('user-avatar', 'UserAvatarComponent | Unit', {
 
 test('it renders', function () {
   var name = 'Hello Test';
-  var url = 'https://someurl.com/someimage.jpg';
+  var url = '/images/travis-crying.png';
 
   var component = this.subject({ url: url, name: name });
   this.render();
 
   ok(component.$().hasClass('avatar'), 'component should have right class');
   equal(component.$('.pseudo-avatar').data('initials'), 'HT', 'initials should be correct');
-  equal(component.$('.real-avatar').attr('src'), 'https://someurl.com/someimage.jpg', 'avatar should be right');
+  equal(component.$('.real-avatar').attr('src'), '/images/travis-crying.png', 'avatar should be right');
 });
