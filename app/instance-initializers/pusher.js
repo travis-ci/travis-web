43b3c516f1eff9ec0580d72b65abcb6708554043
Buck Doyle <b@chromatin.ca>
2016-11-02 11:29:51 +0100

    Disable Pusher in test environment (#762)
    This also removes some unused code.

--- app/instance-initializers/pusher.js
+++ app/instance-initializers/pusher.js
@@ -6,13 +6,16 @@ initialize = function (applicationInstance) {
   const app = applicationInstance.application;
   if (config.pusher.key) {
     app.pusher = new TravisPusher(config.pusher, applicationInstance.lookup('service:ajax'));
-    app.register('pusher:main', app.pusher, {
-      instantiate: false
-    });
-    app.inject('route', 'pusher', 'pusher:main');
-    app.inject('component', 'pusher', 'pusher:main');
-    app.pusher.store = applicationInstance.lookup('service:store');
+  } else {
+    app.pusher = { subscribe: function () {} };
   }
+
+  app.register('pusher:main', app.pusher, {
+    instantiate: false
+  });
+  app.inject('route', 'pusher', 'pusher:main');
+  app.inject('component', 'pusher', 'pusher:main');
+  app.pusher.store = applicationInstance.lookup('service:store');
 };
 
 PusherInitializer = {
