e2a2a55d50b43c6242bc482e0baddbd446b08193
Buck Doyle <buck@travis-ci.org>
2016-11-09 17:32:51 +0100

    Add step to install Heroku toolbelt

--- config/deployment/store-redis-urls.sh
+++ config/deployment/store-redis-urls.sh
@@ -1,3 +1,5 @@
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 export ORG_PRODUCTION_REDIS_URL=`heroku config:get REDIS_URL -a travis-web-production-next`
 export COM_PRODUCTION_REDIS_URL=`heroku config:get REDIS_URL -a travis-pro-web-production-next`
 
