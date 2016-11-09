143cfc7f5ecb6db73933091e35435fe0f1647a98
Buck Doyle <buck@travis-ci.org>
2016-11-09 17:18:21 +0100

    Add script to store Redis URLs

--- config/deployment/deploy-test-master.sh
+++ config/deployment/deploy-test-master.sh
@@ -1,5 +1,7 @@
 export CLEANED_BRANCH_SUBDOMAIN=ember-$EMBER_VERSION
 export DISABLE_SENTRY=true
 
+./config/deployment/store-redis-urls.sh
+
 ember deploy org-$EMBER_VERSION --activate --verbose
 TRAVIS_PRO=true ember deploy com-$EMBER_VERSION --activate --verbose
