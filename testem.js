677a1cc611943d93aa24477d3533794a9564610f
Buck Doyle <b@chromatin.ca>
2016-11-09 14:38:22 +0100

    Remove Sauce Connect references (#782)

--- testem.js
+++ testem.js
@@ -1,58 +1,13 @@
 /* eslint-env node */
-var launchInCI = function () {
-  if (!process.env.TRAVIS || process.env.TRAVIS_PULL_REQUEST) {
-    return ['PhantomJS'];
-  } else {
-    return ['SL_chrome', 'SL_firefox'];
-  }
-};
-
 module.exports = {
-  'framework': 'qunit',
-  'test_page': 'tests/index.html?hidepassed',
-  'launch_in_ci': launchInCI(),
-  'launch_in_dev': [
-    'PhantomJS'
+  "framework": "qunit",
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "PhantomJS"
   ],
-  'launchers': {
-    'SL_chrome': {
-      'exe': './node_modules/.bin/ember-cli-sauce',
-      'args': [
-        '-b',
-        'chrome',
-        '-p',
-        'Windows 8',
-        '--at',
-        '--no-ct',
-        '--u'
-      ],
-      'protocol': 'browser'
-    },
-    'SL_firefox': {
-      'exe': './node_modules/.bin/ember-cli-sauce',
-      'args': [
-        '-b',
-        'firefox',
-        '-p',
-        'Windows 8',
-        '--at',
-        '--no-ct',
-        '--u'
-      ],
-      'protocol': 'browser'
-    },
-    'SL_safari': {
-      'exe': './node_modules/.bin/ember-cli-sauce',
-      'args': [
-        '-b',
-        'safari',
-        '-p',
-        'Mac 10.10',
-        '--at',
-        '--no-ct',
-        '--u'
-      ],
-      'protocol': 'browser'
-    }
-  }
+  "launch_in_dev": [
+    "PhantomJS",
+    "Chrome"
+  ]
 };
