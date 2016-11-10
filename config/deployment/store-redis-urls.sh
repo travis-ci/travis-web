c6880e5e412b1cbd0d85afd51b81a978103be962
Buck Doyle <buck@travis-ci.org>
2016-11-10 08:59:31 +0100

    Remove Heroku success verification logging

--- config/deployment/store-redis-urls.sh
+++ config/deployment/store-redis-urls.sh
@@ -1,5 +1,2 @@
 export ORG_PRODUCTION_REDIS_URL=`heroku config:get REDIS_URL -a travis-web-production-next`
 export COM_PRODUCTION_REDIS_URL=`heroku config:get REDIS_URL -a travis-pro-web-production-next`
-
-echo $ORG_PRODUCTION_REDIS_URL | cut -c1-8
-echo $COM_PRODUCTION_REDIS_URL | cut -c1-8
