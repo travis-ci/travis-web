446bbbe49cbedcc6b2c20211ce8c0dc8ed0d143c
Buck Doyle <buck@travis-ci.org>
2016-11-22 01:09:29 +0100

    Remove scrollTo

--- app/mixins/scroll-reset.js
+++ app/mixins/scroll-reset.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   beforeModel: function () {
     this._super(...arguments);
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
   }
 });
