0170f39dce4b6d133e614890f64ed903439d4417
Curtis Ekstrom <curtis@travis-ci.org>
2016-12-07 13:46:05 +0100

    Disable dashboard hard-coding

--- config/environment.js
+++ config/environment.js
@@ -41,8 +41,7 @@ module.exports = function (environment) {
   ENV.featureFlags = {
     'user-feature-flags': false,
     'debug-logging': false,
-    'pro-version': !!process.env.TRAVIS_PRO || false,
-    'dashboard': true
+    'pro-version': !!process.env.TRAVIS_PRO || false
   };
 
   var statusPageStatusUrl = 'https://pnpcptp8xh9k.statuspage.io/api/v2/status.json';
