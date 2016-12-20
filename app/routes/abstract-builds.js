1fb9e873948dad46c4d0af9007f61760c90c1e53
Ricardo Mendes <rokusu@gmail.com>
2016-12-20 18:53:19 +0000

    Update abstract-builds.js

--- app/routes/abstract-builds.js
+++ app/routes/abstract-builds.js
@@ -3,6 +3,7 @@ import TravisRoute from 'travis/routes/basic';
 
 export default TravisRoute.extend({
   templateName: 'builds',
+  controllerName: 'builds',
 
   titleToken(/* model*/) {
     return this.get('contentType').replace('_', ' ').capitalize();
