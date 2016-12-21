0c5c6bf279e11c2e3a8b9d27c58ecda839820b5e
Buck Doyle <buck@travis-ci.org>
2016-12-21 10:07:15 -0500

    Add explanatory comment
    I was worried that this was buggy but I think it’s okay.
    If there happens to be no gap between the last build
    loaded via the builds API and the last build on the
    default branch, there’ll just be a slight overlap on
    the new builds API call.

--- app/controllers/builds.js
+++ app/controllers/builds.js
@@ -19,7 +19,14 @@ export default Ember.Controller.extend({
 
     const defaultBranchLastBuildNumber = this.get('repo.defaultBranch.lastBuild.number');
 
-    // FIXME this is ridiculous and surely buggy
+    /**
+      * This is a hackish fix for a bug involving a gap in the build history list.
+      * The repos endpoint includes the last build on the default build, and
+      * if there hasn’t been a build on it in a while, the build history query will
+      * only show the 20 most recent builds, so there will be a gap before the default
+      * branch build. Without this workaround, clicking the Show More button will
+      * not load the builds within the gap.
+      */
     if (number === defaultBranchLastBuildNumber) {
       const builds = this.get('builds');
 
