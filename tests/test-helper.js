1d1f3bd3540d986300ad7b8c7af23333159b0f6b
Robert DeLuca <robertdeluca19@gmail.com>
2016-12-28 13:43:33 -0600

    Upgrade x-select to 3.x (#885)
    Thanks to @Robdel12 for the attention to detail!

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,6 @@
 import resolver from './helpers/resolver';
-import registerSelectHelper from './helpers/register-select-helper';
 import './helpers/with-feature';
 
-registerSelectHelper();
 import {
   setResolver
 } from 'ember-qunit';
