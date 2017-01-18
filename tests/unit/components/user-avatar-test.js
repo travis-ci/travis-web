b2d0df7c9852d9d1ca5afe2513717b6f67870c90
lislis <mail@lislis.de>
2017-01-18 18:09:10 +0100

    fix tests

--- tests/unit/components/user-avatar-test.js
+++ tests/unit/components/user-avatar-test.js
@@ -9,10 +9,11 @@ test('it renders', function (assert) {
   const name = 'Hello Test';
   const url = 'https://someurl.com/someimage.jpg';
 
-  const component = this.subject({ url: url, name: name });
+  const component = this.subject({ url: url, name: name, size: 38 });
   this.render();
 
   assert.ok(component.$().hasClass('avatar'), 'component should have right class');
   assert.equal(component.$('.pseudo-avatar').data('initials'), 'HT', 'initials should be correct');
-  assert.equal(component.$('.real-avatar').attr('src'), 'https://someurl.com/someimage.jpg', 'avatar should be right');
+  assert.equal(component.$('.real-avatar').attr('src'), 'https://someurl.com/someimage.jpg?v=3&s=38', 'avatar should display fallback image');
+  assert.equal(component.$('.real-avatar').attr('srcset'), 'https://someurl.com/someimage.jpg?v=3&s=38 1x, https://someurl.com/someimage.jpg?v=3&s=76 2x', 'should provide a low and high res avatar');
 });
