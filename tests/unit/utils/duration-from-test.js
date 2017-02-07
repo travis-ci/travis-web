7f49c6c68536d1ab444f5bd80460e426841b5f2d
Buck Doyle <buck@travis-ci.org>
2017-02-07 10:29:45 -0800

    Update assertion
    This seems like it better exercises the final-missing case.

--- tests/unit/utils/duration-from-test.js
+++ tests/unit/utils/duration-from-test.js
@@ -7,7 +7,7 @@ test('calculates durations in seconds', function (assert) {
   const now = new Date();
 
   assert.equal(durationFrom(now, now), 0);
-  assert.equal(durationFrom(new Date(), null), 0, 'expected a missing finish time to use the current time');
+  assert.equal(durationFrom(new Date(new Date().getTime() - 1000), null), 1, 'expected a missing finish time to use the current time');
 
   const aSecondAfterNow = new Date(now.getTime() + 1000);
   assert.equal(durationFrom(now, aSecondAfterNow), 1);
