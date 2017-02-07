d9af02f1add4d39975ceb176728a35d7b18352b6
Buck Doyle <buck@travis-ci.org>
2017-02-07 10:27:29 -0800

    Remove unused abstraction
    JHW is presumably jasmine-headless-webkit, long gone in the
    testing setup.

--- app/utils/duration-from.js
+++ app/utils/duration-from.js
@@ -2,17 +2,9 @@ function _toUtc(date) {
   return Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds());
 }
 
-function _normalizeDateString(string) {
-  if (window.JHW) {
-    string = string.replace('T', ' ').replace(/-/g, '/');
-    string = string.replace('Z', '').replace(/\..*$/, '');
-  }
-  return string;
-}
-
 export default function durationFrom(started, finished) {
-  started = started && _toUtc(new Date(_normalizeDateString(started)));
-  finished = finished ? _toUtc(new Date(_normalizeDateString(finished))) : _toUtc(new Date());
+  started = started && _toUtc(new Date(started));
+  finished = finished ? _toUtc(new Date(finished)) : _toUtc(new Date());
   if (started && finished) {
     return Math.round((finished - started) / 1000);
   } else {
