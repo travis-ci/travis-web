1a3d948c2e5ee65f17f97c4446cf164170308e91
Curtis Ekstrom <curtis@travis-ci.org>
2017-02-09 16:32:39 +0100

    Fix more test relationships

--- tests/acceptance/builds/current-tab-test.js
+++ tests/acceptance/builds/current-tab-test.js
@@ -23,12 +23,12 @@ test('renders most recent repository without builds', function (assert) {
 });
 
 test('renders most recent repository and most recent build when builds present', function (assert) {
-  let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
+  let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
 
   server.create('branch', {});
-  let build = server.create('build', { number: '5', repository: repo, state: 'passed' });
-  let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let job = server.create('job', { number: '1234.1', repository: repo, state: 'passed', build_id: build.id, buildId: build.id, commit_id: commit.id, config: { language: 'Hello' } });
+  let build = server.create('build', { number: '5', repository, state: 'passed' });
+  let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branchName: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+  let job = server.create('job', { number: '1234.1', repository, state: 'passed', build, commit, config: { language: 'Hello' } });
 
   build.update('commit', commit);
   commit.update('build', build);
--- tests/acceptance/job/basic-layout-test.js
+++ tests/acceptance/job/basic-layout-test.js
@@ -7,12 +7,12 @@ import config from 'travis/config/environment';
 moduleForAcceptance('Acceptance | job/basic layout');
 
 test('visiting job-view', function (assert) {
-  let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
+  let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
   server.create('branch', {});
 
   let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { repository_id: repo.id, state: 'passed', commit_id: commit.id, commit });
-  let job = server.create('job', { number: '1234.1', repository_id: repo.id, state: 'passed', build_id: build.id, commit, build });
+  let build = server.create('build', { repository, state: 'passed', commit });
+  let job = server.create('job', { number: '1234.1', repository, state: 'passed', commit, build });
   commit.update('job', job);
 
   server.create('log', { id: job.id });
