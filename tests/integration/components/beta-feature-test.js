a5bf2519246a65f1a88bdde4af1015c60551161a
lislis <mail@lislis.de>
2017-02-13 15:18:57 +0100

    create component for features

---
+++ tests/integration/components/beta-feature-test.js
@@ -0,0 +1,20 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('beta-feature', 'Integration | Component | beta feature', {
+  integration: true
+});
+
+test('it renders', function (assert) {
+  let feature = {
+    name: 'dasboard',
+    feedbackUrl: 'https://github.com/travis-ci/give-feedback',
+    description: 'super awesome new Dashboard'
+  };
+  this.set('feature', feature);
+  this.render(hbs`{{beta-feature feature=feature}}`);
+
+  assert.equal(this.$().find('.feature-name span').text().trim(), 'Dasboard');
+  assert.equal(this.$().find('.feature-name a').attr('href'), 'https://github.com/travis-ci/give-feedback');
+  assert.equal(this.$().find('p').text().trim(), 'super awesome new Dashboard');
+});
