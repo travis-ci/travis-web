1fe9e3bf9f6278cd3faf7dfdafca487cd03e71b1
lislis <mail@lislis.de>
2017-02-23 16:35:00 +0100

    add error message when feature switch is unsuccessful

--- app/components/feature-toggle.js
+++ app/components/feature-toggle.js
@@ -9,16 +9,22 @@ export default Ember.Component.extend({
   classNameBindings: ['feature.enabled:active', 'disabled:disabled', 'disabled:inline-block'],
 
   features: service(),
+  flashes: service(),
 
   click() {
     this.get('toggleFeatureTask').perform(this.get('feature'));
   },
 
   toggleFeatureTask: task(function* (feature) {
-    feature.toggleProperty('enabled');
-    yield feature.save().then((feature) => {
-      this.applyFeatureState(feature);
-    });
+    try {
+      yield feature.save().then((feature) => {
+        feature.toggleProperty('enabled');
+        this.applyFeatureState(feature);
+      });
+    } catch (e) {
+      // eslint-disable-next-line
+      this.get('flashes').error('There was an error while switching the feature. Please try again.');
+    }
   }),
 
   applyFeatureState(feature) {
