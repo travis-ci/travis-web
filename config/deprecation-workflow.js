778082fab704cfc30758d2a6a6acd29dea5443dc
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-03 15:19:23 +0200

    Hopefully fix lifecycle hook argument deprecation without breakage
    The deprecation guide I followed can be found
    [here](https://emberjs.com/deprecations/v2.x/#toc_id-ember-views-lifecycle-hook-arguments)

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,5 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-views.lifecycle-hook-arguments' }
   ]
 };
