158a9e43193c0ffd6f171175fa512be81b2d7a93
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-03 09:42:36 +0200

    Fix missing semicolon error

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -9,7 +9,7 @@ export default function startApp(attrs) {
   let application;
 
   let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs) // use defaults, but you can override;
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   let clearStorage = (storage) => {
     storage.removeItem('travis.token');
