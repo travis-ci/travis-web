77d046024293bf51d77946b91440dddbab4c56db
lislis <mail@lislis.de>
2017-04-12 16:53:55 +0200

    change branches adapter

--- app/adapters/branch.js
+++ app/adapters/branch.js
@@ -3,13 +3,9 @@ import V3Adapter from 'travis/adapters/v3';
 export default V3Adapter.extend({
   query(store, type, query) {
     const repoId = query.repoId;
-    const active = query.existsOnGithub;
-    const offset = query.offset || 0;
-    delete query.existsOnGithub;
-    delete query.repository_id;
+    delete query.repoId;
 
-    const url = `${this.urlPrefix()}/repo/${repoId}/branches
-?exists_on_github=${active}&include=build.commit&offset=${offset}`;
+    const url = `${this.urlPrefix()}/repo/${repoId}/branches`;
     return this.ajax(url, 'GET', query);
   },
 
