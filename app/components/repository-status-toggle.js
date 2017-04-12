90bb1d748a9538ccfffc4e45525498e1135f078c
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-12 16:20:06 +0200

    Fix derviced class in repository-status-toggle
    This was still referencing the state of `hook` instead of `repository`.

--- app/components/repository-status-toggle.js
+++ app/components/repository-status-toggle.js
@@ -4,7 +4,7 @@ import config from 'travis/config/environment';
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['row'],
-  classNameBindings: ['hook.active:active'],
+  classNameBindings: ['repository.active:active'],
   githubOrgsOauthAccessSettingsUrl: config.githubOrgsOauthAccessSettingsUrl,
 
   actions: {
