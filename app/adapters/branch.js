bc21dd792b065bf0dc38468e4fdba54d197163fe
lislis <mail@lislis.de>
2017-04-12 17:39:45 +0200

    bring back old branch adapter

--- app/adapters/branch.js
+++ app/adapters/branch.js
@@ -3,9 +3,13 @@ import V3Adapter from 'travis/adapters/v3';
 export default V3Adapter.extend({
   query(store, type, query) {
     const repoId = query.repoId;
+    const active = query.existsOnGithub;
+    const offset = query.offset || 0;
+    delete query.existsOnGithub;
     delete query.repoId;
 
-    const url = `${this.urlPrefix()}/repo/${repoId}/branches`;
+    const url = `${this.urlPrefix()}/repo/${repoId}/branches
+?exists_on_github=${active}&include=build.commit&offset=${offset}`;
     return this.ajax(url, 'GET', query);
   },
 
