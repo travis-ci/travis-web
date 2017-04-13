ae1228fe2330ed04dee2ba06e725ee265d21fccf
lislis <mail@lislis.de>
2017-04-13 17:12:24 +0200

    fix branch row test

--- tests/integration/components/branch-row-test.js
+++ tests/integration/components/branch-row-test.js
@@ -14,9 +14,9 @@ test('it renders data correctly', function (assert) {
       name: 'php-test-staging',
       slug: 'travis-repos/php-test-staging'
     },
-    default_branch: true,
-    exists_on_github: true,
-    last_build: {
+    defaultBranch: true,
+    existsOnGithub: true,
+    lastBuild: {
       id: 393177,
       number: '1',
       state: 'passed',
@@ -30,16 +30,9 @@ test('it renders data correctly', function (assert) {
         sha: 'a82f6ba76c7b729375ed6a1d7a26b765f694df12',
         ref: 'refs/heads/master',
         message: 'Add money example as hello world',
-        compare_url: 'https://github.com/travis-repos/php-test-staging/compare/3d86ee98be2b...a82f6ba76c7b',
-        committed_at: '2014-11-20T18:34:04Z',
-        committer: {
-          name: 'Dan Buch',
-          avatar_url: 'https://0.gravatar.com/avatar/563fd372b4d51781853bc85147f06a36'
-        },
-        author: {
-          name: 'Dan Buch',
-          avatar_url: 'https://0.gravatar.com/avatar/563fd372b4d51781853bc85147f06a36'
-        }
+        compareUrl: 'https://github.com/travis-repos/php-test-staging/compare/3d86ee98be2b...a82f6ba76c7b',
+        committedAt: '2014-11-20T18:34:04Z',
+        committerName: 'Dan Buch'
       }
     }
   });
@@ -48,7 +41,7 @@ test('it renders data correctly', function (assert) {
 
   this.render(hbs`{{branch-row branch=branch}}`);
 
-  assert.ok(this.$().hasClass('passed'), 'component should have state class (passed)');
+  assert.ok(this.$().find('.branch-row').hasClass('passed'), 'component should have state class (passed)');
   assert.equal(this.$('.row-name .label-align').text().trim(), 'master', 'should display correct branch name');
   assert.equal(this.$('.row-request .label-align').text().trim(), '#1 passed', 'should display build number and state');
   assert.equal(this.$('.row-commiter .label-align').text().trim(), 'Dan Buch', 'should display correct commiter name');
