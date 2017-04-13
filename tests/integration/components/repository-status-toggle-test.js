9899594dabe8e9d0f022e83829885bbacfbe0d0b
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-13 11:00:36 +0200

    Fix component tests/template references
    Now that `owner` is an object, we need to reference its `login` property.

--- tests/integration/components/repository-status-toggle-test.js
+++ tests/integration/components/repository-status-toggle-test.js
@@ -6,17 +6,19 @@ moduleForComponent('repository-status-toggle', 'RepositoryStatusToggleComponent'
 });
 
 test('it switches state when clicked', function (assert) {
-  this.set('hook', {
+  this.set('repository', {
     id: 10000,
     name: 'foo-bar',
-    owner_name: 'foo',
+    owner: {
+      login: 'foo',
+    },
     description: 'A foo repo',
     active: true,
     urlGithub: 'https://github.com/foo/foobar',
     slug: 'foo/foo-bar'
   });
 
-  this.render(hbs`{{repository-status-toggle hook=hook}}`);
+  this.render(hbs`{{repository-status-toggle repository=repository}}`);
 
   assert.ok(this.$('.switch').hasClass('active'), 'switch should have active class');
   assert.equal(this.$('.profile-repo span').text().trim(), 'A foo repo', 'repo description should be displayed');
