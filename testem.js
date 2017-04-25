0521f091718e9e1add3590c52500cdf3a9d52f49
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-25 13:54:00 +0200

    Use Chrome --headless for testing

--- testem.js
+++ testem.js
@@ -1,12 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'launch_in_ci': [
-    'PhantomJS'
-  ],
-  'launch_in_dev': [
-    'PhantomJS',
-    'Chrome'
-  ]
+  framework: 'qunit',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
+  browser_args: {
+    'Chrome': ['--headless', '--disable-gpu', '--remote-debugging-port=9222'],
+  },
 };
