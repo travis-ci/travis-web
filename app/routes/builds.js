8bda5edfde64ee21f70d04286c6a4d842159bd17
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-27 12:31:46 +0200

    Remove unneeded config for builds route

--- app/routes/builds.js
+++ app/routes/builds.js
@@ -1,11 +1,9 @@
 import TravisRoute from 'travis/routes/basic';
 
-import RenderConfigMixin from 'travis/mixins/builds/rendering';
 import WindowTitleMixin from 'travis/mixins/builds/window-title';
 import StateInitializationMixin from 'travis/mixins/builds/state-initialization';
 
 const mixins = [
-  RenderConfigMixin,
   WindowTitleMixin,
   StateInitializationMixin,
 ];
