c6b143a0c9fe20f7c2bb94cb45e1fb2887b94ca9
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-27 13:15:02 +0200

    Reduce builds route state setup to bare minimum

--- app/routes/builds.js
+++ app/routes/builds.js
@@ -5,25 +5,8 @@ const mixins = [];
 export default TravisRoute.extend(...mixins, {
   setupController() {
     this.controllerFor('repo').activate(this.get('contentType'));
-    this.contentDidChange();
-    this.controllerFor('repo').addObserver(this.get('path'), this, 'contentDidChange');
-    this.controllerFor('build').set('contentType', this.get('contentType'));
-  },
-
-  deactivate() {
-    this.controllerFor('repo').removeObserver(this.get('path'), this, 'contentDidChange');
-    this._super(...arguments);
-  },
-
-  model() {
-    return this.controllerFor('repo').get(this.get('path'));
-  },
-
-  contentDidChange() {
-    // set the controller's model to be a relationship of the currently
-    // rendered repository
-    // `path` will be something like `repo.pullRequests`
     this.controller.set('model', this.controllerFor('repo').get(this.get('path')));
+    this.controllerFor('build').set('contentType', this.get('contentType'));
   },
 
   titleToken() {
