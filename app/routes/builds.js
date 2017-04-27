8fc3a2314cc370346db14fcb61b121c0faa16fa2
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-27 12:35:42 +0200

    Remove need for WindowTitle mixin
    This configuration is "hard-coded" on most other routes and is more straightforward
    then having to mentally parse the code to figure out what will be rendered.

--- app/routes/builds.js
+++ app/routes/builds.js
@@ -1,13 +1,14 @@
 import TravisRoute from 'travis/routes/basic';
-
-import WindowTitleMixin from 'travis/mixins/builds/window-title';
 import StateInitializationMixin from 'travis/mixins/builds/state-initialization';
 
 const mixins = [
-  WindowTitleMixin,
   StateInitializationMixin,
 ];
 
 export default TravisRoute.extend(...mixins, {
+  titleToken() {
+    return 'Builds';
+  },
+
   contentType: 'builds'
 });
--- app/routes/pull-requests.js
+++ app/routes/pull-requests.js
@@ -1,13 +1,14 @@
 import TravisRoute from 'travis/routes/basic';
-
-import WindowTitleMixin from 'travis/mixins/builds/window-title';
 import StateInitializationMixin from 'travis/mixins/builds/state-initialization';
 
 const mixins = [
-  WindowTitleMixin,
   StateInitializationMixin,
 ];
 
 export default TravisRoute.extend(...mixins, {
+  titleToken() {
+    return 'Pull Requests';
+  },
+
   contentType: 'pull_requests',
 });
