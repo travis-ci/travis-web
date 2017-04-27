5de4de6549e659b873f93370ca9031f554634a00
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-27 13:10:38 +0200

    Builds History tab renders without mixins

--- app/routes/builds.js
+++ app/routes/builds.js
@@ -1,11 +1,31 @@
 import TravisRoute from 'travis/routes/basic';
-import StateInitializationMixin from 'travis/mixins/builds/state-initialization';
 
-const mixins = [
-  StateInitializationMixin,
-];
+const mixins = [];
 
 export default TravisRoute.extend(...mixins, {
+  setupController() {
+    this.controllerFor('repo').activate(this.get('contentType'));
+    this.contentDidChange();
+    this.controllerFor('repo').addObserver(this.get('path'), this, 'contentDidChange');
+    this.controllerFor('build').set('contentType', this.get('contentType'));
+  },
+
+  deactivate() {
+    this.controllerFor('repo').removeObserver(this.get('path'), this, 'contentDidChange');
+    this._super(...arguments);
+  },
+
+  model() {
+    return this.controllerFor('repo').get(this.get('path'));
+  },
+
+  contentDidChange() {
+    // set the controller's model to be a relationship of the currently
+    // rendered repository
+    // `path` will be something like `repo.pullRequests`
+    this.controller.set('model', this.controllerFor('repo').get(this.get('path')));
+  },
+
   titleToken() {
     return 'Builds';
   },
