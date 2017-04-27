6ad9207b65260e80595c004b83a4e818c286ac76
Curtis Ekstrom <curtis@travis-ci.org>
2017-04-27 12:22:22 +0200

    Pull request tab renders its own template by convention

--- app/routes/pull-requests.js
+++ app/routes/pull-requests.js
@@ -1,15 +1,14 @@
 import TravisRoute from 'travis/routes/basic';
 
-import RenderConfigMixin from 'travis/mixins/builds/rendering';
 import WindowTitleMixin from 'travis/mixins/builds/window-title';
 import StateInitializationMixin from 'travis/mixins/builds/state-initialization';
 
 const mixins = [
-  RenderConfigMixin,
   WindowTitleMixin,
   StateInitializationMixin,
 ];
 
 export default TravisRoute.extend(...mixins, {
-  contentType: 'pull_requests'
+  contentType: 'pull_requests',
+  controllerName: 'builds',
 });
