919444e709af7fea3d7eeeb9405f235b99c1a848
Curtis Ekstrom <curtis@travis-ci.org>
2017-05-08 11:02:37 +0200

    Add missing google analytics action

--- app/components/landing/pro-page.js
+++ app/components/landing/pro-page.js
@@ -1,4 +1,14 @@
+/* global _gaq */
 import Ember from 'ember';
+import config from 'travis/config/environment';
 
 export default Ember.Component.extend({
+  actions: {
+    gaCta(location) {
+      if (config.gaCode) {
+        _gaq.push(['_trackPageview', `/virtual/signup?${location}`]);
+      }
+      this.auth.signIn();
+    }
+  }
 });
