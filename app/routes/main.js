c0b90b438b7745fc40be971224c3ea149d2ae13a
Curtis Ekstrom <curtis@travis-ci.org>
2017-05-09 10:02:00 +0200

    Fix missing sidebar on main routes

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,4 +1,19 @@
+import Ember from 'ember';
 import TravisRoute from 'travis/routes/basic';
 
 export default TravisRoute.extend({
+  renderTemplate(...args) {
+    if (this.get('auth.signedIn')) {
+      Ember.$('body').attr('id', 'home');
+
+      this._super(args);
+
+      this.render('repos', {
+        outlet: 'left',
+        into: 'main'
+      });
+    } else {
+      return this._super(args);
+    }
+  },
 });
