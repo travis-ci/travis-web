1ebd83125e6e2ae16ad8810efda8c4fab8531988
Curtis Ekstrom <curtis@travis-ci.org>
2017-05-10 16:24:25 +0200

    Render sidebar on `getting_started`

--- app/routes/getting-started.js
+++ app/routes/getting-started.js
@@ -2,6 +2,14 @@ import TravisRoute from 'travis/routes/basic';
 import Ember from 'ember';
 
 export default TravisRoute.extend({
+  renderTemplate(...args) {
+    this._super(args);
+    return this.render('repos', {
+      outlet: 'left',
+      into: 'getting_started'
+    });
+  },
+
   beforeModel() {
     if (!Ember.isEmpty(this.store.peekAll('repo'))) {
       this.transitionTo('/');
