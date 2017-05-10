320be5657a3318370d2485e53f66393aeddc4482
Curtis Ekstrom <curtis@travis-ci.org>
2017-05-10 10:23:05 +0200

    Convert to ember-computed-decorators

--- app/components/owner-repo-tile.js
+++ app/components/owner-repo-tile.js
@@ -1,24 +1,24 @@
 import Ember from 'ember';
+import computed from 'ember-computed-decorators';
 
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['owner-tile', 'row-li'],
   classNameBindings: ['repo.default_branch.last_build.state'],
 
-  ownerName: Ember.computed('repo.slug', function () {
-    return this.get('repo.slug').split(/\//)[0];
-  }),
+  @computed('repo.slug')
+  ownerName(slug) {
+    return slug.split(/\//)[0];
+  },
 
-  repoName: Ember.computed('repo.slug', function () {
-    return this.get('repo.slug').split(/\//)[1];
-  }),
+  @computed('repo.slug')
+  repoName(slug) {
+    return slug.split(/\//)[1];
+  },
 
-  isAnimating: Ember.computed('repo.default_branch.last_build.state', function () {
-    let animationStates, state;
-    state = this.get('repo.default_branch.last_build.state');
-    animationStates = ['received', 'queued', 'started', 'booting'];
-    if (animationStates.indexOf(state) !== -1) {
-      return true;
-    }
-  })
+  @computed('repo.default_branch.last_build.state')
+  isAnimating(state) {
+    const animationStates = ['received', 'queued', 'started', 'booting'];
+    return animationStates.includes(state);
+  }
 });
