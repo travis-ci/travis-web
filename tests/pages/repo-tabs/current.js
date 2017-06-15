1d3610b2e81bcd818bd997befbd1dd50ef06f446
Buck Doyle <b@chromatin.ca>
2017-06-15 11:04:08 -0700

    Update build header to use lone job status (#1131)
    This allows the “booting” state to show on a build when it has only one job.

--- tests/pages/repo-tabs/current.js
+++ tests/pages/repo-tabs/current.js
@@ -10,6 +10,6 @@ export default create({
   visit: visitable('travis-ci/travis-web'),
   currentTabActive: hasClass('active', '#tab_current'),
   showsNoBuildsMessaging: text('.missing-notice h2.page-title'),
-  showsCurrentBuild: hasClass('passed', 'section.build-header'),
+  showsCurrentBuild: hasClass('started', 'section.build-header'),
   noJobsErrorMessage: isVisible('.notice-banner--red')
 });
