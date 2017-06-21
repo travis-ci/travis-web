ebb80b0076a5b6d8e44bacbc5f3839f8ca0a188f
Buck Doyle <b@chromatin.ca>
2017-06-20 21:07:54 -0500

    Remove pre-ember-concurrency leftovers (#1137)
    I should have included these when adding ember-concurrency.

--- app/components/cron-job.js
+++ app/components/cron-job.js
@@ -6,7 +6,6 @@ import { task } from 'ember-concurrency';
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['settings-cron'],
-  isDeleting: false,
   actionType: 'Save',
   store: service(),
   dontRunIfRecentBuildExists: Ember.computed('cron.dont_run_if_recent_build_exists', function () {
--- app/controllers/caches.js
+++ app/controllers/caches.js
@@ -8,7 +8,6 @@ export default Ember.Controller.extend({
   ajax: service(),
   repoController: controller('repo'),
   repo: Ember.computed.alias('repoController.repo'),
-  isDeleting: false,
 
   cachesExist: Ember.computed('model.pushes.length', 'model.pullRequests.length', function () {
     return this.get('model.pushes.length') || this.get('model.pullRequests.length');
