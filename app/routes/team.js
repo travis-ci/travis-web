d39a808503819f502aac9e9433065937f4b3fe82
Buck Doyle <b@chromatin.ca>
2017-06-23 10:21:19 -0500

    Correct typo (#1148)

--- app/routes/team.js
+++ app/routes/team.js
@@ -277,7 +277,7 @@ export default TravisRoute.extend({
       }, {
         name: 'Natalia Saavedra',
         title: 'Design Wizard',
-        nationality: 'columbia',
+        nationality: 'colombia',
         country: 'occupiedcanada',
         image: 'natalia'
       }
