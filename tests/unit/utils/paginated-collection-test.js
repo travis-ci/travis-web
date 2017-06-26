4a6498bd128e674803730b6790278ac63f9c8091
lislis <mail@lislis.de>
2017-06-26 12:32:19 +0200

    try cleanup pagination-collection unit test [skip ci]

--- tests/unit/utils/paginated-collection-test.js
+++ tests/unit/utils/paginated-collection-test.js
@@ -1,11 +1,10 @@
 import PaginatedCollection from 'travis/utils/paginated-collection';
+import { module, test } from 'qunit';
 
-const { module, test } = QUnit;
+module('Unit | Utility | paginated collection');
 
-module('paaginatedCollection');
-
-test('takes content and populates the pagination property accordingly', (assert) => {
-  let content = {
+test('takes content and populates the pagination property accordingly', function (assert) {
+  const value = {
     meta: {
       pagination: {
         limit: 5,
@@ -22,11 +21,15 @@ test('takes content and populates the pagination property accordingly', (assert)
     }
   };
 
-  let collection = PaginatedCollection.create({ content: content });
+  let collection = PaginatedCollection.create({ content: value });
 
   assert.equal(collection.pagination.total, 24, 'returns the total amount of elements');
   assert.equal(collection.pagination.perPage, 5, 'returns the per page limit');
   assert.equal(collection.pagination.isFirst, true, 'knows if it is the first page');
   assert.equal(collection.pagination.isLast, false, 'knows if it is not the last page');
-  assert.equal(collection.pagination.next.offset, 5, 'kann access next page info');
+  assert.equal(collection.pagination.next.offset, 5, 'can access next page info');
+
+  assert.equal(collection.pagination.currentPage, 1, 'calculates correct current page');
+  assert.equal(collection.pagination.numberOfPages, 5, 'calculates correct total number of pages');
+
 });
