fe2e0094cb3528d2dd0ecf864950b7ebf2f69473
lislis <mail@lislis.de>
2017-06-26 14:21:30 +0200

    add pagination-navigation integration test

--- tests/integration/components/pagination-navigation-test.js
+++ tests/integration/components/pagination-navigation-test.js
@@ -6,23 +6,28 @@ moduleForComponent('pagination-navigation', 'Integration | Component | paginatio
 });
 
 test('it renders list of pages', function (assert) {
-  let pagination = {
-    count: 34,
-    limit: 5,
-    offset: 0,
-    is_first: true,
-    is_last: false,
-    prev: null,
-    next: {
-      offset: 5,
-      limit: 5
+  let pageData = {
+    pagination: {
+      total: 97,
+      perPage: 5,
+      offset: 0,
+      isFirst: true,
+      isLast: false,
+      prev: null,
+      next: {
+        offset: 5,
+        limit: 5
+      },
+      currentPage: 1,
+      numberOfPages: 20
     }
   };
 
-  this.set('pagination', pagination);
-  this.render(hbs`{{pagination-navigation pagination=pagination route="someroute"}}`);
+  this.set('pagination', pageData);
+  this.render(hbs`{{pagination-navigation collection=pagination route="someroute"}}`);
 
   assert.equal(this.$().find('a.pagination-button').length, 1, 'on the first page should have one navigation button');
   assert.equal(this.$().find('a.pagination-button').text(), 'next', 'should have a next button on the first page');
-  assert.equal(this.$().find('a.pagination-link').length, 7, 'should calculate correct page count');
+  assert.equal(this.$().find('.pagination-link').length, 4, 'should calculate with pages to display (here the first and last 2)');
+  assert.equal(this.$().find('li:nth-of-type(3)').text().trim(), '...', 'get ... page separator right');
 });
