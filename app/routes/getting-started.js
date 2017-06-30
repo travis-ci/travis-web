aafc57ad52a978d1544b6399893c3a14b21aa30c
Curtis Ekstrom <curtis@travis-ci.org>
2017-06-30 11:08:15 +0200

    Remove dead `renderTemplate` call from `getting_started`

--- app/routes/getting-started.js
+++ app/routes/getting-started.js
@@ -6,10 +6,6 @@ const { service } = Ember.inject;
 export default TravisRoute.extend({
   auth: service(),
 
-  renderTemplate(...args) {
-    this._super(args);
-  },
-
   beforeModel() {
     if (!Ember.isEmpty(this.store.peekAll('repo')) || !this.get('auth.signedIn')) {
       this.transitionTo('/');
