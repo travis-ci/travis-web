cc30d7e71d0ab28d073475b6be663d966f4200be
Curtis Ekstrom <curtis@travis-ci.org>
2017-07-03 12:36:03 +0200

    Reference service state instead of passing `tab` argument in template

--- app/components/no-builds.js
+++ app/components/no-builds.js
@@ -2,13 +2,17 @@ import Ember from 'ember';
 import config from 'travis/config/environment';
 import { task } from 'ember-concurrency';
 
+const { service } = Ember.inject;
+
 export default Ember.Component.extend({
+  tabStates: service(),
+
   isPR: Ember.computed('tab', function () {
-    return this.get('tab') === 'pull_requests';
+    return this.get('tabStates.mainTab') === 'pull_requests';
   }),
 
   isBranch: Ember.computed('tab', function () {
-    return this.get('tab') === 'branches';
+    return this.get('tabStates.mainTab') === 'branches';
   }),
 
   triggerBuild: task(function* () {
