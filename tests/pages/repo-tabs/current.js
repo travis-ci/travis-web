7ec7117f47104f9da7bc74dede4e32474afa554f
Curtis Ekstrom <curtis@travis-ci.org>
2017-07-03 11:58:30 +0200

    Fix selector that caused test to fail
    No idea why this worked before :shrug:

--- tests/pages/repo-tabs/current.js
+++ tests/pages/repo-tabs/current.js
@@ -8,7 +8,7 @@ import {
 
 export default create({
   visit: visitable('travis-ci/travis-web'),
-  currentTabActive: hasClass('active', '#tab_current'),
+  currentTabActive: hasClass('active', '#tab_current a'),
   showsNoBuildsMessaging: text('.missing-notice h2.page-title'),
   showsCurrentBuild: hasClass('started', 'section.build-header'),
   noJobsErrorMessage: isVisible('.notice-banner--red')
