665e8f6d53bd756b9b6c6ab9382734c447a0f1c5
Curtis Ekstrom <curtis@travis-ci.org>
2017-07-03 12:36:42 +0200

    Remove component action referencing commented out template code

--- app/components/no-builds.js
+++ app/components/no-builds.js
@@ -14,16 +14,4 @@ export default Ember.Component.extend({
   isBranch: Ember.computed('tab', function () {
     return this.get('tabStates.mainTab') === 'branches';
   }),
-
-  triggerBuild: task(function* () {
-    const apiEndpoint = config.apiEndpoint;
-
-    yield Ember.$.ajax(`${apiEndpoint}/repo/${this.get('repo.repo.id')}/requests`, {
-      headers: {
-        Authorization: `token ${this.get('repo.auth')}`,
-        'Travis-API-Version': '3'
-      },
-      type: 'POST'
-    });
-  }).drop()
 });
