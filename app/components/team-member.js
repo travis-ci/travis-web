bc7b52025e25d99ae2080b216256a8b936fc8097
Buck Doyle <buck@travis-ci.org>
2017-07-04 11:38:23 -0400

    Add sentence-like forms for country/nationality

--- app/components/team-member.js
+++ app/components/team-member.js
@@ -1,6 +1,29 @@
 import Ember from 'ember';
 
+const countrySentenceOverrides = {
+  ukswitzerland: 'UK/Switzerland',
+  occupiedcanada: 'occupied Canada'
+};
+
+function countryToSentence(country) {
+  const override = countrySentenceOverrides[country];
+
+  if (override) {
+    return override;
+  } else {
+    return country.capitalize();
+  }
+}
+
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['team-member']
+  classNames: ['team-member'],
+
+  countrySentence: Ember.computed('member.country', function () {
+    return countryToSentence(this.get('member.country'));
+  }),
+
+  nationalitySentence: Ember.computed('member.nationality', function () {
+    return countryToSentence(this.get('member.nationality'));
+  })
 });
