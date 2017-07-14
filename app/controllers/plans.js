0331e1f7c9897a4c05f9c0e2bfd2b2ea8b1f2476
Curtis Ekstrom <curtis@travis-ci.org>
2017-07-14 16:06:29 +0200

    Convert plans controller

--- app/controllers/plans.js
+++ app/controllers/plans.js
@@ -1,14 +1,14 @@
 /* global _gaq */
 import Ember from 'ember';
 import config from 'travis/config/environment';
+import { action } from 'ember-decorators/object';
 
 export default Ember.Controller.extend({
-  actions: {
-    gaCta(location) {
-      if (config.gaCode) {
-        _gaq.push(['_trackPageview', '/virtual/signup?' + location]);
-      }
-      this.auth.signIn();
+  @action
+  gaCta(location) {
+    if (config.gaCode) {
+      _gaq.push(['_trackPageview', '/virtual/signup?' + location]);
     }
-  }
+    this.auth.signIn();
+  },
 });
