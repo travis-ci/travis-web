953c5f856ec2cc79518a89c19a06f331059debd2
Buck Doyle <b@chromatin.ca>
2017-07-21 09:27:54 -0400

    Add default for job config
    I’m not even sure about this…???

--- app/components/job-infrastructure-notification.js
+++ app/components/job-infrastructure-notification.js
@@ -23,7 +23,7 @@ export default Ember.Component.extend({
   isTrustySudoFalse: Ember.computed.equal('queue', 'builds.ec2'),
 
   @computed('job.startedAt', 'job.config')
-  isTrustyStable(startedAt, config) {
+  isTrustyStable(startedAt, config = {}) {
     if (config.dist === 'trusty' && config.group === 'stable') {
       const jobRanAfterReleaseDate = Date.parse(startedAt) > Date.parse(LATEST_TRUSTY_RELEASE);
       if (jobRanAfterReleaseDate) {
