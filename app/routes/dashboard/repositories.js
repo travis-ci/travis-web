77c5f6a7a81d769e8d64c09f5eae6ddc69f1930b
Piotr Sarnacki <drogus@gmail.com>
2017-08-01 17:39:46 +0200

    Implement more flexible filtering for the store
    Ember Data's filtering has rather limited capabilities. The biggest
    problem is that it will check if a record should be added to a filterred
    array only when a record is added to the store or a property of a record
    changes. This means that it will not refresh any records in a filtered
    array if you want to depend on anything other than properties defined on
    the record, which may be limiting in some scenarios.

--- app/routes/dashboard/repositories.js
+++ app/routes/dashboard/repositories.js
@@ -4,8 +4,6 @@ import TravisRoute from 'travis/routes/basic';
 let { service } = Ember.inject;
 
 export default TravisRoute.extend({
-  starredRepos: service(),
-
   queryParams: {
     filter: {
       replace: true
@@ -23,7 +21,11 @@ export default TravisRoute.extend({
 
   model(params) {
     return Ember.RSVP.hash({
-      starredRepos: this.get('starredRepos').fetch(),
+      starredRepos: this.store.filter('repo', {
+        active: true,
+        sort_by: 'current_build:desc',
+        starred: true
+      }, (repo) => repo.get('starred'), ['starred']),
       repos: this.store.paginated('repo', {
         active: true,
         sort_by: 'current_build:desc',
