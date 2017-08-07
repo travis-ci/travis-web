e273e7eab0f784b04055d3d9fcbdcf31c16dfe6d
Curtis Ekstrom <curtis@travis-ci.org>
2017-08-07 14:32:02 +0200

    Lint initializers

--- app/initializers/auth.js
+++ app/initializers/auth.js
@@ -1,19 +1,13 @@
-var AuthInitializer, initialize;
-
-initialize = function (app) {
+export function initialize(app) {
   app.inject('route', 'auth', 'service:auth');
   app.inject('controller', 'auth', 'service:auth');
   app.inject('application', 'auth', 'service:auth');
   app.inject('component', 'auth', 'service:auth');
-  return app.inject('service:flashes', 'auth', 'service:auth');
-};
+  app.inject('service:flashes', 'auth', 'service:auth');
+}
 
-AuthInitializer = {
+export default {
   name: 'auth',
   after: 'ember-data',
-  initialize: initialize
+  initialize,
 };
-
-export { initialize };
-
-export default AuthInitializer;
