c0e3a260f1d0ee7498e659d2abb0db96cd15fce7
Curtis Ekstrom <curtis@travis-ci.org>
2017-08-08 09:21:24 +0200

    Lint helpers

--- app/helpers/format-time.js
+++ app/helpers/format-time.js
@@ -1,6 +1,8 @@
 import timeAgoInWords from 'travis/utils/time-ago-in-words';
 import Ember from 'ember';
 
-export default Ember.Helper.helper(function (params) {
-  return new Ember.String.htmlSafe(timeAgoInWords(params[0]) || '-');
+export default Ember.Helper.helper((params) => {
+  const [time] = params;
+  const timeText = timeAgoInWords(time) || '-';
+  return new Ember.String.htmlSafe(timeText);
 });
