f6fd1f695290bac68ca056a07be9638c88bfba13
Buck Doyle <buck@travis-ci.org>
2017-08-08 11:04:58 -0500

    Add flash message acceptance test

---
+++ tests/acceptance/home/flashes-test.js
@@ -0,0 +1,14 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import topPage from 'travis/tests/pages/top';
+
+moduleForAcceptance('Acceptance | home/flashes');
+
+test('the flashes service displays flash messages', function (assert) {
+  visit('/');
+  this.application.__container__.lookup('service:flashes').success('TOTAL SUCCESS');
+
+  andThen(() => {
+    assert.equal(topPage.flashMessage, 'TOTAL SUCCESS');
+  });
+});
