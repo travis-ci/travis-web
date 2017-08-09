83ddee5c876b05e19ec6eb118f9a8ae163041922
Curtis Ekstrom <code@fauxton.io>
2017-08-09 09:35:29 +0200

    Lint initializers (#1214)

--- app/initializers/services.js
+++ app/initializers/services.js
@@ -1,18 +1,13 @@
 import Ember from 'ember';
 import Tailing from 'travis/utils/tailing';
 import ToTop from 'travis/utils/to-top';
-var Initializer, initialize;
 
-initialize = function (application) {
-  application.tailing = new Tailing(Ember.$(window), '#tail', '#log');
-  return application.toTop = new ToTop(Ember.$(window), '.to-top', '#log-container');
-};
+export function initialize(app) {
+  app.tailing = new Tailing(Ember.$(window), '#tail', '#log');
+  app.toTop = new ToTop(Ember.$(window), '.to-top', '#log-container');
+}
 
-Initializer = {
+export default {
   name: 'services',
-  initialize: initialize
+  initialize,
 };
-
-export { initialize };
-
-export default Initializer;
