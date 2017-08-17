63fab25b5d525ff708c5357de2a20d6c6476a731
Buck Doyle <buck@travis-ci.org>
2017-08-17 13:01:16 -0500

    Add beta features route tests
    Perhaps this was a work in progress and then forgotten? This also
    includes a Percy snapshot so we have some protection against the visual
    regression I unknowingly introduced.

--- tests/acceptance/feature-flags/user-sets-flags-test.js
+++ tests/acceptance/feature-flags/user-sets-flags-test.js
@@ -16,9 +16,52 @@ test('visiting /features directly when authenticated', function (assert) {
   const currentUser = server.create('user');
   signInUser(currentUser);
 
+  server.create('feature', {
+    name: 'jorts',
+    description: 'Jorts!',
+    enabled: true
+  });
+
+  const jantsFeature = server.create('feature', {
+    name: 'jants',
+    description: 'Jants?',
+    enabled: false
+  });
+
   featurePage.visit();
+  percySnapshot(assert);
 
   andThen(function () {
     assert.equal(currentURL(), '/features');
+
+    assert.equal(featurePage.features().count, 2, 'expected there to be two features');
+
+    featurePage.features(0).as(jants => {
+      assert.equal(jants.name, 'Jants');
+      assert.equal(jants.description, 'Jants?');
+      assert.notOk(jants.isOn, 'expected the jants switch to be off');
+    });
+
+    featurePage.features(1).as(jorts => {
+      assert.equal(jorts.name, 'Jorts');
+      assert.equal(jorts.description, 'Jorts!');
+      assert.ok(jorts.isOn, 'expected the jorts switch to be on');
+    });
+  });
+
+  let patchRequestBody;
+
+  server.patch(`/user/${currentUser.id}/beta_feature/${jantsFeature.id}`, function (schema, request) {
+    patchRequestBody = JSON.parse(request.requestBody);
+    const feature = schema.features.find(jantsFeature.id);
+    feature.enabled = true;
+    return feature;
+  });
+
+  featurePage.features(0).click();
+
+  andThen(() => {
+    assert.ok(featurePage.features(0).isOn, 'expected the jants switch to now be on');
+    assert.deepEqual(patchRequestBody, { 'beta_feature.enabled': true });
   });
 });
--- tests/pages/features.js
+++ tests/pages/features.js
@@ -1,8 +1,24 @@
 import {
+  clickable,
+  collection,
   create,
+  hasClass,
+  text,
   visitable
 } from 'ember-cli-page-object';
 
 export default create({
-  visit: visitable('/features')
+  visit: visitable('/features'),
+
+  features: collection({
+    itemScope: '.features-list .feature',
+
+    item: {
+      name: text('.feature-name'),
+      description: text('p'),
+      isOn: hasClass('active', '.switch'),
+
+      click: clickable('.switch')
+    }
+  })
 });
