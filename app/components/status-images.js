77d7225bf260873a223bc968165c7c6c44af07cd
lislis <mail@lislis.de>
2017-08-25 15:28:34 +0200

    use repo.defaultBranch instead of hardcoded master

--- app/components/status-images.js
+++ app/components/status-images.js
@@ -3,54 +3,26 @@ import Config from 'travis/config/environment';
 
 import { service } from 'ember-decorators/service';
 import { computed } from 'ember-decorators/object';
-import { alias } from 'ember-decorators/object/computed';
 
 export default Ember.Component.extend({
   @service auth: null,
   @service externalLinks: null,
   @service statusImages: null,
 
-  @alias('popup.popupName') popupName: null,
-
   id: 'status-images',
   attributeBindings: ['id'],
   classNames: ['popup', 'status-images'],
   formats: ['Image URL', 'Markdown', 'Textile', 'Rdoc', 'AsciiDoc', 'RST', 'Pod', 'CCTray'],
 
-  @computed('popupName', 'repo.id')
-  branches(popupName, repoId) {
-    let array = Ember.ArrayProxy.create({ content: [] }),
-      apiEndpoint = Config.apiEndpoint,
-      options = {
-        headers: {
-          'Travis-API-Version': '3'
-        }
-      };
-    array.set('isLoaded', false);
-
-    if (this.get('auth.signedIn')) {
-      options.headers.Authorization = `token ${this.auth.token()}`;
-    }
-
-    let url = `${apiEndpoint}/repo/${repoId}/branches?limit=100`;
-    Ember.$.ajax(url, options).then(response => {
-      if (response.branches.length) {
-        let branchNames = response.branches.map(branch => branch.name);
-        array.pushObjects(branchNames);
-      } else {
-        array.pushObject('master');
-      }
-
-      array.set('isLoaded', true);
-    });
-
-    return array;
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('branch', this.get('repo.defaultBranch.name'));
   },
 
-  @computed('format', 'repo.slug', 'branch')
-  statusString(format, slug, branch) {
+  @computed('format', 'repo.slug', 'branch', 'repo.defaultBranch')
+  statusString(format, slug, branch, defaultBranch) {
     const imageFormat = format || this.get('formats.firstObject');
-    const gitBranch = branch || 'master';
+    const gitBranch = branch || defaultBranch.name;
     return this.formatStatusImage(imageFormat, slug, gitBranch);
   },
 
