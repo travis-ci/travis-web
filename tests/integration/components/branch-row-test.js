ddffee67c3fbd1c700961ee79f238689ad9ed1d0
Curtis Ekstrom <curtis@travis-ci.org>
2017-08-29 12:12:20 +0200

    Fix remaining tests

--- tests/integration/components/branch-row-test.js
+++ tests/integration/components/branch-row-test.js
@@ -52,5 +52,5 @@ test('it renders data correctly', function (assert) {
   assert.equal(this.$('.row-name .label-align').text().trim(), 'master', 'should display correct branch name');
   assert.equal(this.$('.row-request .label-align').text().trim(), '1 passed', 'should display build number and state');
   assert.equal(this.$('.row-commiter .label-align').text().trim(), 'Dan Buch', 'should display correct commiter name');
-  assert.equal(this.$('.row-commit .label-align').text().trim(), 'a82f6ba', 'should display correct commit sha');
+  assert.equal(this.$('.row-commit .inner-underline').text().trim(), 'a82f6ba', 'should display correct commit sha');
 });
