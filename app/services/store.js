c87ff608f0b8ecc6aa7f91d6a35f1abdc21c6b18
Piotr Sarnacki <drogus@gmail.com>
2017-08-30 08:44:39 +0200

    Allow to pass null queryParams to store.filter
    Sometimes we just want to filter existing records and don't make any
    requests at all. This commit allows to pass null value to store.filter
    to achieve that behaviour.

--- app/services/store.js
+++ app/services/store.js
@@ -23,7 +23,9 @@ export default DS.Store.extend({
   //
   // modelName      - a type of the model passed as a string, for example 'repo'
   // queryParams    - params that will be passed to the store.query function when
-  //                  fetching records on the initial call
+  //                  fetching records on the initial call. Passing null or
+  //                  undefined here will stop any requests from happening,
+  //                  filtering will be based only on existing records
   // filterFunction - a function that will be called to determine wheather a
   //                  record should be included in the filtered collection. A
   //                  passed function will be called with a record as an
