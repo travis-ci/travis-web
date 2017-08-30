4c995827fab1a8fc062a268376c8b2b46096140d
Curtis Ekstrom <curtis@travis-ci.org>
2017-08-30 15:38:21 +0200

    Add back in the aria attributes from master

--- app/components/repository-switch.js
+++ app/components/repository-switch.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
+import { computed } from 'ember-decorators/object';
 
 export default Ember.Component.extend({
   tagName: 'button',
   classNames: ['switch'],
   classNameBindings: ['repository.active:active', 'disabled:disabled', 'disabled:inline-block'],
+  attributeBindings: ['aria-checked', 'role'],
+
+  role: 'switch',
+
+  @computed('repository.active')
+  'aria-checked'(active) {
+    if (active) {
+      return 'true';
+    } else {
+      return 'false';
+    }
+  },
 
   click() {
     if (!this.get('disabled')) {
