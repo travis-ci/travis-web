72bc7f1a404de05fe99730c9862c33a69bef8363
Curtis Ekstrom <curtis@travis-ci.org>
2017-09-13 14:19:17 +0200

    Customize URL when fetching organization's repositories

--- app/adapters/repo.js
+++ app/adapters/repo.js
@@ -1,8 +1,22 @@
 import V3Adapter from 'travis/adapters/v3';
+import config from 'travis/config/environment';
+
+const { apiEndpoint } = config;
 
 export default V3Adapter.extend({
   defaultSerializer: '-repo',
 
   includes: 'build.branch,repository.default_branch'
-    + ',repository.current_build,build.commit'
+  + ',repository.current_build,build.commit',
+
+  buildURL(modelName, id, snapshot, requestType, query) {
+    if (query) {
+      const custom = query.custom;
+      if (custom && custom.type === 'byOwner') {
+        const { owner } = custom;
+        return `${apiEndpoint}/owner/${owner}/repos`;
+      }
+    }
+    return this._super(...arguments);
+  },
 });
