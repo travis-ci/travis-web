8dbc362119fa2452f109c94bc254aaf5db9f7466
Piotr Sarnacki <drogus@gmail.com>
2017-09-14 08:58:01 +0200

    Use API V3 for logs endpoint
    This commit switches all of the log calls to API V3. It also simplifies
    fetching logic a little bit, because we don't need to handle redirects
    now.

--- app/models/log.js
+++ app/models/log.js
@@ -1,57 +1,9 @@
 import Ember from 'ember';
+import { fetch, Headers } from 'fetch';
 import config from 'travis/config/environment';
 import { service } from 'ember-decorators/service';
 import { computed } from 'ember-decorators/object';
 import { gt } from 'ember-decorators/object/computed';
-
-let Request = Ember.Object.extend({
-  HEADERS: {
-    accept: 'application/json; chunked=true; version=2, text/plain; version=2'
-  },
-
-  run() {
-    const url = `/jobs/${this.id}/log?cors_hax=true`;
-    return this.get('ajax').ajax(url, 'GET', {
-      dataType: 'text',
-      headers: this.HEADERS,
-      success: (body, status, xhr) => {
-        Ember.run(this, () => this.handle(body, status, xhr));
-      }
-    });
-  },
-
-  handle(body, status, xhr) {
-    if (config.featureFlags['pro-version']) {
-      this.log.set('token', xhr.getResponseHeader('X-Log-Access-Token'));
-    }
-    if (xhr.status === 204) {
-      return Ember.$.ajax({
-        url: this.redirectTo(xhr),
-        type: 'GET',
-        success: (body) => {
-          Ember.run(this, function () { this.handlers.text(body); });
-        }
-      });
-    } else if (this.isJson(xhr)) {
-      return Ember.run(this, function () { this.handlers.json(body); });
-    } else {
-      return Ember.run(this, function () { this.handlers.text(body); });
-    }
-  },
-
-  redirectTo(xhr) {
-    // Firefox can't see the Location header on the xhr response due to the wrong
-    // status code 204. Should be some redirect code but that doesn't work with CORS.
-    return xhr.getResponseHeader('Location');
-  },
-
-  isJson(xhr) {
-    // Firefox can't see the Content-Type header on the xhr response due to the wrong
-    // status code 204. Should be some redirect code but that doesn't work with CORS.
-    let type = xhr.getResponseHeader('Content-Type') || '';
-    return type.indexOf('json') > -1;
-  }
-});
 
 export default Ember.Object.extend({
   @service features: null,
@@ -60,44 +12,6 @@ export default Ember.Object.extend({
   isLoaded: false,
   length: 0,
   @gt('parts.length', 0) hasContent: null,
-
-  fetchMissingParts(partNumbers, after) {
-    let data;
-    if (this.get('notStarted')) {
-      return;
-    }
-    data = {};
-    if (partNumbers) {
-      data['part_numbers'] = partNumbers;
-    }
-    if (after) {
-      data['after'] = after;
-    }
-    const logUrl = `/jobs/${this.get('job.id')}/log`;
-    return this.get('ajax').ajax(logUrl, 'GET', {
-      dataType: 'json',
-      headers: {
-        accept: 'application/json; chunked=true; version=2'
-      },
-      data: data,
-      success: (function (_this) {
-        return function (body) {
-          return Ember.run(_this, function () {
-            let i, len, part, results;
-            let { parts } = body.log;
-            if (parts) {
-              results = [];
-              for (i = 0, len = parts.length; i < len; i++) {
-                part = parts[i];
-                results.push(this.append(part));
-              }
-              return results;
-            }
-          });
-        };
-      })(this)
-    });
-  },
 
   @computed()
   parts() {
@@ -113,33 +27,26 @@ export default Ember.Object.extend({
   },
 
   fetch() {
-    let handlers;
     this.debug('log model: fetching log');
     this.clearParts();
-    handlers = {
-      json: (function (_this) {
-        return function (json) {
-          if (json['log']['removed_at']) {
-            _this.set('removed', true);
-          }
-          return _this.loadParts(json['log']['parts']);
-        };
-      })(this),
-      text: (function (_this) {
-        return function (text) {
-          return _this.loadText(text);
-        };
-      })(this)
-    };
+
     let id = this.get('job.id');
-    if (id) {
-      return Request.create({
-        id,
-        handlers,
-        log: this,
-        ajax: this.get('ajax')
-      }).run();
-    }
+    const url = `${config.apiEndpoint}/job/${id}/log`;
+
+    return fetch(url, {
+      headers: new Headers({
+        'Travis-API-Version': '3'
+      })
+    }).then( (response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw 'error';
+      }
+    }).then( (json) => {
+      this.loadParts(json['log_parts']);
+      this.set('plainTextUrl', json['@raw_url']);
+    });
   },
 
   clear() {
@@ -172,16 +79,6 @@ export default Ember.Object.extend({
       part = parts[i];
       this.append(part);
     }
-    return this.set('isLoaded', true);
-  },
-
-  loadText(text) {
-    this.debug('log model: load text');
-    this.append({
-      number: 1,
-      content: text,
-      final: true
-    });
     return this.set('isLoaded', true);
   },
 
