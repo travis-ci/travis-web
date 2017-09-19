d5b5fcb402d8b10ee2dce131fa67d8b81f890806
Buck Doyle <buck@travis-ci.org>
2017-09-19 09:58:20 +0200

    Split new SSH key test from deletion test
    This was failing with “attempted to handle event didSetProperty”,
    presumably because of the way that the SSH key always uses the
    ID of its repository. This error doesn’t happen in production,
    though. Splitting the tests and faking the non-existence of
    the key in the API is sufficient to test what’s intended.

--- tests/acceptance/repo/settings-test.js
+++ tests/acceptance/repo/settings-test.js
@@ -1,5 +1,7 @@
 /* global moment */
 import { test } from 'qunit';
+import Mirage from 'ember-cli-mirage';
+
 import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
 import settingsPage from 'travis/tests/pages/settings';
 import topPage from 'travis/tests/pages/top';
@@ -318,7 +320,7 @@ test('reload cron branches on branch:created', function (assert) {
   });
 });
 
-test('delete and set SSH keys', function (assert) {
+test('delete SSH key', function (assert) {
   settingsPage.visit({ organization: 'killjoys', repo: 'living-a-feminist-life' });
 
   const deletedIds = [];
@@ -336,8 +338,16 @@ test('delete and set SSH keys', function (assert) {
     assert.equal(settingsPage.sshKey.fingerprint, 'aa:bb:cc:dd');
     assert.ok(settingsPage.sshKey.cannotBeDeleted, 'expected default SSH key not to be deletable');
   });
+});
+
+test('add SSH key', function (assert) {
+  server.schema.db.sshKeys.remove();
 
   const requestBodies = [];
+
+  server.get(`/settings/ssh_key/${this.repository.id}`, function (schema, request) {
+    return new Mirage.Response(429, {}, {});
+  });
 
   server.patch(`/settings/ssh_key/${this.repository.id}`, (schema, request) => {
     const newKey = JSON.parse(request.requestBody);
@@ -348,6 +358,8 @@ test('delete and set SSH keys', function (assert) {
       ssh_key: newKey
     };
   });
+
+  settingsPage.visit({ organization: 'killjoys', repo: 'living-a-feminist-life' });
 
   settingsPage.sshKeyForm.fillDescription('hey');
   settingsPage.sshKeyForm.fillKey('hello');
