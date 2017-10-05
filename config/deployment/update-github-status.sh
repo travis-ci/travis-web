6b4065a41ee49e49778a5fa2c0324aa71d8ad319
Buck Doyle <buck@travis-ci.org>
2017-10-05 12:31:53 +0200

    Add attempt at producing error status updates

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -12,7 +12,15 @@ FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments
 
 echo deployment url: $FULL_URL
 
-curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
-     -H "Authorization: token $GITHUB_TOKEN" \
-     https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+if [[ $DEPLOYMENT_EXIT_CODE ]]
+then
+  curl -X POST \
+       --data "{\"state\": \"error\", \"description\": \"There was a failure with the PR deployment 😢\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+else
+  curl -X POST \
+       --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+fi
