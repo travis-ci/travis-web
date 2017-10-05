976160dda2f9c561ff8b32d120fe12343e744440
Buck Doyle <buck@travis-ci.org>
2017-10-05 12:47:30 +0200

    Change error detection
    BASH

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -12,15 +12,15 @@ FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments
 
 echo deployment url: $FULL_URL
 
-if [[ $DEPLOYMENT_EXIT_CODE ]]
+if [[ $DEPLOYMENT_EXIT_CODE -eq 0 ]]
 then
   curl -X POST \
-       --data "{\"state\": \"error\", \"description\": \"There was a failure with the PR deployment :cry:\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+       --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
        -H "Authorization: token $GITHUB_TOKEN" \
        https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
 else
   curl -X POST \
-       --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+       --data "{\"state\": \"error\", \"description\": \"There was a failure with the PR deployment :cry:\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
        -H "Authorization: token $GITHUB_TOKEN" \
        https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
 fi
