a14671eb3b16a9f096c83e18be6533fb6d895535
Curtis Ekstrom <code@fauxton.io>
2017-10-05 09:20:52 +0200

    Only toggle the property if the request was successful
    The user will be shown an error flash message if something goes wrong.

--- app/components/settings-switch.js
+++ app/components/settings-switch.js
@@ -24,10 +24,9 @@ export default Ember.Component.extend({
   },
 
   save: task(function* () {
-    this.toggleProperty('active');
-
     try {
       yield this.get('repo').saveSetting(this.get('key'), this.get('active'));
+      this.toggleProperty('active');
     } catch (e) {
       this.get('flashes').error('There was an error while saving your settings. Please try again.');
     }
