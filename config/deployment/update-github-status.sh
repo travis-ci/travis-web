cb9c772aa6f125f9713bafffe32c806667ff4384
Buck Doyle <b@chromatin.ca>
2017-10-10 11:36:46 -0400

    Add PR deployment failure status updates (#1337)
    Before this, failures in PR deployment were in the logs only.

--- config/deployment/update-github-status.sh
+++ config/deployment/update-github-status.sh
@@ -12,7 +12,15 @@ FULL_URL=https://`echo $CLEANED_BRANCH_SUBDOMAIN``echo $SUFFIX`.test-deployments
 
 echo deployment url: $FULL_URL
 
-curl -X POST \
-     --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
-     -H "Authorization: token $GITHUB_TOKEN" \
-     https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+if [[ $DEPLOYMENT_EXIT_CODE -eq 0 ]]
+then
+  curl -X POST \
+       --data "{\"state\": \"success\", \"target_url\": \"$FULL_URL\", \"description\": \"Visit a $FULL_ENVIRONMENT deployment for this commit\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+else
+  curl -X POST \
+       --data "{\"state\": \"error\", \"description\": \"There was a failure with the PR deployment\", \"context\": \"deployments/$FULL_ENVIRONMENT\"}" \
+       -H "Authorization: token $GITHUB_TOKEN" \
+       https://api.github.com/repos/travis-ci/travis-web/statuses/$TRAVIS_PULL_REQUEST_SHA
+fi
