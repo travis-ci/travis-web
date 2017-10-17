a295ce11ec80cab65f43dfe9973559b7619ad380
Buck Doyle <b@chromatin.ca>
2017-10-17 16:48:10 -0700

    Update Ember projects to 2.16 (#1368)
    I removed ember-router-service-polyfill, as advised by a notification
    while building.
    
    This includes some simple line-switchings where the modules codemod
    put things in a different order than the blueprints. I also replaced
    testem.js with the blueprint version because our differences were
    negligible, might as well reduce friction during updates.

--- testem.js
+++ testem.js
@@ -1,12 +1,22 @@
 /* eslint-env node */
-
 module.exports = {
-  framework: 'qunit',
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
   browser_args: {
-    'Chrome': ['--headless', '--disable-gpu', '--remote-debugging-port=9222'],
-  },
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=9222',
+        '--window-size=1440,900'
+      ]
+    },
+  }
 };
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,14 +1,12 @@
-import { run } from '@ember/runloop';
-import { merge } from '@ember/polyfills';
 import Application from '../../app';
 import config from '../../config/environment';
+import { merge } from '@ember/polyfills';
+import { run } from '@ember/runloop';
 
 import './sign-in-user';
 import './wait-for-element';
 
 export default function startApp(attrs) {
-  let application;
-
   let attributes = merge({}, config.APP);
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
@@ -21,7 +19,7 @@ export default function startApp(attrs) {
   clearStorage(sessionStorage);
 
   return run(() => {
-    application = Application.create(attributes);
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
     return application;
