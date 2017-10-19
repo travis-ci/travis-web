97d61c3e9e150823f6aee71b6bdbde7e75c94f37
lislis <mail@lislis.de>
2017-10-19 15:42:49 +0200

    re-arrange some stuff

--- app/components/enterprise-banner.js
+++ app/components/enterprise-banner.js
@@ -10,45 +10,33 @@ const DAYS_FROM_NOW_THAT_EXPIRATION_TIME_IS_IMMINENT = 21;
 export default Ember.Component.extend({
   @service ajax: null,
 
-  licenseType: 'regular',
-  billingFrequency: 'annual',
-  maxSeats: 30,
-  licenseExpirationTime: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 30),
-  currentSeats: 27,
-
+  licenseType: 'regular', // todo
+  maxSeats: 30, // todo
+  licenseExpirationTime: null,
+  currentSeats: 27, //todo
 
   init() {
     this._super(...arguments);
 
-    // would we still need to check for enterprise then?
     const url = '/v3/enterprise_license';
     this.get('ajax').get(url).then(response => {
-    // if we don't check for enterprise before
-    // should we check for 404 here?
-
-     Ember.run(() => {
-
-     this.set('licenseExpirationTime', new Date(Date.parse(response.expiration_time)));
-     this.set('licenseType', response.license_type);
-     this.set('billingFrequency', response.billing_frequency);
-     this.set('maxSeats', response.seats);
-
-     this.set('licenseExpirationTime', new Date(Date.parse(response.expiration_time)));
-     });
+      Ember.run(() => {
+        this.set('licenseExpirationTime', new Date(Date.parse(response.expiration_time)));
+        this.set('licenseType', response.license_type);
+        this.set('maxSeats', response.seats)
+      });
     });
   },
-   */
 
   @computed('maxSeats', 'currentSeats')
   exceedingSeats(maxSeats, currentSeats) {
     let tenPercent = maxSeats / 10;
-    debugger
     return (currentSeats + tenPercent) >= maxSeats;
   },
 
   @computed('licenseType', 'billingFrequency')
   isTrial(licenseType, billingFrequency) {
-    return ((licenseType && licenseType === 'trial') || !licenseType) && !billingFrequency;
+    return ((licenseType && licenseType === 'trial') || !licenseType);
   },
 
   @computed('licenseExpirationTime')
