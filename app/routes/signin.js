26f20aaa8bbc7d593a5876491ca1b90bd9f405ad
Buck Doyle <buck@travis-ci.org>
2017-11-07 18:52:59 -0600

    Correct signin call

--- app/routes/signin.js
+++ app/routes/signin.js
@@ -3,7 +3,7 @@ import TravisRoute from 'travis/routes/basic';
 export default TravisRoute.extend({
   beforeModel(transition) {
     if (!this.signedIn()) {
-      this.auth.autoSignIn();
+      this.auth.signIn();
     } else {
       this.transitionTo('/');
     }
