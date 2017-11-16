8d0cb925e5a0f0ac1097ab6a65c4e942ba42700d
Piotr Sarnacki <drogus@gmail.com>
2017-11-16 19:00:58 +0100

    Add `offset: 0` to account/repositories controller
    Providing the default removes the `offset=0` from the query params when
    you enter the profile

--- app/controllers/account/repositories.js
+++ app/controllers/account/repositories.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { computed } from 'ember-decorators/object';
 
 export default Controller.extend({
+  offset: 0,
+
   @computed('model')
   sortedRepositories(repos) {
     return repos.sortBy('name');
