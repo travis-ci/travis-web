567516969c8e6dfe44061bb4678aec5080fd4cb1
Dominic Jodoin <dominic@travis-ci.com>
2017-11-27 19:36:54 -0500

    Fixed indentation.

--- app/components/job-infrastructure-notification.js
+++ app/components/job-infrastructure-notification.js
@@ -10,105 +10,105 @@ const NOVEMBER_2017_RETIREMENT = '2017-11-28T12:00:00-08:00';
 const LATEST_TRUSTY_RELEASE = '2017-07-12T18:00:00-00:00';
 
 export default Component.extend({
-    @alias('job.queue') queue: null,
-    @alias('job.config') jobConfig: null,
+  @alias('job.queue') queue: null,
+  @alias('job.config') jobConfig: null,
 
-    @computed('job.isFinished')
-    conjugatedRun(isFinished) {
-        return isFinished ? 'ran' : 'is running';
-    },
+  @computed('job.isFinished')
+  conjugatedRun(isFinished) {
+    return isFinished ? 'ran' : 'is running';
+  },
 
-    @equal('queue', 'builds.linux') isLegacyInfrastructure: null,
-    @equal('queue', 'builds.ec2') isTrustySudoFalse: null,
+  @equal('queue', 'builds.linux') isLegacyInfrastructure: null,
+  @equal('queue', 'builds.ec2') isTrustySudoFalse: null,
 
-    @computed('job.startedAt', 'job.config')
-    isTrustyStable(startedAt, config = {}) {
-        if (config.dist === 'trusty' && config.group === 'stable') {
-            const jobRanAfterReleaseDate = Date.parse(startedAt) > Date.parse(LATEST_TRUSTY_RELEASE);
-            if (jobRanAfterReleaseDate) {
-                return true;
-            }
-        }
+  @computed('job.startedAt', 'job.config')
+  isTrustyStable(startedAt, config = {}) {
+    if (config.dist === 'trusty' && config.group === 'stable') {
+      const jobRanAfterReleaseDate = Date.parse(startedAt) > Date.parse(LATEST_TRUSTY_RELEASE);
+      if (jobRanAfterReleaseDate) {
+        return true;
+      }
+    }
 
-        return false;
-    },
+    return false;
+  },
 
-    @equal('queue', 'builds.macstadium6') isMacStadium6: null,
+  @equal('queue', 'builds.macstadium6') isMacStadium6: null,
 
-    @computed('queue', 'job.config')
-    isPreciseEOL(queue, config = {}) {
-        if (queue === 'builds.gce' && config.dist === 'precise') {
-            if (config.language !== 'android') {
-                return true;
-            }
-        }
-    },
+  @computed('queue', 'job.config')
+  isPreciseEOL(queue, config = {}) {
+    if (queue === 'builds.gce' && config.dist === 'precise') {
+      if (config.language !== 'android') {
+        return true;
+      }
+    }
+  },
 
-    @alias('jobConfig.osx_image') macOSImage: null,
+  @alias('jobConfig.osx_image') macOSImage: null,
 
-    deprecatedXcodeImages: ['xcode8', 'xcode8.1', 'xcode8.2'],
+  deprecatedXcodeImages: ['xcode8', 'xcode8.1', 'xcode8.2'],
 
-    imageToRetirementDate: {
-        'xcode8': NOVEMBER_2017_RETIREMENT,
-        'xcode8.1': NOVEMBER_2017_RETIREMENT,
-        'xcode8.2': NOVEMBER_2017_RETIREMENT
-    },
+  imageToRetirementDate: {
+    'xcode8': NOVEMBER_2017_RETIREMENT,
+    'xcode8.1': NOVEMBER_2017_RETIREMENT,
+    'xcode8.2': NOVEMBER_2017_RETIREMENT
+  },
 
-    imageToNewImage: {
-        'xcode8': 'xcode8.3',
-        'xcode8.1': 'xcode8.3',
-        'xcode8.2': 'xcode8.3'
-    },
+  imageToNewImage: {
+    'xcode8': 'xcode8.3',
+    'xcode8.1': 'xcode8.3',
+    'xcode8.2': 'xcode8.3'
+  },
 
-    newImageStrings: {
-        'xcode8.3': 'Xcode 8.3.3',
-        'xcode7.3': 'Xcode 7.3.1',
-        'xcode6.4': 'Xcode 6.4'
-    },
+  newImageStrings: {
+    'xcode8.3': 'Xcode 8.3.3',
+    'xcode7.3': 'Xcode 7.3.1',
+    'xcode6.4': 'Xcode 6.4'
+  },
 
-    @computed('isMacStadium6', 'macOSImage')
-    isDeprecatedOrRetiredMacImage(isMacStadium6, macOSImage) {
-        return isMacStadium6 && this.get('deprecatedXcodeImages').includes(macOSImage);
-    },
+  @computed('isMacStadium6', 'macOSImage')
+  isDeprecatedOrRetiredMacImage(isMacStadium6, macOSImage) {
+    return isMacStadium6 && this.get('deprecatedXcodeImages').includes(macOSImage);
+  },
 
-    // eslint-disable-next-line
-    @computed('job.startedAt', 'macOSImage', 'job.isFinished', 'conjugatedRun', 'isDeprecatedOrRetiredMacImage')
-    deprecatedOrRetiredMacImageMessage(startedAt, image, isFinished, conjugatedRun) {
-        const retirementDate = Date.parse(this.get('imageToRetirementDate')[image]);
+  // eslint-disable-next-line
+  @computed('job.startedAt', 'macOSImage', 'job.isFinished', 'conjugatedRun', 'isDeprecatedOrRetiredMacImage')
+  deprecatedOrRetiredMacImageMessage(startedAt, image, isFinished, conjugatedRun) {
+    const retirementDate = Date.parse(this.get('imageToRetirementDate')[image]);
 
-        const newImage = this.get('imageToNewImage')[image];
-        const newImageString = this.get('newImageStrings')[newImage];
-        const newImageAnchor = newImageString.replace(' ', '-');
-        const newImageURL = `https://docs.travis-ci.com/user/osx-ci-environment/#${newImageAnchor}`;
+    const newImage = this.get('imageToNewImage')[image];
+    const newImageString = this.get('newImageStrings')[newImage];
+    const newImageAnchor = newImageString.replace(' ', '-');
+    const newImageURL = `https://docs.travis-ci.com/user/osx-ci-environment/#${newImageAnchor}`;
 
-        const jobRanBeforeRetirementDate = Date.parse(startedAt) < retirementDate;
-        const retirementDateIsInTheFuture = retirementDate > new Date();
+    const jobRanBeforeRetirementDate = Date.parse(startedAt) < retirementDate;
+    const retirementDateIsInTheFuture = retirementDate > new Date();
 
-        const formattedRetirementDate = moment(retirementDate).format('MMMM D, YYYY');
+    const formattedRetirementDate = moment(retirementDate).format('MMMM D, YYYY');
 
-        const retirementLink =
-            `<a href='${newImageURL}'>${retirementDateIsInTheFuture ? 'will be retired' : 'was retired'}
+    const retirementLink =
+      `<a href='${newImageURL}'>${retirementDateIsInTheFuture ? 'will be retired' : 'was retired'}
       on ${formattedRetirementDate}</a>`;
 
-        let retirementSentence, routingSentence;
+    let retirementSentence, routingSentence;
 
-        if (retirementDateIsInTheFuture) {
-            retirementSentence = `This job ${conjugatedRun} on an OS X image that ${retirementLink}.`;
-        } else {
-            retirementSentence = `
+    if (retirementDateIsInTheFuture) {
+      retirementSentence = `This job ${conjugatedRun} on an OS X image that ${retirementLink}.`;
+    } else {
+      retirementSentence = `
         This job ${isFinished ? 'was configured to run on' : 'is configured to run on'}
         an OS X image that ${retirementLink}.`;
-        }
-
-        if (retirementDateIsInTheFuture) {
-            routingSentence =
-                `After that, it will route to our ${newImageString} image.`;
-        } else if (jobRanBeforeRetirementDate) {
-            routingSentence = `New jobs will route to our ${newImageString} image.`;
-        } else {
-            routingSentence = `It was routed to our ${newImageString} image.`;
-        }
-
-        return `${retirementSentence} ${routingSentence}`;
     }
+
+    if (retirementDateIsInTheFuture) {
+      routingSentence =
+        `After that, it will route to our ${newImageString} image.`;
+    } else if (jobRanBeforeRetirementDate) {
+      routingSentence = `New jobs will route to our ${newImageString} image.`;
+    } else {
+      routingSentence = `It was routed to our ${newImageString} image.`;
+    }
+
+    return `${retirementSentence} ${routingSentence}`;
+  }
 });
