0a7cd72f4068a35ae8a30f005e636c16fd2baa25
Buck Doyle <buck@travis-ci.org>
2017-12-06 12:33:10 -0600

    Remove debugging help

--- tests/acceptance/builds/build-stages-test.js
+++ tests/acceptance/builds/build-stages-test.js
@@ -72,7 +72,6 @@ test('visiting build with stages', function (assert) {
 
   andThen(function () {
     assert.equal(buildPage.stages().count, 2, 'expected two build stages');
-    return pauseTest();
 
     buildPage.stages(0).as(stage => {
       assert.equal(stage.name, 'first', 'expected the stages to be numerically sorted');
