079bd27737bf7c66e6a650c36086b7696452ad10
Buck Doyle <buck@travis-ci.org>
2017-12-07 16:38:53 -0600

    Add scroll reset-prevention on first repo load
    I’m not sure if this makes sense.

--- app/mixins/scroll-reset.js
+++ app/mixins/scroll-reset.js
@@ -2,7 +2,12 @@ import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
   beforeModel: function () {
-    window.scrollTo(0, 0);
+    const modelFor = this.modelFor('repo');
+
+    if (modelFor) {
+      window.scrollTo(0, 0);
+    }
+
     return this._super(...arguments);
   }
 });
