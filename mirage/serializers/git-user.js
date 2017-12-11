1406cdb05d5e36b6e6c85dc8d6c0b68cc8c8c721
Buck Doyle <buck@travis-ci.org>
2017-12-11 16:11:50 -0600

    Fix ESLint error
    meh

--- mirage/serializers/git-user.js
+++ mirage/serializers/git-user.js
@@ -2,7 +2,9 @@ import { ActiveModelSerializer } from 'ember-cli-mirage';
 
 export default ActiveModelSerializer.extend({
   serializeSingle(user /* , request, options */) {
-    const { avatar_url, name } = user.attrs;
-    return { avatar_url, name };
+    return {
+      avatar_url: user.attrs.avatar_url,
+      name: user.attrs.name
+    };
   }
 });
