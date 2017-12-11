24eef9ebb2dc2ac7f8832c3f86366aa4fa4898da
Dominic Jodoin <dominic@travis-ci.com>
2017-12-11 16:07:49 -0500

    Add Trusty/GCE banner (#1427)

--- app/components/job-infrastructure-notification.js
+++ app/components/job-infrastructure-notification.js
@@ -5,7 +5,7 @@ import { alias, equal } from 'ember-decorators/object/computed';
 import moment from 'moment';
 
 const NOVEMBER_2017_RETIREMENT = '2017-11-28T12:00:00-08:00';
-const LATEST_TRUSTY_RELEASE = '2017-07-12T18:00:00-00:00';
+const LATEST_TRUSTY_RELEASE = '2017-12-10T00:00:00-00:00';
 
 export default Component.extend({
   @alias('job.queue') queue: null,
@@ -19,9 +19,9 @@ export default Component.extend({
   @equal('queue', 'builds.linux') isLegacyInfrastructure: null,
   @equal('queue', 'builds.ec2') isTrustySudoFalse: null,
 
-  @computed('job.startedAt', 'job.config')
-  isTrustyStable(startedAt, config = {}) {
-    if (config.dist === 'trusty' && config.group === 'stable') {
+  @computed('job.startedAt', 'queue', 'job.config.dist')
+  isTrustySudoRequired(startedAt, queue, dist) {
+    if (queue === 'builds.gce' && dist === 'trusty') {
       const jobRanAfterReleaseDate = Date.parse(startedAt) > Date.parse(LATEST_TRUSTY_RELEASE);
       if (jobRanAfterReleaseDate) {
         return true;
