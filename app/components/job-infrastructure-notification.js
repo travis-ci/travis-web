9258839303ce2fecc0c269a4378abb8fd421a9a4
Dominic Jodoin <dominic@travis-ci.com>
2017-12-11 15:27:58 -0500

    Evaluate config.dist at the last time.

--- app/components/job-infrastructure-notification.js
+++ app/components/job-infrastructure-notification.js
@@ -19,9 +19,9 @@ export default Component.extend({
   @equal('queue', 'builds.linux') isLegacyInfrastructure: null,
   @equal('queue', 'builds.ec2') isTrustySudoFalse: null,
 
-  @computed('job.startedAt', 'queue', 'job.config.dist')
-  isTrustySudoRequired(startedAt, queue, dist) {
-    if (queue === 'builds.gce' && dist === 'trusty') {
+  @computed('job.startedAt', 'queue', 'job.config')
+  isTrustySudoRequired(startedAt, queue, config) {
+    if (queue === 'builds.gce' && job.get('config.dist') === 'trusty') {
       const jobRanAfterReleaseDate = Date.parse(startedAt) > Date.parse(LATEST_TRUSTY_RELEASE);
       if (jobRanAfterReleaseDate) {
         return true;
