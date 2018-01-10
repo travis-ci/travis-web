69eec3a951460da8cc261e5ae2df67b1695b0b67
Berrak Nil <berraknil@gmail.com>
2018-01-10 12:12:48 +0100

    fixed unnecessary space

--- app/helpers/pretty-date.js
+++ app/helpers/pretty-date.js
@@ -4,7 +4,7 @@ import { helper } from '@ember/component/helper';
 
 import moment from 'moment';
 
-export function prettyDate(params)  {
+export function prettyDate(params) {
   let date = new Date(params[0]);
   return new htmlSafe(moment(date).format('MMMM D, YYYY H:mm:ss') || '-');
 }
