9af2e70dd722271dbf92620ee8fb96a06a62833a
Berrak Nil Boya <berraknil@gmail.com>
2018-01-12 14:49:53 +0100

    Define CLEANED_BRANCH_SUBDOMAIN

--- config/deployment/deploy.sh
+++ config/deployment/deploy.sh
@@ -1,3 +1,5 @@
 export ORG_STAGING_REDIS_URL=`heroku config:get REDIS_URL -a travis-web-staging`
+export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_BRANCH | tr '.' '-' | tr '[:upper:]' '[:lower:]'`
+
 
 ember deploy org-staging
