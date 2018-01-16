472efdb46a9d43f8236d7920f9fc57e49b63bca9
Buck Doyle <buck@travis-ci.org>
2018-01-16 15:07:10 +0100

    Replace weirdness with more idomatic weirdness

--- app/components/build-yml-messages.js
+++ app/components/build-yml-messages.js
@@ -3,16 +3,27 @@ import { computed } from 'ember-decorators/object';
 import fetch from 'fetch';
 import { service } from 'ember-decorators/service';
 
+import ObjectProxy from '@ember/object/proxy';
+import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
+let ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
+
 export default Component.extend({
   @service store: null,
+
   @computed('repo.id', 'build.request_id')
   messages(repoId, requestId) {
     const urlRoot = this.get('store').adapterFor('v3').buildURL('repo', repoId);
 
-    return fetch(`${urlRoot}/request/${requestId}/messages`)
-      .then(response => response.json()).then(response => {
-        this.set('actualMessages', response.messages);
-        return response.messages;
-      });
+    return ObjectPromiseProxy.create({
+      promise: fetch(`${urlRoot}/request/${requestId}/messages`)
+        .then(response => response.json()).then(response => {
+          // FIXME this is because of an ESLint rule I don’t understand at the moment
+          response.something;
+          // FIXME this nesting is weird, obvs
+          return {
+            messages: response.messages
+          };
+        })
+    });
   }
 });
