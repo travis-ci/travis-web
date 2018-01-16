dae579a7430afa3cf2b02bad118feb2f0342e92b
Buck Doyle <buck@travis-ci.org>
2018-01-16 13:50:53 +0100

    Clean up some hackery

--- app/components/build-yml-messages.js
+++ app/components/build-yml-messages.js
@@ -8,6 +8,11 @@ export default Component.extend({
   @computed('repo.id', 'build.request_id')
   messages(repoId, requestId) {
     const urlRoot = this.get('store').adapterFor('v3').buildURL('repo', repoId);
-    return fetch(`${urlRoot}/request/${requestId}/messages`).then(response => response.json()).then(response => {window.response = response; this.set('actualMessages', response.messages); return response.messages});
+
+    return fetch(`${urlRoot}/request/${requestId}/messages`)
+      .then(response => response.json()).then(response => {
+        this.set('actualMessages', response.messages);
+        return response.messages;
+      });
   }
 });
