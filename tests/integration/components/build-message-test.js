7ba74ecde562e5b2210362b738a854c80d0b2fab
Buck Doyle <buck@travis-ci.org>
2018-01-17 16:51:28 +0100

    Fix ESLint in test file

--- tests/integration/components/build-message-test.js
+++ tests/integration/components/build-message-test.js
@@ -5,8 +5,7 @@ moduleForComponent('build-message', 'Integration | Component | build message', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
+test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
