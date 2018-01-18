9fb40426e8240390e3a8794768bae9972cfb89ba
Buck Doyle <buck@travis-ci.org>
2018-01-18 19:10:06 +0100

    Remove test for now

--- tests/integration/components/build-message-test.js
+++ tests/integration/components/build-message-test.js
@@ -1,11 +1,11 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent, skip } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('build-message', 'Integration | Component | build message', {
   integration: true
 });
 
-test('it renders', function (assert) {
+skip('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
   // FIXME this is hackish!
