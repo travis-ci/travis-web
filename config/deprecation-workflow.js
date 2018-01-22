e831fa3879e573fe05d6d172eeb52c4bef4b7f9b
Buck Doyle <buck@travis-ci.org>
2018-01-22 17:38:51 +0100

    Restore deprecations as throws

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,5 +1,7 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
+    { handler: 'throw', matchId: 'ember-router.router' },
+    { handler: 'throw', matchId: 'macro-computed-deprecated' },
   ]
 };
