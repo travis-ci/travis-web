b9c68a740d7de14ed15fd785e16aaa383d9a9ab2
Buck Doyle <buck@travis-ci.org>
2018-01-22 17:20:38 +0100

    Restore silenced deprecations
    What will happen

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,7 +1,5 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-router.router' },
-    { handler: 'silence', matchId: 'macro-computed-deprecated' },
   ]
 };
