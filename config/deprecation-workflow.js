c26e8c1dc0ce41ad23f42168a979242d627fdc78
Buck Doyle <b@chromatin.ca>
2018-01-23 14:57:37 +0100

    Remove fixed deprecation (#1486)
    I already handled this during the 2.16 update.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,7 +2,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-router.router' },
     { handler: 'silence', matchId: 'macro-computed-deprecated' },
   ]
 };
