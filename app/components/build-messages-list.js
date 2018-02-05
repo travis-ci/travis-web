40393d81d046f953bb640689fd61cf776bbf9f1c
Buck Doyle <buck@travis-ci.org>
2018-02-05 10:33:37 -0600

    Add guard against missing request ID

--- app/components/build-messages-list.js
+++ app/components/build-messages-list.js
@@ -6,20 +6,27 @@ import { service } from 'ember-decorators/service';
 import ObjectProxy from '@ember/object/proxy';
 import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
 let ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
+import { Promise as EmberPromise } from 'rsvp';
 
 export default Component.extend({
   @service store: null,
 
   @computed('repo.id', 'build.request.id')
   messagesRequest(repoId, requestId) {
-    const urlRoot = this.get('store').adapterFor('v3').buildURL('repo', repoId);
+    if (requestId) {
+      const urlRoot = this.get('store').adapterFor('v3').buildURL('repo', repoId);
 
-    return ObjectPromiseProxy.create({
-      promise: fetch(`${urlRoot}/request/${requestId}/messages`, {
-        headers: {
-          'Travis-API-Version': '3'
-        }})
-        .then(response => response.json()).then(response => ({messages: response.messages}))
-    });
+      return ObjectPromiseProxy.create({
+        promise: fetch(`${urlRoot}/request/${requestId}/messages`, {
+          headers: {
+            'Travis-API-Version': '3'
+          }})
+          .then(response => response.json()).then(response => ({messages: response.messages}))
+      });
+    } else {
+      return ObjectPromiseProxy.create({
+        promise: EmberPromise.resolve([])
+      });
+    }
   }
 });
