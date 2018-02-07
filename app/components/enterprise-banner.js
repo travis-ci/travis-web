6b34d884c8028b5fad60d5081d8077856582f0c2
Carla Drago <carla@travis-ci.com>
2018-02-07 18:03:47 +0100

    initial work to add enterprise-banner

---
+++ app/components/enterprise-banner.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+import { computed } from 'ember-decorators/object';
+import { service } from 'ember-decorators/service';
+
+export default Ember.Component.extend({
+
+  showBanner: true
+});
--- mirage/config.js
+++ mirage/config.js
@@ -471,6 +471,10 @@ export default function () {
     let repo = schema.repositories.find(request.params.repo_id);
     repo.update('starred', false);
   });
+
+  this.get('/enterprise_license', function (schema, request) {
+    return new Mirage.Response(404, {}, {});
+  });
 }
 
 /*
