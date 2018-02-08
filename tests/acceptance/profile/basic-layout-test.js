efd297382c5f2e71a7759eb7544707c4abdf52e9
Piotr Sarnacki <drogus@gmail.com>
2018-02-08 17:36:48 +0100

    Percy testing

--- tests/acceptance/profile/basic-layout-test.js
+++ tests/acceptance/profile/basic-layout-test.js
@@ -1,6 +1,5 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
-import profilePage from 'travis/tests/pages/profile';
 
 moduleForAcceptance('Acceptance | profile/basic layout', {
   beforeEach() {
@@ -68,29 +67,11 @@ moduleForAcceptance('Acceptance | profile/basic layout', {
 });
 
 test('view profile', function (assert) {
-  profilePage.visit({ username: 'feministkilljoy' });
-
-  andThen(function () {
-    assert.equal(document.title, 'Sara Ahmed - Profile - Travis CI');
-
-    assert.equal(profilePage.name, 'Sara Ahmed');
-
-    assert.equal(profilePage.accounts().count, 2, 'expected two accounts');
-
-    assert.equal(profilePage.accounts(0).name, 'Sara Ahmed');
-    assert.equal(profilePage.accounts(0).repositoryCount, '3 repositories');
-
-    assert.equal(profilePage.accounts(1).name, 'Feminist Killjoys');
-    assert.equal(profilePage.accounts(1).repositoryCount, '30 repositories');
-
-    assert.equal(profilePage.administerableRepositories().count, 3, 'expected three repositories');
-
-    assert.equal(profilePage.administerableRepositories(0).name, 'feministkilljoy/affect-theory-reader');
-    assert.ok(profilePage.administerableRepositories(0).isDisabled, 'expected disabled repository to be disabled in UI');
-    assert.equal(profilePage.administerableRepositories(1).name, 'feministkilljoy/living-a-feminist-life');
-    assert.ok(profilePage.administerableRepositories(1).isActive, 'expected active repository to appear active');
-    assert.equal(profilePage.administerableRepositories(2).name, 'feministkilljoy/willful-subjects');
-    assert.notOk(profilePage.administerableRepositories(2).isActive, 'expected inactive repository to appear inactive');
-    percySnapshot(assert);
-  });
+  visit('/');
+  percySnapshot('main page');
+  click("#navigation a.signed-in");
+  percySnapshot('profile page');
+  click(".profile-orgs-list a.org-info");
+  percySnapshot('profile page org');
+  assert.ok(true);
 });
