1328fb45eaa82fccf66627a63192d1f24d9d1c4b
Buck Doyle <buck@travis-ci.org>
2018-02-09 08:44:24 -0600

    Extract subscription into a component
    This is all temporary anyway and this is less disruptive to
    the existing routes.

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,10 +1,7 @@
 import TravisRoute from 'travis/routes/basic';
-import { hash } from 'rsvp';
 
 export default TravisRoute.extend({
-  titleToken(model) {
-    const account = model.account;
-
+  titleToken(account) {
     if (account && account.id) {
       return account.get('name') || account.get('login');
     } else {
@@ -17,16 +14,7 @@ export default TravisRoute.extend({
     let account = this
       .modelFor('accounts')
       .find(acct => acct.get('login') === login);
-    if (account) {
-      let modelHash = {
-        account
-      };
-      // FIX ME
-      if (account.get('login') === 'feministkilljoy') {
-        modelHash.subscription = this.store.findRecord('subscription', '1');
-      }
-      return hash(modelHash);
-    }
+    if (account) { return account; }
 
     return {
       login,
@@ -34,9 +22,7 @@ export default TravisRoute.extend({
     };
   },
 
-  serialize(model) {
-    const account = model.account;
-
+  serialize(account) {
     if (account && account.get) {
       return {
         login: account.get('login')
