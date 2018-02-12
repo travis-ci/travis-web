366009b4e45eec34478cdc2ed6b76249d5f918af
Buck Doyle <buck@travis-ci.org>
2018-02-12 14:46:39 -0600

    Add FIXME for serialiser hack

--- mirage/serializers/v3.js
+++ mirage/serializers/v3.js
@@ -145,6 +145,8 @@ export default JSONAPISerializer.extend({
     if (include) {
       include.split(',').forEach((includeSegment) => {
         let [includeType, includeAttribute] = includeSegment.split('.');
+        // FIXME
+        // eslint-disable-next-line
         if ((includeType === type || (includeType === 'owner' && type === 'user')) && !attributes.includes(includeAttribute)) {
           attributes.push(includeAttribute);
         }
