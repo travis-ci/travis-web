cf51f33e01b0d3209442ae7572486093c05a30a0
Curtis Ekstrom <code@fauxton.io>
2018-03-09 15:50:24 +0100

    Make `more` lowercase

--- app/components/show-more-button.js
+++ app/components/show-more-button.js
@@ -3,5 +3,5 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'button',
   classNames: ['showmore-button', 'button'],
-  label: 'Show More',
+  label: 'Show more',
 });
