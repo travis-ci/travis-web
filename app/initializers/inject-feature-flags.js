676f263efa183658dc3c3f3f3868d8f8647853e9
Curtis Ekstrom <code@fauxton.io>
2018-03-13 12:08:33 +0100

    Remove automatic injection from controllers

--- app/initializers/inject-feature-flags.js
+++ app/initializers/inject-feature-flags.js
@@ -4,7 +4,6 @@ export function initialize(app) {
   let serviceName = config.featureFlagsService || 'features';
   let serviceLookupName = `service:${serviceName}`;
   app.inject('component', serviceName, serviceLookupName);
-  app.inject('controller', serviceName, serviceLookupName);
   if (config.environment === 'development') {
     // eslint-disable-next-line
     console.log('EMBER FEATURE FLAGS were auto-injected into all: routes, controllers, components, adapters and models');
