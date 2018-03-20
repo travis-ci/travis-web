7142f3570c688c04ed72c863f7a08e6f10220e82
Curtis Ekstrom <code@fauxton.io>
2018-03-20 10:55:04 +0100

    Convert unit tests we can

--- tests/unit/helpers/format-config-test.js
+++ tests/unit/helpers/format-config-test.js
@@ -2,40 +2,40 @@ import { safeFormatConfig } from 'travis/helpers/format-config';
 
 import { module, test } from 'qunit';
 
-module('Unit | Helper | format config');
+module('Unit | Helper | format config', function () {
+  test('it returns the filtered config object', (assert) => {
+    const config = [{
+      language: 'ruby',
+      sudo: false,
+      '.result': 'configured',
+      notifications: false
+    }];
+    let result = safeFormatConfig(config);
+    assert.equal(result, '{\n  \"language\": \"ruby\",\n  \"sudo\": false\n}');
+  });
 
-test('it returns the filtered config object', (assert) => {
-  const config = [{
-    language: 'ruby',
-    sudo: false,
-    '.result': 'configured',
-    notifications: false
-  }];
-  let result = safeFormatConfig(config);
-  assert.equal(result, '{\n  \"language\": \"ruby\",\n  \"sudo\": false\n}');
-});
+  test('it only deletes certain keys if they are empty', (assert) => {
+    const config = [{
+      addons: {},
+      language: 'clojure'
+    }];
 
-test('it only deletes certain keys if they are empty', (assert) => {
-  const config = [{
-    addons: {},
-    language: 'clojure'
-  }];
+    let result = safeFormatConfig(config);
+    assert.equal(result, '{\n  \"language\": \"clojure\"\n}');
 
-  let result = safeFormatConfig(config);
-  assert.equal(result, '{\n  \"language\": \"clojure\"\n}');
+    const config2 = [{
+      addons: { foo: 'bar' },
+      language: 'clojure'
+    }];
+    let result2 = safeFormatConfig(config2);
 
-  const config2 = [{
-    addons: { foo: 'bar' },
-    language: 'clojure'
-  }];
-  let result2 = safeFormatConfig(config2);
+    assert.equal(result2, '{\n  \"addons\": {\n    \"foo\": \"bar\"\n  },\n  \"language\": \"clojure\"\n}');
+  });
 
-  assert.equal(result2, '{\n  \"addons\": {\n    \"foo\": \"bar\"\n  },\n  \"language\": \"clojure\"\n}');
-});
+  test('it handles an empty config array', assert => {
+    const config = [];
+    const result = safeFormatConfig(config);
 
-test('it handles an empty config array', assert => {
-  const config = [];
-  const result = safeFormatConfig(config);
-
-  assert.equal(result, '{}');
+    assert.equal(result, '{}');
+  });
 });
