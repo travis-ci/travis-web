894f178aff676c108213b61ee7ef784811d2bf80
Curtis Ekstrom <code@fauxton.io>
2018-03-20 09:58:29 +0100

    Convert loading-indicator to integration test

---
+++ tests/integration/components/loading-indicator-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('loading-indicator', 'Integration | Component | loading indicator', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.set('center', true);
+
+  this.render(hbs`{{loading-indicator center=center}}`);
+
+  assert.ok(this.$('span').hasClass('loading-indicator'), 'component has loading indicator class');
+  assert.ok(this.$('div').hasClass('loading-container'), 'indicator gets parent class if centered flag is given');
+});
