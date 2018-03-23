a4a1eb9d9387cf909325200f472a97a8e707bf07
Buck Doyle <buck@travis-ci.org>
2018-03-22 18:51:13 -0700

    Add preliminary step toward using v3 for accounts
    The tests will fail for various known reasons and probably
    some unknown! There are several obvious places to improve
    on this implementation, but with production data, it at
    least shows the user and orgs in the sidebar.

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -1,16 +1,64 @@
 import TravisRoute from 'travis/routes/basic';
+import { fetch, Headers } from 'fetch';
+import { hash } from 'rsvp';
+import { service } from 'ember-decorators/service';
+import config from 'travis/config/environment';
 
 export default TravisRoute.extend({
+  @service auth: null,
+
   model() {
-    return this.store.query('account', {
-      all: true
+    // FIXME much of this is taken directly from models/log
+
+    let userUrl = `${config.apiEndpoint}/user`;
+    let orgsUrl = `${config.apiEndpoint}/orgs`;
+
+    let headers = new Headers({
+      'Authorization': `token ${this.get('auth.token')}`,
+      'Travis-API-Version': '3'
+    });
+
+    // FIXME this ignores errors from either endpoint
+    return hash({
+      user: fetch(userUrl, {headers}).then(response => response.json()),
+      orgs: fetch(orgsUrl, {headers}).then(response => response.json())
+    }).then(({user, orgs}) => {
+      let models = [];
+
+      models.push(this.store.push({
+        data: [{
+          id: user.id,
+          type: 'user',
+          attributes: {
+            login: user.login,
+            name: user.name,
+            avatarUrl: user.avatar_url,
+            isSyncing: user.is_syncing,
+            syncedAt: user.synced_at
+          }
+        }]
+      })[0]);
+
+      return models.concat(this.store.push({
+        data: orgs.organizations.map(org => ({
+          id: org.id,
+          type: 'org',
+          attributes: {
+            login: org.login,
+            name: org.name,
+            avatarUrl: org.avatar_url,
+            isSyncing: org.is_syncing,
+            syncedAt: org.synced_at
+          }
+        }))
+      }));
     });
   },
 
   setupController(controller, model) {
     let orgs, user;
     user = model.filterBy('type', 'user')[0];
-    orgs = model.filterBy('type', 'organization');
+    orgs = model.filterBy('type', 'org');
     controller.set('user', user);
     controller.set('organizations', orgs);
     return controller.set('model', model);
