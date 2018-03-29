ad804331348eaa85e6b9606db33110697285008a
Curtis Ekstrom <code@fauxton.io>
2018-03-29 16:34:42 +0200

    Convert current tab test

--- tests/acceptance/builds/current-tab-test.js
+++ tests/acceptance/builds/current-tab-test.js
@@ -1,107 +1,93 @@
 /* global Travis */
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
-import currentRepoTab from 'travis/tests/pages/repo-tabs/current';
-import jobTabs from 'travis/tests/pages/job-tabs';
-import jobPage from 'travis/tests/pages/job';
+import { module, test } from 'qunit';
+import { visit, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 import signInUser from 'travis/tests/helpers/sign-in-user';
+import { percySnapshot } from 'ember-percy';
 
-moduleForAcceptance('Acceptance | builds/current tab', {
-  beforeEach() {
+module('Acceptance | builds/current tab', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user');
     signInUser(currentUser);
-  }
-});
-
-test('renders most recent repository without builds', function (assert) {
-  server.create('repository');
-
-  currentRepoTab
-    .visit();
-
-  andThen(function () {
-    assert.ok(currentRepoTab.currentTabActive, 'Current tab is active by default when loading dashboard');
-    assert.equal(currentRepoTab.showsNoBuildsMessaging, 'No builds for this repository', 'Current tab shows no builds message');
   });
-});
 
-test('renders most recent repository and most recent build when builds present, single-job build shows job status instead', function (assert) {
-  let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
+  test('renders most recent repository without builds', async function (assert) {
+    server.create('repository');
 
-  const branch = server.create('branch', { name: 'acceptance-tests' });
-  let  gitUser = server.create('git-user', { name: 'Mr T' });
-  let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { number: '5', state: 'started', repository, branch, commit });
-  let job = server.create('job', { number: '1234.1', state: 'received', build, commit, repository, config: { language: 'Hello' } });
+    await visit('/travis-ci/travis-web');
 
-  commit.update('build', build);
-  commit.update('job', job);
+    assert.dom('[data-test-current-tab-container]').hasClass('active', 'Current tab is active by default when loading dashboard');
+    assert.dom('[data-test-missing-notice-header]').hasText('No builds for this repository', 'Current tab shows no builds message');
+  });
 
-  currentRepoTab
-    .visit();
+  test('renders most recent repository and most recent build when builds present, single-job build shows job status instead', async function (assert) {
+    let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
 
-  andThen(() => {
+    const branch = server.create('branch', { name: 'acceptance-tests' });
+    let  gitUser = server.create('git-user', { name: 'Mr T' });
+    let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+    let build = server.create('build', { number: '5', state: 'started', repository, branch, commit });
+    let job = server.create('job', { number: '1234.1', state: 'received', build, commit, repository, config: { language: 'Hello' } });
+
+    commit.update('build', build);
+    commit.update('job', job);
+
+    await visit('/travis-ci/travis-web');
+
     assert.equal(document.title, 'travis-ci/travis-web - Travis CI');
-    assert.ok(currentRepoTab.currentTabActive, 'Current tab is active by default when loading dashboard');
+    assert.dom('[data-test-current-tab-container]').hasClass('active', 'Current tab is active by default when loading dashboard');
 
-    assert.equal(jobPage.state, '#5 booting', 'expected a single-job build’s state to be the job’s state');
+    assert.dom('[data-test-build-header-build-state]').hasText('#5 booting', 'expected a single-job build’s state to be the job’s state');
+    assert.dom('[data-test-build-header]').hasClass('started');
+
+    assert.dom('[data-test-job-log]').exists();
+    assert.dom('[data-test-job-config-content]').doesNotExist('Job config is hidden');
+    await click('[data-test-job-config-tab]');
+
+    assert.dom('[data-test-job-config-content]').exists();
+    assert.dom('[data-test-job-config-content]').hasText('{ \"language\": \"Hello\" }');
+    assert.dom('[data-test-job-log-content]').doesNotExist();
+
+    percySnapshot(assert);
   });
 
-  andThen(() => {
-    assert.ok(currentRepoTab.showsCurrentBuild, 'Shows current build');
-    assert.ok(jobTabs.logTab.isShowing, 'Displays the log');
-    assert.ok(jobTabs.configTab.isHidden, 'Job config is hidden');
-  });
+  test('renders the repository and subscribes to private log channel for a private repository', async function (assert) {
+    let repository =  server.create('repository', { slug: 'travis-ci/travis-web', private: true });
 
-  jobTabs.configTab.click();
+    const branch = server.create('branch', { name: 'acceptance-tests' });
+    let  gitUser = server.create('git-user', { name: 'Mr T' });
+    let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+    let build = server.create('build', { number: '5', state: 'started', repository, branch, commit });
+    let job = server.create('job', { number: '1234.1', state: 'received', build, commit, repository, config: { language: 'Hello' } });
+    server.create('log', { id: job.id, content: 'teh log' });
 
-  andThen(function () {
-    assert.equal(jobTabs.configTab.contents, '{ \"language\": \"Hello\" }', 'config output is correct');
-    assert.ok(jobTabs.configTab.isShowing, 'Displays the job config');
-    assert.ok(jobTabs.logTab.isHidden, 'Job log is hidden');
-  });
+    commit.update('build', build);
+    commit.update('job', job);
 
-  percySnapshot(assert);
-});
+    await visit('/travis-ci/travis-web');
 
-test('renders the repository and subscribes to private log channel for a private repository', function (assert) {
-  let repository =  server.create('repository', { slug: 'travis-ci/travis-web', private: true });
+    assert.dom('[data-test-build-header]').hasClass('started');
 
-  const branch = server.create('branch', { name: 'acceptance-tests' });
-  let  gitUser = server.create('git-user', { name: 'Mr T' });
-  let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { number: '5', state: 'started', repository, branch, commit });
-  let job = server.create('job', { number: '1234.1', state: 'received', build, commit, repository, config: { language: 'Hello' } });
-  server.create('log', { id: job.id, content: 'teh log' });
-
-  commit.update('build', build);
-  commit.update('job', job);
-
-  currentRepoTab
-    .visit();
-
-  andThen(() => {
-    assert.ok(currentRepoTab.showsCurrentBuild, 'Shows current build');
-    assert.ok(jobTabs.logTab.isShowing, 'Displays the log');
+    assert.dom('[data-test-job-log-content]').exists('Displays the log');
     assert.ok(Travis.pusher.active_channels.includes(`private-job-${job.id}`));
   });
-});
 
-test('error message when build jobs array is empty', function (assert) {
-  let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
+  test('error message when build jobs array is empty', async function (assert) {
+    let repository =  server.create('repository', { slug: 'travis-ci/travis-web' });
 
-  const branch = server.create('branch', { name: 'acceptance-tests' });
-  let  gitUser = server.create('git-user', { name: 'Mr T' });
-  let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { number: '5', state: 'passed', repository, branch, commit });
+    const branch = server.create('branch', { name: 'acceptance-tests' });
+    let  gitUser = server.create('git-user', { name: 'Mr T' });
+    let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+    let build = server.create('build', { number: '5', state: 'passed', repository, branch, commit });
 
-  commit.update('build', build);
+    commit.update('build', build);
 
-  currentRepoTab.visit();
+    await visit('/travis-ci/travis-web');
 
-  andThen(function () {
-    assert.ok(currentRepoTab.noJobsErrorMessage);
+    assert.dom('[data-test-no-jobs-error-message]').exists();
+
+    percySnapshot(assert);
   });
-
-  percySnapshot(assert);
 });
