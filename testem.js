d9496ed93cc44996f3607b1de3467f53e9b99d47
Buck Doyle <buck@travis-ci.org>
2018-04-04 12:12:21 -0700

    Change to Firefox

--- testem.js
+++ testem.js
@@ -2,10 +2,10 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Firefox'
   ],
   launch_in_dev: [
-    'Chrome'
+    'Firefox'
   ],
   browser_args: {
     Chrome: {
