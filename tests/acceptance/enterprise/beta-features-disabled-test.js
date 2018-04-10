4f2591e301443d07c40280adcf67b562a8f906d3
bnferguson <bnferguson@gmail.com>
2018-04-10 15:06:24 +0200

    Fix beta features test to use old style

--- tests/acceptance/enterprise/beta-features-disabled-test.js
+++ tests/acceptance/enterprise/beta-features-disabled-test.js
@@ -4,20 +4,18 @@ import { setupApplicationTest } from 'ember-qunit';
 import signInUser from 'travis/tests/helpers/sign-in-user';
 import { getContext } from '@ember/test-helpers';
 
-module('Acceptance | enterprise/beta features disabled', function (hooks) {
-  setupApplicationTest(hooks);
-
-  hooks.beforeEach(() => {
+module('Acceptance | enterprise/beta features disabled', {
+  beforeEach() {
     const currentUser = server.create('user');
     signInUser(currentUser);
-  });
+  }
+});
 
-  test('does not display link to beta features page', async function (assert) {
-    let { owner } = getContext();
-    owner.lookup('service:features').disable('betaFeatures');
+test('does not display link to beta features page', async function (assert) {
+  let { owner } = getContext();
+  owner.lookup('service:features').disable('betaFeatures');
 
-    await visit('/profile');
+  await visit('/profile');
 
-    assert.dom('[data-test-profile-beta-features-link]').doesNotExist();
-  });
+  assert.dom('[data-test-profile-beta-features-link]').doesNotExist();
 });
