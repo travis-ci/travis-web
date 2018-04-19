3797ba414b56f1457f908846270412fee0d532ca
Buck Doyle <buck@travis-ci.org>
2018-04-19 16:25:25 -0500

    Change timeout again
    ugh!!!!!!!

--- tests/acceptance/auth/github-apps-installation-redirect-test.js
+++ tests/acceptance/auth/github-apps-installation-redirect-test.js
@@ -42,6 +42,6 @@ test('it polls until the GitHub installation ID resolves to an owner', function 
     andThen(() => {
       assert.equal(currentURL(), '/profile/the-org');
     });
-  }, 6000);
+  }, 8000);
   // FIXME huuuuuge timeout
 });
