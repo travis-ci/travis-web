47da56f05f826bcdd836778d3e8bafe6e533f4ba
Buck Doyle <buck@travis-ci.org>
2018-04-27 14:17:34 -0500

    Restore org deployments
    I need to see how things work in this environment.

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -6,8 +6,8 @@ export CLEANED_BRANCH_SUBDOMAIN=github-apps
 
 if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
 then
-  # API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate
-  # DEPLOYMENT_EXIT_CODE=$? TLD=org ENVIRONMENT=staging ./config/deployment/update-github-status.sh
+  API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate
+  DEPLOYMENT_EXIT_CODE=$? TLD=org ENVIRONMENT=staging ./config/deployment/update-github-status.sh
 
   API_ENDPOINT=https://api-staging.travis-ci.com TRAVIS_PRO=true ember deploy com-staging-pull-request --activate
   DEPLOYMENT_EXIT_CODE=$? TLD=com ENVIRONMENT=staging ./config/deployment/update-github-status.sh
@@ -15,8 +15,8 @@ else
     echo "Skipping com- and org-staging PR deployments: no 'staging' in branch name."
 fi
 
-# ember deploy org-production-pull-request --activate
-# DEPLOYMENT_EXIT_CODE=$? TLD=org ENVIRONMENT=production ./config/deployment/update-github-status.sh
+ember deploy org-production-pull-request --activate
+DEPLOYMENT_EXIT_CODE=$? TLD=org ENVIRONMENT=production ./config/deployment/update-github-status.sh
 
 TRAVIS_PRO=true ember deploy com-production-pull-request --activate
 DEPLOYMENT_EXIT_CODE=$? TLD=com ENVIRONMENT=production ./config/deployment/update-github-status.sh
