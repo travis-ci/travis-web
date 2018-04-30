4caf68272f12747bc0cc870eaee15c75c2af321f
Buck Doyle <buck@travis-ci.org>
2018-04-30 13:22:49 -0500

    Restore deployment branch calculation
    This isn’t even being used for this PR!

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -1,6 +1,4 @@
-# FIXME eh
-# export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' | tr '/' '-' | tr '[:upper:]' '[:lower:]'`
-export CLEANED_BRANCH_SUBDOMAIN=github-apps
+export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' | tr '/' '-' | tr '[:upper:]' '[:lower:]'`
 
 ./config/deployment/store-redis-urls.sh
 
