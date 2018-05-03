17aa1ddb9f1b81544450681489b113f46e5a533d
Buck Doyle <buck@travis-ci.org>
2018-05-03 13:50:54 -0500

    Remove unused configuration
    This is superfluous now that the team page has been extracted.

--- config/environment.js
+++ config/environment.js
@@ -67,7 +67,6 @@ module.exports = function (environment) {
     logLimit: 10000,
     emojiPrepend: '',
     statusPageStatusUrl: 'https://pnpcptp8xh9k.statuspage.io/api/v2/status.json',
-    randomiseTeam: process.env.PERCY_ENABLE !== '1'
   };
 
   ENV.featureFlags = {
