db7c0eb74ec98a6e0fdd61901559a3b890231071
Buck Doyle <buck@travis-ci.org>
2018-05-09 08:47:16 -0500

    Change flashes to not use LimitedArray
    The way LimitedArray was being used is now forbidden in Ember 3:
    https://github.com/emberjs/ember.js/pull/16157

--- app/services/flashes.js
+++ app/services/flashes.js
@@ -1,6 +1,5 @@
 import { run } from '@ember/runloop';
 import Service from '@ember/service';
-import LimitedArray from 'travis/utils/limited-array';
 import { computed } from 'ember-decorators/object';
 import { alias } from 'ember-decorators/object/computed';
 import { service } from 'ember-decorators/service';
@@ -39,17 +38,14 @@ export default Service.extend({
   },
 
   setup() {
-    this.set('flashes', LimitedArray.create({
-      limit: 1,
-      content: []
-    }));
+    this.set('flashes', []);
   },
 
   @computed('flashes.[]')
   messages(flashes) {
     let model = [];
     if (flashes.length) {
-      model.pushObjects(flashes.toArray().reverse());
+      model.pushObjects(flashes.toArray());
     }
     return model.uniq();
   },
