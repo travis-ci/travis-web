1050c76a6054c07f03ac472de06b83a7f3a5aa3c
Buck Doyle <buck@travis-ci.org>
2018-05-17 12:28:15 -0500

    Add preliminary plans styling

--- app/components/plans-item.js
+++ app/components/plans-item.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import { computed } from 'ember-decorators/object';
 
 export default Component.extend({
+  classNames: ['plans-item'],
+
   @computed('plan.price')
   price(price) {
     return `$${price / 100} per month`;
