490284a1c09c468dffbf85a419c9ba54c669db2e
Buck Doyle <buck@travis-ci.org>
2018-06-20 12:13:53 -0500

    Move conditionals into component logic

--- app/components/manage-subscription-button.js
+++ app/components/manage-subscription-button.js
@@ -1,4 +1,35 @@
 import Component from '@ember/component';
+import { computed } from 'ember-decorators/object';
 
 export default Component.extend({
+  @computed('subscription', 'subscription.permissions.write', 'account.subscriptionPermissions.create')
+  havePermissions(subscription, writePermissions, createPermissions) {
+    if (subscription) {
+      return writePermissions;
+    } else {
+      return createPermissions;
+    }
+  },
+
+  @computed('subscription.status')
+  label(status) {
+    if (status === 'subscribed') {
+      return 'Edit subscription';
+    } else if (status) {
+      return 'Resubscribe';
+    } else {
+      return 'Create subscription';
+    }
+  },
+
+  @computed('subscription.status')
+  tooltip(status) {
+    if (status === 'subscribed') {
+      return 'You do not have permission to edit this subscription';
+    } else if (status) {
+      return 'You do not have permission to resubscribe';
+    } else {
+      return 'You do not have permission to create a subscription';
+    }
+  }
 });
