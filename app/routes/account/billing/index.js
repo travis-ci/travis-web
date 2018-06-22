164c2b0c8d2c1bb42b8beee9788a68e0b4c5f0b9
Buck Doyle <buck@travis-ci.org>
2018-06-22 11:20:56 -0500

    Remove unused service

--- app/routes/account/billing/index.js
+++ app/routes/account/billing/index.js
@@ -1,9 +1,6 @@
 import TravisRoute from 'travis/routes/basic';
-import { service } from 'ember-decorators/service';
 
 export default TravisRoute.extend({
-  @service raven: null,
-
   model() {
     let accountCompound = this.modelFor('account');
     return accountCompound.account.get('subscription') ||
