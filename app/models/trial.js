cd17bb12d3e3287352bccaba7b8899667a790ec8
paulthegeek <pj2454@gmail.com>
2018-07-12 15:55:30 -0600

    Adding trial states

--- app/models/trial.js
+++ app/models/trial.js
@@ -1,7 +1,7 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo} from 'ember-data/relationships';
-import { equal, or } from 'ember-decorators/object/computed';
+import { and, equal, or } from 'ember-decorators/object/computed';
 
 export default Model.extend({
   buildsRemaining: attr(),
