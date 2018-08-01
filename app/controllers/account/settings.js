f2ad506bc2c2bd27d50e7d0eebe773da9974303b
Buck Doyle <buck@travis-ci.org>
2018-08-01 15:30:09 -0500

    Add conditional rendering for features

--- app/controllers/account/settings.js
+++ app/controllers/account/settings.js
@@ -2,5 +2,6 @@ import Controller from '@ember/controller';
 import { service } from 'ember-decorators/service';
 
 export default Controller.extend({
+  @service features: null,
   @service featureFlags: null,
 });
