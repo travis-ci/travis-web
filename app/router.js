2a238ac6465c08e0c9dad36c113066ec9b1d87dd
Buck Doyle <buck@travis-ci.org>
2018-08-02 09:31:18 -0500

    Remove features route etc

--- app/router.js
+++ app/router.js
@@ -18,7 +18,6 @@ Router.map(function () {
   this.route('dashboard', { resetNamespace: true }, function () {
     this.route('repositories', { path: '/' });
   });
-  this.route('features', { resetNamespace: true });
   this.route('features-tracing', { path: '/features/tracing', resetNamespace: true });
 
   this.route('getting_started');
