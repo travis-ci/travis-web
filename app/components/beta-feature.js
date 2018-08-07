26ebf166569d18ed9dfcc4753067a2b55b185fca
Buck Doyle <buck@travis-ci.org>
2018-08-07 10:26:38 -0500

    Update feature component styling

--- app/components/beta-feature.js
+++ app/components/beta-feature.js
@@ -1,12 +1,6 @@
 import Component from '@ember/component';
-import { computed } from 'ember-decorators/object';
 
 export default Component.extend({
   tagName: 'li',
   classNames: ['feature'],
-
-  @computed('feature.description')
-  description(description) {
-    return description.substr(0, 60);
-  }
 });
