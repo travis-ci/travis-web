8ec8d235d9351decf2df341f1b812f2a577626e1
Buck Doyle <buck@travis-ci.org>
2018-08-08 15:25:28 -0500

    Remove non-starred repo references from root

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -8,8 +8,6 @@ import { computed } from 'ember-decorators/object';
 import dashboardRepositoriesSort from 'travis/utils/dashboard-repositories-sort';
 
 export default Controller.extend({
-  page: 1,
-
   @service flashes: null,
   @service api: null,
 
@@ -53,60 +51,4 @@ export default Controller.extend({
   starredRepos(repositories) {
     return repositories.toArray().sort(dashboardRepositoriesSort);
   },
-
-  @computed('model.repos.[]',
-    'account',
-    'model.accounts',
-    'model.repos.@each.currentBuildState',
-    'model.repos.@each.currentBuildFinishedAt'
-  )
-  filteredRepos(repositories, accountParam, accounts) {
-    let account = accounts.filter((x) => {
-      if (accountParam) {
-        if (x.id === accountParam) {
-          return x;
-        }
-      } else {
-        return null;
-      }
-    });
-    let type = null;
-    if (account && account[0]) {
-      type = account[0].get('type');
-    }
-
-    const repos = repositories.filter((item) => {
-      if (!isBlank(account)) {
-        if (isEqual(type, 'user')) {
-          if (isEqual(item.get('owner.@type'), 'user')) {
-            return item;
-          }
-        } else {
-          if (isEqual(item.get('owner.login'), accountParam)) {
-            return item;
-          }
-        }
-      } else {
-        return item;
-      }
-    }).sort(dashboardRepositoriesSort);
-    return repos;
-  },
-
-  @computed('model.accounts', 'account')
-  selectedOrg(accounts, account) {
-    let filteredAccount = accounts.filter((item) => {
-      if (item.get('login') === account) {
-        return item;
-      }
-    });
-    return filteredAccount[0];
-  },
-
-  actions: {
-    selectOrg(org) {
-      let login = isBlank(org) ? undefined : org.get('login');
-      return this.set('account', login);
-    }
-  }
 });
