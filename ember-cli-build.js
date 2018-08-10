ac072b952b42ee43b017568da58fceebc70c9c3e
Buck Doyle <buck@travis-ci.org>
2018-08-10 11:31:48 -0500

    Replace method of inlining SVG into CSS
    This is adapted from @nerdbeere’s solution here 💞
    https://github.com/JosephClay/sass-inline-image/issues/1#issue-115231808
    
    It’s a hack for now, will refine.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,26 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const Funnel = require('broccoli-funnel');
 const SVGO = require('svgo');
 
+var fs    = require('fs');
+var path  = require('path');
+var types = require('node-sass').types;
+
+var svg = function (buffer) {
+  var svg = buffer.toString()
+    .replace(/\n/g, '')
+    .replace(/\r/g, '')
+    .replace(/\#/g, '%23')
+    .replace(/\"/g, "'");
+
+  return '"data:image/svg+xml;utf8,' + svg + '"';
+};
+
+var img = function (buffer, ext) {
+  return '"data:image/' + ext + ';base64,' + buffer.toString('base64') + '"';
+};
+
+var base = process.cwd() + '/public/images';
+
 module.exports = function () {
   let fingerprint;
 
@@ -80,6 +100,25 @@ module.exports = function () {
             }
           }
         ]
+      }
+    },
+    sassOptions: {
+      functions: {
+        'inline-image($file)': function (file) {
+          // we want to file relative to the base
+          var relativePath = './' + file.getValue();
+          var filePath = path.resolve(base, relativePath);
+
+          // get the file ext
+          var ext = filePath.split('.').pop();
+
+          // read the file
+          var data = fs.readFileSync(filePath);
+
+          var buffer = new Buffer.from(data);
+          var str = ext === 'svg' ? svg(buffer, ext) : img(buffer, ext);
+          return types.String('url(' + str + ')');
+        }
       }
     }
   });
