f6e3c77f7fcd44ea3be3fdb06873a69020d7c83b
Buck Doyle <buck@travis-ci.org>
2018-08-20 14:15:07 -0500

    Remove hideous Ajax-Ember Data-bypass workaround

--- app/components/created-by.js
+++ app/components/created-by.js
@@ -1,24 +1,8 @@
 import Component from '@ember/component';
-import { get } from '@ember/object';
 import { computed } from 'ember-decorators/object';
 
 export default Component.extend({
   tagName: '',
-
-  didInsertElement() {
-    this._super(...arguments);
-
-    let rawUser = this.get('raw-user');
-
-    // FIXME 😱
-    if (rawUser) {
-      this.set('user', {
-        name: get(rawUser, 'name'),
-        login: get(rawUser, 'login'),
-        avatarUrl: get(rawUser, 'avatar_url')
-      });
-    }
-  },
 
   @computed('user.name', 'user.login')
   nameOrLogin(name, login) {
