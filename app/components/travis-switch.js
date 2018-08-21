9645f8910d521b1becdf5a5ccfdd2e534fdb5c9e
Artur Smirnov <mail@artursmirnov.com>
2018-08-21 12:43:21 +0000

    Refactor travis-switch to make it reusable

--- app/components/travis-switch.js
+++ app/components/travis-switch.js
@@ -1,28 +1,28 @@
-import { next } from '@ember/runloop';
 import Component from '@ember/component';
-import { or } from 'ember-decorators/object/computed';
+import { computed } from 'ember-decorators/object';
 
 export default Component.extend({
   tagName: 'button',
-  classNames: ['travis-switch', 'switch'],
-  classNameBindings: ['_active:active'],
+  classNames: ['travis-switch', 'switch', 'inline-block'],
+  classNameBindings: ['active:active', 'disabled:disabled'],
+  attributeBindings: ['aria-checked', 'role', 'type'],
 
-  attributeBindings: ['type'],
   type: 'button',
+  role: 'switch',
+  description: '',
+  active: false,
+  disabled: false,
 
-  @or('target.active', 'active') _active: null,
+  @computed('active')
+  'aria-checked'(active) {
+    return active ? 'true' : 'false';
+  },
+
+  onToggle() {},
 
   click() {
-    let target = this.get('target');
-    if (this.get('toggleAutomatically') !== 'false') {
-      if (target) {
-        this.set('target.active', !this.get('target.active'));
-      } else {
-        this.set('active', !this.get('active'));
-      }
+    if (!this.disabled) {
+      this.onToggle(!this.active);
     }
-    return next(this, function () {
-      return this.sendAction('action', target);
-    });
   }
 });
