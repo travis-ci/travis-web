e24ddb008c615250778d6639a1d8ae524d30e47f
Buck Doyle <buck@travis-ci.org>
2018-08-22 17:05:03 -0500

    Add more styling from existing dashboard row

--- app/components/my-build.js
+++ app/components/my-build.js
@@ -1,6 +1,10 @@
 import Component from '@ember/component';
+import { alias } from 'ember-decorators/object/computed';
 
 export default Component.extend({
   tagName: 'li',
-  classNames: ['rows', 'my-build']
+  classNames: ['rows', 'my-build'],
+  classNameBindings: ['state'],
+
+  @alias('build.state') state: null
 });
