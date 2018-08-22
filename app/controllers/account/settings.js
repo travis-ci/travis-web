5750eff4c5b17678c2c04b58b8679d073bf29854
Artur Smirnov <mail@artursmirnov.com>
2018-08-22 16:31:47 +0000

    Add switch for build emails to account settings

--- app/controllers/account/settings.js
+++ app/controllers/account/settings.js
@@ -1,7 +1,14 @@
 import Controller from '@ember/controller';
 import { service } from 'ember-decorators/service';
+import { task } from 'ember-concurrency';
 
 export default Controller.extend({
   @service features: null,
   @service featureFlags: null,
+
+  buildEmails: false,
+
+  toggleBuildEmails: task(function* (value) {
+    yield this.set('buildEmails', value);
+  }).restartable()
 });
