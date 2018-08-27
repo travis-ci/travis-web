3f48fab262bad8b92614924ff30482c1dc3e64c6
Buck Doyle <buck@travis-ci.org>
2018-08-27 15:19:52 -0500

    Revert channel URL experiment

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,39 +1,30 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
-
-module.exports = function () {
-  return Promise.all([
-    getChannelURL('beta'),
-    getChannelURL('canary')
-  ]).then(([beta, canary]) => {
-    return {
-      scenarios: [
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': beta
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': canary
-            }
-          }
-        },
-        {
-          name: 'ember-data-beta',
-          npm: {
-            devDependencies: {
-              'ember-data': 'beta'
-            }
-          }
+module.exports = {
+  scenarios: [
+    {
+      name: 'ember-beta',
+      npm: {
+        devDependencies: {
+          'ember-source': 'beta'
         }
-      ],
-    };
-  });
+      }
+    },
+    {
+      name: 'ember-canary',
+      npm: {
+        devDependencies: {
+          'ember-source': 'canary'
+        }
+      }
+    },
+    {
+      name: 'ember-data-beta',
+      npm: {
+        devDependencies: {
+          'ember-data': 'beta'
+        }
+      }
+    }
+  ],
 };
