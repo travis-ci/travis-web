997c297b701cac7a190e83bc9892f1a1bcd0d94b
Buck Doyle <buck@travis-ci.org>
2018-08-27 13:07:06 -0500

    Add preliminary attempt at various try configs

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,27 +1,20 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      dependencies: {}
-    },
-    {
-      name: 'release',
-      dependencies: {
-        'ember': 'release'
+      name: 'ember-beta',
+      npm: {
+        devDependencies: {
+          'ember-source': 'beta'
+        }
       }
     },
     {
-      name: 'beta',
-      dependencies: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'canary',
-      dependencies: {
-        'ember': 'canary'
+      name: 'ember-data-beta',
+      npm: {
+        devDependencies: {
+          'ember-data': 'beta'
+        }
       }
     }
   ],
-  bowerOptions: ['--quiet'],
 };
