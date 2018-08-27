ab98f7a6e1d722d1d1d82e9ed715d2e7ccc71abe
Buck Doyle <buck@travis-ci.org>
2018-08-27 14:28:46 -0500

    Add source channel-fetching

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,28 +1,39 @@
-module.exports = {
-  scenarios: [
-    {
-      name: 'ember-beta',
-      npm: {
-        devDependencies: {
-          'ember-source': 'beta'
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function () {
+  return Promise.all([
+    getChannelURL('beta'),
+    getChannelURL('canary')
+  ]).then(([beta, canary]) => {
+    return {
+      scenarios: [
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': beta
+            }
+          }
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-source': canary
+            }
+          }
+        },
+        {
+          name: 'ember-data-beta',
+          npm: {
+            devDependencies: {
+              'ember-data': 'beta'
+            }
+          }
         }
-      }
-    },
-    {
-      name: 'ember-canary',
-      npm: {
-        devDependencies: {
-          'ember-source': 'canary'
-        }
-      }
-    },
-    {
-      name: 'ember-data-beta',
-      npm: {
-        devDependencies: {
-          'ember-data': 'beta'
-        }
-      }
-    }
-  ],
+      ],
+    };
+  });
 };
