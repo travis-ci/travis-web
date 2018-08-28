87e8a05a3503e0b0070dca851865c191db87b7b8
Buck Doyle <buck@travis-ci.org>
2018-08-28 15:37:58 -0500

    Add live updating for build times

---
+++ app/controllers/dashboard/builds.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+import Visibility from 'visibilityjs';
+import { service } from 'ember-decorators/service';
+import config from 'travis/config/environment';
+
+export default Controller.extend({
+  @service('updateTimes') updateTimesService: null,
+
+  init() {
+    this._super(...arguments);
+    if (!Ember.testing) {
+      Visibility.every(config.intervals.updateTimes, this.updateTimes.bind(this));
+    }
+  },
+
+  updateTimes() {
+    let updateTimesService = this.get('updateTimesService');
+
+    updateTimesService.push(this.get('model'));
+  },
+});
