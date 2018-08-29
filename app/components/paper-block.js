9fb3b91ed40527e72dce012601646c31375562b0
Artur Smirnov <mail@artursmirnov.com>
2018-08-29 11:28:35 +0000

    Add paper-block component

---
+++ app/components/paper-block.js
@@ -0,0 +1,26 @@
+import Component from '@ember/component';
+import { computed } from 'ember-decorators/object';
+
+export const MIN_ELEVATION = 0;
+export const MAX_ELEVATION = 4;
+
+export default Component.extend({
+  classNames: ['paper-block'],
+  classNameBindings: ['elevationClass', 'padding::no-padding'],
+
+  title: '',
+  elevation: 1,
+  padding: true,
+
+  @computed('elevation')
+  normalizedElevation(elevation) {
+    elevation = elevation < MIN_ELEVATION ? MIN_ELEVATION : elevation;
+    elevation = elevation > MAX_ELEVATION ? MAX_ELEVATION : elevation;
+    return elevation;
+  },
+
+  @computed('normalizedElevation')
+  elevationClass(normalizedElevation) {
+    return `elevation-x${normalizedElevation}`;
+  }
+});
