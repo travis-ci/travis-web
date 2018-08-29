6e3b7bf170a511b9d612e284a2bcafb80d15bd52
Artur Smirnov <mail@artursmirnov.com>
2018-08-29 12:32:47 +0000

    Add profile controller test

--- tests/unit/controllers/profile-test.js
+++ tests/unit/controllers/profile-test.js
@@ -1,12 +1,36 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
 
-module('Unit | Controller | profile', function(hooks) {
+module('Unit | Controller | profile', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:profile');
+  test('it exists', function (assert) {
+    const controller = this.owner.lookup('controller:profile');
     assert.ok(controller);
+  });
+
+  test('it handles routing properly', function (assert) {
+    const controller = this.owner.lookup('controller:profile');
+    const router = EmberObject.create({
+      currentRouteName: ''
+    });
+    controller.setProperties({ router });
+
+    router.set('currentRouteName', 'account.settings');
+    assert.equal(controller.get('showLoading'), false);
+    assert.equal(controller.get('showProfile'), true);
+
+    router.set('currentRouteName', 'account_loading');
+    assert.equal(controller.get('showLoading'), true);
+    assert.equal(controller.get('showProfile'), true);
+
+    router.set('currentRouteName', 'profile_loading');
+    assert.equal(controller.get('showLoading'), true);
+    assert.equal(controller.get('showProfile'), true);
+
+    router.set('currentRouteName', 'account.settings.unsubscribe');
+    assert.equal(controller.get('showLoading'), false);
+    assert.equal(controller.get('showProfile'), false);
   });
 });
