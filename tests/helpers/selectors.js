fcb043a4405dd2c1bb5d3adfefbde26e9515f2ba
Artur Smirnov <mail@artursmirnov.com>
2018-08-30 09:26:11 +0000

    Implement integration test for email-unsubscribe component

--- tests/helpers/selectors.js
+++ tests/helpers/selectors.js
@@ -1,3 +1,13 @@
 // travis/components/paper-block
+
 export const PAPER_BLOCK_TITLE = '[data-test-paper-block-title]';
 
+// travis/components/email-unsubscribe
+
+export const EMAIL_UNSUBSCRIBE = '.email-unsubscribe';
+export const EMAIL_UNSUBSCRIBE_SADMAIL = '[data-test-email-unsubscribe-sadmail]';
+export const EMAIL_UNSUBSCRIBE_TITLE = '[data-test-email-unsubscribe-title]';
+export const EMAIL_UNSUBSCRIBE_DESCRIPTION = '[data-test-email-unsubscribe-description]';
+export const EMAIL_UNSUBSCRIBE_PRIMARY_BUTTON = '[data-test-email-unsubscribe-primary-button]';
+export const EMAIL_UNSUBSCRIBE_SECONDARY_BUTTON = '[data-test-email-unsubscribe-secondary-button]';
+export const EMAIL_UNSUBSCRIBE_APPENDIX = '[data-test-email-unsubscribe-appendix]';
--- tests/integration/components/email-unsubscribe-test.js
+++ tests/integration/components/email-unsubscribe-test.js
@@ -2,21 +2,40 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import EmberObject from '@ember/object';
+import Service from '@ember/service';
+
+import {
+  EMAIL_UNSUBSCRIBE,
+  EMAIL_UNSUBSCRIBE_SADMAIL,
+  EMAIL_UNSUBSCRIBE_TITLE,
+  EMAIL_UNSUBSCRIBE_DESCRIPTION,
+  EMAIL_UNSUBSCRIBE_PRIMARY_BUTTON
+} from '../../helpers/selectors';
 
 module('Integration | Component | email-unsubscribe', function (hooks) {
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
+  hooks.beforeEach(async function () {
     const repo = server.create('repository');
-    this.set('router', EmberObject.create({
+    repo.isCurrentUserACollaborator = true;
+
+    const routerStub = Service.extend({
       currentURL: `http://travis-ci.org/unsubscribe?repository=${repo.id}`
-    }));
+    });
+
+    this.owner.register('service:router', routerStub);
+
+    await render(hbs`{{email-unsubscribe}}`);
   });
 
   test('it renders', async function (assert) {
-    await render(hbs`tmp`);
+    assert.dom(EMAIL_UNSUBSCRIBE).exists();
+  });
 
-    assert.ok(true);
+  test('it has correct structure', async function (assert) {
+    assert.dom(EMAIL_UNSUBSCRIBE_SADMAIL).exists();
+    assert.dom(EMAIL_UNSUBSCRIBE_TITLE).exists();
+    assert.dom(EMAIL_UNSUBSCRIBE_DESCRIPTION).exists();
+    assert.dom(EMAIL_UNSUBSCRIBE_PRIMARY_BUTTON).exists();
   });
 });
