8ab9f0d5f543cac2cb8255c88f9d572e26f1c884
Artur Smirnov <mail@artursmirnov.com>
2018-08-30 10:12:21 +0000

    Hide resubscribe list when emails are disabled in settings

--- app/controllers/account/settings.js
+++ app/controllers/account/settings.js
@@ -1,12 +1,17 @@
 import Controller from '@ember/controller';
 import { service } from 'ember-decorators/service';
 import { task } from 'ember-concurrency';
+import { and } from 'ember-decorators/object/computed';
 
 export default Controller.extend({
   @service features: null,
   @service featureFlags: null,
 
   buildEmails: false,
+  unsubscribedRepos: null,
+
+  @and('buildEmails', 'unsubscribedRepos.length')
+  showResubscribeList: false,
 
   toggleBuildEmails: task(function* (value) {
     // TODO implement API integration
