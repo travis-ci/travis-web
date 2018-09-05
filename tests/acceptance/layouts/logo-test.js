89edfdee0e93feb06e04923498acf1cab433af69
Paul Jackson <pj2454@gmail.com>
2018-09-05 08:28:40 -0600

    Pj 135 top bar content (#1737)
    Update topbar links and styling

--- tests/acceptance/layouts/logo-test.js
+++ tests/acceptance/layouts/logo-test.js
@@ -15,10 +15,7 @@ test('logo page renders correct header/footer', function (assert) {
     assert.equal(defaultHeader.navigationLinks[0].title, 'About Us', 'Shows link to About Us');
     assert.equal(defaultHeader.navigationLinks[1].title, 'Blog', 'Shows link to Blog');
     assert.equal(defaultHeader.navigationLinks[2].title, 'Status', 'Shows link to Status');
-
-    assert.ok(defaultHeader.helpDropdownPresent, 'Default header has help dropdown');
-    assert.equal(defaultHeader.helpLinks[0].title, 'Read Our Docs', 'Shows Docs help link');
-    assert.equal(defaultHeader.helpLinks[1].title, 'Imprint', 'Shows Link to Imprint');
+    assert.equal(defaultHeader.navigationLinks[3].title, 'Documentation', 'Shows link to Documentation');
 
     assert.ok(defaultHeader.loginLinkPresent, 'Default header has login button');
 
