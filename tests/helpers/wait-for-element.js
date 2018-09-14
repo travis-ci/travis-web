766f7d1d351eccad2ac3e3795770afd4beb101d3
Buck Doyle <buck@travis-ci.org>
2018-09-14 10:22:33 -0500

    Update waitForElement to work with new tests
    I haven’t checked whether this breaks the older ones, TBD

--- tests/helpers/wait-for-element.js
+++ tests/helpers/wait-for-element.js
@@ -8,15 +8,22 @@ import Ember from 'ember';
   * Avoid use except on events outside Ember that aren’t handled by the built-in test helpers
   * as this slows down the tests.
   */
-export default registerAsyncHelper('waitForElement', function (app, element) {
+
+export function waitForElement(element) {
   return Ember.Test.promise(function (resolve) {
     Ember.Test.adapter.asyncStart();
     var interval = setInterval(function () {
       if ($(element).length > 0) {
+        console.log('found it!');
         clearInterval(interval);
         Ember.Test.adapter.asyncEnd();
         run(null, resolve, true);
       }
+      console.log('didnt find it', element);
     }, 10);
   });
+}
+
+registerAsyncHelper('waitForElement', function (app, element) {
+  waitForElement(element);
 });
