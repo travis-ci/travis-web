99baee78b99ed027074f0e4172131c12713b787e
Buck Doyle <buck@travis-ci.org>
2018-09-17 14:09:53 -0500

    Remove pagination for now

--- app/routes/dashboard/builds.js
+++ app/routes/dashboard/builds.js
@@ -4,21 +4,9 @@ import { service } from 'ember-decorators/service';
 export default TravisRoute.extend({
   @service auth: null,
 
-  queryParams: {
-    page: {
-      refreshModel: true
-    },
-  },
-
   model(params) {
     let currentUserId = this.get('auth.currentUser.id');
-    let query = {
-      limit: 30
-    };
-
-    if (params.page) {
-      query.offset = 30 * (params.page - 1);
-    }
+    let query = {};
 
     return this.store.filter('build', query, build => build.get('createdBy.id') == currentUserId);
   },
