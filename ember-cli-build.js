b58f4995d62922e6669221a20da2e8594adc071d
Curtis Ekstrom <code@fauxton.io>
2018-09-19 11:29:16 +0200

    Disable opera, firefox os and yandex icons (not enough use imho)
    Also fix indenting of the comments explaining the `favicons` module options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -61,21 +61,21 @@ module.exports = function () {
         developerName: 'Travis CI GmbH',
         developerURL: 'https://travis-ci.com',
 
-				appName: 'Travis CI',                            // Your application's name. `string`
-        appDescription: 'The simplest way to test and deploy your projects.',                     // Your application's description. `string`
-        developerName: 'Travis CI',                      // Your (or your developer's) name. `string`
-        developerURL: 'https://travis-ci.com',                       // Your (or your developer's) URL. `string`
-        dir: "auto",                              // Primary text direction for name, short_name, and description
-        lang: "en-US",                            // Primary language for name and short_name
-        background: "#fff",                       // Background colour for flattened icons. `string`
-        theme_color: "#fff",                      // Theme color user for example in Android's task switcher. `string`
-        appleStatusBarStyle: "black-translucent", // Style for Apple status bar: "black-translucent", "default", "black". `string`
-        display: "standalone",                    // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
-        orientation: "any",                       // Default orientation: "any", "natural", "portrait" or "landscape". `string`
-        scope: "/",                               // set of URLs that the browser considers within your app
-        start_url: "/",              // Start URL when launching the application from a device. `string`
-        version: "1.0",                           // Your application's version string. `string`
-        logging: false,                           // Print logs to console? `boolean`
+				appName: 'Travis CI',                                                  // Your application's name. `string`
+        appDescription: 'The simplest way to test and deploy your projects.',  // Your application's description. `string`
+        developerName: 'Travis CI',                                            // Your (or your developer's) name. `string`
+        developerURL: 'https://travis-ci.com',                                 // Your (or your developer's) URL. `string`
+        dir: "auto",                                                           // Primary text direction for name, short_name, and description
+        lang: "en-US",                                                         // Primary language for name and short_name
+        background: "#fff",                                                    // Background colour for flattened icons. `string`
+        theme_color: "#fff",                                                   // Theme color user for example in Android's task switcher. `string`
+        appleStatusBarStyle: "black-translucent",                              // Style for Apple status bar: "black-translucent", "default", "black". `string`
+        display: "standalone",                                                 // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
+        orientation: "any",                                                    // Default orientation: "any", "natural", "portrait" or "landscape". `string`
+        scope: "/",                                                            // set of URLs that the browser considers within your app
+        start_url: "/",                                                        // Start URL when launching the application from a device. `string`
+        version: "1.0",                                                        // Your application's version string. `string`
+        logging: false,                                                        // Print logs to console? `boolean`
         icons: {
             // Platform Options:
             // - offset - offset in percentage
@@ -90,8 +90,11 @@ module.exports = function () {
             android: true,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
             appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
             appleStartup: true,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+            coast: false,               // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
             favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+            firefox: false,             // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
             windows: true,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+            yandex: false               // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
         }
       }
     },
