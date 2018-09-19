d0f59d7aeaa2218f34d43beccc39263ca5617194
Curtis Ekstrom <code@fauxton.io>
2018-09-19 11:43:41 +0200

    Fix ESLint errors
    I think the comments are important here to understanding how the config works should we want to update it later...

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint max-len: [2, {"code": 100, "ignoreComments": true}] */
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const Funnel = require('broccoli-funnel');
@@ -56,40 +57,40 @@ module.exports = function () {
     },
     favicons: {
       faviconsConfig: {
-				appName: 'Travis CI',                                                  // Your application's name. `string`
+        appName: 'Travis CI',                                                  // Your application's name. `string`
         appDescription: 'The simplest way to test and deploy your projects.',  // Your application's description. `string`
         developerName: 'Travis CI',                                            // Your (or your developer's) name. `string`
         developerURL: 'https://travis-ci.com',                                 // Your (or your developer's) URL. `string`
-        dir: "auto",                                                           // Primary text direction for name, short_name, and description
-        lang: "en-US",                                                         // Primary language for name and short_name
-        background: "#fff",                                                    // Background colour for flattened icons. `string`
-        theme_color: "#fff",                                                   // Theme color user for example in Android's task switcher. `string`
-        appleStatusBarStyle: "black-translucent",                              // Style for Apple status bar: "black-translucent", "default", "black". `string`
-        display: "standalone",                                                 // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
-        orientation: "any",                                                    // Default orientation: "any", "natural", "portrait" or "landscape". `string`
-        scope: "/",                                                            // set of URLs that the browser considers within your app
-        start_url: "/",                                                        // Start URL when launching the application from a device. `string`
-        version: "1.0",                                                        // Your application's version string. `string`
+        dir: 'auto',                                                           // Primary text direction for name, short_name, and description
+        lang: 'en-US',                                                         // Primary language for name and short_name
+        background: '#fff',                                                    // Background colour for flattened icons. `string`
+        theme_color: '#fff',                                                   // Theme color user for example in Android's task switcher. `string`
+        appleStatusBarStyle: 'black-translucent',                              // Style for Apple status bar: 'black-translucent', 'default', 'black'. `string`
+        display: 'standalone',                                                 // Preferred display mode: 'fullscreen', 'standalone', 'minimal-ui' or 'browser'. `string`
+        orientation: 'any',                                                    // Default orientation: 'any', 'natural', 'portrait' or 'landscape'. `string`
+        scope: '/',                                                            // set of URLs that the browser considers within your app
+        start_url: '/',                                                        // Start URL when launching the application from a device. `string`
+        version: '1.0',                                                        // Your application's version string. `string`
         logging: false,                                                        // Print logs to console? `boolean`
         icons: {
-            // Platform Options:
-            // - offset - offset in percentage
-            // - background:
-            //   * false - use default
-            //   * true - force use default, e.g. set background for Android icons
-            //   * color - set background for the specified icons
-            //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
-            //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
-            //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
-            //
-            android: true,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            appleStartup: true,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            coast: false,               // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            firefox: false,             // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            windows: true,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            yandex: false               // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          // Platform Options:
+          // - offset - offset in percentage
+          // - background:
+          //   * false - use default
+          //   * true - force use default, e.g. set background for Android icons
+          //   * color - set background for the specified icons
+          //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
+          //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
+          //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
+          //
+          android: true,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          appleStartup: true,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          coast: false,               // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          firefox: false,             // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          windows: true,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+          yandex: false               // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
         }
       }
     },
