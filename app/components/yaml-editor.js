71a4a0f6a7ed59df6171a33aed3d1c35e37ed190
Buck Doyle <buck@travis-ci.org>
2018-09-24 15:07:01 -0500

    Add hints when annotations are hidden

--- app/components/yaml-editor.js
+++ app/components/yaml-editor.js
@@ -43,6 +43,16 @@ export default Component.extend({
 
     $('textarea').scroll((e) => {
       $('.annotation').css('margin-top', -e.target.scrollTop);
+
+      let containerHeight = $('.annotations').outerHeight();
+      let anyAbove = false, anyBelow = false;
+
+      $('.annotation').each((index, element) => {
+        if (element.offsetTop < 0) anyAbove = true;
+        if (element.offsetTop + element.offsetHeight > containerHeight) anyBelow = true;
+      });
+
+      $('.annotations').toggleClass('any-above', anyAbove).toggleClass('any-below', anyBelow);
     });
   }
 });
