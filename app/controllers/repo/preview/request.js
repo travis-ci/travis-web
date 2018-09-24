1881721ec626c222accc2be0e91e8b9b29f51a70
Buck Doyle <buck@travis-ci.org>
2018-09-24 13:25:12 -0500

    Add buffer to wait until submission
    This is meh but probably for the best unless the API can
    be sped up dramatically.

--- app/controllers/repo/preview/request.js
+++ app/controllers/repo/preview/request.js
@@ -1,9 +1,22 @@
 import Controller from '@ember/controller';
+import { computed } from 'ember-decorators/object';
+import BufferedProxy from 'ember-buffered-proxy/proxy';
 
 export default Controller.extend({
   queryParams: [
     'eventType',
     'commitMessage',
     'yaml',
-  ]
+  ],
+
+  @computed('request.id')
+  buffer() {
+    return BufferedProxy.create(this);
+  },
+
+  actions: {
+    submit() {
+      this.get('buffer').applyBufferedChanges();
+    }
+  }
 });
