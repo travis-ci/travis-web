d08a0e0f919bbbf1572ef73f9d4674bdd77fd305
Buck Doyle <buck@travis-ci.org>
2018-09-25 12:25:16 -0500

    Add annotation-positioning on insertion

--- app/components/yaml-editor.js
+++ app/components/yaml-editor.js
@@ -9,17 +9,23 @@ export default Component.extend({
     this._super(...arguments);
 
     $('textarea').scroll((e) => {
-      $('.annotation').css('margin-top', -e.target.scrollTop);
-
-      let containerHeight = $('.annotations').outerHeight();
-      let anyAbove = false, anyBelow = false;
-
-      $('.annotation').each((index, element) => {
-        if (element.offsetTop < 0) anyAbove = true;
-        if (element.offsetTop + element.offsetHeight > containerHeight) anyBelow = true;
-      });
-
-      $('.annotations').toggleClass('any-above', anyAbove).toggleClass('any-below', anyBelow);
+      updateAnnotations(-e.target.scrollTop);
     });
+
+    updateAnnotations(0);
   }
 });
+
+function updateAnnotations(textareaScrollTop) {
+  $('.annotation').css('margin-top', textareaScrollTop);
+
+  let containerHeight = $('.annotations').outerHeight();
+  let anyAbove = false, anyBelow = false;
+
+  $('.annotation').each((index, element) => {
+    if (element.offsetTop < 0) anyAbove = true;
+    if (element.offsetTop + element.offsetHeight > containerHeight) anyBelow = true;
+  });
+
+  $('.annotations').toggleClass('any-above', anyAbove).toggleClass('any-below', anyBelow);
+}
