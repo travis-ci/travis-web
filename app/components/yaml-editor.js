620e4075d13edcca3e5a0d6946cc63c4f8fac749
Buck Doyle <buck@travis-ci.org>
2018-09-25 12:10:19 -0500

    Move annotation-construction to controller
    This should be extracted but for now I just want to know which
    messages don’t have annotations.

--- app/components/yaml-editor.js
+++ app/components/yaml-editor.js
@@ -1,41 +1,8 @@
 import Component from '@ember/component';
-import { computed } from 'ember-decorators/object';
-import yamlKeyFinder from 'travis/utils/yaml-key-finder';
 import $ from 'jquery';
 
 export default Component.extend({
   tagName: '',
-
-  @computed('yaml', 'messages.[]')
-  annotations(yaml, messages) {
-    return messages.reduce((annotations, message) => {
-      let code = message.code;
-      let key = message.key;
-
-      if (code === 'unknown_key') {
-        if (key === 'root') {
-          key = message.args.key;
-        } else {
-          key = `${key}.${message.args.key}`;
-        }
-      } else if (key === 'root') {
-        if (code === 'alias') {
-          key = message.args.alias;
-        }
-      }
-
-      let lineNumber = yamlKeyFinder(yaml, key);
-
-      if (lineNumber === 0 || lineNumber) {
-        annotations.push({
-          message,
-          lineNumber
-        });
-      }
-
-      return annotations;
-    }, []);
-  },
 
   // FIXME Is there no way to bind to scrollTop? 😐
   didRender() {
--- app/controllers/repo/preview/request.js
+++ app/controllers/repo/preview/request.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { computed } from 'ember-decorators/object';
+import yamlKeyFinder from 'travis/utils/yaml-key-finder';
 import BufferedProxy from 'ember-buffered-proxy/proxy';
 
 export default Controller.extend({
@@ -12,6 +13,37 @@ export default Controller.extend({
   @computed('request.id')
   buffer() {
     return BufferedProxy.create({content: this});
+  },
+
+  @computed('buffer.yaml', 'build.request.messages.[]')
+  annotations(yaml, messages) {
+    return messages.reduce((annotations, message) => {
+      let code = message.code;
+      let key = message.key;
+
+      if (code === 'unknown_key') {
+        if (key === 'root') {
+          key = message.args.key;
+        } else {
+          key = `${key}.${message.args.key}`;
+        }
+      } else if (key === 'root') {
+        if (code === 'alias') {
+          key = message.args.alias;
+        }
+      }
+
+      let lineNumber = yamlKeyFinder(yaml, key);
+
+      if (lineNumber === 0 || lineNumber) {
+        annotations.push({
+          message,
+          lineNumber
+        });
+      }
+
+      return annotations;
+    }, []);
   },
 
   actions: {
