feaaa6a01b5750775bfe4254cf9913814fc1c300
Artur Smirnov <mail@artursmirnov.com>
2018-09-26 13:53:24 +0000

    Move subscriptions/trials logic from accounts route

--- app/models/owner.js
+++ app/models/owner.js
@@ -2,9 +2,14 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 import { computed } from 'ember-decorators/object';
+import { bool } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 import config from 'travis/config/environment';
 
 export default Model.extend({
+  accounts: service(),
+  raven: service(),
+
   name: attr('string'),
   login: attr('string'),
   isSyncing: attr('boolean'),
@@ -13,15 +18,27 @@ export default Model.extend({
   githubId: attr('string'),
   education: attr('boolean'),
 
-  // These are inserted artificially by routes:accounts
-  subscription: attr(),
-  subscriptionError: attr('boolean'),
-  trial: attr(),
+  // This is set by serializers:subscription
+  subscriptionPermissions: attr(),
 
   installation: belongsTo('installation', { async: false }),
 
-  // This is set by serializers:subscription
-  subscriptionPermissions: attr(),
+  subscriptionError: bool('accounts.subscriptionError'),
+
+  @computed('accounts.subscriptions.@each.{validTo,owner,isSubscribed}', 'login')
+  subscription(subscriptions = [], login) {
+    const accountSubscriptions = subscriptions.filterBy('owner.login', login) || [];
+    const activeAccountSubscriptions = accountSubscriptions.filterBy('isSubscribed') || [];
+    if (activeAccountSubscriptions.length > 1) this.logMultipleSubscriptionsError();
+    return activeAccountSubscriptions.get('firstObject') || accountSubscriptions.get('lastObject');
+  },
+
+  @computed('accounts.trials.@each.{created_at,owner,hasTrial}', 'login')
+  trial(trials = [], login) {
+    const accountTrials = trials.filterBy('owner.login', login) || [];
+    const activeAccountTrials = accountTrials.filterBy('hasTrial') || [];
+    return activeAccountTrials.get('firstObject') || accountTrials.get('lastObject');
+  },
 
   @computed('subscription', 'subscription.permissions.write', 'subscriptionPermissions.create')
   hasSubscriptionPermissions(subscription, writePermissions, createPermissions) {
@@ -38,5 +55,10 @@ export default Model.extend({
   newSubscriptionUrl(isUser, login) {
     let id = isUser ? 'user' : login;
     return `${config.billingEndpoint}/subscriptions/new?id=${id}`;
+  },
+
+  logMultipleSubscriptionsError() {
+    const exception = new Error(`Account ${this.login} has more than one active subscription!`);
+    this.raven.logException(exception, true);
   }
 });
--- app/services/accounts.js
+++ app/services/accounts.js
@@ -1,18 +1,22 @@
 import Service from '@ember/service';
 import { service } from 'ember-decorators/service';
 import { computed } from 'ember-decorators/object';
-import { reads } from 'ember-decorators/object/computed';
+import { reads, bool } from '@ember/object/computed';
 import { task } from 'ember-concurrency';
+import config from 'travis/config/environment';
+
+const { billingEndpoint } = config;
 
 export default Service.extend({
   @service store: null,
   @service auth: null,
 
-  @reads('auth.currentUser')
-  user: null,
+  user: reads('auth.currentUser'),
+  organizations: reads('fetchOrganizations.lastSuccessful.value'),
 
-  @reads('fetchOrganizations.lastSuccessful.value')
-  organizations: null,
+  subscriptions: reads('fetchSubscriptions.lastSuccessful.value'),
+  subscriptionError: bool('fetchSubscriptions.last.error'),
+  trials: reads('fetchTrials.lastSuccessful.value'),
 
   @computed('user', 'organizations.@each')
   all(user, organizations = []) {
@@ -24,11 +28,26 @@ export default Service.extend({
     return yield this.store.findAll('organization') || [];
   }).keepLatest(),
 
+  fetchSubscriptions: task(function* () {
+    const subscriptions = yield this.store.findAll('subscription') || [];
+    return subscriptions.sortBy('validTo');
+  }),
+
+  fetchTrials: task(function* () {
+    const trials = yield this.store.findAll('trial') || [];
+    return trials.sortBy('created_at');
+  }),
+
   init() {
     this.fetchOrganizations.perform();
+    if (billingEndpoint) {
+      this.fetchSubscriptions.perform();
+      this.fetchTrial.perform();
+    }
   },
 
   fetch() {
     return this.fetchOrganizations.perform().then(() => this.all);
   }
+
 });
