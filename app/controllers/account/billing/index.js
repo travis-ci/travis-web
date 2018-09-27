0238d09f40729644f6a21e49b3127afefe55c11c
paulthegeek <pj2454@gmail.com>
2018-09-26 22:43:35 -0600

    Add logic and messaging for educational users in billing

--- app/controllers/account/billing/index.js
+++ app/controllers/account/billing/index.js
@@ -23,6 +23,13 @@ export default Controller.extend({
     }
   },
 
+  @computed('model', 'account')
+  isTrial(model, account) {
+    if(!model.subscriptions) {
+      return !model.subscriptions && !account.education;
+    }
+  },
+
   @action
   helpscoutTrigger() {
     HS.beacon.open();
