57bb480f57cc19df1ee7edc560d6025de5deac16
Curtis Ekstrom <code@fauxton.io>
2018-10-01 15:36:42 +0200

    Fix linting errors

--- config/manifest.js
+++ config/manifest.js
@@ -1,18 +1,18 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
+module.exports = function (/* environment, appConfig */) {
   // See https://github.com/san650/ember-web-app#documentation for a list of
   // supported properties
 
   return {
-    name: "Travis CI",
-    short_name: "Travis CI",
-    description: "The simplest way to test and deploy your projects.",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#fff",
-    theme_color: "#fff",
+    name: 'Travis CI',
+    short_name: 'Travis CI',
+    description: 'The simplest way to test and deploy your projects.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
     icons: [
       {
         src: '/favicon.png',
@@ -23,4 +23,4 @@ module.exports = function(/* environment, appConfig */) {
       tileColor: '#fff'
     }
   };
-}
+};
