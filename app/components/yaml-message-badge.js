fcbc48deab787076f08476d41642fd0b9c7b7409
Buck Doyle <buck@travis-ci.org>
2018-10-02 13:25:57 -0500

    Add level colouring to badge

--- app/components/yaml-message-badge.js
+++ app/components/yaml-message-badge.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from 'ember-decorators/object';
 import { alias, filter } from 'ember-decorators/object/computed';
 
 export default Component.extend({
@@ -10,4 +11,13 @@ export default Component.extend({
   },
 
   @alias('filteredMessages.length') count: null,
+
+  @computed('filteredMessages.@each.level')
+  levelClass(messages) {
+    if (messages.any(message => message.level === 'error')) {
+      return 'error';
+    } else {
+      return 'warn';
+    }
+  }
 });
--- tests/integration/components/yaml-message-badge-test.js
+++ tests/integration/components/yaml-message-badge-test.js
@@ -15,6 +15,22 @@ module('Integration | Component | yaml-message-badge', function (hooks) {
     assert.dom('.count').hasText('1');
   });
 
+  test('it has an error class if any errors exist', async function (assert) {
+    this.set('messages', [{level: 'error'}, {level: 'warn'}]);
+
+    await render(hbs`<YamlMessageBadge @messages={{messages}} />`);
+
+    assert.dom('.badge--yaml').hasClass('level-error');
+  });
+
+  test('it has a warn class if no errors exist', async function (assert) {
+    this.set('messages', [{level: 'info'}, {level: 'warn'}]);
+
+    await render(hbs`<YamlMessageBadge @messages={{messages}} />`);
+
+    assert.dom('.badge--yaml').hasClass('level-warn');
+  });
+
   test('it renders no badge with no messages', async function (assert) {
     this.set('messages', []);
 
