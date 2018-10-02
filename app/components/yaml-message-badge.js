c20c7857f4acf16df0f0a7eafa4e138e4e38a957
Buck Doyle <buck@travis-ci.org>
2018-10-02 13:13:34 -0500

    Remove info messages from badge count

--- app/components/yaml-message-badge.js
+++ app/components/yaml-message-badge.js
@@ -1,5 +1,13 @@
 import Component from '@ember/component';
+import { alias, filter } from 'ember-decorators/object/computed';
 
 export default Component.extend({
-  tagName: ''
+  tagName: '',
+
+  @filter('messages')
+  filteredMessages(message) {
+    return message.level !== 'info';
+  },
+
+  @alias('filteredMessages.length') count: null,
 });
