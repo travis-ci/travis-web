712ac1db9057ce9ed03eae30dc8d07c8e15d0fc5
Curtis Ekstrom <code@fauxton.io>
2018-10-05 11:02:07 +0200

    Silence additional deprecation warnings (#1828)
    * Silence additional deprecation warnings
    
    * Fix ESLint errors

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -3,5 +3,7 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
     { handler: 'silence', matchId: 'macro-computed-deprecated' },
+    { handler: 'silence', matchId: 'ember-routing.route-router' },
+    { handler: 'silence', matchId: 'ember-component.send-action' }
   ]
 };
