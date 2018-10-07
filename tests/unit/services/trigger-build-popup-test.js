f06c10082ab989c12270ea0e17bc2838cf1c8858
Josh Soref <jsoref@users.noreply.github.com>
2018-10-07 17:54:17 +0000

    spelling: initially

--- tests/unit/services/trigger-build-popup-test.js
+++ tests/unit/services/trigger-build-popup-test.js
@@ -6,7 +6,7 @@ module('Unit | Service | trigger build popup', function (hooks) {
 
   test('it toggles the isShowingFlag', function (assert) {
     let service = this.owner.lookup('service:trigger-build-popup');
-    assert.equal(service.get('isShowingTriggerBuildModal'), false, 'initally the modal is hidden');
+    assert.equal(service.get('isShowingTriggerBuildModal'), false, 'initially the modal is hidden');
     service.toggleTriggerBuildModal();
     assert.equal(service.get('isShowingTriggerBuildModal'), true, 'after toggling the modal is visible');
   });
