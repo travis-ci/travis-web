299ac8626fb709425c97d4671e86c23bb98f77bf
Curtis Ekstrom <code@fauxton.io>
2018-10-12 14:23:34 +0200

    Ensure `light-theme` class applied when theme is active

--- tests/integration/components/log-content-test.js
+++ tests/integration/components/log-content-test.js
@@ -28,4 +28,36 @@ module('Integration | Component | log-content', function (hooks) {
 
     assert.dom('[data-test-log-content-job-not-started-notice]').exists();
   });
+
+  test('it renders default log theme (dark)', async function (assert) {
+    this.owner.register('service:log-theme', logThemeDark);
+
+    const { owner } = getContext();
+    const store = owner.lookup('service:store');
+    const repository = store.createRecord('repo');
+    const job = this.set('job', store.createRecord('job', { repo: repository }));
+
+    await render(hbs`{{log-content job=job}}`);
+
+    assert.dom('[data-test-log-content-container]').hasClass('log');
+    assert.dom('[data-test-log-content-header]').hasClass('log-header');
+    assert.dom('[data-test-log-content-download-log-button]').hasClass('download-log-button');
+    assert.dom('[data-test-log-content-log-body]').hasClass('log-body');
+  });
+
+  test('it modifies elements with `light-theme` class when active', async function (assert) {
+    this.owner.register('service:log-theme', logThemeLight);
+
+    const { owner } = getContext();
+    const store = owner.lookup('service:store');
+    const repository = store.createRecord('repo');
+    const job = this.set('job', store.createRecord('job', { repo: repository }));
+
+    await render(hbs`{{log-content job=job}}`);
+
+    assert.dom('[data-test-log-content-container]').hasClass('light-theme');
+    assert.dom('[data-test-log-content-header]').hasClass('light-theme');
+    assert.dom('[data-test-log-content-download-log-button]').hasClass('light-theme');
+    assert.dom('[data-test-log-content-log-body]').hasClass('light-theme');
+  });
 });
