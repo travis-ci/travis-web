553866b4f1e40edd29106b9f993b957560d48bc6
Artur Smirnov <mail@artursmirnov.com>
2018-10-29 06:20:45 -0700

    Fix horizontal scrolling issue

--- app/components/tooltip-on-element.js
+++ app/components/tooltip-on-element.js
@@ -1,5 +1,6 @@
 import TooltipOnElementComponent from 'ember-tooltips/components/tooltip-on-element';
 
 export default TooltipOnElementComponent.extend({
-  enableLazyRendering: true
+  enableLazyRendering: true,
+  keepInWindow: true
 });
