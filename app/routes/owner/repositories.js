85eaa2f1b26cb9ffbcacbde79c7f33f507347e68
Artur Smirnov <mail@artursmirnov.com>
2018-11-05 00:24:17 -0800

    Implement PR feedback

--- app/routes/owner/repositories.js
+++ app/routes/owner/repositories.js
@@ -1,9 +1,9 @@
 import TravisRoute from 'travis/routes/basic';
 import config from 'travis/config/environment';
-import { inject as service } from '@ember/service';
+import { service } from 'ember-decorators/service';
 
 export default TravisRoute.extend({
-  features: service(),
+  @service features: null,
 
   needsAuth: false,
 
