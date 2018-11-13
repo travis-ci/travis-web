51d782d0f6dae8f9331ab29034cb721cf55531ff
Buck Doyle <buck@travis-ci.org>
2018-11-13 13:47:59 -0600

    Change to escaped computed style

--- app/components/yaml-annotation.js
+++ app/components/yaml-annotation.js
@@ -1,10 +1,16 @@
 import Component from '@ember/component';
 import { computed } from 'ember-decorators/object';
+import { htmlSafe } from '@ember/string';
 
 export default Component.extend({
   @computed('annotation.line')
   top(line) {
     let lineHeight = 18; // FIXME ???
     return lineHeight * line;
+  },
+
+  @computed('top')
+  style(top) {
+    return htmlSafe('top: ${top}px;');
   }
 });
