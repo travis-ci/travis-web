3b8cff30ef7a403a161b258f864c6ebd1dbca410
Buck Doyle <buck@travis-ci.org>
2018-11-16 13:28:14 -0600

    Add button to copy .travis.yml

--- app/components/annotated-yaml.js
+++ app/components/annotated-yaml.js
@@ -1,5 +1,23 @@
 import Component from '@ember/component';
+import { computed } from 'ember-decorators/object';
 
 export default Component.extend({
-  tagName: ''
+  tagName: '',
+
+  copied: false,
+
+  @computed('copied')
+  buttonLabel(copied) {
+    if (copied) {
+      return 'Copied!';
+    } else {
+      return 'Copy .travis.yml';
+    }
+  },
+
+  actions: {
+    copied() {
+      this.set('copied', true);
+    }
+  }
 });
