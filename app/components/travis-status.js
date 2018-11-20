2f153dd3906c9c7c286b70b5ff176f0fb70b96e6
Artur Smirnov <mail@artursmirnov.com>
2018-11-20 18:21:18 +0300

    Refactor travis-status to make it reusable

--- app/components/travis-status.js
+++ app/components/travis-status.js
@@ -1,11 +1,19 @@
 import Component from '@ember/component';
 import { service } from 'ember-decorators/service';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
+  classNames: ['travis-status'],
+  classNameBindings: ['status', 'colorizeText:colorize-text'],
+
   @service appLoading: null,
+
+  colorizeText: false,
+
+  status: reads('appLoading.fetchTravisStatus.last.value'),
 
   didInsertElement() {
     this._super(...arguments);
-    return this.get('appLoading.fetchTravisStatus').perform();
-  },
+    this.appLoading.fetchTravisStatus.perform();
+  }
 });
