6892716df245e5f3f3f444f8eb3415363c9d0322
Artur Smirnov <mail@artursmirnov.com>
2018-11-21 15:04:32 +0300

    Add navigation links to welcome text

--- app/controllers/help.js
+++ app/controllers/help.js
@@ -1,19 +1,51 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { reads } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { reads, equal } from '@ember/object/computed';
 import moment from 'moment';
+import config from 'travis/config/environment';
 
 const UTC_START_TIME = moment.utc({ h: 9, m: 0, s: 0 });
 const UTC_END_TIME = moment.utc({ h: 23, m: 0, s: 0 });
 const DATE_FORMAT = 'LT';
 
+export const ANCHOR = {
+  TOP: 'top',
+  DOCS: 'docs',
+  COMMUNITY: 'community',
+  FORM: 'form'
+};
+
+const { docs, community } = config.urls;
+
 export default Controller.extend({
   auth: service(),
 
+  queryParams: ['anchor'],
+  anchor: ANCHOR.TOP,
+
   isLoggedIn: reads('auth.signedIn'),
+
+  toTop: equal('anchor', ANCHOR.TOP),
+  toDocs: equal('anchor', ANCHOR.DOCS),
+  toCommunity: equal('anchor', ANCHOR.COMMUNITY),
+  toForm: equal('anchor', ANCHOR.FORM),
+
+  docsUrl: computed(() => docs),
+  communityUrl: computed(() => `${community}/top`),
 
   startTime: UTC_START_TIME.local().format(DATE_FORMAT),
   endTime: UTC_END_TIME.local().format(DATE_FORMAT),
-  timezone: moment.tz(moment.tz.guess()).format('z')
+  timezone: moment.tz(moment.tz.guess()).format('z'),
+
+  actions: {
+
+    setAnchor(anchor) {
+      if (Object.values(ANCHOR).includes(anchor)) {
+        this.set('anchor', anchor);
+      }
+    }
+
+  }
 
 });
