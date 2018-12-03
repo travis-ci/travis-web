a96c8049fa0b8d78b73473871cbbb048093cc78d
paulthegeek <pj2454@gmail.com>
2018-12-03 10:53:28 -0700

    Initial Commit

--- app/models/job.js
+++ app/models/job.js
@@ -1,3 +1,4 @@
+
 /* global Travis */
 
 import { observer, computed } from '@ember/object';
@@ -86,7 +87,7 @@ export default Model.extend(DurationCalculations, DurationAttributes, {
 
   notStarted: computed('state', function () {
     let state = this.get('state');
-    let waitingStates = ['queued', 'created', 'received'];
+    let waitingStates = ['created', 'queued', 'received', 'requeued'];
     return waitingStates.includes(state);
   }),
 
