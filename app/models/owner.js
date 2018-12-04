c0ed98b779ff4f9c45f69a0bd66eab8b00778011
Buck Doyle <buck@travis-ci.org>
2018-12-04 15:10:51 -0600

    Remove double-subscription-logging in testing

--- app/models/owner.js
+++ app/models/owner.js
@@ -5,6 +5,7 @@ import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import config from 'travis/config/environment';
+import Ember from 'ember';
 
 export default Model.extend({
   accounts: service(),
@@ -74,7 +75,9 @@ export default Model.extend({
   }),
 
   logMultipleSubscriptionsError() {
-    const exception = new Error(`Account ${this.login} has more than one active subscription!`);
-    this.raven.logException(exception, true);
+    if (!Ember.testing) {
+      const exception = new Error(`Account ${this.login} has more than one active subscription!`);
+      this.raven.logException(exception, true);
+    }
   }
 });
