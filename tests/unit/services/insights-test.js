0bf7ae42540874f73a4ad3aafd92ba8935522ae3
Sophie Orta <sophie@travis-ci.org>
2018-12-06 11:24:49 -0500

    Add service interval settings test

--- tests/unit/services/insights-test.js
+++ tests/unit/services/insights-test.js
@@ -4,9 +4,18 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | insights', function (hooks) {
   setupTest(hooks);
 
+  hooks.beforeEach(function () {
+    this.server.create('user');
+  });
+
   // Replace this with your real tests.
-  test('it exists', function (assert) {
-    let service = this.owner.lookup('service:insights');
+  test('default interval settings', function (assert) {
+    const service = this.owner.lookup('service:insights');
+    const intervalSettings = service.getIntervalSettings({ day: {subInterval: '1min'} });
+    const intervalSettings2 = service.getIntervalSettings();
+    intervalSettings2.day.subInterval = '1min';
+
     assert.ok(service);
+    assert.deepEqual(intervalSettings, intervalSettings2);
   });
 });
