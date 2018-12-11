c72887753efc10e493f5bf48725c5529b009d0a7
Buck Doyle <buck@travis-ci.org>
2018-12-11 15:13:38 -0600

    Remove unused action
    This is being passed in but never used…?

--- app/components/owner-not-found.js
+++ app/components/owner-not-found.js
@@ -4,10 +4,4 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   auth: service(),
   features: service(),
-
-  actions: {
-    signIn() {
-      this.get('signIn')();
-    }
-  }
 });
