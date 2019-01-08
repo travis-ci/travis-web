d5dead927e427269795a1eab224d1d7e7920f6b4
Paul Jackson <pj2454@gmail.com>
2019-01-08 11:56:09 -0700

    Pj 52 better waiting (#1922)
    Create UI for job waiting stages

--- app/helpers/humanize-state.js
+++ app/helpers/humanize-state.js
@@ -2,6 +2,11 @@ import { helper } from '@ember/component/helper';
 
 export default helper((params) => {
   let [state] = params;
+
+  if (state === 'created') {
+    return 'received';
+  }
+
   if (state === 'received') {
     return 'booting';
   }
