409e79da0e4074a8264f00d658d37bc723bd9c60
Buck Doyle <buck@travis-ci.org>
2019-01-14 14:12:11 -0600

    Fix another schema access

--- mirage/serializers/owner.js
+++ mirage/serializers/owner.js
@@ -5,7 +5,7 @@ export default Serializer.extend({
     const user = Object.assign({}, object.attrs);
     user['@type'] = 'user';
 
-    user.repositories = object.schema.repositories.where(repo => {
+    user.repositories = object._schema.repositories.where(repo => {
       return repo.slug.indexOf(user.login) === 0;
     }).models.map(repo => {
       const data = Object.assign({}, repo.attrs);
