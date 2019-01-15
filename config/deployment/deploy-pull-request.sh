1b16cba141c97bf04b06f93769c67f6a9ce64c56
Buck Doyle <buck@travis-ci.org>
2019-01-15 10:54:58 -0600

    Add temporary staging deployment override
    It would be nice to have another way to get PR deployments to
    the staging environments… BUT HOW

--- config/deployment/deploy-pull-request.sh
+++ config/deployment/deploy-pull-request.sh
@@ -2,7 +2,8 @@ export CLEANED_BRANCH_SUBDOMAIN=`echo $TRAVIS_PULL_REQUEST_BRANCH | tr '.' '-' |
 
 eval $(./config/deployment/store-redis-urls.sh)
 
-if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
+# FIXME restore this
+if [[ $TRAVIS_PULL_REQUEST_BRANCH = *branch* ]]
 then
   API_ENDPOINT=https://api-staging.travis-ci.org ember deploy org-staging-pull-request --activate
   DEPLOYMENT_EXIT_CODE=$? TLD=org ENVIRONMENT=staging ./config/deployment/update-github-status.sh
