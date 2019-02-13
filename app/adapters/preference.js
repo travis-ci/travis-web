ce94097797fe6713bdefb8411c6bd4577d9de73c
Sophie Orta <sophie@travis-ci.org>
2019-02-13 17:21:33 -0500

    Moving permissions/settings changes to a different PR

--- app/adapters/preference.js
+++ app/adapters/preference.js
@@ -1,23 +1,8 @@
 import V3Adapter from 'travis/adapters/v3';
 
 export default V3Adapter.extend({
-  pathPrefix(modelName, id, snapshot, type, query) {
-    let prefix = 'v3';
-
-    if (type === 'query' && query.organization_id) {
-      prefix += `/org/${query.organization_id}`;
-      // Remove unnecessary query param from API call
-      delete query.organization_id;
-    }
-
-    if (type === 'updateRecord' &&
-      (typeof snapshot.adapterOptions) !== 'undefined' &&
-      snapshot.adapterOptions.hasOwnProperty('organization_id')
-    ) {
-      prefix += `/org/${snapshot.adapterOptions.organization_id}`;
-    }
-
-    return prefix;
+  pathPrefix() {
+    return 'v3';
   },
 
   ajax(url, method, ...rest) {
