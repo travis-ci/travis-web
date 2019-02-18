a999210d0f37892bfd3c9989df7b257b4e77e947
Artur Smirnov <mail@artursmirnov.com>
2019-02-18 11:52:42 +0300

    Add ability to deploy travis-web locally

---
+++ config/deployment/deploy-local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export CLEANED_BRANCH_SUBDOMAIN=`git name-rev --name-only HEAD | tr '.' '-' | tr '/' '-' | tr '[:upper:]' '[:lower:]'`
+
+eval $(./config/deployment/store-redis-urls.sh)
+
+if [[ $TRAVIS_PULL_REQUEST_BRANCH = *staging* ]]
+then
+  echo "Staging branch is not allowed to be deployed locally"
+  exit 1
+fi
+
+ember deploy org-production-pull-request --activate
+TRAVIS_PRO=true ember deploy com-production-pull-request --activate
+
+echo "Deployment URLs:"
+echo "- https://$CLEANED_BRANCH_SUBDOMAIN.test-deployments.travis-ci.org"
+echo "- https://$CLEANED_BRANCH_SUBDOMAIN.test-deployments.travis-ci.com"
