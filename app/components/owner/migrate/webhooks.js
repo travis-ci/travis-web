d019d332fb4f47ccae2620e1a732ad601e219e35
Artur Smirnov <mail@artursmirnov.com>
2019-02-26 22:39:16 +0300

    Fix layout for lists with either one or no repositories

--- app/components/owner/migrate/webhooks.js
+++ app/components/owner/migrate/webhooks.js
@@ -2,4 +2,14 @@ import Component from '@ember/component';
 import SelectableRepositoriesList from 'travis/mixins/components/selectable-repositories-list';
 
 export default Component.extend(SelectableRepositoriesList, {
+
+  init() {
+    this._super(...arguments);
+
+    const { selectedRepositories, repositories } = this;
+    if (repositories.length === 1) {
+      selectedRepositories.addObjects(repositories);
+    }
+  }
+
 });
