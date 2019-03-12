60398729f57bfb4c360b842dd8ab0d358c2820f3
Artur Smirnov <mail@artursmirnov.com>
2019-03-12 09:14:10 +0300

    Test pr deployments

---
+++ tests/acceptance/pr-deployments-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | pr deployments', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /pr-deployments', async function (assert) {
+    await visit('/pr-deployments');
+
+    assert.equal(currentURL(), '/pr-deployments');
+  });
+});
