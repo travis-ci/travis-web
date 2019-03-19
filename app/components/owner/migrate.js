8d724a16203652ccfab38b43fa083cfc1656918e
Artur Smirnov <mail@artursmirnov.com>
2019-03-19 23:18:53 +0300

    Implement activate on github apps step

--- app/components/owner/migrate.js
+++ app/components/owner/migrate.js
@@ -1,7 +1,29 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
+import config from 'travis/config/environment';
+
+const { appName = 'travis-ci' } = config.githubApps;
 
 export default Component.extend({
 
-  owner: null
+  owner: null,
+
+  repositories: reads('owner.githubAppsRepositoriesOnOrg'),
+
+  showActivationStep: reads('repositories.isEmpty'),
+
+  activateAllUrl: computed('owner.githubId', function () {
+    const { githubId } = this.owner;
+    return `https://github.com/apps/${appName}/installations/new/permissions?suggested_target_id=${githubId}`;
+  }),
+
+  actions: {
+
+    activateAll() {
+      window.location.href = this.activateAllUrl;
+    }
+
+  }
 
 });
