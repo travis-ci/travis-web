08bd449f46d6b4d5f2cb087ee5bb00742d309a03
Artur Smirnov <mail@artursmirnov.com>
2019-03-19 22:28:38 +0300

    Add data loading and reusable components for route templates

---
+++ app/routes/organization/migrate.js
@@ -0,0 +1,18 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+  page: 1,
+
+  model({ page }) {
+    this.setProperties({ page });
+    return this.modelFor('organization');
+  },
+
+  afterModel(model) {
+    if (model && !model.error) {
+      model.githubAppsRepositoriesOnOrg.switchToPage(this.page);
+    }
+  }
+
+});
