480acc8cf5aa9ed0b0e23bf94b610fe895404501
Artur Smirnov <mail@artursmirnov.com>
2019-03-20 18:39:20 +0300

    Add list of repositories to migrate

--- app/components/owner/migrate.js
+++ app/components/owner/migrate.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import config from 'travis/config/environment';
@@ -6,10 +7,21 @@ import config from 'travis/config/environment';
 const { appName = 'travis-ci' } = config.githubApps;
 
 export default Component.extend({
+  router: service(), // router.currentRouteName for pagination
 
   owner: null,
 
   repositories: reads('owner.githubAppsRepositoriesOnOrg'),
+  selectedRepositories: computed(() => []),
+
+  isAllSelected: computed('selectedRepositories.[]', 'repositories.[]', function () {
+    const { selectedRepositories, repositories } = this;
+    return repositories.every(repo => selectedRepositories.includes(repo));
+  }),
+
+  showSelectAll: computed('repositories.[]', function () {
+    return this.repositories.length > 1;
+  }),
 
   showActivationStep: reads('repositories.isEmpty'),
 
@@ -18,10 +30,40 @@ export default Component.extend({
     return `https://github.com/apps/${appName}/installations/new/permissions?suggested_target_id=${githubId}`;
   }),
 
+  init() {
+    this._super(...arguments);
+
+    const { selectedRepositories, repositories } = this;
+    if (repositories.length === 1) {
+      selectedRepositories.addObjects(repositories);
+    }
+  },
+
   actions: {
 
     activateAll() {
       window.location.href = this.activateAllUrl;
+    },
+
+    toggleRepository(repo) {
+      const { selectedRepositories } = this;
+      const isSelected = selectedRepositories.includes(repo);
+
+      if (isSelected) {
+        selectedRepositories.removeObject(repo);
+      } else {
+        selectedRepositories.addObject(repo);
+      }
+    },
+
+    toggleAll() {
+      const { isAllSelected, repositories, selectedRepositories } = this;
+
+      if (isAllSelected) {
+        selectedRepositories.removeObjects(repositories.toArray());
+      } else {
+        selectedRepositories.addObjects(repositories.toArray());
+      }
     }
 
   }
