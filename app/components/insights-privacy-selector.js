7b05ec723b74df436700e15e8dfecf0cf9c53f70
Sophie Orta <sophie@travis-ci.org>
2019-03-21 16:42:19 -0400

    Update privacy selector styles

--- app/components/insights-privacy-selector.js
+++ app/components/insights-privacy-selector.js
@@ -8,6 +8,7 @@ export default Component.extend({
 
   isPrivateViewable: false,
   includePrivate: false,
+  showFrame: false,
 
   availableOptions: computed('isPrivateViewable', 'includePrivate', function () {
     const options = [];
