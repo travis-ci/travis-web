4ad604bea196c45f0da98b3f83f556ca17196d15
Sophie Orta <sophie@travis-ci.org>
2019-03-22 12:03:59 -0400

    Update insights tabs component to utilize service interval definitions

--- app/components/insights-tabs.js
+++ app/components/insights-tabs.js
@@ -1,16 +1,19 @@
 import Component from '@ember/component';
+import { DEFAULT_INSIGHTS_INTERVAL, INSIGHTS_INTERVALS } from 'travis/services/insights';
+
+export const INSIGHTS_TABS = [
+  { slug: INSIGHTS_INTERVALS.WEEK, title: INSIGHTS_INTERVALS.WEEK.capitalize() },
+  { slug: INSIGHTS_INTERVALS.MONTH, title: INSIGHTS_INTERVALS.MONTH.capitalize() },
+];
 
 export default Component.extend({
   tagName: 'ul',
   classNames: ['travistab-nav travistab-nav--insights insights-tabs'],
 
-  selectedTab: 'month',
+  selectedTab: DEFAULT_INSIGHTS_INTERVAL,
   setTab: () => {},
 
-  tabs: [
-    { slug: 'week', title: 'Week' },
-    { slug: 'month', title: 'Month' },
-  ],
+  tabs: INSIGHTS_TABS,
 
   actions: {
     setInsightTab(selection) {
--- tests/integration/components/insights-tabs-test.js
+++ tests/integration/components/insights-tabs-test.js
@@ -2,28 +2,38 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { DEFAULT_INSIGHTS_INTERVAL, INSIGHTS_INTERVALS } from 'travis/services/insights';
 
 module('Integration | Component | insights-tabs', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('month active by default', async function (assert) {
+  test('default active by default', async function (assert) {
     await render(hbs`{{insights-tabs}}`);
 
     assert.dom('.insights-tabs').exists();
     assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
     assert.dom('.insights-tabs .insights-tab.active').exists({ count: 1 });
-    assert.dom('.insights-tabs .insights-tab.active').hasText('Month');
+    assert.dom('.insights-tabs .insights-tab.active').hasText(DEFAULT_INSIGHTS_INTERVAL.capitalize());
+  });
+
+  test('month active', async function (assert) {
+    await render(hbs`{{insights-tabs}}`);
+
+    assert.dom('.insights-tabs').exists();
+    assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
+    assert.dom('.insights-tabs .insights-tab.active').exists({ count: 1 });
+    assert.dom('.insights-tabs .insights-tab.active').hasText(INSIGHTS_INTERVALS.MONTH.capitalize());
   });
 
   test('week active', async function (assert) {
-    this.set('interval', 'week');
+    this.set('interval', INSIGHTS_INTERVALS.WEEK);
 
     await render(hbs`{{insights-tabs selectedTab=interval}}`);
 
     assert.dom('.insights-tabs').exists();
     assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
     assert.dom('.insights-tabs .insights-tab.active').exists({ count: 1 });
-    assert.dom('.insights-tabs .insights-tab.active').hasText('Week');
+    assert.dom('.insights-tabs .insights-tab.active').hasText(INSIGHTS_INTERVALS.WEEK.capitalize());
   });
 
   test('day active', async function (assert) {
