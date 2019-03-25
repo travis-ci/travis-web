e1abab1e969f8269c2708fc9ff0275103622a5b6
Sophie Orta <sophie@travis-ci.org>
2019-03-25 13:44:58 -0400

    Cleanup

--- app/components/insights-tabs.js
+++ app/components/insights-tabs.js
@@ -1,13 +1,11 @@
 import Component from '@ember/component';
-import { DEFAULT_INSIGHTS_INTERVAL, INSIGHTS_INTERVALS } from 'travis/services/insights';
+import { INSIGHTS_INTERVALS } from 'travis/services/insights';
 
 export const INSIGHTS_TABS = Object.values(INSIGHTS_INTERVALS).map(slug => ({ slug, title: slug.capitalize() }));
 
 export default Component.extend({
   tagName: 'ul',
   classNames: ['travistab-nav travistab-nav--insights insights-tabs'],
-
-  selectedTab: DEFAULT_INSIGHTS_INTERVAL,
 
   tabs: INSIGHTS_TABS,
 });
--- tests/integration/components/insights-tabs-test.js
+++ tests/integration/components/insights-tabs-test.js
@@ -7,42 +7,10 @@ import { INSIGHTS_INTERVALS } from 'travis/services/insights';
 module('Integration | Component | insights-tabs', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('default active by default', async function (assert) {
-    await render(hbs`{{insights-tabs timeInterval=timeInterval}}`);
-
-    assert.dom('.insights-tabs').exists();
-    assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
-    // assert.dom('.insights-tabs .insights-tab .active').exists({ count: 1 });
-    // assert.dom('.insights-tabs .insights-tab .active').hasText(DEFAULT_INSIGHTS_INTERVAL.capitalize());
-  });
-
-  test('month active', async function (assert) {
+  test('display', async function (assert) {
     await render(hbs`{{insights-tabs}}`);
 
     assert.dom('.insights-tabs').exists();
-    assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
-    // assert.dom('.insights-tabs .insights-tab .active').exists({ count: 1 });
-    // assert.dom('.insights-tabs .insights-tab .active').hasText(INSIGHTS_INTERVALS.MONTH.capitalize());
-  });
-
-  test('week active', async function (assert) {
-    this.set('interval', INSIGHTS_INTERVALS.WEEK);
-
-    await render(hbs`{{insights-tabs selectedTab=interval}}`);
-
-    assert.dom('.insights-tabs').exists();
-    assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
-    // assert.dom('.insights-tabs .insights-tab .active').exists({ count: 1 });
-    // assert.dom('.insights-tabs .insights-tab .active').hasText(INSIGHTS_INTERVALS.WEEK.capitalize());
-  });
-
-  test('day active', async function (assert) {
-    this.set('interval', 'day');
-
-    await render(hbs`{{insights-tabs selectedTab=interval}}`);
-
-    assert.dom('.insights-tabs').exists();
-    assert.dom('.insights-tabs .insights-tab').exists({ count: 2 });
-    assert.dom('.insights-tabs .insights-tab .active').doesNotExist();
+    assert.dom('.insights-tabs .insights-tab').exists({ count: Object.values(INSIGHTS_INTERVALS).length });
   });
 });
