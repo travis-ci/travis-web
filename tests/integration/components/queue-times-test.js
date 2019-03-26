db68a37dd27f811954bc6e73565a56a9e5b8f792
Sophie Orta <sorta@users.noreply.github.com>
2019-03-26 12:29:01 -0400

    Add queue-times component (#1988)

---
+++ tests/integration/components/queue-times-test.js
@@ -0,0 +1,47 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled, waitFor } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { INSIGHTS_INTERVALS } from 'travis/services/insights';
+
+module('Integration | Component | queue-times', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    const user = this.server.create('user');
+    this.setProperties({
+      ownerData: user,
+      private: true,
+    });
+  });
+
+  test('stats exist', async function (assert) {
+    this.set('interval', INSIGHTS_INTERVALS.MONTH);
+
+    this.server.createList('insight-metric', 15);
+
+    await render(hbs`{{queue-times interval=interval owner=ownerData private=private}}`);
+    await settled();
+
+    assert.dom('.insights-glance').doesNotHaveClass('insights-glance--loading');
+    assert.dom('.insights-glance__title').hasText('Average Queue Time');
+    assert.dom('.insights-glance__stat').hasText('0.6 mins');
+    assert.dom('.insights-glance-delta').hasAttribute('data-dir', '-');
+    assert.dom('.insights-glance-delta').hasAttribute('title', 'Averaged 1 min the previous month');
+    assert.dom('.insights-glance-delta__stat').hasText('40%');
+    assert.dom('.insights-glance__chart .chart-component').exists();
+  });
+
+  test('loading state renders', async function (assert) {
+    this.set('interval', INSIGHTS_INTERVALS.WEEK);
+
+    render(hbs`{{queue-times interval=interval owner=ownerData private=private}}`);
+    await waitFor('.insights-glance--loading');
+
+    assert.dom('.insights-glance').hasClass('insights-glance--loading');
+    assert.dom('.insights-glance__title').hasText('Average Queue Time');
+    assert.dom('.insights-glance__stat').hasText('');
+    assert.dom('.insights-glance__chart .chart-component').doesNotExist();
+    assert.dom('.insights-glance__chart-placeholder').exists();
+  });
+});
