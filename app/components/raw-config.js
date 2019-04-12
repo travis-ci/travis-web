17bdd5d61f9624b28b8fc72a05f9e465ff755d00
Artur Kremens <artur@assembla.com>
2019-04-12 21:49:26 +0200

    Old code refactor

--- app/components/raw-config.js
+++ app/components/raw-config.js
@@ -1,20 +1,18 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { later } from '@ember/runloop';
 
 export default Component.extend({
   copied: false,
 
   buttonLabel: computed('copied', function () {
-    let copied = this.get('copied');
-    if (copied) {
-      return 'Copied!';
-    }
-    return 'Copy to clipboard';
+    return this.get('copied') ? 'Copied!' : 'Copy to clipboard';
   }),
 
   actions: {
     copied() {
       this.set('copied', true);
+      later(() => this.set('copied', false), 3000);
     }
   }
 });
