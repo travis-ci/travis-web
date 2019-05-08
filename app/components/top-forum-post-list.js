af905db8541a56a4bb0752bd5a03b24868c5b545
Sophie Orta <sophie@travis-ci.org>
2019-05-08 15:21:17 -0400

    Add popular topics logic to community section

--- app/components/top-forum-post-list.js
+++ app/components/top-forum-post-list.js
@@ -1,15 +1,20 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 import { task } from 'ember-concurrency';
 import config from 'travis/config/environment';
 
 const { community } = config.urls;
+export const NUM_TOPICS = 5;
 
 export default Component.extend({
-  // tagName: 'ul',
+  tagName: 'ul',
 
-  ajax: service(),
   raven: service(),
+  externalLinks: service(),
+
+  numberOfTopics: NUM_TOPICS,
 
   fetchTopics: task(function* () {
     const url = `${community}/top.json`;
@@ -24,8 +29,21 @@ export default Component.extend({
     });
   }).drop(),
 
+  isLoading: reads('fetchTopics.isRunning'),
+  topics: reads('fetchTopics.lastSuccessful.value.topic_list.topics'),
+
+  topicsToShow: computed('topics.[]', 'numberOfTopics', function () {
+    const { topics = [], numberOfTopics } = this;
+    return topics
+      .filter((item, index, self) => index < numberOfTopics)
+      .map(topic => {
+        topic.url = this.externalLinks.communityTopicLink(topic.slug, topic.id);
+        return topic;
+      });
+  }),
+
   // Request topic data
   didReceiveAttrs() {
-    // this.fetchTopics.perform();
+    this.fetchTopics.perform();
   }
 });
