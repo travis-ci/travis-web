fcebef97c9e138ab52a8e79fb717884bdf705f76
Danish Khan <danish@travis-ci.com>
2019-05-23 15:59:30 -0700

    Backport #2029 Repo activation 409 handle (#2030)

--- tests/integration/components/repository-status-toggle-test.js
+++ tests/integration/components/repository-status-toggle-test.js
@@ -1,5 +1,8 @@
-import { test, moduleForComponent } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { Response } from 'ember-cli-mirage';
 
 moduleForComponent('repository-status-toggle', 'RepositoryStatusToggleComponent', {
   integration: true,
@@ -23,5 +26,68 @@ test('it switches state when clicked', function (assert) {
 
   this.render(hbs`{{repository-status-toggle repository=repository}}`);
 
-  assert.ok(this.$('.switch').hasClass('active'), 'switch should have active class');
+    assert.dom('.switch').hasClass('active', 'switch should have active class');
+  });
+
+  test('should display correct error message on 409 fail', async function (assert) {
+    let store = this.owner.lookup('service:store');
+    let repo = store.push({
+      data: {
+        id: 10001,
+        type: 'repo',
+        attributes: {
+          slug: 'travis-ci/travis-web',
+          active: false,
+          permissions: {
+            admin: true
+          },
+        }
+      }
+    });
+
+    this.set('repository', repo);
+
+    server.post('/repo/:id/activate', (schema, request) => {
+      return new Response(409, {}, {});
+    });
+
+    await render(hbs`{{repository-status-toggle repository=repository}}`);
+    assert.dom('.switch').findElement().click();
+    settled().then(() => {
+      assert.dom('.repositories-error').hasText(
+        'Request cannot be completed because the repository ssh key is still pending to be created. Please retry in a bit, or try syncing the repository if this condition does not resolve.'
+      );
+    });
+  });
+
+  test('should display correct error message on non 409 fail', async function (assert) {
+    let store = this.owner.lookup('service:store');
+    let repo = store.push({
+      data: {
+        id: 10001,
+        type: 'repo',
+        attributes: {
+          slug: 'travis-ci/travis-web',
+          active: false,
+          permissions: {
+            admin: true
+          },
+        }
+      }
+    });
+
+    this.set('repository', repo);
+
+    server.post('/repo/:id/activate', (schema, request) => {
+      return new Response(404, {}, {});
+    });
+
+    await render(hbs`{{repository-status-toggle repository=repository}}`);
+    assert.dom('.switch').findElement().click();
+    settled().then(() => {
+      assert.dom('.repositories-error').hasText(
+        'An error happened when we tried to alter settings on GitHub. It may be caused by API restrictions, please review and add your authorized Orgs.'
+      );
+    });
+  });
 });
