940f20362e90cb47d689a1653800286ee140c46a
Artur Smirnov <mail@artursmirnov.com>
2019-05-24 22:37:26 +0300

    Make migration beta button change label depending on the state

--- app/components/profile-nav.js
+++ app/components/profile-nav.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { reads, or, and, not } from '@ember/object/computed';
+import { reads, or, and, not, filterBy, notEmpty } from '@ember/object/computed';
 import { computed } from '@ember/object';
 import config from 'travis/config/environment';
 
@@ -18,16 +18,21 @@ export default Component.extend({
   user: reads('accounts.user'),
   organizations: reads('accounts.organizations'),
 
+  isProVersion: reads('features.proVersion'),
+  isNotProVersion: not('isProVersion'),
+
+  accountsForBeta: filterBy('accounts.all', 'isMigrationBetaRequested', false),
+  hasAccountsForBeta: notEmpty('accountsForBeta'),
+
   accountName: or('model.name', 'model.login'),
   billingUrl: or('model.subscription.billingUrl', 'model.billingUrl'),
 
   showSubscriptionStatusBanner: and('checkSubscriptionStatus', 'model.subscriptionError'),
-  showMigrationBetaBanner: not('features.proVersion'),
+  showMigrationBetaBanner: and('isNotProVersion', 'hasAccountsForBeta'),
 
   isOrganization: reads('model.isOrganization'),
   hasAdminPermissions: reads('model.permissions.admin'),
   isOrganizationAdmin: and('isOrganization', 'hasAdminPermissions'),
-  isProVersion: reads('features.proVersion'),
   showOrganizationSettings: and('isOrganizationAdmin', 'isProVersion'),
 
   get githubOrgsOauthAccessSettingsUrl() {
