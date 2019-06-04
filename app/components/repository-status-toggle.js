0a1223a89a27f79fb0995e945f2b6c22ef42ea24
Danish Khan <danish@travis-ci.com>
2019-06-04 15:35:18 -0700

    Jaroslaw enterprise backport 2029 (#2040)
    * Backport #2029 into Enterprise 2.2 corrections
    
    * Backport #2029 into Enterprise 2.2 corrections v2
    
    * Backport #2029 into Enterprise 2.2 corrections v3
    
    * Backport #2029 into Enterprise 2.2 error handling correction
    
    * Spec and indentation corrected

--- app/components/repository-status-toggle.js
+++ app/components/repository-status-toggle.js
@@ -1,8 +1,7 @@
 import Component from '@ember/component';
 import config from 'travis/config/environment';
 import { task } from 'ember-concurrency';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
+import { computed } from 'ember-decorators/object';
 import hasErrorWithStatus from 'travis/utils/api-errors';
 
 export default Component.extend({
@@ -25,18 +24,34 @@ export default Component.extend({
     }
   },
 
+  @computed('repository.permissions')
+  admin(permissions) {
+    if (permissions) {
+      return permissions.admin;
+    }
+  },
+
   toggleRepositoryTask: task(function* () {
-    const repository = this.repository;
-    try {
-      yield repository.toggle();
-      yield repository.reload();
-      this.pusher.subscribe(`repo-${repository.id}`);
-    } catch (error) {
-      this.set('apiError', error);
+    if (!this.get('disabled')) {
+      this.sendAction('onToggle');
+
+      let repository = this.get('repository');
+
+      let pusher = this.get('pusher'),
+        repoId = repository.get('id');
+
+      yield repository.toggle().then(() => {
+        pusher.subscribe(`repo-${repoId}`);
+        this.toggleProperty('repository.active');
+      }, (error) => {
+        this.set('apiError', error);
+        this.sendAction('onToggleError', repository);
+      });
     }
   }),
 
-  is409error: computed('apiError', function () {
-    return hasErrorWithStatus(this.apiError, '409');
-  })
+  @computed('apiError')
+  is409error(apiError) {
+    return hasErrorWithStatus(apiError, '409');
+  }
 });
