0b7e30c1fb7cd75b2eb926eec0a528754335a0e2
sorta <sorta@users.noreply.github.com>
2019-06-14 09:56:43 -0400

    Update owner routes to use ember-data to retrieve owner model (#2017)
    * Update owner route to use ember-data to retrieve owner model
    
    * Remove redundant properties from user and organization models that can now be inherited from the owner model

--- app/models/owner.js
+++ app/models/owner.js
@@ -2,7 +2,7 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 import { computed } from '@ember/object';
-import { reads, or } from '@ember/object/computed';
+import { reads, or, equal } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import config from 'travis/config/environment';
 import dynamicQuery from 'travis/utils/dynamic-query';
@@ -22,6 +22,10 @@ export default Model.extend({
   githubId: attr('string'),
   education: attr('boolean'),
   fullName: or('name', 'login'),
+  permissions: attr(),
+  type: attr('string'),
+  isUser: equal('type', 'user'),
+  isOrganization: equal('type', 'organization'),
 
   // This is set by serializers:subscription
   subscriptionPermissions: attr(),
