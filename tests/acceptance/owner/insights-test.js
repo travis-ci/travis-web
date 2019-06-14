5a17cea33eca98d99266765224b1c0603da7055e
Sophie Orta <sophie@travis-ci.org>
2019-06-14 12:39:19 -0400

    Update logged out tests

--- tests/acceptance/owner/insights-test.js
+++ tests/acceptance/owner/insights-test.js
@@ -98,9 +98,51 @@ test('the owner insights page handles a lack of data', async function (assert) {
 
   // No Build Overlay
   assert.ok(insightsPage.noBuildOverlay.isVisible);
-  assert.equal(insightsPage.noBuildOverlay.title, 'Build to get monthly insights');
-  assert.equal(insightsPage.noBuildOverlay.text, 'All the build status results from the last 30 days will appear here.');
-  assert.equal(insightsPage.noBuildOverlay.link, 'Let\'s get you going');
+});
+
+test('the owner insights page shows insights components', async function (assert) {
+  server.createList('insight-metric', 15);
+
+  await insightsPage.visit({ username: 'user-login' });
+  await settled();
+
+  assert.equal(insightsPage.glances.length, 4);
+  assert.equal(insightsPage.odysseys.length, 1);
+
+  // Build count component
+  insightsPage.glances[0].as(glance => {
+    assert.equal(glance.name, 'Total Builds');
+    assert.equal(glance.keyStat, 448);
+  });
+
+  // Build minutes component
+  insightsPage.glances[1].as(glance => {
+    assert.equal(glance.name, 'Total Job Minutes');
+    assert.equal(glance.keyStat, '6 mins');
+  });
+
+  // Queue times component
+  insightsPage.glances[2].as(glance => {
+    assert.equal(glance.name, 'Average Queue Time');
+    assert.equal(glance.keyStat, '0.6 mins');
+  });
+
+  // Active repos component
+  insightsPage.glances[3].as(glance => {
+    assert.equal(glance.name, 'Active Repositories');
+    assert.equal(glance.keyStat, '75');
+  });
+
+  // Build statuses component
+  insightsPage.odysseys[0].as(odyssey => {
+    assert.equal(odyssey.name, 'Build Statuses');
+    assert.notEqual(odyssey.chart.trim(), 'No builds this month.');
+  });
+
+  // No Build Overlay
+  assert.ok(insightsPage.noBuildOverlay.isHidden);
+
+  percySnapshot(assert);
 });
 
 test('the owner insights page displays privacy selector on PRO version', async function (assert) {
@@ -114,4 +156,15 @@ test('the owner insights page displays privacy selector on PRO version', async f
 
   assert.ok(privacySelector.isVisible);
   assert.equal(privacySelector.mainField, INSIGHTS_PRIVACY_OPTIONS.PRIVATE);
+});
+
+// No-build overlay states
+test('No-build overlay displays correctly when not logged in', async function (assert) {
+  await insightsPage.visit({ username: 'user-login' });
+  await settled();
+
+  assert.ok(insightsPage.noBuildOverlay.isVisible);
+  assert.equal(insightsPage.noBuildOverlay.title, 'Build to get monthly insights');
+  assert.equal(insightsPage.noBuildOverlay.text, 'All the build status results from the last 30 days will appear here. Have you tried logging in?');
+  assert.equal(insightsPage.noBuildOverlay.link, 'Sign in with GitHub');
 });
