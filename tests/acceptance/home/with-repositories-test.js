ad818bdacf3aaba870e973a77dd2912564957859
Curtis Ekstrom <code@fauxton.io>
2019-06-19 16:42:21 +0200

    Convert home/with-repositories to new testing style

--- tests/acceptance/home/with-repositories-test.js
+++ tests/acceptance/home/with-repositories-test.js
@@ -1,5 +1,6 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { settled, visit, getContext } from '@ember/test-helpers';
 import sidebarPage from 'travis/tests/pages/sidebar';
 import jobPage from 'travis/tests/pages/job';
 import generatePusherPayload from 'travis/tests/helpers/generate-pusher-payload';
@@ -17,8 +18,10 @@ const repositoryTemplate = {
   name: 'repository-name'
 };
 
-moduleForAcceptance('Acceptance | home/with repositories', {
-  beforeEach() {
+module('Acceptance | home/with repositories', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user', {
       name: 'User Name',
       login: 'user-login'
@@ -62,15 +65,11 @@ moduleForAcceptance('Acceptance | home/with repositories', {
       slug: 'other/other',
       skipPermissions: true
     });
-  }
-});
+  });
 
-test('the home page shows the repositories', function (assert) {
-  sidebarPage.visit();
+  test('the home page shows the repositories', async function (assert) {
+    await visit('/');
 
-  // TODO: Remove this
-  andThen(() => {});
-  andThen(() => {
     assert.equal(sidebarPage.sidebarRepositories.length, 3, 'expected three repositories in the sidebar');
 
     sidebarPage.sidebarRepositories[0].as(yetAnother => {
@@ -90,64 +89,61 @@ test('the home page shows the repositories', function (assert) {
 
     assert.equal(sidebarPage.sidebarRepositories[2].name, 'org-login/repository-name');
   });
-});
 
-test('Pusher events change the main display', function (assert) {
-  assert.expect(5);
-  sidebarPage.visit();
+  test('Pusher events change the main display', async function (assert) {
+    assert.expect(5);
+    await visit('/');
 
-  // TODO: Remove this
-  andThen(() => {});
-  andThen(() => {
     assert.equal(sidebarPage.repoTitle, 'org-login / yet-another-repository-name', 'expected the displayed repository to be the one with a running build');
-  });
 
-  let createdBy = server.create('user', { login: 'srivera', name: 'Sylvia Rivera' });
+    let createdBy = server.create('user', { login: 'srivera', name: 'Sylvia Rivera' });
 
-  const commit = server.create('commit', {
-    id: 100,
-    sha: 'acab',
-    branch: 'primary',
-    message: 'Add new chapter',
-    committed_at: '2016-12-02T22:02:34Z',
-  });
+    const commit = server.create('commit', {
+      id: 100,
+      sha: 'acab',
+      branch: 'primary',
+      message: 'Add new chapter',
+      committed_at: '2016-12-02T22:02:34Z',
+    });
 
-  const build = this.branch.createBuild({
-    id: 100,
-    number: 15,
-    repository: this.repository,
-    pull_request: false,
-    event_type: 'push',
-    state: 'passed',
-    finished_at: '2017-03-27T12:00:00Z',
-    createdBy
-  });
-  this.branch.lastBuild = build;
-  this.branch.save();
+    const build = this.branch.createBuild({
+      id: 100,
+      number: 15,
+      repository: this.repository,
+      pull_request: false,
+      event_type: 'push',
+      state: 'passed',
+      finished_at: '2017-03-27T12:00:00Z',
+      createdBy
+    });
+    this.branch.lastBuild = build;
+    this.branch.save();
 
-  const job = build.createJob({
-    id: 100,
-    repository: this.repository,
-    build,
-    commit,
-    number: '15.1',
-    state: 'passed',
-    finished_at: '2017-03-27T12:00:00Z'
-  });
+    const job = build.createJob({
+      id: 100,
+      repository: this.repository,
+      build,
+      commit,
+      number: '15.1',
+      state: 'passed',
+      finished_at: '2017-03-27T12:00:00Z'
+    });
 
-  this.repository.defaultBranch = this.branch;
-  this.repository.save();
+    this.repository.defaultBranch = this.branch;
+    this.repository.save();
 
-  andThen(() => {
-    this.application.pusher.receive('job:created', generatePusherPayload(job));
-    this.application.pusher.receive('build:created', {
+    const { owner } = getContext();
+    const app = owner.application;
+
+    app.pusher.receive('job:created', generatePusherPayload(job));
+    app.pusher.receive('build:created', {
       build: generatePusherPayload(build),
       commit: generatePusherPayload(commit),
       repository: generatePusherPayload(this.repository, { current_build_id: build.id })
     });
 
-    this.application.pusher.receive('job:queued', generatePusherPayload(job, { state: 'queued' }));
-    this.application.pusher.receive('job:received', generatePusherPayload(job, { state: 'received' }));
+    app.pusher.receive('job:queued', generatePusherPayload(job, { state: 'queued' }));
+    app.pusher.receive('job:received', generatePusherPayload(job, { state: 'received' }));
 
     // This is necessary to have the log fetch not fail and put the log in an error state.
     server.create('log', { id: job.id });
@@ -157,37 +153,31 @@ test('Pusher events change the main display', function (assert) {
     build.save();
     // After this line, the displayed repository should change, because it will
     // now have a running build
-    this.application.pusher.receive('build:started', {
+    await app.pusher.receive('build:started', {
       build: generatePusherPayload(build, { state: 'started', finished_at: null }),
       commit: generatePusherPayload(commit),
       repository: generatePusherPayload(this.repository, { current_build_id: build.id })
     });
-  });
-
-  andThen(() => {
+    await settled();
     assert.equal(sidebarPage.repoTitle, 'org-login / repository-name', 'the displayed repository should have changed');
     assert.equal(jobPage.createdBy.text, 'Sylvia Rivera');
-  });
+    await app.pusher.receive('job:started', generatePusherPayload(job, { state: 'started' }));
 
-  andThen(() => {
-    this.application.pusher.receive('job:started', generatePusherPayload(job, { state: 'started' }));
-
-    this.application.pusher.receive('job:log', {
+    await app.pusher.receive('job:log', {
       id: job.id,
       number: 1,
       final: false,
       _log: 'another log line'
     });
 
-    this.application.pusher.receive('job:log', {
+    await app.pusher.receive('job:log', {
       id: job.id,
       number: 0,
       final: false,
       _log: '\u001B[0K\u001B[33;1mThe first line'
     });
-  });
 
-  andThen(() => {
+    await settled();
     assert.equal(jobPage.logLines[0].text, 'The first line');
     assert.ok(jobPage.logLines[0].isYellow, 'expected the first line to be yellow');
   });
