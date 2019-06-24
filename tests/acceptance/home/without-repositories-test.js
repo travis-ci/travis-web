6d5cde426180793cbeb832eb2e069c1dd6d19ccb
Curtis Ekstrom <code@fauxton.io>
2019-06-24 12:59:47 +0200

    Convert  another test

--- tests/acceptance/home/without-repositories-test.js
+++ tests/acceptance/home/without-repositories-test.js
@@ -1,22 +1,24 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
-import sidebarPage from 'travis/tests/pages/sidebar';
+import {
+  currentURL,
+  visit,
+} from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import signInUser from 'travis/tests/helpers/sign-in-user';
+import { percySnapshot } from 'ember-percy';
 
-moduleForAcceptance('Acceptance | home/without repositories', {
-  beforeEach() {
+module('Acceptance | home/without repositories', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user');
     signInUser(currentUser);
-  }
-});
-
-test('signed in but without repositories', function (assert) {
-  sidebarPage.visit();
-
-  // TODO: Remove this
-  andThen(() => {});
-  andThen(function () {
-    assert.equal(currentURL(), '/getting_started');
   });
-  percySnapshot(assert);
+
+  test('signed in but without repositories', async function (assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/getting_started');
+    percySnapshot(assert);
+  });
 });
