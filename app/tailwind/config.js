f5af99f2f8845e51d55fc784a50432ef96f12d50
Sophie Orta <sophie@travis-ci.org>
2019-06-25 16:02:49 -0400

    Basic tailwind setup

---
+++ app/tailwind/config.js
@@ -0,0 +1,18 @@
+/* eslint-env node */
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      grey: {
+        onyx: '#333',
+        asphalt: '#666',
+      }
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
