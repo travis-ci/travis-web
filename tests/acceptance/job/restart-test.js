ec18864af084cd3321d9d670dccaaa6a0686e2b3
Curtis Ekstrom <code@fauxton.io>
2019-06-26 19:52:59 +0200

    Convert jobs/restart test

--- tests/acceptance/job/restart-test.js
+++ tests/acceptance/job/restart-test.js
@@ -1,36 +1,36 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import jobPage from 'travis/tests/pages/job';
 import topPage from 'travis/tests/pages/top';
 import signInUser from 'travis/tests/helpers/sign-in-user';
 
-moduleForAcceptance('Acceptance | jobs/restart', {
-  beforeEach() {
+module('Acceptance | jobs/restart', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user');
     signInUser(currentUser);
-  }
-});
+  });
 
-test('restarting job', function (assert) {
-  let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
-  server.create('branch', {});
+  test('restarting job', async function (assert) {
+    let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
+    server.create('branch', {});
 
-  let  gitUser = server.create('git-user', { name: 'Mr T' });
-  let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { repository: repo, state: 'failed', commit });
-  let job = server.create('job', { number: '1234.1', repository: repo, state: 'failed', commit, build });
-  commit.job = job;
+    let  gitUser = server.create('git-user', { name: 'Mr T' });
+    let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+    let build = server.create('build', { repository: repo, state: 'failed', commit });
+    let job = server.create('job', { number: '1234.1', repository: repo, state: 'failed', commit, build });
+    commit.job = job;
 
-  job.save();
-  commit.save();
+    job.save();
+    commit.save();
 
-  server.create('log', { id: job.id });
+    server.create('log', { id: job.id });
 
-  jobPage
-    .visit()
-    .restartJob();
+    await jobPage
+      .visit()
+      .restartJob();
 
-  andThen(function () {
     assert.equal(topPage.flashMessage.text, 'The job was successfully restarted.', 'restarted notification should display proper job restarted text');
   });
 });
