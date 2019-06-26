4f35e81c0be7be619afaec5a679f44193fffbd32
Curtis Ekstrom <code@fauxton.io>
2019-06-26 16:40:49 +0200

    Convert user settings test

--- tests/acceptance/profile/user-settings-test.js
+++ tests/acceptance/profile/user-settings-test.js
@@ -1,12 +1,17 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { currentURL, visit } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import profilePage from 'travis/tests/pages/profile';
 import signInUser from 'travis/tests/helpers/sign-in-user';
 import topPage from 'travis/tests/pages/top';
+import { enableFeature } from 'ember-feature-flags/test-support';
 import { INSIGHTS_VIS_OPTIONS } from 'travis/controllers/account/settings';
+import { percySnapshot } from 'ember-percy';
 
-moduleForAcceptance('Acceptance | user settings', {
-  beforeEach() {
+module('Acceptance | user settings', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const user = server.create('user');
     signInUser(user);
     this.user = user;
@@ -21,28 +26,26 @@ moduleForAcceptance('Acceptance | user settings', {
     });
 
     server.loadFixtures('preferences');
-  }
-});
-
-test('changing feature flags', function (assert) {
-  server.create('feature', {
-    name: 'jorts',
-    description: 'Jorts!',
-    enabled: true
   });
 
-  const jantsFeature = server.create('feature', {
-    name: 'jants',
-    description: 'Jants?',
-    enabled: false
-  });
+  test('changing feature flags', async function (assert) {
+    server.create('feature', {
+      name: 'jorts',
+      description: 'Jorts!',
+      enabled: true
+    });
 
-  profilePage.visit();
-  profilePage.settings.visit();
+    const jantsFeature = server.create('feature', {
+      name: 'jants',
+      description: 'Jants?',
+      enabled: false
+    });
 
-  percySnapshot(assert);
+    await profilePage.visit();
+    await profilePage.settings.visit();
 
-  andThen(function () {
+    percySnapshot(assert);
+
     assert.equal(profilePage.settings.features.length, 2, 'expected there to be two features');
 
     profilePage.settings.features[0].as(jants => {
@@ -56,175 +59,167 @@ test('changing feature flags', function (assert) {
       assert.equal(jorts.description, 'Jorts!');
       assert.ok(jorts.isOn, 'expected the jorts switch to be on');
     });
-  });
 
-  let patchRequestBody;
+    let patchRequestBody;
 
-  server.patch(`/user/${this.user.id}/beta_feature/${jantsFeature.id}`, function (schema, request) {
-    patchRequestBody = JSON.parse(request.requestBody);
-    const feature = schema.features.find(jantsFeature.id);
-    feature.enabled = true;
-    return feature;
-  });
+    server.patch(`/user/${this.user.id}/beta_feature/${jantsFeature.id}`, function (schema, request) {
+      patchRequestBody = JSON.parse(request.requestBody);
+      const feature = schema.features.find(jantsFeature.id);
+      feature.enabled = true;
+      return feature;
+    });
 
-  profilePage.settings.features[0].click();
+    await profilePage.settings.features[0].click();
 
-  andThen(() => {
     assert.ok(profilePage.settings.features[0].isOn, 'expected the jants switch to now be on');
     assert.deepEqual(patchRequestBody, { 'beta_feature.enabled': true });
   });
-});
 
-test('no settings for org', function (assert) {
-  profilePage.visit();
+  test('no settings for org', async function (assert) {
+    await profilePage.visit();
 
-  andThen(() => {
     assert.ok(profilePage.settings.isPresent);
-  });
 
-  profilePage.settings.visit();
-  profilePage.accounts[1].visit();
+    await profilePage.settings.visit();
+    await profilePage.accounts[1].visit();
 
-  andThen(() => {
     assert.ok(profilePage.settings.isHidden);
     assert.equal(currentURL(), '/organizations/org-login/repositories');
-  });
 
-  visit('/organizations/org-login/preferences');
+    await visit('/organizations/org-login/preferences');
 
-  andThen(() => {
     assert.equal(currentURL(), '/organizations/org-login/repositories');
   });
-});
 
-test('Email settings are listed', async function (assert) {
-  await profilePage.visit({ username: 'testuser' });
-  await profilePage.settings.visit();
+  test('Email settings are listed', async function (assert) {
+    await profilePage.visit({ username: 'testuser' });
+    await profilePage.settings.visit();
 
-  const { emailSettings } = profilePage.settings;
+    const { emailSettings } = profilePage.settings;
 
-  assert.ok(emailSettings.isVisible);
-  assert.ok(emailSettings.title.length > 0);
-  assert.ok(emailSettings.description.length > 0);
-  assert.ok(emailSettings.toggle.isVisible);
-});
-
-test('Email settings can be toggled', async function (assert) {
-  const AMOUNT_OF_REPOS = 3;
-
-  server.createList('repository', AMOUNT_OF_REPOS, {
-    email_subscribed: false
+    assert.ok(emailSettings.isVisible);
+    assert.ok(emailSettings.title.length > 0);
+    assert.ok(emailSettings.description.length > 0);
+    assert.ok(emailSettings.toggle.isVisible);
   });
 
-  await profilePage.visit({ username: 'testuser' });
-  await profilePage.settings.visit();
+  test('Email settings can be toggled', async function (assert) {
+    const AMOUNT_OF_REPOS = 3;
 
-  const { emailSettings } = profilePage.settings;
+    server.createList('repository', AMOUNT_OF_REPOS, {
+      email_subscribed: false
+    });
 
-  assert.ok(!emailSettings.toggle.isOn);
-  assert.ok(!emailSettings.resubscribeList.isPresent);
+    await profilePage.visit({ username: 'testuser' });
+    await profilePage.settings.visit();
 
-  await emailSettings.toggle.click();
+    const { emailSettings } = profilePage.settings;
 
-  percySnapshot(assert);
+    assert.ok(!emailSettings.toggle.isOn);
+    assert.ok(!emailSettings.resubscribeList.isPresent);
 
-  assert.ok(emailSettings.toggle.isOn);
-  assert.ok(emailSettings.resubscribeList.isPresent);
-  assert.equal(emailSettings.resubscribeList.items.length, AMOUNT_OF_REPOS);
-});
+    await emailSettings.toggle.click();
 
-test('User can resubscribe to repository', async function (assert) {
-  server.create('repository', { email_subscribed: false });
+    percySnapshot(assert);
 
-  await profilePage.visit({ username: 'testuser' });
-  await profilePage.settings.visit();
+    assert.ok(emailSettings.toggle.isOn);
+    assert.ok(emailSettings.resubscribeList.isPresent);
+    assert.equal(emailSettings.resubscribeList.items.length, AMOUNT_OF_REPOS);
+  });
 
-  const { emailSettings } = profilePage.settings;
+  test('User can resubscribe to repository', async function (assert) {
+    server.create('repository', { email_subscribed: false });
 
-  await emailSettings.toggle.click();
+    await profilePage.visit({ username: 'testuser' });
+    await profilePage.settings.visit();
 
-  assert.equal(emailSettings.resubscribeList.items.length, 1);
+    const { emailSettings } = profilePage.settings;
 
-  await emailSettings.resubscribeList.items[0].click();
+    await emailSettings.toggle.click();
 
-  assert.ok(!emailSettings.resubscribeList.isPresent);
-});
+    assert.equal(emailSettings.resubscribeList.items.length, 1);
 
-test('Insights settings are not listed in non-PRO version', async function (assert) {
-  await profilePage.visit({ username: this.user.login });
-  await profilePage.settings.visit();
+    await emailSettings.resubscribeList.items[0].click();
 
-  const { insightsSettings } = profilePage.settings;
+    assert.ok(!emailSettings.resubscribeList.isPresent);
+  });
 
-  assert.ok(!insightsSettings.isVisible);
-});
+  test('Insights settings are not listed in non-PRO version', async function (assert) {
+    await profilePage.visit({ username: this.user.login });
+    await profilePage.settings.visit();
 
-test('Insights settings are listed in PRO version', async function (assert) {
-  withFeature('proVersion');
-  await profilePage.visit({ username: this.user.login });
-  await profilePage.settings.visit();
+    const { insightsSettings } = profilePage.settings;
 
-  const { insightsSettings } = profilePage.settings;
+    assert.ok(!insightsSettings.isVisible);
+  });
 
-  assert.ok(insightsSettings.isVisible);
-  assert.ok(insightsSettings.title.length > 0);
-  assert.ok(insightsSettings.description.length > 0);
-  assert.equal(insightsSettings.description,
-    `Make more informed decisions about your development workflow using your build Insights. View ${this.user.name}'s Insights`
-  );
-  assert.ok(insightsSettings.visibilityList.isVisible);
-});
+  test('Insights settings are listed in PRO version', async function (assert) {
+    enableFeature('proVersion');
+    await profilePage.visit({ username: this.user.login });
+    await profilePage.settings.visit();
 
-test('User can select a different privacy setting', async function (assert) {
-  withFeature('proVersion');
-  await profilePage.visit({ username: this.user.login });
-  await profilePage.settings.visit();
+    const { insightsSettings } = profilePage.settings;
 
-  const { submit, visibilityList } = profilePage.settings.insightsSettings;
-  const { insightsSettingsModal } = profilePage.settings;
-  const [privateOption, publicOption] = visibilityList.items;
+    assert.ok(insightsSettings.isVisible);
+    assert.ok(insightsSettings.title.length > 0);
+    assert.ok(insightsSettings.description.length > 0);
+    assert.equal(insightsSettings.description,
+      `Make more informed decisions about your development workflow using your build Insights. View ${this.user.name}'s Insights`
+    );
+    assert.ok(insightsSettings.visibilityList.isVisible);
+  });
 
-  assert.equal(visibilityList.items.length, INSIGHTS_VIS_OPTIONS.length);
-  const expectedPrivate = INSIGHTS_VIS_OPTIONS.find(option => option.key === 'private');
-  const expectedPublic = INSIGHTS_VIS_OPTIONS.find(option => option.key === 'public');
+  test('User can select a different privacy setting', async function (assert) {
+    enableFeature('proVersion');
+    await profilePage.visit({ username: this.user.login });
+    await profilePage.settings.visit();
 
-  // Default state
-  assert.equal(privateOption.description, expectedPrivate.description);
-  assert.notOk(privateOption.isSelected);
-  assert.equal(publicOption.description, expectedPublic.description);
-  assert.ok(publicOption.isSelected);
-  assert.ok(submit.isDisabled);
-  assert.notOk(insightsSettingsModal.isVisible);
+    const { submit, visibilityList } = profilePage.settings.insightsSettings;
+    const { insightsSettingsModal } = profilePage.settings;
+    const [privateOption, publicOption] = visibilityList.items;
 
-  // Select option
-  await privateOption.click();
-  assert.ok(privateOption.isSelected);
-  assert.notOk(submit.isDisabled);
+    assert.equal(visibilityList.items.length, INSIGHTS_VIS_OPTIONS.length);
+    const expectedPrivate = INSIGHTS_VIS_OPTIONS.find(option => option.key === 'private');
+    const expectedPublic = INSIGHTS_VIS_OPTIONS.find(option => option.key === 'public');
 
-  // Click save, modal should show
-  await submit.click();
-  assert.ok(insightsSettingsModal.isVisible);
-  assert.equal(insightsSettingsModal.title, 'Restrict visibility of your private build insights');
-  assert.equal(insightsSettingsModal.description, expectedPrivate.modalText);
+    // Default state
+    assert.equal(privateOption.description, expectedPrivate.description);
+    assert.notOk(privateOption.isSelected);
+    assert.equal(publicOption.description, expectedPublic.description);
+    assert.ok(publicOption.isSelected);
+    assert.ok(submit.isDisabled);
+    assert.notOk(insightsSettingsModal.isVisible);
 
-  // Close modal with close button
-  await insightsSettingsModal.closeButton.click();
-  assert.notOk(insightsSettingsModal.isVisible);
+    // Select option
+    await privateOption.click();
+    assert.ok(privateOption.isSelected);
+    assert.notOk(submit.isDisabled);
 
-  // Reopen modal
-  await submit.click();
-  assert.ok(insightsSettingsModal.isVisible);
+    // Click save, modal should show
+    await submit.click();
+    assert.ok(insightsSettingsModal.isVisible);
+    assert.equal(insightsSettingsModal.title, 'Restrict visibility of your private build insights');
+    assert.equal(insightsSettingsModal.description, expectedPrivate.modalText);
 
-  // Close modal with cancel button
-  await insightsSettingsModal.cancelButton.click();
-  assert.notOk(insightsSettingsModal.isVisible);
+    // Close modal with close button
+    await insightsSettingsModal.closeButton.click();
+    assert.notOk(insightsSettingsModal.isVisible);
 
-  // Reopen modal
-  await submit.click();
-  assert.ok(insightsSettingsModal.isVisible);
+    // Reopen modal
+    await submit.click();
+    assert.ok(insightsSettingsModal.isVisible);
 
-  // Confirm save
-  await insightsSettingsModal.confirmButton.click();
-  assert.notOk(insightsSettingsModal.isVisible);
-  assert.equal(topPage.flashMessage.text, 'Your private build insights are now private.');
+    // Close modal with cancel button
+    await insightsSettingsModal.cancelButton.click();
+    assert.notOk(insightsSettingsModal.isVisible);
+
+    // Reopen modal
+    await submit.click();
+    assert.ok(insightsSettingsModal.isVisible);
+
+    // Confirm save
+    await insightsSettingsModal.confirmButton.click();
+    assert.notOk(insightsSettingsModal.isVisible);
+    assert.equal(topPage.flashMessage.text, 'Your private build insights are now private.');
+  });
 });
