dc4a7917631fc415d15e58055788cd1ecc3d4b75
Curtis Ekstrom <code@fauxton.io>
2019-06-26 14:48:01 +0200

    Convert builds restart test

--- tests/acceptance/builds/restart-test.js
+++ tests/acceptance/builds/restart-test.js
@@ -1,31 +1,31 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import buildPage from 'travis/tests/pages/build';
 import topPage from 'travis/tests/pages/top';
 import signInUser from 'travis/tests/helpers/sign-in-user';
 
-moduleForAcceptance('Acceptance | builds/restart', {
-  beforeEach() {
+module('Acceptance | builds/restart', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user');
     signInUser(currentUser);
-  }
-});
+  });
 
-test('restarting build', function (assert) {
-  let repository =  server.create('repository');
-  server.create('branch', {});
+  test('restarting build', async function (assert) {
+    let repository =  server.create('repository');
+    server.create('branch', {});
 
-  let  gitUser = server.create('git-user', { name: 'Mr T' });
-  let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
-  let build = server.create('build', { number: '5', repository, state: 'passed', commit });
-  let job = server.create('job', { number: '1234.1', repository, state: 'passed', build, commit });
-  server.create('log', { id: job.id });
+    let  gitUser = server.create('git-user', { name: 'Mr T' });
+    let commit = server.create('commit', { author: gitUser, committer: gitUser, branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
+    let build = server.create('build', { number: '5', repository, state: 'passed', commit });
+    let job = server.create('job', { number: '1234.1', repository, state: 'passed', build, commit });
+    server.create('log', { id: job.id });
 
-  buildPage
-    .visit({ owner: 'travis-ci', repo: 'travis-web', build_id: build.id })
-    .restartBuild();
+    await buildPage
+      .visit({ owner: 'travis-ci', repo: 'travis-web', build_id: build.id })
+      .restartBuild();
 
-  andThen(() => {
     assert.equal(topPage.flashMessage.text, 'The build was successfully restarted.', 'restarted notification should display proper build restarted text');
     assert.equal(buildPage.singleJobLogText, 'Hello log', 'shows log text of single build job');
   });
