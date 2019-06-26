95fdf88d000d209eb0dfb247d937f54143bc3014
Curtis Ekstrom <code@fauxton.io>
2019-06-26 17:04:31 +0200

    Convert profile filtering test

--- tests/acceptance/profile/filtering-test.js
+++ tests/acceptance/profile/filtering-test.js
@@ -1,10 +1,14 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import profilePage from 'travis/tests/pages/profile';
 import signInUser from 'travis/tests/helpers/sign-in-user';
+import { enableFeature } from 'ember-feature-flags/test-support';
+import { percySnapshot } from 'ember-percy';
 
-moduleForAcceptance('Acceptance | profile/filtering', {
-  beforeEach() {
+module('Acceptance | profile/filtering', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user', {
       name: 'User Name',
       login: 'user-login',
@@ -63,73 +67,61 @@ moduleForAcceptance('Acceptance | profile/filtering', {
       },
       active: true
     });
-  }
-});
+  });
 
-test('filter profile repositories', function (assert) {
-  profilePage.visit();
+  test('filter profile repositories', async function (assert) {
+    await profilePage.visit();
 
-  andThen(function () {
     assert.equal(profilePage.administerableRepositories.length, 3, 'expected three repositories');
 
-    profilePage.filter('patriarchy');
+    await profilePage.filter('patriarchy');
 
-    andThen(function () {
-      assert.equal(profilePage.administerableRepositories.length, 0, 'expected no repositories');
-      assert.equal(profilePage.noRepositoriesFoundByFilter, 'Sorry, no results found.');
-    });
+    assert.equal(profilePage.administerableRepositories.length, 0, 'expected no repositories');
+    assert.equal(profilePage.noRepositoriesFoundByFilter, 'Sorry, no results found.');
 
-    profilePage.filter('spec');
-    andThen(function () {
-      percySnapshot(assert);
-      assert.equal(profilePage.administerableRepositories.length, 1, 'expected one repository');
+    await profilePage.filter('spec');
+    percySnapshot(assert);
+    assert.equal(profilePage.administerableRepositories.length, 1, 'expected one repository');
 
-      assert.equal(profilePage.administerableRepositories[0].name, 'specific-repository-name');
-    });
+    assert.equal(profilePage.administerableRepositories[0].name, 'specific-repository-name');
   });
-});
 
-test('paginate and filter GitHub Apps-managed repositories', function (assert) {
-  withFeature('github-apps');
+  test('paginate and filter GitHub Apps-managed repositories', async function (assert) {
+    enableFeature('github-apps');
 
-  server.create('installation', {
-    owner: this.user,
-    github_id: 2691
-  });
-  this.user.save();
-
-  for (let i = 0; i < 15; i++) {
-    server.create('repository', {
-      name: `github-apps-public-repository-${(i + '').padStart(3, '0')}`,
-      owner: {
-        login: 'user-login',
-      },
-      active: true,
-      managed_by_installation: true,
-      private: false
+    server.create('installation', {
+      owner: this.user,
+      github_id: 2691
     });
-  }
+    this.user.save();
 
-  profilePage.visit();
+    for (let i = 0; i < 15; i++) {
+      server.create('repository', {
+        name: `github-apps-public-repository-${(i + '').padStart(3, '0')}`,
+        owner: {
+          login: 'user-login',
+        },
+        active: true,
+        managed_by_installation: true,
+        private: false
+      });
+    }
 
-  andThen(() => {
+    await profilePage.visit();
+
     assert.equal(profilePage.githubAppsRepositories.length, 10, 'expected 10 GitHub Apps-managed repositories on the first page');
     assert.equal(profilePage.githubAppsPages.length, 2, 'expected 2 pages of not-locked repositories');
 
     assert.equal(profilePage.githubAppsRepositories[0].name, 'github-apps-public-repository-000');
     assert.equal(profilePage.githubAppsRepositories[9].name, 'github-apps-public-repository-009');
-  });
 
-  profilePage.githubAppsPages[1].visit();
+    await profilePage.githubAppsPages[1].visit();
 
-  andThen(() => {
     assert.equal(profilePage.githubAppsRepositories.length, 5, 'expected 5 GitHub Apps-managed repositories on the second page');
     assert.equal(profilePage.githubAppsRepositories[0].name, 'github-apps-public-repository-010');
-  });
 
-  profilePage.githubAppsFilter('9');
+    await profilePage.githubAppsFilter('9');
 
-  andThen(() => {
     assert.equal(profilePage.githubAppsPages.length, 0, 'expected pagination to be hidden when filtering');
     assert.equal(profilePage.githubAppsRepositories.length, 1, 'expected one filtered repository');
     assert.equal(profilePage.githubAppsRepositories[0].name, 'github-apps-public-repository-009');
