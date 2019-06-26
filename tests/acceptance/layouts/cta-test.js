38936e759c97dd1adaba138f38df8fa7d499f618
Curtis Ekstrom <code@fauxton.io>
2019-06-26 18:09:18 +0200

    Convert cta layout test

--- tests/acceptance/layouts/cta-test.js
+++ tests/acceptance/layouts/cta-test.js
@@ -1,41 +1,41 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import existingRepoPage from 'travis/tests/pages/repo-tabs/current';
 import defaultHeader from 'travis/tests/pages/header/default';
 import signInUser from 'travis/tests/helpers/sign-in-user';
+import signOutUser from 'travis/tests/helpers/sign-out-user';
+import { enableFeature } from 'ember-feature-flags/test-support';
 
-moduleForAcceptance('Acceptance | layouts/cta');
+module('Acceptance | layouts/cta', function (hooks) {
+  setupApplicationTest(hooks);
 
-test('cta is shown on .org when not on landing page and unauthenticated', function (assert) {
-  withFeature('landingPageCta');
-  server.create('repository');
-  existingRepoPage.visit();
+  test('cta is shown on .org when not on landing page and unauthenticated', async function (assert) {
+    enableFeature('landingPageCta');
+    const currentUser = server.create('user');
+    signOutUser(currentUser);
+    server.create('repository');
+    await existingRepoPage.visit();
 
-  andThen(function () {
     assert.equal(defaultHeader.cta.text, 'Help make Open Source a better place and start building better software today!', 'Shows correct CTA text');
   });
-});
 
-test('cta is shown for an open-source repository when GitHub Apps is present and not authenticated', function (assert) {
-  withFeature('github-apps');
-  server.create('repository');
-  existingRepoPage.visit();
+  test('cta is shown for an open-source repository when GitHub Apps is present and not authenticated', async function (assert) {
+    enableFeature('github-apps');
+    server.create('repository');
+    await existingRepoPage.visit();
 
-  andThen(function () {
     assert.equal(defaultHeader.cta.text, 'Join over 500,000 developers testing and building on Travis CI');
   });
-});
 
-test('cta is not shown for an open-source repository when GitHub Apps is present and authenticated', function (assert) {
-  withFeature('github-apps');
-  server.create('repository');
+  test('cta is not shown for an open-source repository when GitHub Apps is present and authenticated', async function (assert) {
+    enableFeature('github-apps');
+    server.create('repository');
 
-  const currentUser = server.create('user');
-  signInUser(currentUser);
+    const currentUser = server.create('user');
+    signInUser(currentUser);
 
-  existingRepoPage.visit();
+    await existingRepoPage.visit();
 
-  andThen(function () {
     assert.ok(defaultHeader.cta.isHidden);
   });
 });
