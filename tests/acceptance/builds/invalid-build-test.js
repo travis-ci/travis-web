3fd354e6aa9c7fed40986df66c07b78755ad39eb
Curtis Ekstrom <code@fauxton.io>
2019-06-26 14:54:51 +0200

    Convert invalid builds test

--- tests/acceptance/builds/invalid-build-test.js
+++ tests/acceptance/builds/invalid-build-test.js
@@ -1,35 +1,36 @@
+import { visit } from '@ember/test-helpers';
 import Ember from 'ember';
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import buildPage from 'travis/tests/pages/build';
 
 let adapterException;
 let loggerError;
 
-moduleForAcceptance('Acceptance | builds/invalid build', {
-  beforeEach() {
+module('Acceptance | builds/invalid build', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     adapterException = Ember.Test.adapter.exception;
     loggerError = Ember.Logger.error;
     Ember.Test.adapter.exception = () => {};
     Ember.Logger.error = () => null;
-  },
+  });
 
-  afterEach() {
+  hooks.afterEach(function () {
     Ember.Test.adapter.exception = adapterException;
     Ember.Logger.error = loggerError;
-  },
-});
+  });
 
-test('viewing invalid build shows error', function (assert) {
-  // create incorrect repository as this is resolved first, errors otherwise
-  server.create('repository', { slug: 'travis-ci/travis-api' });
+  test('viewing invalid build shows error', async function (assert) {
+    // create incorrect repository as this is resolved first, errors otherwise
+    server.create('repository', { slug: 'travis-ci/travis-api' });
 
-  const repository = server.create('repository', { slug: 'travis-ci/travis-web' });
-  const incorrectSlug = 'travis-ci/travis-api';
-  const build = server.create('build', { repository });
-  visit(`${incorrectSlug}/builds/${build.id}`);
+    const repository = server.create('repository', { slug: 'travis-ci/travis-web' });
+    const incorrectSlug = 'travis-ci/travis-api';
+    const build = server.create('build', { repository });
+    await visit(`${incorrectSlug}/builds/${build.id}`);
 
-  andThen(() => {
     assert.equal(buildPage.buildNotFoundMessage, 'Oops, we couldn\'t find that build!', 'Shows missing build message');
   });
 });
