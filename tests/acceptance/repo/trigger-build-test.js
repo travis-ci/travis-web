1f4d2e33ab051cae74c3a4e70fd8a538fe9a5f44
Curtis Ekstrom <code@fauxton.io>
2019-06-26 20:18:10 +0200

    Convert repo trigger build

--- tests/acceptance/repo/trigger-build-test.js
+++ tests/acceptance/repo/trigger-build-test.js
@@ -1,14 +1,18 @@
-import { test, skip } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { currentURL } from '@ember/test-helpers';
+import { module, skip, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import triggerBuildPage from 'travis/tests/pages/trigger-build';
 import topPage from 'travis/tests/pages/top';
 import { Response } from 'ember-cli-mirage';
 import signInUser from 'travis/tests/helpers/sign-in-user';
+import signOutUser from 'travis/tests/helpers/sign-out-user';
+import { enableFeature } from 'ember-feature-flags/test-support';
+import { percySnapshot } from 'ember-percy';
 
-moduleForAcceptance('Acceptance | repo/trigger build');
+module('Acceptance | repo/trigger build', function (hooks) {
+  setupApplicationTest(hooks);
 
-moduleForAcceptance('Acceptance | repo/trigger build', {
-  beforeEach() {
+  hooks.beforeEach(function () {
     this.currentUser = server.create('user', {
       name: 'Ada Lovelace',
       login: 'adal',
@@ -52,109 +56,92 @@ moduleForAcceptance('Acceptance | repo/trigger build', {
 
     this.repo.currentBuild = latestBuild;
     this.repo.save();
-  }
-});
+  });
 
-test('trigger link is not visible to users without proper permissions', function (assert) {
-  this.repo.update('permissions', { create_request: false });
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+  test('trigger link is not visible to users without proper permissions', async function (assert) {
+    signOutUser()
+    this.repo.update('permissions', { create_request: false });
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
 
-  andThen(() => {
     assert.notOk(triggerBuildPage.popupTriggerLinkIsPresent, 'trigger build link is not rendered');
   });
-});
 
-test('trigger link is present when user has the proper permissions and has been migrated on com', function (assert) {
-  this.repo.update('migration_status', 'migrated');
+  test('trigger link is present when user has the proper permissions and has been migrated on com', async function (assert) {
+    this.repo.update('migration_status', 'migrated');
 
-  withFeature('proVersion');
-  signInUser(this.currentUser);
+    enableFeature('proVersion');
+    signInUser(this.currentUser);
 
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
 
-  andThen(() => {
     assert.ok(triggerBuildPage.popupTriggerLinkIsPresent, 'trigger build link is rendered');
   });
-});
 
-// We currently get an error related to /v3/enterprise_license returning a 404 from mirage.
-// I'm not sure what our desired behavior is, so leaving this alone to be able to progress on the migration work.
-skip('trigger link is present when user has the proper permissions and has been migrated on enterprise', function (assert) {
-  this.repo.update('migration_status', 'migrated');
-  withFeature('enterpriseVersion');
-  signInUser(this.currentUser);
+  // We currently get an error related to /v3/enterprise_license returning a 404 from mirage.
+  // I'm not sure what our desired behavior is, so leaving this alone to be able to progress on the migration work.
+  skip('trigger link is present when user has the proper permissions and has been migrated on enterprise', async function (assert) {
+    this.repo.update('migration_status', 'migrated');
+    enableFeature('enterpriseVersion');
+    signInUser(this.currentUser);
 
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
 
-  andThen(() => {
     assert.ok(triggerBuildPage.popupTriggerLinkIsPresent, 'trigger build link is rendered');
   });
-});
 
-test('trigger link is not visible on org if repository has already been migrated', function (assert) {
-  this.repo.update('migration_status', 'migrated');
-  signInUser(this.currentUser);
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
-  assert.notOk(triggerBuildPage.popupTriggerLinkIsPresent, 'trigger build link is not rendered');
-});
+  test('trigger link is not visible on org if repository has already been migrated', async function (assert) {
+    this.repo.update('migration_status', 'migrated');
+    signInUser(this.currentUser);
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+    assert.notOk(triggerBuildPage.popupTriggerLinkIsPresent, 'trigger build link is not rendered');
+  });
 
-test('triggering a custom build via the dropdown', function (assert) {
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+  test('triggering a custom build via the dropdown', async function (assert) {
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
 
-  andThen(() => {
     assert.equal(currentURL(), 'adal/difference-engine', 'we are on the repo page');
     assert.ok(triggerBuildPage.popupIsHidden, 'modal is hidden');
-  });
 
-  triggerBuildPage.openPopup();
+    await triggerBuildPage.openPopup();
 
-  andThen(() => {
     assert.ok(triggerBuildPage.popupIsVisible, 'modal is visible after click');
 
     assert.equal(triggerBuildPage.branches.length, 1, 'expected the not-on-GitHub branch to be hidden');
     assert.equal(triggerBuildPage.branches[0].value, 'master');
-  });
 
-  triggerBuildPage.selectBranch('master');
-  triggerBuildPage.writeMessage('This is a demo build');
-  triggerBuildPage.writeConfig('script: echo "Hello World"');
-  percySnapshot(assert);
-  triggerBuildPage.clickSubmit();
+    await triggerBuildPage.selectBranch('master');
+    await triggerBuildPage.writeMessage('This is a demo build');
+    await triggerBuildPage.writeConfig('script: echo "Hello World"');
+    percySnapshot(assert);
+    await triggerBuildPage.clickSubmit();
 
-  andThen(() => {});
-
-  andThen(() => {
     assert.ok(triggerBuildPage.popupIsHidden, 'modal is hidden again');
     assert.equal(currentURL(), '/adal/difference-engine/builds/9999', 'we transitioned after the build was triggered');
   });
-});
 
-test('an error triggering a build is displayed', function (assert) {
-  server.post('/repo/:repo_id/requests', function (schema, request) {
-    return new Response(500, {}, {});
-  });
+  test('an error triggering a build is displayed', async function (assert) {
+    server.post('/repo/:repo_id/requests', function (schema, request) {
+      return new Response(500, {}, {});
+    });
 
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
-  triggerBuildPage.openPopup();
-  triggerBuildPage.selectBranch('master');
-  triggerBuildPage.clickSubmit();
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+    await triggerBuildPage.openPopup();
+    await triggerBuildPage.selectBranch('master');
+    await triggerBuildPage.clickSubmit();
 
-  andThen(() => {
     assert.equal(topPage.flashMessage.text, 'Oops, something went wrong, please try again.');
   });
-});
 
-test('a 429 shows a specific error message', function (assert) {
-  server.post('/repo/:repo_id/requests', function (schema, request) {
-    return new Response(429, {}, {});
-  });
+  test('a 429 shows a specific error message', async function (assert) {
+    server.post('/repo/:repo_id/requests', function (schema, request) {
+      return new Response(429, {}, {});
+    });
 
-  triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
-  triggerBuildPage.openPopup();
-  triggerBuildPage.selectBranch('master');
-  triggerBuildPage.clickSubmit();
+    await triggerBuildPage.visit({ owner: 'adal', repo: 'difference-engine' });
+    await triggerBuildPage.openPopup();
+    await triggerBuildPage.selectBranch('master');
+    await triggerBuildPage.clickSubmit();
 
-  andThen(() => {
     assert.equal(topPage.flashMessage.text, 'You’ve exceeded the limit for triggering builds, please wait a while before trying again.');
   });
 });
