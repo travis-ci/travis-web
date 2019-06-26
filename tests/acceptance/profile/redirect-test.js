b0c7b6c57c4ae5f69c9aca8a59db9d1de0e80e59
Curtis Ekstrom <code@fauxton.io>
2019-06-26 15:57:16 +0200

    Convert profile redirect test

--- tests/acceptance/profile/redirect-test.js
+++ tests/acceptance/profile/redirect-test.js
@@ -1,52 +1,55 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { currentURL, visit } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import signInUser from 'travis/tests/helpers/sign-in-user';
 
-moduleForAcceptance('Acceptance | profile/redirect', {
-  beforeEach() {
+module('Acceptance | profile/redirect', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     this.user = server.create('user', { login: 'test-user' });
     this.org = server.create('organization', { login: 'test-org' });
     signInUser(this.user);
-  }
-});
+  });
 
-test('visiting /profile redirects to /account/repositories', async function (assert) {
-  await visit('/profile');
-  assert.equal(currentURL(), '/account/repositories');
-});
+  test('visiting /profile redirects to /account/repositories', async function (assert) {
+    await visit('/profile');
+    assert.equal(currentURL(), '/account/repositories');
+  });
 
-test('visiting /profile/:username/repositories redirects to /account/repositories', async function (assert) {
-  await visit(`/profile/${this.user.login}/repositories`);
-  assert.equal(currentURL(), '/account/repositories');
-});
+  test('visiting /profile/:username/repositories redirects to /account/repositories', async function (assert) {
+    await visit(`/profile/${this.user.login}/repositories`);
+    assert.equal(currentURL(), '/account/repositories');
+  });
 
-test('visiting /profile/:username redirects to /account/repositories', async function (assert) {
-  await visit(`/profile/${this.user.login}`);
-  assert.equal(currentURL(), '/account/repositories');
-});
+  test('visiting /profile/:username redirects to /account/repositories', async function (assert) {
+    await visit(`/profile/${this.user.login}`);
+    assert.equal(currentURL(), '/account/repositories');
+  });
 
-test('visiting /profile/:username/settings redirects to /account/preferences', async function (assert) {
-  await visit(`/profile/${this.user.login}/settings`);
-  assert.equal(currentURL(), '/account/preferences');
-});
+  test('visiting /profile/:username/settings redirects to /account/preferences', async function (assert) {
+    await visit(`/profile/${this.user.login}/settings`);
+    assert.equal(currentURL(), '/account/preferences');
+  });
 
-test('visiting /profile/:username/subscription redirects to /account/subscription', async function (assert) {
-  await visit(`/profile/${this.user.login}/subscription`);
-  assert.equal(currentURL(), '/account/subscription');
-});
+  test('visiting /profile/:username/subscription redirects to /account/subscription', async function (assert) {
+    await visit(`/profile/${this.user.login}/subscription`);
+    assert.equal(currentURL(), '/account/subscription');
+  });
 
-test('visiting /profile/:org redirects to /organisations/:org/repositories', async function (assert) {
-  await visit(`/profile/${this.org.login}`);
-  assert.equal(currentURL(), `/organizations/${this.org.login}/repositories`);
-});
+  test('visiting /profile/:org redirects to /organisations/:org/repositories', async function (assert) {
+    await visit(`/profile/${this.org.login}`);
+    assert.equal(currentURL(), `/organizations/${this.org.login}/repositories`);
+  });
 
-test('visiting /profile/:org/repositories redirects to /organisations/:org/repositories', async function (assert) {
-  await visit(`/profile/${this.org.login}/repositories`);
-  assert.equal(currentURL(), `/organizations/${this.org.login}/repositories`);
-});
+  test('visiting /profile/:org/repositories redirects to /organisations/:org/repositories', async function (assert) {
+    await visit(`/profile/${this.org.login}/repositories`);
+    assert.equal(currentURL(), `/organizations/${this.org.login}/repositories`);
+  });
 
-test('visiting /profile/:org/subscription redirects to /organisations/:org/subscription', async function (assert) {
-  await visit(`/profile/${this.org.login}/subscription`);
-  assert.equal(currentURL(), `/organizations/${this.org.login}/subscription`);
+  test('visiting /profile/:org/subscription redirects to /organisations/:org/subscription', async function (assert) {
+    await visit(`/profile/${this.org.login}/subscription`);
+    assert.equal(currentURL(), `/organizations/${this.org.login}/subscription`);
+  });
 });
 
