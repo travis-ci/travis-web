043c07076541fac74a17c6cfa1d11a0b9162f582
Curtis Ekstrom <code@fauxton.io>
2019-06-26 20:36:13 +0200

    Convert invalid job log test

--- tests/acceptance/job/invalid-log-test.js
+++ tests/acceptance/job/invalid-log-test.js
@@ -1,35 +1,36 @@
+import { visit } from '@ember/test-helpers';
 import Ember from 'ember';
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import jobPage from 'travis/tests/pages/job';
 
 let adapterException;
 let loggerError;
 
-moduleForAcceptance('Acceptance | job/invalid log', {
-  beforeEach() {
+module('Acceptance | job/invalid log', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     adapterException = Ember.Test.adapter.exception;
     loggerError = Ember.Logger.error;
     Ember.Test.adapter.exception = () => {};
     Ember.Logger.error = () => null;
-  },
+  });
 
-  afterEach() {
+  hooks.afterEach(function () {
     Ember.Test.adapter.exception = adapterException;
     Ember.Logger.error = loggerError;
-  }
-});
+  });
 
-test('viewing invalid job shows error', function (assert) {
-  // create incorrect repository as this is resolved first, errors otherwise
-  server.create('repository', { slug: 'travis-ci/travis-api' });
+  test('viewing invalid job shows error', async function (assert) {
+    // create incorrect repository as this is resolved first, errors otherwise
+    server.create('repository', { slug: 'travis-ci/travis-api' });
 
-  const repository = server.create('repository', { slug: 'travis-ci/travis-web' });
-  const incorrectSlug = 'travis-ci/travis-api';
-  const job = server.create('job', { repository });
-  visit(`${incorrectSlug}/jobs/${job.id}`);
+    const repository = server.create('repository', { slug: 'travis-ci/travis-web' });
+    const incorrectSlug = 'travis-ci/travis-api';
+    const job = server.create('job', { repository });
+    await visit(`${incorrectSlug}/jobs/${job.id}`);
 
-  andThen(() => {
     assert.equal(jobPage.jobNotFoundMessage, 'Oops, we couldn\'t find that job!', 'Shows missing job message');
   });
 });
