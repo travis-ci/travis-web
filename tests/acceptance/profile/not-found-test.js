237951f933998efb83ed760f917d78173bbd7d10
Curtis Ekstrom <code@fauxton.io>
2019-06-26 15:53:19 +0200

    Convert profile not found test

--- tests/acceptance/profile/not-found-test.js
+++ tests/acceptance/profile/not-found-test.js
@@ -1,23 +1,24 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import profilePage from 'travis/tests/pages/profile';
 import signInUser from 'travis/tests/helpers/sign-in-user';
+import { percySnapshot } from 'ember-percy';
 
-moduleForAcceptance('Acceptance | profile/not found', {
-  beforeEach() {
+module('Acceptance | profile/not found', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function () {
     const currentUser = server.create('user', {
       name: 'User Name',
       login: 'user-login',
     });
 
     signInUser(currentUser);
-  }
-});
+  });
 
-test('try to view account that does not exist', function (assert) {
-  profilePage.visitOrganization({ name: 'random-org' });
+  test('try to view account that does not exist', async function (assert) {
+    await profilePage.visitOrganization({ name: 'random-org' });
 
-  andThen(() => {
     percySnapshot(assert);
     assert.equal(document.title, 'Account - Profile - Travis CI');
     assert.equal(profilePage.notFoundOrgName, 'random-org');
