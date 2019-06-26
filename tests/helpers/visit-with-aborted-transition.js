5ab61f047570a8945998d58ca0d41390c82956b9
Curtis Ekstrom <code@fauxton.io>
2019-06-26 11:44:28 +0200

    Fix ESLint errors

--- tests/helpers/visit-with-aborted-transition.js
+++ tests/helpers/visit-with-aborted-transition.js
@@ -3,10 +3,10 @@ import { visit } from '@ember/test-helpers';
 export async function visitWithAbortedTransition(url) {
   try {
     await visit(url);
-  } catch(error) {
+  } catch (error) {
     const { message } = error;
     if (message !== 'TransitionAborted') {
       throw error;
     }
   }
-};
+}
