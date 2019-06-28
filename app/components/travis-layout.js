b7a66abcffde14ca4f36061bb633a8b7961c3d09
Sophie Orta <sophie@travis-ci.org>
2019-06-28 13:27:19 -0400

    Layout trickery to get footer to behave more consistently with center layout

--- app/components/travis-layout.js
+++ app/components/travis-layout.js
@@ -1,3 +1,12 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
-export default Component.extend({});
+export default Component.extend({
+  classNameBindings: ['layoutClass'],
+
+  layoutName: '',
+
+  layoutClass: computed(function () {
+    return this.layoutName.replace('/', '-');
+  })
+});
