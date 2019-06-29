d2c0670819289de827ec57c96a51eb3e07e8de46
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-06-29 09:19:24 +0100

    Implement yearly/monthly plans toggle functionality

--- app/components/billing-information.js
+++ app/components/billing-information.js
@@ -1,25 +1,50 @@
 import Component from '@ember/component';
-import { gt } from '@ember/object/computed';
 import { getCountries } from '../utils/countries';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  plans: null,
   selectedPlan: null,
   monthlyPlans: null,
+  yearlyPlans: null,
+  showAnnual: false,
   countries: getCountries(),
-  multipleJobs: gt('selectedPlan', 1),
+  plansToShow: ['Bootstrap', 'Startup', 'Premium', 'Small Business'],
+  defaultPlan: 'Startup',
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    const defaultPlan = 'Startup';
-    const monthlyPlans = this.get('monthlyPlans');
-    const startUpPlan = monthlyPlans.findBy('name', defaultPlan);
-    this.set('selectedPlan', startUpPlan);
-  },
+  monthlyPlans: computed('plans', function () {
+    const monthlyPlans = this.plans.filter(plan => {
+      const { annual, builds, name } = plan;
+      return !annual && builds <= 10 && this.plansToShow.includes(name);
+    });
+    return monthlyPlans;
+  }),
+
+  yearlyPlans: computed('plans', function () {
+    const yearlyPlans = this.plans.filter(plan => {
+      const { annual, builds, name } = plan;
+      return annual && builds <= 10 && this.plansToShow.includes(name);
+    });
+    return yearlyPlans;
+  }),
+
+  displayedPlans: computed('showAnnual', 'monthlyPlans', 'yearlyPlans', function () {
+    let displayedPlans = this.monthlyPlans;
+    if (this.showAnnual) {
+      const startUpPlan = this.yearlyPlans.findBy('name', this.defaultPlan);
+      this.set('selectedPlan', startUpPlan);
+      displayedPlans = this.yearlyPlans;
+    } else {
+      const startUpPlan = this.monthlyPlans.findBy('name', this.defaultPlan);
+      this.set('selectedPlan', startUpPlan);
+    }
+    return displayedPlans;
+  }),
 
   actions: {
     selectPlan(planId) {
-      const monthlyPlans = this.get('monthlyPlans');
-      const selectedPlan = monthlyPlans.findBy('id', planId);
+      const displayedPlans = this.get('displayedPlans');
+      const selectedPlan = displayedPlans.findBy('id', planId);
       this.set('selectedPlan', selectedPlan);
     }
   }
--- app/controllers/account/billing.js
+++ app/controllers/account/billing.js
@@ -1,16 +1,6 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
-  allPlans: alias('model.plans'),
-  plansToShow: ['Bootstrap', 'Startup', 'Premium', 'Small Business'],
-
-  monthlyPlans: computed('allPlans', function () {
-    const monthlyPlans =  this.allPlans.filter(plan => {
-      const { annual, builds, name } = plan;
-      return !annual && builds <= 10 && this.plansToShow.includes(name);
-    });
-    return monthlyPlans;
-  })
+  plans: alias('model.plans'),
 });
