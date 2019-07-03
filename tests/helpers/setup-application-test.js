70bee6c2987e5e8fe5f186709239a118c36bb8cd
Curtis Ekstrom <code@fauxton.io>
2019-07-03 11:41:39 +0200

    Clear local storage between tests as well

--- tests/helpers/setup-application-test.js
+++ tests/helpers/setup-application-test.js
@@ -4,6 +4,8 @@ import signOutUser from 'travis/tests/helpers/sign-out-user';
 export function setupApplicationTest(hooks) {
   hooks.beforeEach(function () {
     signOutUser();
+    localStorage.clear();
+    sessionStorage.clear();
   });
   return _super(...arguments);
 }
