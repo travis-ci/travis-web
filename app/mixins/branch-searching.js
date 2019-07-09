628bd0123aa1ef1c7473c02efd6a1d69e1adefb7
Artur Smirnov <mail@artursmirnov.com>
2019-07-09 12:25:26 +0300

    Update trigger build form to use travis-form

---
+++ app/mixins/branch-searching.js
@@ -0,0 +1,23 @@
+import Mixin from '@ember/object/mixin';
+import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import config from 'travis/config/environment';
+
+export default Mixin.create({
+  store: service(),
+
+  searchBranch: task(function* (repositoryId, query, filter = []) {
+    yield timeout(config.intervals.searchDebounceRate);
+    let branches = yield this.store.query('branch', {
+      repository_id: repositoryId,
+      data: {
+        name: query,
+        sort_by: 'name',
+        limit: 10,
+        exists_on_github: true
+      }
+    });
+    return branches.reject(branch => (filter.includes(branch.name)));
+  }).restartable()
+});
+
