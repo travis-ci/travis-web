5fb27b1eb5fdb69734c5711451ac85ad57f1109e
Sophie Orta <sophie@travis-ci.org>
2019-07-10 18:11:39 -0400

    Dynamically get plans

--- app/controllers/plans.js
+++ app/controllers/plans.js
@@ -1,12 +1,31 @@
 /* global _gaq */
 import Controller from '@ember/controller';
 import config from 'travis/config/environment';
+import { reads, filterBy, sort } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   config,
 
   auth: service(),
+
+  plans: reads('model'),
+  sortedPlans: sort('plans', (a, b) => (
+    a.price > b.price ? 1 : a.price < b.price ? -1 : 0
+  )),
+  annualPlans: filterBy('sortedPlans', 'annual', true),
+  monthlyPlans: filterBy('sortedPlans', 'annual', false),
+  filteredPlans: computed(
+    'showAnnual',
+    'annualPlans',
+    'monthlyPlans',
+    function () {
+      return this.showAnnual ? this.annualPlans : this.monthlyPlans;
+    }
+  ),
+
+  showAnnual: true,
 
   actions: {
     gaCta(location) {
--- app/routes/plans.js
+++ app/routes/plans.js
@@ -11,5 +11,21 @@ export default BasicRoute.extend(TailwindBaseMixin, {
     if (!this.get('features.proVersion')) {
       return this.transitionTo('/');
     }
+  },
+
+  model() {
+    return this.store.filter('plan',
+      {},
+      (plan) => [
+        'travis-ci-one-build',
+        'travis-ci-two-builds',
+        'travis-ci-five-builds',
+        'travis-ci-ten-builds',
+        'travis-ci-one-build-annual',
+        'travis-ci-two-builds-annual',
+        'travis-ci-five-builds-annual',
+        'travis-ci-ten-builds-annual',
+      ].includes(plan.id)
+    );
   }
 });
