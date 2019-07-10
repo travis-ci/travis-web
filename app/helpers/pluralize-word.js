e5df75381192fc80c5cc0c26b141581e334711cf
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-07-10 11:17:09 +0100

    refactor pluralize-word helper

--- app/helpers/pluralize-word.js
+++ app/helpers/pluralize-word.js
@@ -1,11 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function pluralizeWord([value, singularWord, pluralWord]) {
-  let pluralizedWord = pluralWord;
-  if (value === 1) {
-    pluralizedWord = singularWord;
-  }
-  return pluralizedWord;
+  return value === 1 ? singularWord : pluralWord;
 }
 
 export default helper(pluralizeWord);
