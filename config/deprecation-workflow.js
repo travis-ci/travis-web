75d85ca0c9463231a05a08da3216ca1ba4dc1514
Artur Smirnov <mail@artursmirnov.com>
2019-07-15 23:23:54 +0300

    Fix all deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,8 +2,5 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-component.send-action' },
-    { handler: 'silence', matchId: 'ember-data:method-calls-on-destroyed-store' },
-    { handler: 'silence', matchId: 'ember-keyboard-shortcuts.mixins' }
   ]
 };
