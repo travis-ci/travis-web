4bc64f116467a3101b8263e463e3f6c8e54d6a4c
Artur Smirnov <mail@artursmirnov.com>
2019-07-15 15:46:12 +0300

    Update status image form to use travis-form

--- app/components/status-images.js
+++ app/components/status-images.js
@@ -1,65 +1,47 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
+import { reads } from '@ember/object/computed';
+import { bindKeyboardShortcuts, unbindKeyboardShortcuts } from 'ember-keyboard-shortcuts';
+import { IMAGE_FORMATS } from 'travis/services/status-images';
 
-export default Component.extend(KeyboardShortcuts, {
+export default Component.extend({
+  classNames: ['popup', 'status-images'],
+
   auth: service(),
   externalLinks: service(),
   statusImages: service(),
 
-  id: 'status-images',
-  attributeBindings: ['id'],
-  classNames: ['popup', 'status-images'],
-  formats: ['Image URL', 'Markdown', 'Textile', 'Rdoc', 'AsciiDoc', 'RST', 'Pod', 'CCTray'],
+  repo: null,
+  onClose() {},
+
+  formats: Object.keys(IMAGE_FORMATS),
 
   keyboardShortcuts: {
     'esc': 'toggleStatusImageModal'
   },
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    this.set('branch', this.get('repo.defaultBranch.name'));
-  },
+  branch: reads('repo.defaultBranch.name'),
+  format: reads('formats.firstObject'),
 
-  statusString: computed('format', 'repo.slug', 'branch', 'repo.defaultBranch.name', function () {
-    let format = this.get('format');
-    let branch = this.get('branch');
-    let defaultBranchName = this.get('repo.defaultBranch.name');
-
-    const repo = this.get('repo');
-    if (repo) {
-      const imageFormat = format || this.get('formats.firstObject');
-      const gitBranch = branch || defaultBranchName;
-
-      return this.formatStatusImage(imageFormat, repo, gitBranch);
-    }
+  statusString: computed('format', 'repo.slug', 'branch', function () {
+    const { repo, branch, format } = this;
+    return this.statusImages[IMAGE_FORMATS[format]](repo, branch);
   }),
 
-  formatStatusImage(format, repo, branch) {
-    switch (format) {
-      case 'Image URL':
-        return this.get('statusImages').imageUrl(repo, branch);
-      case 'Markdown':
-        return this.get('statusImages').markdownImageString(repo, branch);
-      case 'Textile':
-        return this.get('statusImages').textileImageString(repo, branch);
-      case 'Rdoc':
-        return this.get('statusImages').rdocImageString(repo, branch);
-      case 'AsciiDoc':
-        return this.get('statusImages').asciidocImageString(repo, branch);
-      case 'RST':
-        return this.get('statusImages').rstImageString(repo, branch);
-      case 'Pod':
-        return this.get('statusImages').podImageString(repo, branch);
-      case 'CCTray':
-        return this.get('statusImages').ccXml(repo, branch);
-    }
+  didInsertElement() {
+    this._super(...arguments);
+    bindKeyboardShortcuts(this);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    unbindKeyboardShortcuts(this);
   },
 
   actions: {
     toggleStatusImageModal() {
-      this.get('onClose')();
+      this.onClose();
     }
   }
 });
