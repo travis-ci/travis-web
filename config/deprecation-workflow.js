47241b05e34071f627d4800183966b256df0a442
Artur Smirnov <mail@artursmirnov.com>
2019-07-16 16:32:17 +0300

    Add deprecation workflow rules

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,5 +2,8 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
+    { handler: 'throw', matchId: 'ember-views.curly-components.jquery-element' },
+    { handler: 'silence', matchId: 'computed-property.volatile' },
+    { handler: 'silence', matchId: 'computed-property.override' }
   ]
 };
