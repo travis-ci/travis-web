178dae02f588c6c6353675249ac8c0d63008dd42
Artur Smirnov <mail@artursmirnov.com>
2019-07-16 18:07:15 +0300

    Silence deprecation messages

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,7 +2,7 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'throw', matchId: 'ember-views.curly-components.jquery-element' },
+    { handler: 'silence', matchId: 'ember-views.curly-components.jquery-element' },
     { handler: 'silence', matchId: 'computed-property.volatile' },
     { handler: 'silence', matchId: 'computed-property.override' }
   ]
