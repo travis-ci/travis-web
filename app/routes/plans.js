f514834ddedad6b92e28aeaafb74cd06c84526a9
Sophie Orta <sophie@travis-ci.org>
2019-07-17 11:25:06 -0400

    Quick fix for PR deployment. Maybe eventually use Emmanuel's config instead?

--- app/routes/plans.js
+++ app/routes/plans.js
@@ -14,18 +14,33 @@ export default BasicRoute.extend(TailwindBaseMixin, {
   },
 
   model() {
-    return this.store.filter('plan',
-      {},
-      (plan) => [
-        'travis-ci-one-build',
-        'travis-ci-two-builds',
-        'travis-ci-five-builds',
-        'travis-ci-ten-builds',
-        'travis-ci-one-build-annual',
-        'travis-ci-two-builds-annual',
-        'travis-ci-five-builds-annual',
-        'travis-ci-ten-builds-annual',
-      ].includes(plan.id)
-    );
+    let plans;
+    try {
+      plans = this.store.filter('plan',
+        {},
+        (plan) => [
+          'travis-ci-one-build',
+          'travis-ci-two-builds',
+          'travis-ci-five-builds',
+          'travis-ci-ten-builds',
+          'travis-ci-one-build-annual',
+          'travis-ci-two-builds-annual',
+          'travis-ci-five-builds-annual',
+          'travis-ci-ten-builds-annual',
+        ].includes(plan.id)
+      );
+    } catch (e) {
+      plans = [
+        {name: 'Bootstrap', builds: 1, price: 6900, annual: false, currency: 'USD'},
+        {name: 'Startup', builds: 2, price: 12900, annual: false, currency: 'USD'},
+        {name: 'Premium', builds: 10, price: 48900, annual: false, currency: 'USD'},
+        {name: 'Small Business', builds: 5, price: 24900, annual: false, currency: 'USD'},
+        {name: 'Small Business', builds: 5, price: 273900, annual: true, currency: 'USD'},
+        {name: 'Bootstrap', builds: 1, price: 75900, annual: true, currency: 'USD'},
+        {name: 'Premium', builds: 10, price: 537900, annual: true, currency: 'USD'},
+        {name: 'Startup', builds: 2, price: 141900, annual: true, currency: 'USD'}
+      ];
+    }
+    return plans;
   }
 });
