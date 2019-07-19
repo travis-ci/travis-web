ffb9f7205fca9422a07e95074bad153d90f7a975
Sophie Orta <sophie@travis-ci.org>
2019-07-19 17:12:52 -0400

    Variant approach

--- app/components/styleguides/product-cta.js
+++ app/components/styleguides/product-cta.js
@@ -2,4 +2,6 @@ import Component from '@ember/component';
 
 export default Component.extend({
   products: null,
+
+  ctaAction() {},
 });
--- app/components/styleguides/travis-button.js
+++ app/components/styleguides/travis-button.js
@@ -1,31 +1,52 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import abstractMethod from 'travis/utils/abstract-method';
-import twProps from 'travis/mixins/styleguides/tailwind-props';
-import generatePropVariants from 'travis/utils/tailwind-variants';
+import { computed } from '@ember/object';
 
-const styleProps = ['bg', 'font', 'px', 'py', 'rounded', 'text'];
-const generatedProps = generatePropVariants(styleProps);
+export const DEFAULT_COLOR = 'blue';
+export const COLORS = {
+  blue: { main: 'blue-400', light: 'blue-100', dark: 'blue-500'},
+  green: { main: 'green-400', light: 'green-100', dark: 'green-500'},
+};
 
-export default Component.extend(twProps, {
+export const DEFAULT_VARIANT = 'fill';
+export const VARIANTS = {
+  fill: (colors) => [
+    `bg-${colors['main']}`,
+    'text-white',
+    `hover:bg-${colors['dark']}`,
+  ],
+
+  outline: (colors) => [
+    `text-${colors['main']}`,
+    'border',
+    'border-solid',
+    `border-${colors['main']}`,
+    `hover:bg-${colors['light']}`,
+  ],
+};
+
+export default Component.extend({
   tagName: 'button',
-  classNames: [],
-  classNameBindings: ['disabled'],
+  classNameBindings: ['disabled', 'defaultClasses', 'variantClasses'],
   attributeBindings: ['role', 'type'],
-
-  tailwindProps: computed(...generatedProps, function () {
-    return generatedProps.filter(prop => typeof this[prop] !== 'undefined');
-  }),
-  bg: 'grey-700',
-  font: 'bold',
-  px: 3,
-  py: '2',
-  rounded: true,
-  text: 'white',
 
   type: 'button',
   role: 'button',
   disabled: false,
+
+  defaultClasses: 'rounded uppercase px-3 py-2 font-bold',
+  color: DEFAULT_COLOR,
+  variant: DEFAULT_VARIANT,
+
+  variantClasses: computed('color', 'variant', function () {
+    const { color: colorName, variant: variantName } = this;
+    const colorSet = COLORS[colorName] || COLORS[DEFAULT_COLOR];
+    const variant = VARIANTS[variantName] || VARIANTS[DEFAULT_VARIANT];
+
+    const classes = variant(colorSet);
+
+    return classes.join(' ');
+  }),
 
   onToggle: abstractMethod('onToggle'),
 
