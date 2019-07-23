7b6ccde54e852c1cb3c94988f7efda59134c37cd
Rafael <rafael@travis-ci.org>
2019-07-22 23:50:20 -0300

    Fetch onganizations vcs fields

--- app/adapters/organization.js
+++ app/adapters/organization.js
@@ -1,7 +1,11 @@
 import V3Adapter from 'travis/adapters/v3';
 
 export default V3Adapter.extend({
-  includes: 'owner.installation',
+  includes: [
+    'owner.installation',
+    'owner.vcsId',
+    'owner.vcsType',
+  ],
 
   pathForType: function () {
     return 'orgs';
