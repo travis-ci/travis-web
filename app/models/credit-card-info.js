8406c5af455ae0d0c92c88ba710edd466be77dde
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-07-24 10:47:47 +0100

    rebase billing branch on master

--- app/models/credit-card-info.js
+++ app/models/credit-card-info.js
@@ -2,11 +2,7 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default Model.extend({
   lastDigits: attr(),
-<<<<<<< HEAD
 
   subscription: belongsTo('subscription'),
-=======
   token: attr(),
-  subscription: belongsTo(),
->>>>>>> add subscription save handler
 });
