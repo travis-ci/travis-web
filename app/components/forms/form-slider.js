48300c3bc7ea46e4f89327251ab66fb992bb6b65
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-07-24 17:20:18 +0100

    Create slider input component

---
+++ app/components/forms/form-slider.js
@@ -0,0 +1,21 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['travis-form__field--slider'],
+
+  max: '',
+  min: '',
+  value: '',
+  step: 1,
+
+  input({ target }) {
+    this.set('value', target.value);
+    this.onChange(this.value);
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.onInit(this.elementId);
+  },
+
+});
