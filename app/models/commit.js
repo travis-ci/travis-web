1bc0bf1bb98e70c4407987cca13434478d165162
Rafael <rafael@travis-ci.org>
2019-07-29 21:50:23 -0300

    Fix lint offenses

--- app/models/commit.js
+++ app/models/commit.js
@@ -1,6 +1,4 @@
 import Model, { attr, belongsTo } from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
 import { computed } from '@ember/object';
 
 import vcsLinks from 'travis/utils/vcs-links';
