dd275f56632a5c33c8df044245300c47cf5694e1
Rafael <rafael@travis-ci.org>
2019-07-31 14:27:58 -0300

    Correctly import @ember-data/model

--- app/models/commit.js
+++ app/models/commit.js
@@ -1,4 +1,4 @@
-import Model, { attr, belongsTo } from 'ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 import { computed } from '@ember/object';
 
 import vcsLinks from 'travis/utils/vcs-links';
