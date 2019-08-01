5ff4c024a84651f5dc9476ebe30b5904915e7be3
Sophie Orta <sophie@travis-ci.org>
2019-08-01 17:52:21 -0400

    Make grid components more explicit

--- app/components/ui-kit/grid-item.js
+++ app/components/ui-kit/grid-item.js
@@ -1,53 +1,69 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import config from 'travis/config/environment';
-
-const { screens } = config;
-const screenNames = Object.keys(screens);
-const gridClassDependencies = [
-  'sizePrefix',
-  ...screenNames,
-  ...screenNames.map(name => `grid.${name}`),
-];
 
 export default Component.extend({
-  classNameBindings: ['gridClassNames'],
+  classNameBindings: ['baseClass', 'smClass', 'mdClass', 'lgClass', 'xlClass'],
 
+  // Public interface
   grid: null,
 
-  sizePrefix: computed('grid.dir', function () {
-    return this.grid.dir.includes('col') ? 'h' : 'w';
+  // Private
+  sizePrefix: computed('grid.isCol', function () {
+    return this.grid.isCol ? 'h' : 'w';
   }),
 
-  gridClasses: computed(...gridClassDependencies, function () {
-    const { grid, sizePrefix } = this;
-
-    const classes = screenNames.reduce((classes, name) => {
-      const screenVal = this[name] || grid[name];
-      const screenValType = typeof screenVal;
-      let size = '';
-
-      if (screenValType === 'string') {
-        size = `flex-${screenVal}`;
-      } else if (screenValType === 'number') {
-        size = `${sizePrefix}-1/${screenVal}`.replace('1/1', 'full');
-      } else {
-        return classes;
-      }
-
-      const screen = screens[name];
-      const { prefix } = screen;
-      const screenPrefix = prefix.length === 0 ? '' : `${prefix}:`;
-      const newClass = `${screenPrefix}${size}`;
-      classes.push(newClass);
-
-      return classes;
-    }, []);
-
-    return classes;
+  baseClass: computed('sizePrefix', 'base', 'grid.base', function () {
+    const val = this.base || this.grid.base;
+    if (typeof val === 'string') {
+      return val;
+    }
+    if (typeof val === 'number') {
+      return `${this.sizePrefix}-1/${val}`.replace('1/1', 'full');
+    }
+    return '';
   }),
 
-  gridClassNames: computed('gridClasses.[]', function () {
-    return this.gridClasses.join(' ');
+  smClass: computed('sizePrefix', 'sm', 'grid.sm', function () {
+    const val = this.sm || this.grid.sm;
+    if (typeof val === 'string') {
+      return `sm:${val}`;
+    }
+    if (typeof val === 'number') {
+      return `sm:${this.sizePrefix}-1/${val}`.replace('1/1', 'full');
+    }
+    return '';
+  }),
+
+  mdClass: computed('sizePrefix', 'md', 'grid.md', function () {
+    const val = this.md || this.grid.md;
+    if (typeof val === 'string') {
+      return `md:${val}`;
+    }
+    if (typeof val === 'number') {
+      return `md:${this.sizePrefix}-1/${val}`.replace('1/1', 'full');
+    }
+    return '';
+  }),
+
+  lgClass: computed('sizePrefix', 'lg', 'grid.lg', function () {
+    const val = this.lg || this.grid.lg;
+    if (typeof val === 'string') {
+      return `lg:${val}`;
+    }
+    if (typeof val === 'number') {
+      return `lg:${this.sizePrefix}-1/${val}`.replace('1/1', 'full');
+    }
+    return '';
+  }),
+
+  xlClass: computed('sizePrefix', 'xl', 'grid.xl', function () {
+    const val = this.xl || this.grid.xl;
+    if (typeof val === 'string') {
+      return `xl:${val}`;
+    }
+    if (typeof val === 'number') {
+      return `xl:${this.sizePrefix}-1/${val}`.replace('1/1', 'full');
+    }
+    return '';
   }),
 });
--- app/components/ui-kit/grid.js
+++ app/components/ui-kit/grid.js
@@ -1,35 +1,19 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-
-const prefix = (prefix, val) => ((val) => `${prefix}-${val}`);
-const props = {
-  dir: prefix('flex'),
-  wrap: prefix('flex'),
-  items: prefix('items'),
-  justify: prefix('justify'),
-  inline: (val) => (val ? 'inline-flex' : 'flex'),
-};
-const propNames = Object.keys(props);
+import { match } from '@ember/object/computed';
 
 export default Component.extend({
-  classNameBindings: ['defaultClasses'],
 
+  // Public interface
+  tagName: '',
+  tag: 'div',
   dir: 'row',
   wrap: 'wrap',
+  alignItems: 'stretch',
   justify: 'between',
   inline: false,
-
-  defaultClasses: computed(...propNames, function () {
-    const classes = propNames.reduce((classes, name) => {
-      if (typeof this[name] !== 'undefined' && props.hasOwnProperty(name)) {
-        const newClass = props[name](this[name]);
-        classes.push(newClass);
-      }
-      return classes;
-    }, []);
-
-    return classes.join(' ');
-  }),
-
   base: 1,
+
+  // Private
+  isRow: match('dir', /^row/),
+  isCol: match('dir', /^col/),
 });
