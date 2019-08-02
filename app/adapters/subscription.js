95e6f3d02d8d25b404a32b33735973be265b23e7
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-08-02 20:42:21 +0100

    Implement edit billing information

--- app/adapters/subscription.js
+++ app/adapters/subscription.js
@@ -1,3 +1,32 @@
 import V3Adapter from 'travis/adapters/v3';
 
-export default V3Adapter.extend();
+export default V3Adapter.extend({
+  buildURL(modelName, id, snapshot, requestType) {
+    let url = this._super(...arguments);
+    if (requestType === 'updateRecord') {
+      url = `${url}/address`;
+    }
+    return url;
+  },
+
+  updateRecord(store, type, snapshot) {
+    let data = {};
+    let serializer = store.serializerFor(type.modelName);
+
+    serializer.serializeIntoHash(data, type, snapshot);
+
+    let url = this.buildURL(type.modelName, snapshot.id, snapshot, 'updateRecord');
+
+    const updatedBillingInfo = Object.keys(data).reduce((accumulator, attribute) => {
+      const splitAttribute = attribute.split('.');
+      const leftAttribute = splitAttribute[0];
+      const rightAttribute = splitAttribute[1];
+      if (leftAttribute === 'billing_info') {
+        accumulator[`subscription.${rightAttribute}`] = data[`billing_info.${rightAttribute}`];
+      }
+      return accumulator;
+    }, {});
+
+    return this.ajax(url, 'PATCH', { data: updatedBillingInfo });
+  }
+});
--- app/components/billing-address.js
+++ app/components/billing-address.js
@@ -1,5 +1,23 @@
 import Component from '@ember/component';
-
+import { task } from 'ember-concurrency';
 export default Component.extend({
-  classNames: ['address']
+  classNames: ['address'],
+  openEditContactForm: false,
+  openEditBillingForm: false,
+
+  editContact: task(function* () {
+    yield this.subscription.save();
+    this.set('openEditContactForm', false);
+    this.set('openEditBillingForm', false);
+  }).drop(),
+
+  actions: {
+    toggleEditContactForm() {
+      this.toggleProperty('openEditContactForm');
+    },
+
+    toggleEditBillingForm() {
+      this.toggleProperty('openEditBillingForm');
+    }
+  }
 });
