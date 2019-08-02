fa4e4db361eb6a14dc7c7b62be911d251749e895
Sophie Orta <sophie@travis-ci.org>
2019-08-02 14:33:06 -0400

    Add positions prop to Box

--- app/components/ui-kit/box.js
+++ app/components/ui-kit/box.js
@@ -1,11 +1,20 @@
 import Component from '@ember/component';
 import { checkDictionary } from 'travis/utils/ui-kit/assertions';
 import spacingMixin from 'travis/mixins/ui-kit/spacing';
+import { reads } from '@ember/object/computed';
 
 const DISPLAYS = {
   BLOCK: 'block',
   INLINE_BLOCK: 'inline-block',
   FLEX: 'flex',
+};
+
+const POSITIONS = {
+  STATIC: 'static',
+  FIXED: 'fixed',
+  ABSOLUTE: 'absolute',
+  RELATIVE: 'relative',
+  STICKY: 'sticky',
 };
 
 export default Component.extend(spacingMixin, {
@@ -14,12 +23,18 @@ export default Component.extend(spacingMixin, {
   // Public interface
   tag: 'div',
   display: null,
+  position: null,
   margin: null,
+
+  // Private
+  displayClass: reads('display'),
+  positionClass: reads('position'),
 
   // Lifecycle
   init() {
     this._super(...arguments);
 
     checkDictionary(this.display, DISPLAYS, 'Display', 'Box');
+    checkDictionary(this.position, POSITIONS, 'Position', 'Box');
   },
 });
