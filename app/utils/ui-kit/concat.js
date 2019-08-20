0a27db509484ff8271bd10c88255d8a9c77f3926
Sophie Orta <sophie@travis-ci.org>
2019-08-20 10:03:56 -0400

    Simplify concat util

--- app/utils/ui-kit/concat.js
+++ app/utils/ui-kit/concat.js
@@ -2,14 +2,6 @@ import { computed } from '@ember/object';
 
 export default function concat(...props) {
   return computed(...props, function () {
-    const results = props.reduce((collection, prop) => {
-      const val = this.get(prop);
-      if (val !== undefined && val !== null) {
-        collection.push(val);
-      }
-      return collection;
-    }, []);
-
-    return results.join(' ');
+    return props.reduce((collection, prop) => [...collection, this.get(prop)], []).compact().join(' ');
   });
 }
