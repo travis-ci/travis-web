f1d6b662af431382048c6c44b371dac008f88f51
Sophie Orta <sophie@travis-ci.org>
2019-08-22 17:27:09 -0400

    Simplify format-domain helper

--- app/helpers/format-domain.js
+++ app/helpers/format-domain.js
@@ -1,9 +1,7 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(([url]) => {
-  if (url.indexOf('//') > -1) {
-    return url.split('/')[2];
-  } else {
-    return url.split('/')[0];
-  }
-});
+export default helper(([url]) => (
+  url.indexOf('//') > -1
+    ? url.split('/')[2]
+    : url.split('/')[0]
+));
