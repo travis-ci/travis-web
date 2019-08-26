9e6faf3e075ff2aba60a2f86941f237e5f91ae25
Rafael <rafael@travis-ci.org>
2019-08-26 15:28:52 -0400

    Use ember's pluralize inflector

--- app/helpers/vcs-vocab.js
+++ app/helpers/vcs-vocab.js
@@ -1,9 +1,10 @@
+import { pluralize } from 'ember-inflector';
 import { helper } from '@ember/component/helper';
 import { vcsVocab } from 'travis/utils/vcs';
 
 export default helper(([vcsType, vocabKey], { plural, lower }) => {
   let text = vcsVocab(vcsType, vocabKey);
-  text = plural ? `${text}s` : text;
+  text = plural ? pluralize(text) : text;
   text = lower ? text.toLowerCase() : text;
   return text;
 });
