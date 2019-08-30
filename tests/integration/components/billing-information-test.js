32302bcb8a1280789533ba8211d3eb224d4c8fee
Emmanuel Patrick <emmanuel@travis-ci.org>
2019-08-30 10:46:45 +0100

    Test billing information renders form correctly

--- tests/integration/components/billing-information-test.js
+++ tests/integration/components/billing-information-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import profilePage from 'travis/tests/pages/profile';
 
 module('Integration | Component | billing-information', function (hooks) {
   setupRenderingTest(hooks);
@@ -10,15 +11,16 @@ module('Integration | Component | billing-information', function (hooks) {
       id: 1,
       name: 'A',
       builds: 5,
-      amount: 20000,
+      price: 20000,
       annual: false
     }, {
       id: 1,
       name: 'B',
       builds: 10,
-      amount: 30000,
+      price: 30000,
       annual: true
     }];
+    this.plans = plans;
 
     const newSubscription = {
       billingInfo: {
@@ -51,16 +53,26 @@ module('Integration | Component | billing-information', function (hooks) {
 
   test('it renders billing information form correctly', async function (assert) {
     await render(hbs`
-    {{billing-information 
-      selectedPlan=selectedPlan 
-      displayedPlans=displayedPlans 
-      showAnnual=showAnnual
-      newSubscription=newSubscription
-      next=(action 'next')
-      back=(action 'back')
-      goToFirstStep=(action 'goToFirstStep')
-    }}`);
+      {{billing-information 
+        selectedPlan=selectedPlan 
+        displayedPlans=displayedPlans 
+        showAnnual=showAnnual
+        newSubscription=newSubscription
+        next=(action 'next')
+        back=(action 'back')
+        goToFirstStep=(action 'goToFirstStep')
+      }}`
+    );
 
     assert.dom('[data-test-contact-details-title]').hasText('Contact details');
+    assert.equal(profilePage.billing.selectedPlanOverview.heading.text, 'summary');
+    assert.equal(profilePage.billing.selectedPlanOverview.name.text, `${this.plans[0].name} plan`);
+    assert.equal(profilePage.billing.selectedPlanOverview.jobs.text, `${this.plans[0].builds} concurrent jobs`);
+    assert.equal(profilePage.billing.selectedPlanOverview.price.text, `$${this.plans[0].price / 100}`);
+    assert.equal(profilePage.billing.period.text, '/month');
+    assert.equal(profilePage.billing.selectedPlanOverview.changePlan.text, 'change plan');
+
+    assert.dom('input').isVisible({ count: 9 });
+    assert.dom('.ember-power-select-trigger').isVisible({ count: 1 });
   });
 });
