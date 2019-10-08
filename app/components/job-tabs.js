c98984d4671ea025c0653414ff3328ae8b73c9e4
Sven Fuchs <me@svenfuchs.com>
2019-10-08 15:00:43 +0200

    add a badge to "view config" if msgs > info are present

--- app/components/job-tabs.js
+++ app/components/job-tabs.js
@@ -1,10 +1,17 @@
+import { computed } from '@ember/object';
 import { isEmpty } from '@ember/utils';
 import Component from '@ember/component';
 
 export default Component.extend({
-
   tagName: 'div',
   classNames: ['travistab'],
+
+  messagesMaxLevel: computed('this.job.build.request', function () {
+    let msgs = this.get('job.build.request.messages').rejectBy('level', 'info');
+    if (msgs.length > 0) {
+      return msgs.sortBy('level')[0].level;
+    }
+  }),
 
   didRender() {
     // Set the log to be default active tab unless something else is active
