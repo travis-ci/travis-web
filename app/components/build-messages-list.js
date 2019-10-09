c6f76b458500ad02e9d5b57c6df95494ac7a4569
Sven Fuchs <me@svenfuchs.com>
2019-10-10 00:06:00 +0200

    apply various suggestions from code review

--- app/components/build-messages-list.js
+++ app/components/build-messages-list.js
@@ -1,83 +1,60 @@
-import { A } from '@ember/array';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { not } from '@ember/object/computed';
+import { filter, notEmpty } from '@ember/object/computed';
+import { pluralize } from 'ember-inflector';
+
+const READABLE_LEVELS = {
+  'alert': 'alert',
+  'error': 'error',
+  'warn': 'warning',
+};
 
 export default Component.extend({
   tagName: '',
-  toggleStatus: undefined,
-  readableLevels: {
-    'alert': 'alert',
-    'error': 'error',
-    'warn': 'warning',
-  },
+  toggleStatus: null,
 
-  nonInfoMessages: computed('request.messages', function () {
-    return A(this.get('request.messages')).rejectBy('level', 'info');
-  }),
-
-  maxLevel: computed('job.build.request', function () {
-    let msgs = this.get('nonInfoMessages');
-    if (msgs.length > 0) {
-      return msgs.sortBy('level')[0].level;
-    }
-  }),
-
-  iconClass: computed('maxLevel', function () {
-    let level = this.get('maxLevel');
-    return `icon icon-${level}`;
-  }),
-
-  summary: computed('nonInfoMessages', function () {
-    let counts = this.countBy(this.get('nonInfoMessages'), 'level');
-    if (Object.entries(counts).length > 0) {
-      let summary = Object.entries(counts).map((entry) => this.formatLevel(...entry)).join(', ');
-      return `(${summary})`;
-    }
-  }),
-
-  formatLevel: function (level, count) {
-    return [count, this.readableLevel(level, count)].join(' ');
-  },
-
-  readableLevel: function (level, count) {
-    level = this.get('readableLevels')[level];
-    if (count > 1) {
-      level = `${level}s`;
-    }
-    return level;
-  },
-
-  isExpanded: computed('nonInfoMessages', 'toggleStatus', function () {
-    if (this.get('toggleStatus') != undefined) {
-      return this.get('toggleStatus');
-    }
-    return this.get('nonInfoMessages').length > 0;
-  }),
-
-  isCollapsed: not('isExpanded'),
+  isExpanded: notEmpty('nonInfoMessages'),
 
   toggleStatusClass: computed('isExpanded', function () {
     return this.get('isExpanded') ? 'expanded' : 'collapsed';
   }),
 
-  toggle: function () {
-    this.set('toggleStatus', !this.get('isExpanded'));
-  },
+  nonInfoMessages: filter('request.messages', (message) =>
+    message.level !== 'info'
+  ),
+
+  maxLevel: computed('nonInfoMessages.[].level', function () {
+    return this.nonInfoMessages.sortBy('level')[0].level;
+  }),
+
+  iconClass: computed('maxLevel', function () {
+    return `icon icon-${this.get('maxLevel')}`;
+  }),
+
+  summary: computed('nonInfoMessages', function () {
+    let counts = countBy(this.get('nonInfoMessages'), 'level');
+    if (Object.entries(counts).length > 0) {
+      return Object.entries(counts).map((entry) => formatLevel(...entry)).join(', ');
+    }
+  }),
 
   actions: {
-    toggle: function () {
-      this.toggle();
+    toggle() {
+      this.toggleProperty('isExpanded');
     }
-  },
-
-  countBy: function (objs, name) {
-    return objs.reduce((counts, obj) => {
-      if (!counts[obj[name]]) {
-        counts[obj[name]] = 0;
-      }
-      counts[obj[name]] += 1;
-      return counts;
-    }, {});
   }
 });
+
+function formatLevel(level, count) {
+  return pluralize(count, READABLE_LEVELS[level]);
+}
+
+function countBy(objs, name) {
+  return objs.reduce((counts, obj) => {
+    if (!counts[obj[name]]) {
+      counts[obj[name]] = 0;
+    }
+    counts[obj[name]] += 1;
+    return counts;
+  }, {});
+}
