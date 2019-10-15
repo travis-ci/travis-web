24de150557e2d6f702f638c711f2c11fb8f4be1d
Patrick <emmanuel@travis-ci.org>
2019-10-15 11:20:28 +0100

    Show a different message when redirecting to account page

--- app/routes/signin.js
+++ app/routes/signin.js
@@ -1,9 +1,15 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Route.extend({
   auth: service(),
+  router: service(),
   needsAuth: false,
+
+  isRedirectingToAccountPage: computed('model.redirectUri', function () {
+    return this.router.urlFor('account.billing') === this.model.redirectUri;
+  }),
 
   beforeModel() {
     if (this.get('auth.signedIn')) {
