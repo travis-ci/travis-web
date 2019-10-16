9af0e351c957edc44ec3bc64a144b258ede3ef50
Patrick <emmanuel@travis-ci.org>
2019-10-16 10:57:51 +0100

    fix tests

--- app/controllers/signin.js
+++ app/controllers/signin.js
@@ -2,17 +2,21 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import config from 'travis/config/environment';
 import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 
 export default Controller.extend({
-  auth: service(),
-  router: service(),
-
+  auth: service('auth'),
+  router: service('router'),
+  redirectUri: reads('model.redirectUri'),
   endpoint: config.authEndpoint || config.apiEndpoint,
 
-  isRedirectingToAccountPage: computed('model.redirectUri', function () {
-    const redirectTo = new URL(this.model.redirectUri);
-    const { host, protocol, href: fullRedirectUrl } = redirectTo;
-    const accountUrl = this.router.urlFor('account.billing');
-    return `${protocol}//${host}${accountUrl}` === fullRedirectUrl;
+  isRedirectingToAccountPage: computed('redirectUri', function () {
+    if (this.redirectUri) {
+      const redirectTo = new URL(this.redirectUri);
+      const { host, protocol, href: fullRedirectUrl } = redirectTo;
+      const accountUrl = this.router.urlFor('account.billing');
+      return `${protocol}//${host}${accountUrl}` === fullRedirectUrl;
+    }
+    return false;
   }),
 });
