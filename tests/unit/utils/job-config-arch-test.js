809e5c0e59552ca2cee471d72e0df3c110392ab2
Pavel Dotsulenko <pavel@assembla.com>
2019-10-21 15:15:24 +0300

    Fix syntax error

--- tests/unit/utils/job-config-arch-test.js
+++ tests/unit/utils/job-config-arch-test.js
@@ -12,11 +12,11 @@ module('jobConfigArch', function () {
   });
 
   test('a job with an amd64 arch value returns AMD64', (assert) => {
-    assert.equal(jobConfigArch({ arch: amd64 }), 'AMD64');
+    assert.equal(jobConfigArch({ arch: 'amd64' }), 'AMD64');
   });
 
   test('a job with an arm64 arch value returns Arm64', (assert) => {
-    assert.equal(jobConfigArch({ arch: arm64 }), 'Arm64');
+    assert.equal(jobConfigArch({ arch: 'arm64' }), 'Arm64');
   });
 
   test('a job with any other arch value returns the original value', (assert) => {
