b623eb10577d47eb5c7e91f76743a6c387aa7459
Patrick <emmanuel@travis-ci.org>
2019-10-24 10:38:57 +0100

    Remove endpoint

--- app/controllers/signin.js
+++ app/controllers/signin.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import config from 'travis/config/environment';
 import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
 
@@ -8,7 +7,6 @@ export default Controller.extend({
   auth: service('auth'),
   router: service('router'),
   redirectUri: reads('model.redirectUri'),
-  endpoint: config.authEndpoint || config.apiEndpoint,
 
   isRedirectingToAccountPage: computed('redirectUri', function () {
     if (this.redirectUri) {
