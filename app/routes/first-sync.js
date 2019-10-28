b9b3a10176efdc4ceb4d623b19cf8966395691c3
Artur Smirnov <reg@artursmirnov.com>
2019-10-28 18:12:49 +0300

    Revert preventing first_sync page manual visits (#2205)

--- app/routes/first-sync.js
+++ app/routes/first-sync.js
@@ -1,19 +1,8 @@
 import { later } from '@ember/runloop';
 import config from 'travis/config/environment';
 import SimpleLayoutRoute from 'travis/routes/simple-layout';
-import { inject as service } from '@ember/service';
 
 export default SimpleLayoutRoute.extend({
-  auth: service(),
-
-  beforeModel(transition) {
-    const { currentUser: user } = this.auth;
-    const isFirstSync = !!user && user.isSyncing && !user.syncedAt;
-    if (!isFirstSync) {
-      transition.abort();
-      return this.transitionTo('dashboard');
-    }
-  },
 
   activate() {
     const controller = this.controllerFor('firstSync');
