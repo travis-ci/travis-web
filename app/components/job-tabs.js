1a1b975c27030846fc91b3bac99a44c1d3669b11
Sophie Orta <sophie@travis-ci.org>
2019-11-05 14:18:36 -0500

    Fix issue where subtabs are sometimes not highlighted properly

--- app/components/job-tabs.js
+++ app/components/job-tabs.js
@@ -1,10 +1,17 @@
 import { computed } from '@ember/object';
+import { match, not } from '@ember/object/computed';
 import { isEmpty } from '@ember/utils';
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: 'div',
   classNames: ['travistab'],
+
+  router: service(),
+
+  isConfig: match('router.currentRouteName', /config$/),
+  isLog: not('isConfig'),
 
   messagesMaxLevel: computed('job.build.request.messages.@each.level', function () {
     const msgs = this.get('job.build.request.messages');
