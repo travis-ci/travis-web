d5e431437c9a4fa0f223e702dd0cde0b59cc74a9
Chris Thoburn <cthoburn@linkedin.com>
2019-11-08 10:00:17 -0800

    watch for async test leaks

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,8 @@
-import Application from '../app';
-import config from '../config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import Application from "../app";
+import config from "../config/environment";
+import { setApplication } from "@ember/test-helpers";
+import { start } from "ember-qunit";
 
 setApplication(Application.create(config.APP));
 
-start();
+start({ setupTestIsolationValidation: true });
