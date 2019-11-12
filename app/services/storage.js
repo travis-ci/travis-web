e08edecdd2116c698fb630f5efdd399f3ac7c202
Patrick <emmanuel@travis-ci.org>
2019-11-12 16:11:00 +0100

    Give option for resubscribing to expire stripe plan

--- app/services/storage.js
+++ app/services/storage.js
@@ -5,6 +5,7 @@ export default Service.extend({
   get signupUsers() {
     return JSON.parse(this.getItem('travis.signup.users')) || [];
   },
+
   set signupUsers(value) {
     this.setItem('travis.signup.users', JSON.stringify(value || []));
   },
