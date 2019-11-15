47e491f0b904664cb30d8219087bce2c1facd2d5
Emmanuel Patrick <52037013+emmanuel-ci@users.noreply.github.com>
2019-11-15 09:47:56 +0100

    GitHub subscription view (#2215)
    * Initial commit
    
    * Implement github active subscription view
    
    * Use correct github badge
    
    * Test github subscription view
    
    * Simplify conditionals
    
    * Create canceled github trial view
    
    * Refactor billing summary status component
    
    * Clear unused mark up
    
    * refacfor summary component
    
    * Format cancelled message
    
    * Refactor subscription retrieval
    
    * Fix inconsistent subscription handling
    
    * Add test
    
    * Give option for resubscribing to expire stripe plan
    
    * make filter more concise
    
    * Make UI consistent
    
    * Remove unnecessary attribute
    
    * fix minor spacing
    
    * Assert for expired marketplace trial
    
    * fix tracked computed properties
    
    * track all array items

--- app/components/billing/summary.js
+++ app/components/billing/summary.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { reads, or, not, and } from '@ember/object/computed';
+import { reads, or, not, and, bool } from '@ember/object/computed';
 
 export default Component.extend({
 
@@ -7,16 +7,23 @@ export default Component.extend({
   account: null,
 
   isEditPlanLoading: reads('subscription.changePlan.isLoading'),
-
-  isCanceled: reads('subscription.isCanceled'),
   isIncomplete: reads('subscription.isIncomplete'),
-  isExpired: reads('subscription.isExpired'),
-  isPending: reads('subscription.isPending'),
+  isComplete: not('isIncomplete'),
+  authenticationNotRequired: not('subscription.clientSecret'),
+  isPending: and('subscription.isPending', 'authenticationNotRequired'),
   isNotCanceled: not('isCanceled'),
   isNotPending: not('isPending'),
   hasNotExpired: not('isExpired'),
-  isComplete: not('isIncomplete'),
+  isCanceled: reads('subscription.isCanceled'),
+  isSubscribed: reads('subscription.isSubscribed'),
+  isExpired: or('subscription.isExpired', 'subscription.manualSubscriptionExpired'),
   canceledOrExpired: or('isExpired', 'isCanceled'),
   isCompleteAndNotExpired: and('hasNotExpired', 'isComplete'),
   showBillingInfo: and('subscription.isStripe', 'isCompleteAndNotExpired'),
+  trial: reads('account.trial'),
+  isGithubSubscription: reads('subscription.isGithub'),
+  isGithubTrial: and('isGithubSubscription', 'trial.hasActiveTrial'),
+  isNotGithubTrial: not('isGithubTrial'),
+  expiredStripeSubscription: reads('account.expiredStripeSubscription'),
+  hasExpiredStripeSubscription: bool('expiredStripeSubscription'),
 });
