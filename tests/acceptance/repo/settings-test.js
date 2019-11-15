5fbd61e6ec33a8247ff798e90f68f7a9d52d1f6b
Sophie Orta <sophie@travis-ci.org>
2019-11-15 12:36:58 -0500

    Refactor time-ago-in-words to not use jQuery

--- tests/acceptance/repo/settings-test.js
+++ tests/acceptance/repo/settings-test.js
@@ -123,7 +123,7 @@ module('Acceptance | repo settings', function (hooks) {
       assert.equal(cron.lastRun.text, 'Ran less than a minute ago');
       assert.equal(cron.lastRun.title, this.dailyCron.last_run.toISOString());
 
-      assert.equal(cron.nextRun.text, 'Scheduled in about 24 hours from now');
+      assert.equal(cron.nextRun.text, 'Scheduled in a day from now');
       assert.equal(cron.nextRun.title, this.dailyCron.next_run.toISOString());
 
       assert.ok(cron.dontRunIfRecentBuildExists.text.indexOf('Always run') === 0, 'expected cron to run even if there is a build in the last 24h');
