044c1e052f18233790e5bce188e00b5cb0b74a2a
Sophie Orta <sophie@travis-ci.org>
2019-11-21 17:43:56 -0500

    Remove jquery from repo-list-item component

--- app/components/repos-list-item.js
+++ app/components/repos-list-item.js
@@ -1,23 +1,23 @@
-import $ from 'jquery';
 import Component from '@ember/component';
 import Polling from 'travis/mixins/polling';
 import colorForState from 'travis/utils/color-for-state';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend(Polling, {
   tagName: 'li',
   pollModels: 'repo',
   classNames: ['repo'],
 
+  scroller: service(),
+
   color: computed('repo.currentBuild.state', function () {
     return colorForState(this.get('repo.currentBuild.state'));
   }),
 
   scrollTop() {
-    if (window.scrollY > 0) {
-      return $('html, body').animate({
-        scrollTop: 0
-      }, 200);
+    if (window && window.pageYOffset) {
+      this.scroller.scrollTo(0, 200);
     }
   }
 });
