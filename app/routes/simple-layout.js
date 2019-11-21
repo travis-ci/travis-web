0392c2a405d9aae4211b3b08eac970fd89b691fe
Sophie Orta <sophie@travis-ci.org>
2019-11-21 14:09:17 -0500

    Remove jquery used to set body class

--- app/routes/simple-layout.js
+++ app/routes/simple-layout.js
@@ -1,9 +1,4 @@
-import $ from 'jquery';
 import TravisRoute from 'travis/routes/basic';
 
 export default TravisRoute.extend({
-  setupController: function () {
-    $('body').attr('id', 'simple');
-    return this._super(...arguments);
-  }
 });
