8cd7ac07309bb8a9bbb1ecdc1aba88b13cb5599e
Sophie Orta <sophie@travis-ci.org>
2019-11-22 12:36:00 -0500

    Reset test-helper to original state

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,9 +2,6 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import QUnit from 'qunit';
-
-QUnit.config.testTimeout = 6000;
 
 setApplication(Application.create(config.APP));
 
