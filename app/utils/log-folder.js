12bf720d78bb22d50e7155fdcb36f869bfbc416b
Sophie Orta <sophie@travis-ci.org>
2019-11-25 10:55:41 -0500

    Split from old PR

--- app/utils/log-folder.js
+++ app/utils/log-folder.js
@@ -1,27 +1,29 @@
-import $ from 'jquery';
-
 export default (function () {
   function LogFolder(element) {
     this.element = element;
-    let handlerSelector = '.fold .log-line:first-of-type, .fold .fold-name';
+    // let handlerSelector = '.fold .log-line:first-of-type, .fold .fold-name';
+    // const handledElements = this.element.querySelectorAll(handlerSelector);
     if (this.element) {
-      this.element
-        .off('click', handlerSelector) // remove any previous click handlers
-        .on('click', handlerSelector, (function (_this) {
-          return function (event) {
-            let folder = _this.getFolderFromLine($(event.target));
-            _this.toggle(folder);
-            event.preventDefault();
-            return false;
-          };
-        })(this));
+      const _this = this;
+      const handleClick = (event) => {
+        let folder = _this.getFolderFromLine(event.target);
+        _this.toggle(folder);
+        event.preventDefault();
+        return false;
+      };
+      const replaceEventListener = (el) => {
+        el.removeEventListener('click', handleClick);
+        el.addEventListener('click', handleClick);
+      };
+      replaceEventListener(this.element);
+      // handledElements.forEach(replaceEventListener);
     }
   }
 
   LogFolder.prototype.fold = function (line) {
     let folder;
     folder = this.getFolderFromLine(line);
-    if (folder.hasClass('open')) {
+    if (folder.classList.contains('open')) {
       return this.toggle(folder);
     }
   };
@@ -29,17 +31,24 @@ export default (function () {
   LogFolder.prototype.unfold = function (line) {
     let folder;
     folder = this.getFolderFromLine(line);
-    if (!folder.hasClass('open')) {
+    if (!folder.classList.contains('open')) {
       return this.toggle(folder);
     }
   };
 
   LogFolder.prototype.toggle = function (folder) {
-    return folder.toggleClass('open');
+    return folder.classList && folder.classList.toggle('open');
   };
 
   LogFolder.prototype.getFolderFromLine = function (line) {
-    return line.parents('.fold').first();
+    let currentLine = line;
+    while (currentLine.parentNode) {
+      currentLine = currentLine.parentNode;
+      if (currentLine.classList && currentLine.classList.contains('fold')) {
+        break;
+      }
+    }
+    return currentLine;
   };
 
   return LogFolder;
--- app/utils/tailing.js
+++ app/utils/tailing.js
@@ -1,13 +1,13 @@
-import $ from 'jquery';
 import { throttle, later } from '@ember/runloop';
 
 export default (function () {
   function Tailing(window1, tailSelector, logSelector) {
     this.window = window1;
+    this.document = this.window.document;
     this.tailSelector = tailSelector;
     this.logSelector = logSelector;
-    this.position = this.window.scrollTop();
-    this.window.scroll(() => {
+    this.position = this._scrollPosTop();
+    this.document.addEventListener('scroll', () => {
       throttle(this, this.onScroll, [], 200, false);
     });
     return this;
@@ -18,55 +18,66 @@ export default (function () {
   };
 
   Tailing.prototype.tail = function () {
-    return $(this.tailSelector);
+    return this.document.querySelector(this.tailSelector);
   };
 
   Tailing.prototype.log = function () {
-    return $(this.logSelector);
+    return this.document.querySelector(this.logSelector);
   };
 
   Tailing.prototype.run = function () {
     this.autoScroll();
     this.positionButton();
-    if (this.active()) {
+    if (this.isActive()) {
       return later(this.run.bind(this), this.options.timeout);
     }
   };
 
   Tailing.prototype.toggle = function () {
-    if (this.active()) {
+    if (this.isActive()) {
       return this.stop();
     } else {
       return this.start();
     }
   };
 
-  Tailing.prototype.active = function () {
-    return this.tail().hasClass('active');
-  };
-
   Tailing.prototype.start = function () {
-    this.tail().addClass('active');
+    const tail = this.tail();
+    if (tail) {
+      tail.classList.add('active');
+    }
     return this.run();
   };
 
   Tailing.prototype.isActive = function () {
-    return this.tail().hasClass('active');
+    const tail = this.tail();
+    return tail && tail.classList.contains('active');
   };
 
   Tailing.prototype.stop = function () {
-    return this.tail().removeClass('active');
+    const tail = this.tail();
+    return tail && tail.classList.remove('active');
   };
 
   Tailing.prototype.autoScroll = function () {
-    let logBottom, winBottom;
-    if (!this.active()) {
+    if (!this.isActive()) {
       return false;
     }
-    logBottom = this.log().offset().top + this.log().outerHeight() + 40;
-    winBottom = this.window.scrollTop() + this.window.height();
-    if (logBottom - winBottom > 0) {
-      this.window.scrollTop(logBottom - this.window.height());
+    const log = this.log();
+    const logOffset = this._offsetTop(log);
+    const logHeight = log.offsetHeight;
+    const logBottom = logOffset + logHeight + 40;
+
+    const scrollPosTop = this._scrollPosTop();
+    const windowHeight = this.window.innerHeight;
+    const winBottom = scrollPosTop + windowHeight;
+
+    const logWinDifference = logBottom - winBottom;
+
+    if (logWinDifference > 0) {
+      const newYpos = logBottom - windowHeight;
+      const newXpos = this._scrollPosLeft();
+      this.window.scrollTo(newXpos, newYpos);
       return true;
     } else {
       return false;
@@ -76,7 +87,7 @@ export default (function () {
   Tailing.prototype.onScroll = function () {
     let position;
     this.positionButton();
-    position = this.window.scrollTop();
+    position = this._scrollPosTop();
     if (position < this.position) {
       this.stop();
     }
@@ -84,25 +95,42 @@ export default (function () {
   };
 
   Tailing.prototype.positionButton = function () {
-    let max, offset, tail;
-    tail = $('#tail');
-    if (tail.length === 0 || tail.css('position').search(/sticky/i) >= 0) {
+    let max, offset;
+    const tail = this.tail();
+    const log = this.log();
+    if (!tail || getComputedStyle(tail)['position'].search(/sticky/i) >= 0) {
       return;
     }
-    offset = $(window).scrollTop() - $('#log').offset().top;
-    max = $('#log').height() - $('#tail').height() + 5;
+
+    offset = this._scrollPosTop() - this._offsetTop(log);
+    max = log.clientHeight - tail.clientHeight + 5;
     if (offset > max) {
       offset = max;
     }
+
+    let newOffset = 0;
     if (offset > 0) {
-      return tail.css({
-        top: offset - 2
-      });
-    } else {
-      return tail.css({
-        top: 0
-      });
+      newOffset = offset - 2;
     }
+    tail.style.top = newOffset;
+
+    return newOffset;
+  };
+
+  Tailing.prototype._offsetTop = function (el) {
+    const { top } = el.getBoundingClientRect();
+    const scrollTop = this._scrollPosTop();
+    return top + scrollTop;
+  };
+
+  Tailing.prototype._scrollPosTop = function () {
+    const { document, window } = this;
+    return window.scrollY || window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || document.getElementsByTagName('html')[0].scrollTop;
+  };
+
+  Tailing.prototype._scrollPosLeft = function () {
+    const { document, window } = this;
+    return window.scrollX || window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || document.getElementsByTagName('html')[0].scrollTop;
   };
 
   return Tailing;
