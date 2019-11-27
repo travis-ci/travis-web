386ea39cb6adfcc34783464b76533a06a2c3c95c
Piotr Milcarz <piotrm@travis-ci.org>
2019-11-27 13:09:55 +0100

    Missing file

---
+++ tests/unit/services/gtm-test.js
@@ -0,0 +1,30 @@
+import Service from '@ember/service';
+import { visit } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'travis/tests/helpers/setup-application-test';
+import signInUser from 'travis/tests/helpers/sign-in-user';
+import { stubService } from 'travis/tests/helpers/stub-service';
+
+module('Acceptance | auth/call gtm', function (hooks) {
+  setupApplicationTest(hooks);
+
+  let callCounter = 0;
+
+  const gtmServiceStub = Service.extend({
+    callCounter: 0,
+    trackEvent: (params) => {
+      callCounter++;
+    }
+  });
+
+  hooks.beforeEach(function(assert) {
+    this.currentUser = server.create('user');
+    signInUser(this.currentUser);
+    stubService('metrics', gtmServiceStub);
+  });
+
+  test('gtm call is performed once user signs up', async function (assert) {
+    await visit('/');
+    assert.ok(callCounter === 1);
+  });
+});
