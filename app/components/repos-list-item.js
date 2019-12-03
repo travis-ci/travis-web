64f5b6fe432d3205e6489c8828f19f19593b8afd
Sophie Orta <sophie@travis-ci.org>
2019-12-03 10:17:07 -0500

    Match other previous adjustments

--- app/components/repos-list-item.js
+++ app/components/repos-list-item.js
@@ -2,14 +2,11 @@ import Component from '@ember/component';
 import Polling from 'travis/mixins/polling';
 import colorForState from 'travis/utils/color-for-state';
 import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 export default Component.extend(Polling, {
   tagName: 'li',
   pollModels: 'repo',
   classNames: ['repo'],
-
-  scroller: service(),
 
   color: computed('repo.currentBuild.state', function () {
     return colorForState(this.get('repo.currentBuild.state'));
