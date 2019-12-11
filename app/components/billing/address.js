c1e0c51131272ee67968bb4203e196a441dcb450
Patrick <emmanuel@travis-ci.org>
2019-12-11 11:11:14 +0100

    Handle update vat error

--- app/components/billing/address.js
+++ app/components/billing/address.js
@@ -1,17 +1,30 @@
 import Component from '@ember/component';
 import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 import { countries } from 'travis/utils/countries';
 
 export default Component.extend({
   countries,
+  flashes: service(),
   classNames: ['address'],
   openEditContactForm: false,
   openEditBillingForm: false,
 
   editContact: task(function* () {
-    yield this.subscription.save();
-    this.closeEditForms();
+    try {
+      yield this.subscription.save();
+      this.closeEditForms();
+    } catch (e) {
+      this.handleErrors(e);
+    }
   }).drop(),
+
+  handleErrors(e) {
+    if (e.errors.length) {
+      const message = e.errors[0].detail;
+      this.flashes.error(message);
+    }
+  },
 
   closeEditForms() {
     this.set('openEditContactForm', false);
