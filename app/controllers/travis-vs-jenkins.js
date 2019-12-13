eecd21f6f9a8f92a3e748d6b81c89acbf4fbc8cf
Sophie Orta <sophie@travis-ci.org>
2019-12-13 11:56:29 -0500

    Add page and header

---
+++ app/controllers/travis-vs-jenkins.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import { later } from '@ember/runloop';
+
+export default Controller.extend({
+  scrollToContact: false,
+  toggleContactScroll() {
+    this.set('scrollToContact', true);
+    later(() => this.set('scrollToContact', false), 500);
+  },
+});
