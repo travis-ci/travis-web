c7828fa83bbe919e3b79d8a791bfa552de1acbae
Piotr Milcarz <piotrm@travis-ci.org>
2019-12-13 13:12:34 +0100

    Running jobs orrdered by job.number

--- app/components/repository-sidebar.js
+++ app/components/repository-sidebar.js
@@ -82,7 +82,13 @@ export default Component.extend({
       let showRunningJobs = this.get('features.showRunningJobsInSidebar');
       let runningJobs = this.get('jobState.runningJobs');
       if (!showRunningJobs) { return []; }
-      return runningJobs;
+      return runningJobs.sort((a, b) => {
+        const aNumber = parseFloat(a.number);
+        const bNumber = parseFloat(b.number);
+        if (aNumber > bNumber) return 1;
+        if (aNumber < bNumber) return -1;
+        return 0;
+      });
     }
   ),
 
