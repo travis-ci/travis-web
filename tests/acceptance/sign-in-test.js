c01413b40e8abbc86adf106e85b64a4e92b38213
Sophie Orta <sophie@travis-ci.org>
2020-01-07 13:44:09 -0500

    Prepare to remove auto signin test, since behavior is changing

--- tests/acceptance/sign-in-test.js
+++ tests/acceptance/sign-in-test.js
@@ -1,38 +1,38 @@
 import { currentURL, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'travis/tests/helpers/setup-application-test';
-import Service from '@ember/service';
+// import Service from '@ember/service';
 import signInUser from 'travis/tests/helpers/sign-in-user';
-import { stubService } from 'travis/tests/helpers/stub-service';
+// import { stubService } from 'travis/tests/helpers/stub-service';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Acceptance | sign in', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('visiting /signin starts auth flow if unauthenticated', async function (assert) {
-    assert.expect(2);
+  // test('visiting /signin starts auth flow if unauthenticated', async function (assert) {
+  //   assert.expect(2);
 
-    // avoid actually contacting GitHub
-    const mockAuthService = Service.extend({
-      signedIn: false,
-      signIn() {
-        assert.ok(true);
-      },
-      afterSignOut() {
-        return undefined;
-      },
-      autoSignIn() {
-        return undefined;
-      },
-    });
+  //   // avoid actually contacting GitHub
+  //   const mockAuthService = Service.extend({
+  //     signedIn: false,
+  //     signIn() {
+  //       assert.ok(true);
+  //     },
+  //     afterSignOut() {
+  //       return undefined;
+  //     },
+  //     autoSignIn() {
+  //       return undefined;
+  //     },
+  //   });
 
-    stubService('auth', mockAuthService);
+  //   stubService('auth', mockAuthService);
 
-    await visit('/signin');
+  //   await visit('/signin');
 
-    assert.equal(currentURL(), '/signin');
-  });
+  //   assert.equal(currentURL(), '/signin');
+  // });
 
   test('visiting signin redirects to index if authenticated', async function (assert) {
     const currentUser = this.server.create('user', 'withRepository');
