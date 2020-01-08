8ccaa6fe1f807c998c8efe59e58e274c0414934a
Patrick <emmanuel@travis-ci.org>
2020-01-08 12:43:52 +0100

    show vat error messages to user

--- app/components/billing/address.js
+++ app/components/billing/address.js
@@ -16,9 +16,13 @@ export default Component.extend({
       yield this.subscription.save();
       this.closeEditForms();
       this.flashes.clear();
-    } catch (e) {
-      this.flashes.error('There was an error updating your contact. Please verify you provided a valid VAT number');
-      this.raven.logException(e);
+    } catch (error) {
+      let errorMessage = 'There was an error updating your contact. Please verify you provided a valid VAT number';
+      if (error && error.errors && error.errors.length > 0) {
+        errorMessage = error.errors[0].detail;
+      }
+      this.flashes.error(errorMessage);
+      this.raven.logException(error);
     }
   }).drop(),
 
