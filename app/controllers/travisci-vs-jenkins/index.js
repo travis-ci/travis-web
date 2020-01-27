2d14474278dbf4671efa8fc9576a83282cef7179
Sophie Orta <sophie@travis-ci.org>
2020-01-27 11:35:13 -0500

    Reintroduce case study url

--- app/controllers/travisci-vs-jenkins/index.js
+++ app/controllers/travisci-vs-jenkins/index.js
@@ -1,8 +1,13 @@
 import Controller from '@ember/controller';
 import { later } from '@ember/runloop';
+import config from 'travis/config/environment';
+
+const { blog: blogUrl } = config.urls;
 
 export default Controller.extend({
   referralSourceName: 'travisci-vs-jenkins',
+
+  caseStudyUrl: `${blogUrl}/2019-06-5-case-study-ibm-cloud-kubernetes-service`,
 
   scrollToContact: false,
   toggleContactScroll() {
