7abe2194c8c9ac9a04b66991de3531d73fff2cda
sorta <sorta@users.noreply.github.com>
2020-01-27 09:57:09 -0500

    [TVJ] add contact section (#2328)
    * Add contact section and thank you page
    
    * Update transition to correct thanks page on success.

--- app/controllers/travisci-vs-jenkins/index.js
+++ app/controllers/travisci-vs-jenkins/index.js
@@ -2,9 +2,15 @@ import Controller from '@ember/controller';
 import { later } from '@ember/runloop';
 
 export default Controller.extend({
+  referralSourceName: 'travisci-vs-jenkins',
+
   scrollToContact: false,
   toggleContactScroll() {
     this.set('scrollToContact', true);
     later(() => this.set('scrollToContact', false), 500);
+  },
+
+  contactSuccess() {
+    this.transitionToRoute('travisci-vs-jenkins.thank-you');
   },
 });
