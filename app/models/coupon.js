4c315df9076f4358f095b8c41cd1621381c8c045
Patrick <emmanuel@travis-ci.org>
2020-01-30 16:20:53 +0100

    remove unwanted code

--- app/models/coupon.js
+++ app/models/coupon.js
@@ -1,9 +1,8 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default Model.extend({
   name: attr('string'),
   percentOff: attr('number'),
   amountOff: attr('number'),
   valid: attr('boolean'),
-  subscription: belongsTo('subscription')
 });
