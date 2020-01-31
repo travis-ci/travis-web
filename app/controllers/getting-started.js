fba5281d85284435d1325f5300a0958c6af361a3
Sophie Orta <sophie@travis-ci.org>
2020-01-31 17:23:57 -0500

    Move sidebar layout to component. Add basic logic for showing different content by provider

--- app/controllers/getting-started.js
+++ app/controllers/getting-started.js
@@ -1,7 +1,14 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { equal } from '@ember/object/computed';
 
 export default Controller.extend({
   auth: service(),
   features: service(),
+
+  provider: 'bitbucket', // Eventually get this from a service
+
+  showGithub: equal('provider', 'github'),
+  showBitbucket: equal('provider', 'bitbucket'),
+  showAssembla: equal('provider', 'assembla'),
 });
