7da56fa15197a272505ce8de50b069041556a903
Sophie Orta <sophie@travis-ci.org>
2020-02-03 13:57:25 -0500

    Add bitbucket step 1

---
+++ app/components/getting-started-step.js
@@ -0,0 +1,33 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+const COLORS = {
+  bitbucket: {
+    title: 'blue'
+  }
+};
+
+export default Component.extend({
+  tagName: '',
+
+  number: null,
+  provider: null,
+  providerImg: false,
+  title: '',
+
+  dir: computed('number', function () {
+    const { number } = this;
+    return (number && number % 2 === 1) ? 'row' : 'row-reverse';
+  }),
+
+  imageName: computed('number', 'provider', 'providerImg', function () {
+    const { number, provider, providerImg } = this;
+    const affix = providerImg ? `-${provider}` : '';
+    return `getting-started-step-${number}${affix}`;
+  }),
+
+  colors: computed('provider', function () {
+    const { provider } = this;
+    return COLORS[provider];
+  }),
+});
