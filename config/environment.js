58092e7eb9f09eaefb246836c6fe919d3a909227
Sven Fuchs <me@svenfuchs.com>
2020-02-03 19:15:03 +0100

    link merge modes to docs

--- config/environment.js
+++ config/environment.js
@@ -70,6 +70,7 @@ module.exports = function (environment) {
       status: 'https://www.traviscistatus.com/',
       support: 'mailto:support@travis-ci.com',
       terms: 'https://docs.travis-ci.com/legal/terms-of-service/',
+      triggerBuildMergeModes: 'https://docs.travis-ci.com/user/triggering-builds/#merge-modes',
       tutorial: 'https://docs.travis-ci.com/user/tutorial/',
       twitter: 'https://twitter.com/travisci',
     },
