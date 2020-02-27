19863f26e5cd792a638f9f8d7afc0ca50c72f79c
the-bionic <inem.patrick@gmail.com>
2020-02-27 12:42:30 +0100

    extract options

--- app/adapters/build-config.js
+++ app/adapters/build-config.js
@@ -5,13 +5,18 @@ export default V3Adapter.extend({
   ajax: service(),
   host: 'https://yml-staging.travis-ci.org',
 
-  queryRecord(store, type, query) {
-    return this.ajax.request('/configs', 'POST', {
+  getOptions(query) {
+    return {
       host: this.host,
       data: query.data,
       headers: {
         Authorization: 'Basic eDpqZm5DcWJKbGJ2eFpsWDQwdUUwREtn'
-      }
-    });
+      },
+    },
+  },
+
+  queryRecord(store, type, query) {
+    const options = this.getOptions(query);
+    return this.ajax.request('/configs', 'POST', options);
   }
 });
