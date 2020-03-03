8e95de71ccbf218faf6b5bfe579fb20be41e2d13
Piotr Milcarz <piotrm@travis-ci.org>
2020-03-03 12:04:16 +0100

    fix

--- app/components/repository-layout.js
+++ app/components/repository-layout.js
@@ -17,9 +17,9 @@ export default Component.extend({
     return this.get('statusImages').imageUrl(repo, branchName);
   }),
 
-  repoUrl: computed('repo.{vcsType,slug}', function () {
-    const owner = this.get('item.repo.ownerName');
-    const repo = this.get('item.repo.vcsName');
+  repoUrl: computed('repo.{ownerName,vcsName,vcsType}', function () {
+    const owner = this.get('repo.ownerName');
+    const repo = this.get('repo.vcsName');
     const vcsType = this.get('repo.vcsType');
 
     return this.externalLinks.repoUrl(vcsType, { owner, repo });
