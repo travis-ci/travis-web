bc9bc643c2217b12a80923253e10d4b43a0fc62f
Piotr Milcarz <piotrm@travis-ci.org>
2020-03-09 13:35:32 +0100

    fix

--- app/components/owner-repo-tile.js
+++ app/components/owner-repo-tile.js
@@ -6,6 +6,11 @@ export default Component.extend({
   classNames: ['owner-tile', 'rows', 'rows--owner', 'fade-out'],
   classNameBindings: ['repo.defaultBranch.lastBuild.state'],
 
+  ownerName: computed('repo.slug', function () {
+    let slug = this.get('repo.slug');
+    return slug.split(/\//)[0];
+  }),
+
   repoName: computed('repo.slug', function () {
     let slug = this.get('repo.slug');
     return slug.split(/\//)[1];
