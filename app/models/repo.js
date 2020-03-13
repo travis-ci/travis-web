18825500422543177357c8c3b2874116017fc5b5
Piotr Milcarz <piotrm@travis-ci.org>
2020-03-13 13:20:36 +0100

    Job owner checking refactored due to spaces in repo slug

--- app/models/repo.js
+++ app/models/repo.js
@@ -81,6 +81,13 @@ const Repo = VcsEntity.extend({
     }
   }),
 
+  // slug built from normalized (by provider) owner and repo name
+  vcsSlug: computed('ownerName', 'vcsName', function () {
+    const ownerName = this.get('ownerName');
+    const vcsName = this.get('vcsName');
+    return `${ownerName}/${vcsName}`;
+  }),
+
   formattedSlug: computed('owner.login', 'name', function () {
     let login = this.get('owner.login');
     let name = this.name;
