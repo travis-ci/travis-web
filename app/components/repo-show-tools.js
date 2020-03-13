d89c149c49076153fdbf76d571fcec518d3b1fde
Sven Fuchs <me@svenfuchs.com>
2020-03-13 19:37:32 +0100

    extract mixing canTriggerBuild

--- app/components/repo-show-tools.js
+++ app/components/repo-show-tools.js
@@ -3,8 +3,9 @@ import config from 'travis/config/environment';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import CanTriggerBuild from 'travis/mixins/components/can-trigger-build';
 
-export default Component.extend({
+export default Component.extend(CanTriggerBuild, {
   auth: service(),
   permissions: service(),
   features: service(),
@@ -36,23 +37,6 @@ export default Component.extend({
     let repo = this.repo;
     return this.permissions.hasPermission(repo);
   }),
-
-  displayTriggerBuildLink: computed(
-    'repo.migrationStatus',
-    'repo.permissions.create_request',
-    'features.{enterpriseVersion,proVersion}',
-    function () {
-      let migrationStatus = this.get('repo.migrationStatus');
-      let canTriggerBuild = this.get('repo.permissions.create_request');
-      let enterprise = this.get('features.enterpriseVersion');
-      let pro = this.get('features.proVersion');
-
-      if (enterprise || pro) {
-        return canTriggerBuild;
-      }
-      return canTriggerBuild && migrationStatus !== 'migrated';
-    }
-  ),
 
   actions: {
     triggerBuildModal() {
