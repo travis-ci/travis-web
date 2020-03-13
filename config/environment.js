1f4597f80e0716c6157c0e18459d0c429f9d8063
Sven Fuchs <me@svenfuchs.com>
2020-03-13 19:18:03 +0100

    add feature flag show-new-config-view

--- config/environment.js
+++ config/environment.js
@@ -191,6 +191,7 @@ module.exports = function (environment) {
     'github-apps': false,
     'enable-assembla-login': false,
     'enable-bitbucket-login': false,
+    'show-new-config-view': true,
   };
 
   if (TRAVIS_PRO) {
