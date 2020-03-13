3fedf28d53bfee97d4195ca38877ba88d2f41f2d
Sven Fuchs <me@svenfuchs.com>
2020-03-13 19:37:32 +0100

    extract mixing canTriggerBuild

--- app/components/repo-show-tools.js
+++ app/components/repo-show-tools.js
@@ -2,9 +2,16 @@ import Component from '@ember/component';
 import config from 'travis/config/environment';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+<<<<<<< HEAD
 import { alias } from '@ember/object/computed';
+||||||| merged common ancestors
+import { alias, bool } from '@ember/object/computed';
+=======
+import { alias, bool } from '@ember/object/computed';
+import CanTriggerBuild from 'travis/mixins/components/can-trigger-build';
+>>>>>>> extract mixing canTriggerBuild
 
-export default Component.extend({
+export default Component.extend(CanTriggerBuild, {
   auth: service(),
   permissions: service(),
   features: service(),
@@ -36,23 +43,6 @@ export default Component.extend({
     let repo = this.repo;
     return this.permissions.hasPermission(repo);
   }),
-
-  displayTriggerBuildLink: computed(
-    'repo.migrationStatus',
-    'repo.permissions.create_request',
-    'features.{enterpriseVersion,proVersion}',
-    function () {
-      let migrationStatus = this.get('repo.migrationStatus');
-      let canTriggerBuild = this.get('repo.permissions.create_request');
-      let enterprise = this.get('features.enterpriseVersion');
-      let pro = this.get('features.proVersion');
-
-      if (enterprise || pro) {
-        return canTriggerBuild;
-      }
-      return canTriggerBuild && migrationStatus !== 'migrated';
-    }
-  ),
 
   actions: {
     triggerBuildModal() {
--- app/components/request/configs.js
+++ app/components/request/configs.js
@@ -1,10 +1,13 @@
 import Component from '@ember/component';
+import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import CanTriggerBuild from 'travis/mixins/components/can-trigger-build';
 
-export default Component.extend({
+export default Component.extend(CanTriggerBuild, {
   tagName: '',
 
   features: service(),
 
   showNewConfigView: reads('features.showNewConfigView'),
+
 });
