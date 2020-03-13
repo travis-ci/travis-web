1ad3fc3b71f8d5c4cc640995e0444160b36d8537
Sven Fuchs <me@svenfuchs.com>
2020-03-13 19:37:32 +0100

    extract mixing canTriggerBuild

---
+++ app/mixins/components/can-trigger-build.js
@@ -0,0 +1,9 @@
+import Mixin from '@ember/object/mixin';
+import { and, equal, not, reads } from '@ember/object/computed';
+
+export default Mixin.create({
+  canCreateRequest: reads('repo.permissions.create_request'),
+  repoMigrating: equal('repo.MigrationStatus', 'migrated'),
+  repoNotMigrating: not('repoMigrating'),
+  displayTriggerBuild: and('canCreateRequest', 'repoNotMigrating'),
+});
