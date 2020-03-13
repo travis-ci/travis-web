d028cfd5096b7e0973538bfcf9c5c974b067132e
Sven Fuchs <me@svenfuchs.com>
2020-03-13 23:18:53 +0100

    add config customize form

--- app/components/request/configs.js
+++ app/components/request/configs.js
@@ -1,8 +1,15 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { equal, or, reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import CanTriggerBuild from 'travis/mixins/components/can-trigger-build';
 import TriggerBuild from 'travis/mixins/components/trigger-build';
+
+function truncated(property, chars) {
+  return computed(property, function () {
+    return this.get(property).slice(0, 7);
+  });
+}
 
 export default Component.extend(CanTriggerBuild, TriggerBuild, {
   tagName: 'div',
@@ -14,6 +21,9 @@ export default Component.extend(CanTriggerBuild, TriggerBuild, {
 
   status: 'closed',
   closed: equal('status', 'closed'),
+  customizing: equal('status', 'customize'),
+  previewing: equal('status', 'preview'),
+  replacing: equal('mergeMode', 'replace'),
   showNewConfigView: reads('features.showNewConfigView'),
 
   repo: reads('request.repo'),
@@ -21,11 +31,13 @@ export default Component.extend(CanTriggerBuild, TriggerBuild, {
   messages: reads('requestConfig.messages'),
   loading: reads('requestConfig.loading'),
 
-  sha: reads('originalSha'),
+  refType: 'sha',
+  sha: truncated('originalSha', 10),
   branch: reads('originalBranch'),
   message: reads('request.commit.message'),
   config: reads('request.apiConfig.config'),
   mergeMode: reads('originalMergeMode'), // TODO store and serve merge mode for api request configs
+  defaultMergeMode: 'deep_merge_append',
 
   originalSha: or('requestSha', 'repoDefaultBranchLastCommitSha'),
   originalBranch: or('requestBranch', 'repoDefaultBranch'),
@@ -41,6 +53,15 @@ export default Component.extend(CanTriggerBuild, TriggerBuild, {
       this.set('status', 'open');
     } else if (['open', 'customize', 'preview'].includes(this.status)) {
       this.submit();
+    }
+  },
+
+  onCustomize() {
+    if (this.status == 'customize') {
+      this.set('status', 'open');
+      this.reset();
+    } else {
+      this.set('status', 'customize');
     }
   },
 
@@ -65,4 +86,19 @@ export default Component.extend(CanTriggerBuild, TriggerBuild, {
     this.set('processing', true);
     this.submitBuildRequest.perform();
   },
+
+  actions: {
+    formFieldChanged(key, value) {
+      this.set('customized', true);
+      this.set(key, value);
+      if (key === 'config') {
+        this.loadConfigs(true);
+      } else {
+        this.loadConfigs();
+      }
+    },
+    submit() {
+      this.submit();
+    }
+  }
 });
