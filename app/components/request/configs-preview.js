709a7916ef8d75175f2f46038605a539a535031c
Sven Fuchs <me@svenfuchs.com>
2020-03-14 16:07:14 +0100

    fix preview, refactor

--- app/components/request/configs-preview.js
+++ app/components/request/configs-preview.js
@@ -1,19 +1,15 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { reads } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
+import { reads, mapBy } from '@ember/object/computed';
 
 export default Component.extend({
   tagName: 'div',
-  classNames: ['request-configs-preview', 'status'],
+  classNames: ['request-configs-preview'],
 
-  result: service('request-config'),
-
-  repo: reads('request.repo'),
-  requestConfig: reads('result.requestConfig.config'),
-  jobConfigs: reads('result.jobConfigs'),
-  messages: reads('result.messages'),
-  loading: reads('result.loading'),
+  requestConfig: reads('preview.requestConfig.config'),
+  jobConfigs: mapBy('preview.jobConfigs', 'config'),
+  messages: reads('preview.messages'),
+  loading: reads('preview.loading'),
 
   formattedRequestConfig: computed('requestConfig', function () {
     return JSON.stringify(this.requestConfig, null, 2);
@@ -22,24 +18,4 @@ export default Component.extend({
   formattedJobConfigs: computed('jobConfigs', function () {
     return JSON.stringify(this.jobConfigs, null, 2);
   }),
-
-  didInsertElement() {
-    let id = this.get('repo.id');
-    let data = this.data();
-    this.result.loadConfigs.perform(id, data);
-  },
-
-  data() {
-    return {
-      repo: {
-        slug: this.repo.get('slug'),
-        private: this.repo.get('private'),
-        default_branch: this.repo.get('defaultBranch.name'),
-      },
-      ref: this.ref,
-      mode: this.mergeMode,
-      config: this.config,
-      type: 'api'
-    };
-  },
 });
