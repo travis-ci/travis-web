c7daf665241f38adbe693e60fa8dfd94bd7accca
Sven Fuchs <me@svenfuchs.com>
2020-03-14 01:11:31 +0100

    add request configs preview

--- app/components/request/configs-trigger-build-notice.js
+++ app/components/request/configs-trigger-build-notice.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 import { pluralize } from 'ember-inflector';
 
 export default Component.extend({
@@ -7,9 +8,14 @@ export default Component.extend({
   classNames: ['request-configs-trigger-build-notice'],
 
   configs: 0,
-  customize: false,
+  status: undefined,
+  customizing: equal('status', 'customize'),
+  previewing: equal('status', 'preview'),
 
   message: computed('customize', 'configs', function () {
+    if (this.previewing) {
+      return 'The triggered build will have the following build config and job matrix.';
+    }
     let configs = pluralize(this.configs + 1, 'config', { withoutCount: true });
     if (this.customize) configs = `details and ${configs}`;
     return `Trigger a build request with the following build ${configs}`;
