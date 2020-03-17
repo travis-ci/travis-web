6327dc5eee92317eacf48b4c4fb5d96ac6d8a879
Sven Fuchs <me@svenfuchs.com>
2020-03-17 16:26:45 +0100

    use buildURL

--- app/adapters/request-config.js
+++ app/adapters/request-config.js
@@ -1,11 +1,8 @@
 import V3Adapter from 'travis/adapters/v3';
-import config from 'travis/config/environment';
-
-const { apiEndpoint } = config;
 
 export default V3Adapter.extend({
   queryRecord(store, type, { id, data }) {
-    const url = `${apiEndpoint}/repo/${id}/request/config`;
+    const url = `${this.buildURL('repo', id)}/request/config`;
     return this.ajax(url, 'POST', { data });
   },
 });
