43bd7173b09d0bf200a43231cecbbb0c11c57d92
Artur Smirnov <reg@artursmirnov.com>
2020-04-01 17:03:04 +0300

    As save utm params to server (#2426)
    * Make utm parameters persist to localStorage and then save to server
    
    * Clean up
    
    * Make utm parameters reset after GA call
    
    * Fix test
    
    * Implement feedback
    
    * Move pageview tracking to controller for consistency
    
    * Save UTM paramaters only for new users

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import QueryParams from 'ember-parachute';
+import { Promise } from 'rsvp';
+import config from 'travis/config/environment';
 import { UTM_FIELDS, UTM_FIELD_NAMES } from 'travis/services/utm';
-import { next } from '@ember/runloop';
+import { later } from '@ember/runloop';
+
+const { utmParametersResetDelay } = config.timing;
 
 export const UTM_QUERY_PARAMS = new QueryParams({
   [UTM_FIELDS.CAMPAIGN]: { defaultValue: null, replace: true, refresh: true },
@@ -15,17 +19,45 @@ export const UTM_QUERY_PARAMS = new QueryParams({
 export default Controller.extend(UTM_QUERY_PARAMS.Mixin, {
   features: service(),
   utm: service(),
+  router: service(),
+  metrics: service(),
 
   setup({ queryParams }) {
-    const hasUtms = this.utm.capture(queryParams);
-    if (hasUtms) {
-      next(() => this.resetUTMs());
-    }
+    this.utm.capture(queryParams);
+  },
+
+  reportPage() {
+    return new Promise(resolve => {
+      try {
+        this.metrics.trackPage({
+          page: this.router.currentURL,
+          hitCallback: () => resolve()
+        });
+        // If page is not reported to GA for some reason,
+        // just resolve after a timeout to unblock teardown
+        later(() => resolve(), utmParametersResetDelay);
+      } catch (err) {
+        resolve(); // skip error and continue
+      }
+    });
+  },
+
+  handleRouteChange() {
+    this.reportPage().then(() => {
+      if (this.utm.hasData) {
+        this.resetUTMs();
+      }
+    });
   },
 
   resetUTMs() {
     try {
       this.resetQueryParams([...UTM_FIELD_NAMES]);
     } catch (e) {}
+  },
+
+  init() {
+    this._super(...arguments);
+    this.router.on('routeDidChange', () => this.handleRouteChange());
   }
 });
