0178663c3fd00ea8534f4b4bbc268e8403c76583
the-bionic <inem.patrick@gmail.com>
2020-04-02 16:52:01 +0100

    remove cache redirect

--- app/routes/caches.js
+++ app/routes/caches.js
@@ -6,15 +6,6 @@ export default TravisRoute.extend({
 
   needsAuth: true,
 
-  redirect() {
-    const repo = this.modelFor('repo');
-    const isAdmin = repo.get('permissions.admin');
-    if (!isAdmin) {
-      const { provider, ownerName, vcsName } = repo;
-      this.transitionTo(`/${provider}/${ownerName}/${vcsName}`);
-    }
-  },
-
   setupController(/* controller*/) {
     this._super(...arguments);
     return this.controllerFor('repo').activate('caches');
