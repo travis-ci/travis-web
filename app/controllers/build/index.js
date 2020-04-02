b90b18d649365c8e0e1ff2e8b557bb44f6376eaf
Sven Fuchs <me@svenfuchs.com>
2020-04-03 01:43:34 +0200

    always display request messages, alternative style

--- app/controllers/build/index.js
+++ app/controllers/build/index.js
@@ -1,12 +1,14 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
+import { alias, reads } from '@ember/object/computed';
 
 export default Controller.extend({
   repositories: service(),
 
   latestCurrentBuild: alias('repositories.accessible.firstObject.currentBuild'),
+
+  request: reads('build.request'),
 
   build: computed('model', 'latestCurrentBuild', function () {
     let model = this.model;
