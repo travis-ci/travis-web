75b659f69280f0963fa08489d00e89b64a0e6e76
Sven Fuchs <me@svenfuchs.com>
2020-04-02 15:55:38 +0200

    safeguard against missing property value

--- app/utils/computed.js
+++ app/utils/computed.js
@@ -2,6 +2,7 @@ import { computed } from '@ember/object';
 
 export default function truncate(property, chars) {
   return computed(property, function () {
-    return this.get(property).slice(0, chars);
+    const value = this.get(property);
+    return value ? value.slice(0, chars) : '';
   });
 }
