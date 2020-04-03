fbbfdd4a15a9baa05ad12be2d6ce8680caf5cf88
the-bionic <inem.patrick@gmail.com>
2020-04-03 14:30:11 +0100

    render an error page if no admin permissions

--- app/controllers/requests.js
+++ app/controllers/requests.js
@@ -1,12 +1,15 @@
 import Controller, { inject as controller } from '@ember/controller';
 import { computed } from '@ember/object';
+import { reads, bool } from '@ember/object/computed';
 
 export default Controller.extend({
   repoController: controller('repo'),
 
   queryParams: ['requestId'],
+  repo: reads('repoController.repo'),
+  isAdmin: bool('repo.permissions.admin'),
 
-  lintUrl: computed('repoController.repo.slug', function () {
+  lintUrl: computed('repo.slug', function () {
     let slug = this.get('repoController.repo.slug');
     return `https://lint.travis-ci.org/${slug}`;
   })
--- app/routes/requests.js
+++ app/routes/requests.js
@@ -2,15 +2,6 @@ import TravisRoute from 'travis/routes/basic';
 
 export default TravisRoute.extend({
 
-  redirect() {
-    const repo = this.modelFor('repo');
-    const isAdmin = repo.get('permissions.admin');
-    if (!isAdmin) {
-      const { provider, ownerName, vcsName } = repo;
-      this.transitionTo(`/${provider}/${ownerName}/${vcsName}`);
-    }
-  },
-
   setupController() {
     this._super(...arguments);
     return this.controllerFor('repo').activate('requests');
