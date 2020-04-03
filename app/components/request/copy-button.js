a06144d7d2cc72f505d16fee91e67097834bca9b
Sven Fuchs <me@svenfuchs.com>
2020-04-03 15:50:36 +0200

    move copy config button into the config header element (#2437)
    * move copy config button into the header
    
    * use a custom copy button to prevent click from bubbling up

---
+++ app/components/request/copy-button.js
@@ -0,0 +1,30 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/request/copy-button';
+
+export default Component.extend({
+  layout: layout,
+  classNames: ['copy-button'],
+
+  didInsertElement(element) {
+    this.clipboard = this.createClipboard();
+  },
+
+  createClipboard() {
+    const clipboard = new window.ClipboardJS(this.element, { text: this.copyText });
+    clipboard.on('success', this.success);
+    return clipboard;
+  },
+
+  willDestroyElement(element) {
+    if (this.clipboard) {
+      this.clipboard.destroy();
+    }
+  },
+
+  actions: {
+    copy(event) {
+      event.stopPropagation();
+      this.clipboard.onClick(event);
+    },
+  },
+});
