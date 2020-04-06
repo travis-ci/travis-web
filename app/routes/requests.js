0c91d1dee879fb81ab9ec86dc258e27279ed07b0
the-bionic <inem.patrick@gmail.com>
2020-04-06 11:37:05 +0100

    remove space changes

--- app/routes/requests.js
+++ app/routes/requests.js
@@ -1,7 +1,6 @@
 import TravisRoute from 'travis/routes/basic';
 
 export default TravisRoute.extend({
-
   setupController() {
     this._super(...arguments);
     return this.controllerFor('repo').activate('requests');
