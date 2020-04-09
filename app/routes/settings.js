4a48862e44f1b44aee65538b445850c9bced4d61
Artur Smirnov <mail@artursmirnov.com>
2020-04-09 21:13:22 +0300

    Refactor permissions fetching

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -63,14 +63,6 @@ export default TravisRoute.extend({
     return this.api.get(`/repo/${repoId}`).then(response => response.active);
   },
 
-  hasPushAccess() {
-    const repoId = parseInt(this.modelFor('repo').get('id'));
-    return this.get('auth.currentUser').get('pushPermissionsPromise').then((permissions) => {
-      const hasPushAccess = permissions.filter(p => p === repoId);
-      return hasPushAccess;
-    });
-  },
-
   beforeModel() {
     const repo = this.modelFor('repo');
     const hasPushPermission = this.permissions.hasPushPermission(repo);
@@ -81,13 +73,15 @@ export default TravisRoute.extend({
   },
 
   model() {
+    const repo = this.modelFor('repo');
+
     return hash({
-      settings: this.modelFor('repo').fetchSettings.perform(),
-      repository: this.modelFor('repo'),
+      settings: repo.fetchSettings.perform(),
+      repository: repo,
       envVars: this.fetchEnvVars(),
       sshKey: this.fetchSshKey(),
       customSshKey: this.fetchCustomSshKey(),
-      hasPushAccess: this.hasPushAccess(),
+      hasPushAccess: this.permissions.hasPushPermission(repo),
       repositoryActive: this.fetchRepositoryActiveFlag()
     });
   }
