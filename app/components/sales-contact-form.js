f112d87ea7acd95397a9c6a697e540cee54d4589
Artur Smirnov <mail@artursmirnov.com>
2020-04-10 15:02:48 +0300

    Add utm parameters to pardot frame url

--- app/components/sales-contact-form.js
+++ app/components/sales-contact-form.js
@@ -1,11 +1,19 @@
 import Component from '@ember/component';
 import config from 'travis/config/environment';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 const pardotFormUrl = config.urls.pardotHost + config.urls.pardotForm;
 
 export default Component.extend({
   tagName: '',
-  pardotFormUrl,
+
+  utm: service(),
+
+  pardotFormUrl: computed('utm.existing', function () {
+    return `${pardotFormUrl}?${this.utm.existing}`;
+  }),
+
   setHeight(element) {
     window.addEventListener('message', (event) => {
       if (event.origin === config.urls.pardotHost && event.data) {
