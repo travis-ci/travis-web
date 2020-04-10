5d579b1bf295216abe92e799d429975f432fd031
Artur Smirnov <mail@artursmirnov.com>
2020-04-10 17:31:54 +0300

    Fix computed properties deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,8 +2,5 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember-views.curly-components.jquery-element' },
-    { handler: 'silence', matchId: 'computed-property.volatile' },
-    { handler: 'silence', matchId: 'computed-property.override' }
   ]
 };
