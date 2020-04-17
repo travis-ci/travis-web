9d4395d59343225d1e6c352cd85d5f119651d34e
Sven Fuchs <me@svenfuchs.com>
2020-04-17 17:30:20 +0200

    try using createRecord and save for config validation/expansion

--- app/serializers/request-config.js
+++ app/serializers/request-config.js
@@ -3,7 +3,28 @@ import V3Serializer from 'travis/serializers/v3';
 export default V3Serializer.extend({
   normalize(modelClass, resourceHash) {
     resourceHash.id = new Date().getTime();
+    console.log(resourceHash);
 
     return this._super(...arguments);
   },
+
+  serialize(snapshot, options) {
+    return {
+      type: 'api',
+      repo: {
+        slug: snapshot.belongsTo('repo').attr('slug'),
+        private: snapshot.belongsTo('repo').attr('slug'),
+        default_branch: snapshot.belongsTo('repo').belongsTo('defaultBranch').attr('name'),
+      },
+      branch: snapshot.attr('branch'),
+      sha: snapshot.attr('sha'),
+      mode: snapshot.attr('mode'),
+      config: snapshot.attr('config') || '',
+      data: {
+        repo: snapshot.belongsTo('repo').attr('slug'),
+        branch: snapshot.attr('branch'),
+        message: snapshot.attr('message'),
+      }
+    };
+  }
 });
