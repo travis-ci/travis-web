d7e7fa524c29eb8f7f0e73a39683f0127b8e29b7
the-bionic <inem.patrick@gmail.com>
2020-04-20 13:19:23 +0100

    implement multiple emails

--- app/components/billing/information.js
+++ app/components/billing/information.js
@@ -1,6 +1,27 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 import { countries } from 'travis/utils/countries';
 
 export default Component.extend({
-  countries
+  countries,
+  billingInfoEmail: reads('newSubscription.billingInfo.billingEmail'),
+  billingEmails: computed('billingInfoEmail', function () {
+    const emails = this.billingInfoEmail || '';
+    return emails.split(',').map(email => ({ value: email }));
+  }),
+
+  actions: {
+
+    handleBlur() {
+      const emails = this.billingEmails.map(email => email.value);
+      this.newSubscription.billingInfo.set('billingEmail', emails.join(','));
+    },
+
+    addEmail(e) {
+      e.preventDefault();
+      this.set('billingInfoEmail', `${this.billingInfoEmail},`);
+    },
+  }
+
 });
