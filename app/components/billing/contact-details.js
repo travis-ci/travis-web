6bca48605fdbf320b27395e898b4521eb8f98375
the-bionic <inem.patrick@gmail.com>
2020-04-24 11:35:10 +0100

    extract multiple inputs into service for DRY

--- app/components/billing/contact-details.js
+++ app/components/billing/contact-details.js
@@ -1,43 +1,36 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
 
 export default Component.extend({
+  multipleInput: service(),
   billingEmail: reads('info.billingEmail'),
-  billingEmails: computed('billingEmail', {
-    get() {
-      const emails = this.billingEmail || '';
-      return emails.split(',').map((email, index) => ({
-        label: `Billing Email Address ${index + 1}`,
-        value: email,
-        required: index === 0
-      }));
-    },
-    set(_, value) {
-      return value;
-    }
-  }),
+
+  init() {
+    this._super(...arguments);
+    this.multipleInput.setProperties({
+      inputString: this.billingEmail,
+      label: 'Billing Email Address',
+      required: 'first',
+    });
+  },
+
+  billingEmails: reads('multipleInput.inputs'),
 
   actions: {
 
     handleBlur() {
-      const emails = this.billingEmails.map(email => email.value);
-      this.info.set('billingEmail', emails.join(','));
+      this.info.set('billingEmail', this.multipleInput.joinInputs());
     },
 
     addEmail(e) {
       e.preventDefault();
-      const nextEmailNumber = this.billingEmails.length + 1;
-      this.set('billingEmails', [
-        ...this.billingEmails,
-        { label: `Billing Email Address ${nextEmailNumber}`, value: '', required: false }
-      ]);
+      this.multipleInput.addInput();
     },
 
     deleteEmail(emailLabel, e) {
       e.preventDefault();
-      const remainingEmails = this.billingEmails.filter(email => email.label !== emailLabel);
-      this.set('billingEmails', remainingEmails);
+      this.multipleInput.deleteInput(emailLabel);
     }
   }
 });
