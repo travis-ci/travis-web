84c5dc11560f40d8184161c85aa630f7e1c1816c
Sven Fuchs <me@svenfuchs.com>
2020-04-27 18:15:18 +0200

    Use component composition to stop bubbling (#2470)
    Co-authored-by: Sophie Orta <sophie@travis-ci.org>

--- app/components/request/copy-button.js
+++ app/components/request/copy-button.js
@@ -1,31 +1,9 @@
-import ClipboardJS from 'clipboard';
 import Component from '@ember/component';
-import layout from 'travis/templates/components/request/copy-button';
 
 export default Component.extend({
-  layout,
   tagName: '',
 
-  didInsertElement(element) {
-    this.clipboard = this.createClipboard(element);
-  },
-
-  createClipboard(element) {
-    const clipboard = new ClipboardJS('body', { text: this.copyText });
-    clipboard.on('success', this.success);
-    return clipboard;
-  },
-
-  willDestroyElement(element) {
-    if (this.clipboard) {
-      this.clipboard.destroy();
-    }
-  },
-
-  actions: {
-    copy(event) {
-      event.stopPropagation();
-      this.clipboard.onClick(event);
-    },
+  stopBubble(event) {
+    event.stopPropagation();
   },
 });
