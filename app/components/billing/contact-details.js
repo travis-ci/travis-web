91cefa3bed145de619e561fefefd5a28b7f0f00f
the-bionic <inem.patrick@gmail.com>
2020-04-30 10:26:26 +0100

    move multiple fields to contact-details

--- app/components/billing/contact-details.js
+++ app/components/billing/contact-details.js
@@ -1,39 +1,9 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { reads } from '@ember/object/computed';
 
 export default Component.extend({
-  multipleInput: service(),
-
-  inputsString: reads('multipleInput.inputsString'),
-  billingEmails: reads('multipleInput.inputs'),
-  billingEmail: reads('info.billingEmail'),
-
-  init() {
-    this._super(...arguments);
-    this.multipleInput.setProperties({
-      inputsString: this.billingEmail,
-      label: 'Billing Email Address',
-      required: 'first',
-    });
-  },
-
-
   actions: {
-
-    handleBlur() {
-      this.multipleInput.joinInputs();
-      this.info.set('billingEmail', this.inputsString);
+    updateValue(value) {
+      this.newSubscription.billingInfo.set('billingEmail', value);
     },
-
-    addEmail(e) {
-      e.preventDefault();
-      this.multipleInput.addInput();
-    },
-
-    deleteEmail(emailLabel, e) {
-      e.preventDefault();
-      this.multipleInput.deleteInput(emailLabel);
-    }
   }
 });
