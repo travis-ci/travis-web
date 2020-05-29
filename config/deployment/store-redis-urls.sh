17470f868b23f2c6f493ad9cc16b7bd0c963ece2
Arek Rus <57007917+r-arek@users.noreply.github.com>
2020-05-29 15:30:25 +0200

    Remove heroku redis urls (#2493)

--- config/deployment/store-redis-urls.sh
+++
@@ -1,2 +0,0 @@
-echo export ORG_PRODUCTION_REDIS_URL=`heroku config:get REDIS_URL -a travis-web-production-next`
-echo export COM_PRODUCTION_REDIS_URL=`heroku config:get REDIS_URL -a travis-pro-web-production-next`
