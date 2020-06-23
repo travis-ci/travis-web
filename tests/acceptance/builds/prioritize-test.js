11d15b6d8acaad9e792400730ab9c82872b35c45
Rahul Garg <rgarg@celestialsys.com>
2020-06-23 14:45:00 +0530

    ITC-35 Acceptance Test Review Cycle-2

--- tests/acceptance/builds/prioritize-test.js
+++ tests/acceptance/builds/prioritize-test.js
@@ -13,12 +13,12 @@ module('Acceptance | builds/prioritize', function (hooks) {
     signInUser(currentUser);
   });
 
-  test('prioritizing build button does not exsist if the build is not having permission to prioritize becuase of the organization', async function (assert) {
+  test('prioritizing build button does not exsist if the build is not having permission to prioritize because of the organization', async function (assert) {
     let repository =  this.server.create('repository', { slug: 'travis-ci/travis-web' });
     let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
     let gitUser = this.server.create('git-user', { name: 'Mr T' });
     let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
-    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': false }, repository, commit, branch, priority: true, });
+    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': false }, repository, commit, branch, priority: false, });
     let job = this.server.create('job', { number: '1234.1', state: 'queued', repository, commit, build });
     this.server.create('log', { id: job.id });
 
@@ -31,7 +31,7 @@ module('Acceptance | builds/prioritize', function (hooks) {
     let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
     let gitUser = this.server.create('git-user', { name: 'Mr T' });
     let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
-    let build = this.server.create('build', { number: '5', state: 'passed', permissions: { 'prioritize': false }, repository, commit, branch, priority: true, });
+    let build = this.server.create('build', { number: '5', state: 'passed', permissions: { 'prioritize': true }, repository, commit, branch, priority: false, });
     let job = this.server.create('job', { number: '1234.1', state: 'passed', repository, commit, build });
     this.server.create('log', { id: job.id });
 
@@ -39,12 +39,25 @@ module('Acceptance | builds/prioritize', function (hooks) {
     assert.dom('[data-test-repo-actions-prioritize-button]').doesNotExist();
   });
 
-  test('prioritizing build with favouorable conditions', async function (assert) {
+  test('prioritizing build button does not exsist if the build is already prioritized', async function (assert) {
     let repository =  this.server.create('repository', { slug: 'travis-ci/travis-web' });
     let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
     let gitUser = this.server.create('git-user', { name: 'Mr T' });
     let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
-    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': true }, repository, commit, branch, priority: true, });
+    let build = this.server.create('build', { number: '5', state: 'passed', permissions: { 'prioritize': true }, repository, commit, branch, priority: true });
+    let job = this.server.create('job', { number: '1234.1', state: 'passed', repository, commit, build });
+    this.server.create('log', { id: job.id });
+
+    await visit('/travis-ci/travis-web');
+    assert.dom('[data-test-repo-actions-prioritize-button]').doesNotExist();
+  });
+
+  test('the Cancel button funcationality', async function (assert) {
+    let repository =  this.server.create('repository', { slug: 'travis-ci/travis-web' });
+    let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
+    let gitUser = this.server.create('git-user', { name: 'Mr T' });
+    let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
+    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': true }, repository, commit, branch, priority: false, });
     let job = this.server.create('job', { number: '1234.1', state: 'queued', repository, commit, build });
     this.server.create('log', { id: job.id });
 
@@ -52,33 +65,91 @@ module('Acceptance | builds/prioritize', function (hooks) {
     assert.dom('[data-test-repo-actions-prioritize-button]').exists();
 
     await click('[data-test-repo-actions-prioritize-button]');
-    assert.dom('[prioritize-modal-header]').hasText('Prioritize your build');
-    assert.dom('[prioritize-checkbox]').exists({ count: 1 });
-    assert.dom('[prioritize-checkbox]').hasText('Place build at the top of the queue');
-    assert.dom('[prioritize-checkbox-with-cancel]').exists({ count: 1 });
-    assert.dom('[prioritize-checkbox-with-cancel]').hasText('Place build at the top of the queue and cancel all running jobs');
+    assert.dom('[data-test-prioritize-modal-header]').hasText('Prioritize your build');
+    assert.dom('[data-test-prioritize-checkbox]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox]').hasText('Place build at the top of the queue');
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').hasText('Place build at the top of the queue and cancel all running jobs');
 
     // Checking the Cancel button.
-    assert.dom('[prioritize-cancel-button]').hasText('Cancel');
-    await click('[prioritize-cancel-button]');
-    assert.dom('[prioritize-modal-header]').doesNotExist();
+    assert.dom('[data-test-prioritize-cancel-button]').hasText('Cancel');
+    await click('[data-test-prioritize-cancel-button]');
+    assert.dom('[data-test-prioritize-modal-header]').doesNotExist();
+  });
+
+  test('the Close button funcationality', async function (assert) {
+    let repository =  this.server.create('repository', { slug: 'travis-ci/travis-web' });
+    let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
+    let gitUser = this.server.create('git-user', { name: 'Mr T' });
+    let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
+    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': true }, repository, commit, branch, priority: false, });
+    let job = this.server.create('job', { number: '1234.1', state: 'queued', repository, commit, build });
+    this.server.create('log', { id: job.id });
+
+    await visit('/travis-ci/travis-web');
+    assert.dom('[data-test-repo-actions-prioritize-button]').exists();
+    await click('[data-test-repo-actions-prioritize-button]');
+    assert.dom('[data-test-prioritize-modal-header]').hasText('Prioritize your build');
+    assert.dom('[data-test-prioritize-checkbox]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox]').hasText('Place build at the top of the queue');
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').hasText('Place build at the top of the queue and cancel all running jobs');
 
     // Checking the Close button.
     await click('[data-test-repo-actions-prioritize-button]');
-    assert.dom('[prioritize-modal-header]').hasText('Prioritize your build');
-    assert.dom('[prioritize-close-button]').exists();
-    await click('[prioritize-close-button]');
-    assert.dom('[prioritize-modal-header]').doesNotExist();
+    assert.dom('[data-test-prioritize-close-button]').exists();
+    await click('[data-test-prioritize-close-button]');
+    assert.dom('[data-test-prioritize-modal-header]').doesNotExist();
+  });
+
+  test('the Escape key event funcationality', async function (assert) {
+    let repository =  this.server.create('repository', { slug: 'travis-ci/travis-web' });
+    let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
+    let gitUser = this.server.create('git-user', { name: 'Mr T' });
+    let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
+    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': true }, repository, commit, branch, priority: false, });
+    let job = this.server.create('job', { number: '1234.1', state: 'queued', repository, commit, build });
+    this.server.create('log', { id: job.id });
+
+    await visit('/travis-ci/travis-web');
+    assert.dom('[data-test-repo-actions-prioritize-button]').exists();
+
+    await click('[data-test-repo-actions-prioritize-button]');
+    assert.dom('[data-test-prioritize-modal-header]').hasText('Prioritize your build');
+    assert.dom('[data-test-prioritize-checkbox]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox]').hasText('Place build at the top of the queue');
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').hasText('Place build at the top of the queue and cancel all running jobs');
 
     // Checking the Esc Button Event functionality.
     await click('[data-test-repo-actions-prioritize-button]');
     await triggerKeyEvent('button', 'keydown', 'Escape');
-    assert.dom('[prioritize-modal-header]').doesNotExist();
+    assert.dom('[data-test-prioritize-modal-header]').doesNotExist();
+  });
+
+  test('prioritizing build with favouorable conditions', async function (assert) {
+    let repository =  this.server.create('repository', { slug: 'travis-ci/travis-web' });
+    let branch = this.server.create('branch', { repository, name: 'acceptance-tests', default_branch: true });
+    let gitUser = this.server.create('git-user', { name: 'Mr T' });
+    let commit = this.server.create('commit', { author: gitUser, committer: gitUser, committer_name: 'Mr T', message: 'This is a message' });
+    let build = this.server.create('build', { number: '5', state: 'queued', permissions: { 'prioritize': true }, repository, commit, branch, priority: false, });
+    let job = this.server.create('job', { number: '1234.1', state: 'queued', repository, commit, build });
+    this.server.create('log', { id: job.id });
+
+    await visit('/travis-ci/travis-web');
+    assert.dom('[data-test-repo-actions-prioritize-button]').exists();
+
+    await click('[data-test-repo-actions-prioritize-button]');
+    assert.dom('[data-test-prioritize-modal-header]').hasText('Prioritize your build');
+    assert.dom('[data-test-prioritize-checkbox]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox]').hasText('Place build at the top of the queue');
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').exists({ count: 1 });
+    assert.dom('[data-test-prioritize-checkbox-with-cancel]').hasText('Place build at the top of the queue and cancel all running jobs');
 
     // Checking the Prioritize build button check.
     await click('[data-test-repo-actions-prioritize-button]');
-    assert.dom('[prioritize-button]').hasText('Prioritize build');
-    await click('[prioritize-button]');
+    assert.dom('[data-test-prioritize-button]').hasText('Prioritize build');
+    await click('[data-test-prioritize-button]');
     assert.dom('[data-test-components-flash-item]').exists();
   });
 });
