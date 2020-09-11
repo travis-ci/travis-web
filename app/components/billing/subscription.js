0be012b3d6e3c480487062c3172a548b73ee2110
Andrii Mysko <usandrewmisko@gmail.com>
2020-09-11 09:59:54 +0200

    Show plan selector with GH subscription

--- app/components/billing/subscription.js
+++ app/components/billing/subscription.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import { reads, bool, empty, not } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
+
   account: null,
   newSubscriptionProcess: false,
 
@@ -10,5 +14,28 @@ export default Component.extend({
   v2subscription: reads('account.v2subscription'),
   isV2SubscriptionEmpty: empty('account.v2subscription'),
   hasV2Subscription: not('isV2SubscriptionEmpty'),
-  hasExpiredStripev2Subscription: bool('account.expiredStripev2Subscription'),
+
+  newSubscription: computed(function () {
+    const plan = this.store.createRecord('v2-plan-config');
+    const billingInfo = this.store.createRecord('v2-billing-info');
+    const creditCardInfo = this.store.createRecord('v2-credit-card-info');
+    billingInfo.setProperties({
+      firstName: '',
+      lastName: '',
+      address: '',
+      city: '',
+      zipCode: '',
+      country: '',
+      billingEmail: ''
+    });
+    creditCardInfo.setProperties({
+      token: '',
+      lastDigits: ''
+    });
+    return this.store.createRecord('v2-subscription', {
+      billingInfo,
+      plan,
+      creditCardInfo,
+    });
+  }),
 });
