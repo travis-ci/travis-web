0d3abf83c9621bce8e679469ea1b725000bdc36d
AndriiMysko <69478317+AndriiMysko@users.noreply.github.com>
2020-09-14 17:54:45 +0300

    Fix plan page route (#2540)
    * Fix plan page route
    
    * Fix failing spec

--- app/router.js
+++ app/router.js
@@ -38,14 +38,12 @@ Router.map(function () {
     this.route('settings', { path: '/preferences' });
     this.route('billing', { path: '/plan' });
     this.route('migrate');
-    this.route('plan', { path: '/plan' });
   });
   this.route('organization', { path: '/organizations/:login' }, function () {
     this.route('repositories');
     this.route('settings', { path: '/preferences' });
     this.route('billing', { path: '/plan' });
     this.route('migrate');
-    this.route('plan', { path: '/plan' });
   });
   this.route('unsubscribe', { path: '/account/preferences/unsubscribe' });
   this.route('profile', { path: '/profile/:login' });
