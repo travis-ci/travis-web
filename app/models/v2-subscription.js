c0b1f29536cbc17e38092219af3d2a479995ab09
Piotr Milcarz <piotr.milcarz@gmail.com>
2020-10-13 10:38:25 +0200

    fix #54 regression

--- app/models/v2-subscription.js
+++ app/models/v2-subscription.js
@@ -42,33 +42,27 @@ export default Model.extend({
         private: emptyUsage
       };
     }
-    const publicUsages = this.addons.reduce((processed, addon) => {
-      if (addon.type === 'credit_public') {
-        if (addon.current_usage.active) {
-          processed.totalCredits += addon.current_usage.addon_quantity;
-          processed.usedCredits += addon.current_usage.addon_usage;
-          processed.remainingCredits += addon.current_usage.remaining;
-        }
+    const addonReduce = (type) => (processed, addon) => {
+      if (addon.type === type) {
+        processed.totalCredits += addon.current_usage.addon_quantity;
+        processed.usedCredits += addon.current_usage.addon_usage;
+        processed.remainingCredits += addon.current_usage.remaining;
+        processed.validDate = Date.parse(addon.current_usage.valid_to);
+        processed.purchaseDate = Date.parse(addon.current_usage.purchase_date);
       }
 
       return processed;
-    }, {
+    };
+    const publicUsages = this.addons.reduce(addonReduce('credit_public'), {
+      validDate: Date.now(),
+      purchaseDate: Date.now(),
       totalCredits: 0,
       usedCredits: 0,
       remainingCredits: 0,
     });
-    const privateUsages = this.addons.reduce((processed, addon) => {
-      if (addon.type === 'credit_private') {
-        if (addon.current_usage.active) {
-          processed.totalCredits += addon.current_usage.addon_quantity;
-          processed.usedCredits += addon.current_usage.addon_usage;
-          processed.remainingCredits += addon.current_usage.remaining;
-        }
-      }
-
-      return processed;
-    }, {
-      purchaseDate: Date(2000, 1, 1),
+    const privateUsages = this.addons.reduce(addonReduce('credit_private'), {
+      validDate: Date.now(),
+      purchaseDate: Date.now(),
       totalCredits: 0,
       usedCredits: 0,
       remainingCredits: 0,
