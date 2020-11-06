8a88c81c130bc5c92967e8b4e2bab0949047d18c
Piotr Milcarz <piotr.milcarz@gmail.com>
2020-11-06 18:49:53 +0100

    User management modal

--- app/components/dialogs/user-management-modal.js
+++ app/components/dialogs/user-management-modal.js
@@ -2,48 +2,124 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
-import moment from 'moment';
+import { task } from 'ember-concurrency';
+import { later } from '@ember/runloop';
 
 export default Component.extend({
-  store: service(),
-  dateCenter: null,
-  dateRange: null,
-  showDatePicker: false,
+  raven: service(),
+  flashes: service(),
   perPage: 10,
-  users: reads('owner.executionsPerSender'),
-  usersCount: reads('owner.executionsPerSender.length'),
+  selectedUserIds: {},
+  filter: '',
+  changePermissionsTaskIsRunning: reads('context.changePermissions.isRunning'),
+  buildPermissions: reads('context.buildPermissions'),
+  filteredBuildPermissionsCount: reads('filteredBuildPermissions.length'),
+  filteredBuildPermissions: computed('buildPermissions', 'filter', function () {
+    if (this.filter === '') {
+      return this.buildPermissions;
+    }
+    return this.buildPermissions.filter(perm => {
+      if (perm.user.name.indexOf(this.filter) >= 0 || perm.user.login.indexOf(this.filter) >= 0) {
+        return true;
+      }
+    });
+  }),
 
   init() {
     this._super(...arguments);
     this.page = 1;
+    this.context.fetchBuildPermissions.perform();
   },
 
-  showPrev: computed('page', 'usersCount', function () {
+  showPrev: computed('page', 'filteredBuildPermissionsCount', function () {
     return this.page > 1;
   }),
 
-  showNext: computed('page', 'usersCount', function () {
+  showNext: computed('page', 'filteredBuildPermissionsCount', function () {
     return this.page < this.maxPages;
   }),
 
-  maxPages: computed('usersCount', function () {
-    return Math.ceil(this.usersCount / this.perPage);
+  maxPages: computed('filteredBuildPermissionsCount', function () {
+    return Math.ceil(this.filteredBuildPermissionsCount / this.perPage);
   }),
 
-  usersToShow: computed('users', 'page', function () {
-    return this.users.slice((this.page - 1) * this.perPage, this.page * this.perPage);
+  buildPermissionsToShow: computed('filteredBuildPermissions', 'page', function () {
+    return this.filteredBuildPermissions.slice((this.page - 1) * this.perPage, this.page * this.perPage);
+  }),
+
+  isAllSelected: computed('selectedUserIds', 'buildPermissionsToShow', function () {
+    const selectedUserIds = this.get('selectedUserIds');
+    if (Object.keys(selectedUserIds).length === 0) {
+      return false;
+    }
+    return this.buildPermissionsToShow.reduce((previousValue, perm) => (selectedUserIds[perm.user.id] === false ? false : previousValue), true);
   }),
 
   actions: {
-    datePicker() {
-      this.set('showDatePicker', !this.showDatePicker);
-      if (!this.showDatePicker) {
-        this.account.fetchExecutionsPerSender.perform(moment(this.dateRange.start).format('YYYY-MM-DD'),
-          moment(this.dateRange.end).format('YYYY-MM-DD'));
-      }
-    },
     setPage(newPage) {
-      this.page = newPage;
+      this.set('page', newPage);
+    },
+
+    setUserId(id) {
+      this.selectedUserIds[id] = !this.selectedUserIds[id];
+      this.notifyPropertyChange('selectedUserIds');
+    },
+
+    setFilter(event) {
+      this.set('filter', event.target.value);
+    },
+
+    setAllUserIds() {
+      const newState = !this.isAllSelected;
+      this.buildPermissionsToShow.forEach(perm => {
+        this.selectedUserIds[perm.user.id] = newState;
+      });
+      this.notifyPropertyChange('selectedUserIds');
     }
-  }
+  },
+
+  changePermissions: task(function* (userId, newState) {
+    try {
+      yield this.context.changePermissions.perform(userId, newState);
+    } catch (e) {
+      this.raven.logException(e);
+      this.flashes.error('There was an error while saving your permissions. Please try again.');
+    }
+  }).drop(),
+
+  bulkActivate: task(function* () {
+    const userIds = [];
+    for (let key in this.selectedUserIds) {
+      if (this.selectedUserIds[key] === true) {
+        userIds.push(parseInt(key));
+      }
+    }
+    try {
+      yield this.context.changePermissions.perform(userIds, true);
+      this.set('selectedUserIds', {});
+      this.notifyPropertyChange('selectedUserIds');
+      later(() => this.context.fetchBuildPermissions.perform(), 500);
+    } catch (e) {
+      this.raven.logException(e);
+      this.flashes.error('There was an error while saving your permissions. Please try again.');
+    }
+  }).drop(),
+
+  bulkDeactivate: task(function* () {
+    const userIds = [];
+    for (let key in this.selectedUserIds) {
+      if (this.selectedUserIds[key] === true) {
+        userIds.push(parseInt(key));
+      }
+    }
+    try {
+      yield this.context.changePermissions.perform(userIds, false);
+      this.set('selectedUserIds', {});
+      this.notifyPropertyChange('selectedUserIds');
+      later(() => this.context.fetchBuildPermissions.perform(), 500);
+    } catch (e) {
+      this.raven.logException(e);
+      this.flashes.error('There was an error while saving your permissions. Please try again.');
+    }
+  }).drop(),
 });
