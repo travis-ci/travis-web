9d354f978d57971ce4a893656ef329f24af47900
Artur Kremens <artur.kremens@gmail.com>
2021-03-16 16:04:50 +0100

    Fix typo

--- app/controllers/account/settings.js
+++ app/controllers/account/settings.js
@@ -61,7 +61,7 @@ export default Controller.extend({
     return (!this.auth.currentUser.emails || this.auth.currentUser.emails.length === 0);
   }),
 
-  userConfirmedAt: reads('auth.currentUse.confirmedAt'),
+  userConfirmedAt: reads('auth.currentUser.confirmedAt'),
 
   confirmationButtonClass: computed('userHasNoEmails', function () {
     if (this.userHasNoEmails) { return 'button--white-and-teal disabled'; }
