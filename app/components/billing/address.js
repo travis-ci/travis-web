12fd155000d6015ff806d6e497177500b1447618
Andrii Mysko <usandrewmisko@gmail.com>
2021-11-26 12:14:31 +0200

    Fix being able to open both billing forms at once

--- app/components/billing/address.js
+++ app/components/billing/address.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { task } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
+import { observer } from '@ember/object';
 import { countries, nonZeroVatThresholdCountries } from 'travis/utils/countries';
 
 export default Component.extend({
@@ -33,6 +34,24 @@ export default Component.extend({
       this.raven.logException(error);
     }
   }).drop(),
+
+  billingFormTriggered: observer('openEditBillingForm', function () {
+    if (!this.openEditBillingForm && this.subscription.billingInfo.hasDirtyAttributes) {
+      this.subscription.billingInfo.rollbackAttributes();
+    }
+    if (this.openEditBillingForm && this.openEditContactForm) {
+      this.set('openEditContactForm', false);
+    }
+  }),
+
+  contactFormTriggered: observer('openEditContactForm', function () {
+    if (!this.openEditContactForm && this.subscription.billingInfo.hasDirtyAttributes) {
+      this.subscription.billingInfo.rollbackAttributes();
+    }
+    if (this.openEditContactForm && this.openEditBillingForm) {
+      this.set('openEditBillingForm', false);
+    }
+  }),
 
   closeEditForms() {
     this.set('openEditContactForm', false);
