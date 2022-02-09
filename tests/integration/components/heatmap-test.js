a78643751dc421dd41dee18ecfad3f4fd7ef00d8
prembalaji39 <prembalaji@accolitedigital.com>
2022-02-09 13:50:56 +0530

    build errors fix heatmap-test.js

--- tests/integration/components/heatmap-test.js
+++ tests/integration/components/heatmap-test.js
@@ -1,18 +1,18 @@
-import { module, test } from "qunit";
-import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
-import hbs from "htmlbars-inline-precompile";
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
-module("Integration | Component | heatmap", function (hooks) {
+module('Integration | Component | heatmap', function (hooks) {
   setupRenderingTest(hooks);
 
-  test("it renders", async function (assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`<Heatmap />`);
 
-    assert.equal(this.element.textContent.trim(), "");
+    assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module("Integration | Component | heatmap", function (hooks) {
       </Heatmap>
     `);
 
-    assert.equal(this.element.textContent.trim(), "template block text");
+    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
