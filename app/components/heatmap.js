5640e7901af18f8316619bd5698ddc8b2f27271b
prembalaji39 <prembalaji@accolitedigital.com>
2022-02-10 12:36:07 +0530

    Code Optimized for api calls || fix es lint error

--- app/components/heatmap.js
+++ app/components/heatmap.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 const BUILDS_FILTER_LABELS = {
   all: 'All Builds',
@@ -50,129 +51,12 @@ export default Component.extend({
     new Date().getFullYear() - 4,
     new Date().getFullYear() - 5,
   ],
-  actions: {
-    setBuildFilter(filter, dropdown) {
-      document.getElementById('insights-heatmap').innerHTML = '';
-      dropdown.actions.close();
-      this.set('buildFilterLabel', BUILDS_FILTER_LABELS[filter]);
-      this.set('buildMaxColor', BUILDS_MAX_COLOR[filter]);
-      this.set('buildMinColor', BUILDS_MIN_COLOR[filter]);
-      this.set('buildStatus', BUILDS_QUERY_PARAMS[filter]);
-      let url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31`;
-      if (this.buildStatus !== 'all') {
-        url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31&build_status=${this.buildStatus}`;
-      }
-      this.api
-        .get(url)
-        .then(async (result) => {
-          let data = {};
-          await result.data.map((r) => {
-            let dateConverted = Date.parse(r.time) / 1000;
-            let prev = data[dateConverted];
-            if (data[dateConverted] === undefined) {
-              data[dateConverted] = r.builds;
-            } else {
-              let current = r.builds;
-              let total = prev + current;
-              data[dateConverted] = total;
-            }
-          });
-          this.set('heatmapData', data);
-          // eslint-disable-next-line no-use-before-define
-          let cal = new CalHeatMap();
-          cal.init({
-            itemSelector: '#insights-heatmap',
-            domain: 'month',
-            range: 12,
-            start: new Date(this.buildYear, 0, 1),
-            subDomain: 'day',
-            itemName: ['Build'],
-            cellSize: 14,
-            cellRadius: 0,
-            cellPadding: 1,
-            displayLegend: true,
-            tooltip: true,
-            domainMargin: [1, 1, 1, 1],
-            legendHorizontalPosition: 'right',
-            legendColors: {
-              min: this.buildMinColor,
-              max: this.buildMaxColor,
-              empty: this.buildEmptyColor,
-            },
-            considerMissingDataAsZero: true,
-            legend: [25, 50, 75, 100],
-            legendCellSize: 14,
-            data: this.heatmapData,
-          });
-        })
-        .catch((error) => {
-          throw new Error(error);
-        });
-    },
-    setBuildYear(filter, dropdown) {
-      document.getElementById('insights-heatmap').innerHTML = '';
-      dropdown.actions.close();
-      this.set('buildYear', filter);
-      let url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31`;
-      if (this.buildStatus !== 'all') {
-        url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31&build_status=${this.buildStatus}`;
-      }
-      this.api
-        .get(url)
-        .then(async (result) => {
-          let data = {};
-          await result.data.map((r) => {
-            let dateConverted = Date.parse(r.time) / 1000;
-            let prev = data[dateConverted];
-            if (data[dateConverted] === undefined) {
-              data[dateConverted] = r.builds;
-            } else {
-              let current = r.builds;
-              let total = prev + current;
-              data[dateConverted] = total;
-            }
-          });
-          this.set('heatmapData', data);
-          // eslint-disable-next-line no-use-before-define
-          let cal = new CalHeatMap();
-          cal.init({
-            itemSelector: '#insights-heatmap',
-            domain: 'month',
-            range: 12,
-            start: new Date(this.buildYear, 0, 1),
-            subDomain: 'day',
-            itemName: ['Build'],
-            cellSize: 14,
-            cellRadius: 0,
-            cellPadding: 1,
-            displayLegend: true,
-            tooltip: true,
-            domainMargin: [1, 1, 1, 1],
-            legendHorizontalPosition: 'right',
-            legendColors: {
-              min: this.buildMinColor,
-              max: this.buildMaxColor,
-              empty: this.buildEmptyColor,
-            },
-            considerMissingDataAsZero: true,
-            legend: [25, 50, 75, 100],
-            legendCellSize: 14,
-            data: this.heatmapData,
-          });
-        })
-        .catch((error) => {
-          throw new Error(error);
-        });
-    },
-  },
-  didInsertElement() {
-    this.api
-      .get(
-        `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31`
-      )
-      .then(async (result) => {
+  fetchHeatMapData: task(function* (url) {
+    return yield this.api
+      .get(url)
+      .then((result) => {
         let data = {};
-        await result.data.map((r) => {
+        result.data.map((r) => {
           let dateConverted = Date.parse(r.time) / 1000;
           let prev = data[dateConverted];
           if (data[dateConverted] === undefined) {
@@ -184,8 +68,7 @@ export default Component.extend({
           }
         });
         this.set('heatmapData', data);
-        // eslint-disable-next-line no-use-before-define
-        let cal = new CalHeatMap();
+        let cal = new CalHeatMap(); // eslint-disable-line
         cal.init({
           itemSelector: '#insights-heatmap',
           domain: 'month',
@@ -214,5 +97,34 @@ export default Component.extend({
       .catch((error) => {
         throw new Error(error);
       });
+  }),
+  actions: {
+    setBuildFilter(filter, dropdown) {
+      document.getElementById('insights-heatmap').innerHTML = '';
+      dropdown.actions.close();
+      this.set('buildFilterLabel', BUILDS_FILTER_LABELS[filter]);
+      this.set('buildMaxColor', BUILDS_MAX_COLOR[filter]);
+      this.set('buildMinColor', BUILDS_MIN_COLOR[filter]);
+      this.set('buildStatus', BUILDS_QUERY_PARAMS[filter]);
+      let url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31`;
+      if (this.buildStatus !== 'all') {
+        url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31&build_status=${this.buildStatus}`;
+      }
+      this.fetchHeatMapData.perform(url);
+    },
+    setBuildYear(filter, dropdown) {
+      document.getElementById('insights-heatmap').innerHTML = '';
+      dropdown.actions.close();
+      this.set('buildYear', filter);
+      let url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31`;
+      if (this.buildStatus !== 'all') {
+        url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31&build_status=${this.buildStatus}`;
+      }
+      this.fetchHeatMapData.perform(url);
+    },
+  },
+  didInsertElement() {
+    let url = `/insights_spotlight_summary?time_start=${this.buildYear}-01-01&time_end=${this.buildYear}-12-31`;
+    this.fetchHeatMapData.perform(url);
   },
 });
