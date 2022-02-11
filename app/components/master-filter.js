eeea48cb9c9d2cc732b6a6bd3634fbb9a6c9542e
RutujaAwate <rutujaramchandra.awate@accolitedigital.com>
2022-02-11 13:15:18 +0530

    MASTER-FILTER Insights-Spotlight

--- app/components/master-filter.js
+++ app/components/master-filter.js
@@ -3,35 +3,33 @@ import { task, timeout } from 'ember-concurrency';
 import config from 'travis/config/environment';
 import { inject as service } from '@ember/service';
 
-
 export default Component.extend({
   api: service(),
   store: service(),
 
-    buildFilterLabel: "All builds",
-    isAllSelected: false,
-    allRepositories: [],
-    selectedRepos:[],
-    query: '',
-    
-    search: task(function* () {
-      console.log(this.query);
-        yield timeout(config.intervals.repositoryFilteringDebounceRate);
-        yield this.allRepositories.applyFilter(this.query);
-    }),
-    didInsertElement() {
-      return this.api.get('/repos').then((result) => {
-        this.set('allRepositories',result.repositories); 
-      });
-    },
+  buildFilterLabel: 'All builds',
+  isAllSelected: false,
+  allRepositories: [],
+  selectedRepos: [],
+  query: '',
 
-    actions: {   
-      toggleRepoCheck(repository){
-        if (this.selectedRepos.includes(repository)) {
-          this.selectedRepos.removeObject(repository);
-        } else {
-          this.selectedRepos.pushObject(repository);
-        }
+  search: task(function* () {
+    yield timeout(config.intervals.repositoryFilteringDebounceRate);
+    yield this.allRepositories.applyFilter(this.query);
+  }),
+  didInsertElement() {
+    return this.api.get('/repos').then((result) => {
+      this.set('allRepositories', result.repositories);
+    });
+  },
+
+  actions: {
+    toggleRepoCheck(repository) {
+      if (this.selectedRepos.includes(repository)) {
+        this.selectedRepos.removeObject(repository);
+      } else {
+        this.selectedRepos.pushObject(repository);
       }
     }
+  }
 });
