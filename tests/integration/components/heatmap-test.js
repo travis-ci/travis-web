0f992019ee5919e46b6984d12cdccb502264c401
prembalaji39 <prembalaji@accolitedigital.com>
2022-02-15 16:47:28 +0530

    Added Integration Test Cases

--- tests/integration/components/heatmap-test.js
+++ tests/integration/components/heatmap-test.js
@@ -15,7 +15,7 @@ module('Integration | Component | heatmap', function (hooks) {
       .exists('Heatmap Container Exists');
   });
 
-  test('it renders heatmap dropdowns builds and years', async function (assert) {
+  test('it renders heatmap dropdowns builds', async function (assert) {
     await render(hbs`<Heatmap />`);
     assert
       .dom('[data-test-range-selector-heatmap]')
@@ -24,6 +24,9 @@ module('Integration | Component | heatmap', function (hooks) {
       .dom('.build-filter-label')
       .exists('Heatmap should contain build label');
     assert.dom('.build-filter-label').hasText('All Builds');
+    assert.dom('.build-filter-label').hasClass('build-filter-label');
+    assert.dom('[data-test-down-arrow]').hasClass('caret');
+    assert.dom('[data-test-down-arrow]').hasClass('down');
 
     await click('.build-filter-label');
 
@@ -31,26 +34,62 @@ module('Integration | Component | heatmap', function (hooks) {
       .dom('[data-test-build-filter-all]')
       .exists('Heatmap should contain build label - All Builds');
     assert.dom('[data-test-build-filter-all]').hasText('All Builds');
+    assert
+      .dom('[data-test-box-all]')
+      .exists('All Builds dropdown should contain color box container');
+    assert.dom('[data-test-box-all]').hasClass('box');
+    assert.dom('[data-test-box-all]').hasClass('all');
 
     assert
       .dom('[data-test-build-filter-success]')
       .exists('Heatmap should contain build label - Successful Builds');
     assert.dom('[data-test-build-filter-success]').hasText('Successful Builds');
+    assert
+      .dom('[data-test-box-success]')
+      .exists('Success Builds dropdown should contain color box container');
+    assert.dom('[data-test-box-success]').hasClass('box');
+    assert.dom('[data-test-box-success]').hasClass('successful');
 
     assert
       .dom('[data-test-build-filter-failed]')
       .exists('Heatmap should contain build label - Failed Builds');
     assert.dom('[data-test-build-filter-failed]').hasText('Failed Builds');
+    assert
+      .dom('[data-test-box-failed]')
+      .exists('Failed Builds dropdown should contain color box container');
+    assert.dom('[data-test-box-failed]').hasClass('box');
+    assert.dom('[data-test-box-failed]').hasClass('failed');
 
     assert
       .dom('[data-test-build-filter-error]')
       .exists('Heatmap should contain build label - Errored Builds');
     assert.dom('[data-test-build-filter-error]').hasText('Errored Builds');
+    assert
+      .dom('[data-test-box-error]')
+      .exists('Error Builds dropdown should contain color box container');
+    assert.dom('[data-test-box-error]').hasClass('box');
+    assert.dom('[data-test-box-error]').hasClass('errored');
 
     assert
       .dom('[data-test-build-filter-cancel]')
       .exists('Heatmap should contain build label - Canceled Builds');
     assert.dom('[data-test-build-filter-cancel]').hasText('Canceled Builds');
+    assert
+      .dom('[data-test-box-cancel]')
+      .exists('Cancel Builds dropdown should contain color box container');
+    assert.dom('[data-test-box-cancel]').hasClass('box');
+    assert.dom('[data-test-box-cancel]').hasClass('canceled');
+  });
+
+  test('it renders build year dropdown', async function (assert) {
+    await render(hbs`<Heatmap />`);
+
+    assert
+      .dom('[data-test-build-year-dropdown-container]')
+      .exists('Heatmap should contain build years dropdown container');
+
+    assert.dom('.build-year').exists('Heatmap should contain build year label');
+    assert.dom('.build-year').hasText(new Date().getFullYear().toString());
   });
 
   test('it renders weekday on side of heatmap', async function (assert) {
@@ -62,36 +101,43 @@ module('Integration | Component | heatmap', function (hooks) {
     assert
       .dom('[data-test-day-label-mon]')
       .exists('Heatmap should contain day label - Mon');
+    assert.dom('[data-test-day-label-mon]').hasClass('day-label');
     assert.dom('[data-test-day-label-mon]').hasText('Mon');
 
     assert
       .dom('[data-test-day-label-tue]')
       .exists('Heatmap should contain day label - Tue');
+    assert.dom('[data-test-day-label-tue]').hasClass('day-label');
     assert.dom('[data-test-day-label-tue]').hasText('Tue');
 
     assert
       .dom('[data-test-day-label-wed]')
       .exists('Heatmap should contain day label - Wed');
+    assert.dom('[data-test-day-label-wed]').hasClass('day-label');
     assert.dom('[data-test-day-label-wed]').hasText('Wed');
 
     assert
       .dom('[data-test-day-label-thu]')
       .exists('Heatmap should contain day label - Thu');
+    assert.dom('[data-test-day-label-thu]').hasClass('day-label');
     assert.dom('[data-test-day-label-thu').hasText('Thu');
 
     assert
       .dom('[data-test-day-label-fri]')
       .exists('Heatmap should contain day label - Fri');
+    assert.dom('[data-test-day-label-fri]').hasClass('day-label');
     assert.dom('[data-test-day-label-fri]').hasText('Fri');
 
     assert
       .dom('[data-test-day-label-sat]')
       .exists('Heatmap should contain day label - Sat');
+    assert.dom('[data-test-day-label-sat]').hasClass('day-label');
     assert.dom('[data-test-day-label-sat]').hasText('Sat');
 
     assert
       .dom('[data-test-day-label-sun]')
       .exists('Heatmap should contain day label - sun');
+    assert.dom('[data-test-day-label-sun]').hasClass('day-label');
     assert.dom('[data-test-day-label-sun]').hasText('Sun');
   });
 
