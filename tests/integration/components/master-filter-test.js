b2fc84903e17f7fe0f0f8ee165204c922db871c1
RutujaAwate <rutujaramchandra.awate@accolitedigital.com>
2022-02-17 22:27:11 +0530

    removed one test case

--- tests/integration/components/master-filter-test.js
+++ tests/integration/components/master-filter-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, find } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
@@ -25,12 +25,5 @@ module('Integration | Component | master-filter', function (hooks) {
     assert.dom('.build-filter-label').hasText('All Builds');
 
     await click('.build-filter-label');
-  });
-
-  test('it renders AllRepositories', async function (assert) {
-    await render(hbs`<MasterFilter checked=true />`);
-    await click('[data-test-down-caret]');
-    await click('[data-test-checkbox-label]');
-    assert.ok(find('input[type="checkbox"]').checked);
   });
 });
