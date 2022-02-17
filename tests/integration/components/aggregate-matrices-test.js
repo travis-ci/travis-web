6590434857e7a3a00244ac625180c24787840ab8
Ravi Kumar Ranjan <94518993+RaviRanjan86@users.noreply.github.com>
2022-02-17 18:52:15 +0530

    Adding Integration Testing

---
+++ tests/integration/components/aggregate-matrices-test.js
@@ -0,0 +1,49 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+
+module('Integration | Component | aggregate-matrices', function (hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  test('it renders aggregate-matrices container', async function (assert) {
+    await render(hbs`<AggregateMatrices />`);
+    assert
+      .dom('[data-test-aggregate-matrices-container]')
+      .exists('Aggregate Matrices Container Exists');
+  });
+
+  test('build details', async function (assert) {
+    await render(hbs`<AggregateMatrices />`);
+    assert
+      .dom('[data-test-builds]')
+      .exists('Aggregate Matrices Build Exists');
+    assert.dom('[data-test-builds-label]').exists('Build label present');
+    assert.dom('[data-test-builds-count]').exists('Build count present');
+    assert.dom('[data-test-builds-percent]').exists('Build percentage exits');
+  });
+
+  test('minute details ', async function (assert) {
+    await render(hbs`<AggregateMatrices />`);
+    assert
+      .dom('[data-test-minutes]')
+      .exists('Aggregate Matrices minute Exists');
+    assert.dom('[data-test-minutes-label]').exists('minute label present');
+    assert.dom('[data-test-minutes-count]').exists('minute count present');
+    assert.dom('[data-test-minutes-percent]').exists('minute percentage exits');
+  });
+
+  test('credit details', async function (assert) {
+    await render(hbs`<AggregateMatrices />`);
+    assert
+      .dom('[data-test-credits]')
+      .exists('Aggregate Matrices credit Exists');
+    assert.dom('[data-test-credits-label]').exists('credit label present');
+    assert.dom('[data-test-credits-count]').exists('credit count present');
+    assert.dom('[data-test-credits-percent]').exists('credit percentage exits');
+  });
+
+});
