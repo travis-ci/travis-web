f1a60a2c3ea227b649af96dc0ccaa6a02738784c
RutujaAwate <rutujaramchandra.awate@accolitedigital.com>
2022-02-17 17:02:16 +0530

    Implement Unit Testing And Pass Repo Id

--- tests/integration/components/master-filter-test.js
+++ tests/integration/components/master-filter-test.js
@@ -1,26 +1,37 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+// import EmberObject from '@ember/object';
 
 module('Integration | Component | master-filter', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<MasterFilter />`);
+    await click('[data-test-down-caret]');
+    assert.dom('[data-test-search-icon]').exists('renders the search svg');
+  });
 
-    assert.equal(this.element.textContent.trim(), 'All builds');
+  test('it renders Repositories label', async function (assert) {
+    await render(hbs`<MasterFilter />`);
+    assert
+      .dom('[data-test-filter-container]')
+      .exists('All Repositories are exist');
+    assert
+      .dom('.build-filter-label')
+      .exists('Contain All Repo Name');
+    assert.dom('.build-filter-label').hasText('All Builds');
 
-    // Template block usage:
-    await render(hbs`
-      <MasterFilter>
-        template block text
-      </MasterFilter>
-    `);
+    await click('.build-filter-label');
+  });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it renders AllRepositories', async function (assert) {
+    await render(hbs`<MasterFilter checked=true />`);
+    await click('[data-test-down-caret]');
+    await click('[data-test-checkbox-label]');
+    assert.ok(find('input[type="checkbox"]').checked);
   });
 });
