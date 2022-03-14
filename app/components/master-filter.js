a59f281b48cb8f03a4762c6ae0ed33c0734a0b00
Vitalie D <vitalie.dev@gmail.com>
2022-03-14 18:33:00 +0200

    Travis Insights Spotlight

---
+++ app/components/master-filter.js
@@ -0,0 +1,53 @@
+import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+import config from 'travis/config/environment';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
+
+export default Component.extend({
+  api: service(),
+  store: service(),
+
+  buildFilterLabel: 'All Builds',
+  isAllSelected: false,
+  allRepositories: [],
+  selectedRepos: [],
+  repoIds: '',
+  repoData: [],
+  query: '',
+  isEmpty: true,
+
+  search: task(function* () {
+    yield timeout(config.intervals.repositoryFilteringDebounceRate);
+    this.allRepositories.filter(item => item.name.indexOf(this.query) !== -1);
+  }).restartable(),
+
+  didInsertElement() {
+    return this.api.get('/repos').then((result) => {
+      this.set('allRepositories', result.repositories);
+    });
+  },
+
+  actions: {
+    toggleRepoCheck(repository) {
+      if (this.selectedRepos.includes(repository)) {
+        this.selectedRepos.removeObject(repository);
+      } else {
+        this.selectedRepos.pushObject(repository);
+      }
+      let repoIds = '';
+      repoIds = this.get('selectedRepos').join(',');
+      this.set('repoIds', repoIds);
+      this.setSelectedRepoIds(repoIds);
+
+      if (!isEmpty(this.selectedRepos)) {
+        this.set('isEmpty', false);
+      }
+    },
+    allRepoSelection() {
+      this.set('selectedRepos', []);
+      this.setSelectedRepoIds('');
+      this.set('isEmpty', true);
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -90,6 +90,7 @@ Router.map(function () {
   this.route('legacy-repo-url', { path: '/:owner/:repo/:method/:id/:view' });
 
   this.route('insights', { resetNamespace: true }, function () {
+    this.route('spotlight');
     this.route('notifications');
     this.route('probes');
     this.route('plugins');
