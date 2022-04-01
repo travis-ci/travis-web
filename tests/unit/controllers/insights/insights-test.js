efbe49e46ae7751323f0aae0109d7b8fc5a26147
Ravi Kumar Ranjan <94518993+RaviRanjan86@users.noreply.github.com>
2022-04-01 12:20:31 +0530

    Route Changes

--- tests/unit/controllers/insights/insights-test.js
+++ tests/unit/controllers/insights/insights-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | insights/spotlight', function (hooks) {
+module('Unit | Controller | index', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:insights/spotlight');
-    assert.ok(route);
+    let controller = this.owner.lookup('controller:index');
+    assert.ok(controller);
   });
 });
