1b7b3fab39306b1cd0a4d1ce3ff85e8d90e0a40c
Murtaza Khan <imurtaza.swati@gmail.com>
2022-05-12 14:18:30 +0500

    Removed header/footer menus from login/sign-up pages on .com

--- app/components/page-footer.js
+++ app/components/page-footer.js
@@ -5,6 +5,7 @@ import { inject as service } from '@ember/service';
 import config from 'travis/config/environment';
 
 export default Component.extend({
+  router: service(),
   features: service(),
 
   config,
