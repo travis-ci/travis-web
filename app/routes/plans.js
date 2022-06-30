faac7ff33152dba3431f3d18924c64b9270e31f7
Murtaza Khan <imurtaza.swati@gmail.com>
2022-06-30 14:58:55 +0500

    non logged in user plan page redirect to wordpress

--- app/routes/plans.js
+++ app/routes/plans.js
@@ -1,11 +1,20 @@
 import BasicRoute from 'travis/routes/basic';
 import { inject as service } from '@ember/service';
 import TailwindBaseMixin from 'travis/mixins/tailwind-base';
+import config from 'travis/config/environment';
 
 export default BasicRoute.extend(TailwindBaseMixin, {
   needsAuth: false,
 
   features: service(),
+  auth: service(),
+
+  beforeModel() {
+    let pro = this.get('features.proVersion');
+    if (!this.auth.signedIn && config.environment !== 'test' && pro) {
+      window.location.replace('https://www.travis-ci.com/pricing');
+    }
+  },
 
   redirect() {
     if (!this.get('features.proVersion')) {
