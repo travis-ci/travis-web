ab74fcab796ac91a18056f3a8bac8607eae26761
ShahzaibAwan <shahzaibawan@Shahzaibs-MacBook-Pro.local>
2022-12-07 07:19:20 +0500

    Restrict non-admin users to access orgs plan details

--- app/routes/organization/billing.js
+++ app/routes/organization/billing.js
@@ -4,8 +4,12 @@ import AccountBillingMixin from 'travis/mixins/route/account/billing';
 
 export default TravisRoute.extend(AccountBillingMixin, {
   model() {
+    const organization = this.modelFor('organization');
+    if (organization.permissions.admin !== true) {
+      this.transitionTo('organization.repositories', organization);
+    }
     return hash({
-      account: this.modelFor('organization'),
+      account: organization,
     });
   }
 });
