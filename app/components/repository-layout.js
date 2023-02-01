735aa00fe56cd1b17865fdf8de15638a4b5a9b66
Stanislav Kolotinskiy <stanislav@assembla.com>
2023-02-01 19:07:53 +0200

    [PRD] TCI/Assembla Handshake with SVN/P4

--- app/components/repository-layout.js
+++ app/components/repository-layout.js
@@ -14,6 +14,21 @@ export default Component.extend({
   currentUser: alias('auth.currentUser'),
   userRoMode: reads('currentUser.roMode'),
 
+  repositoryProvider: computed('repo.provider', function () {
+    return this.repo.provider.capitalize();
+  }),
+
+  repositoryType: computed('repo.serverType', function () {
+    switch (this.repo.serverType) {
+      case 'git':
+        return 'GIT';
+      case 'subversion':
+        return 'SVN';
+      case 'perforce':
+        return 'P4';
+    }
+  }),
+
   repoUrl: computed('repo.{ownerName,vcsName,vcsType}', function () {
     const owner = this.get('repo.ownerName');
     const repo = this.get('repo.vcsName');
