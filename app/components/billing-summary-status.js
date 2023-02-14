6a9582f58d9154f26ccc37a957a4b02d68a7b285
Erno Emmanuel Ban <ban.emmanuel91@gmail.com>
2023-02-14 14:34:36 +0200

    changed education plan view handling

--- app/components/billing-summary-status.js
+++ app/components/billing-summary-status.js
@@ -7,6 +7,7 @@ export default Component.extend({
   trial: reads('account.trial'),
   isGithubTrial: and('subscription.isGithub', 'trial.hasActiveTrial'),
   hasGithubTrialEnded: and('subscription.isGithub', 'trial.isEnded'),
-  noSubscription: empty('subscription'),
-  isDefaultEducationView: and('noSubscription', 'account.education')
+  isDefaultEducationView: computed('subscription', 'account.education', 'subscription.plan_name', function() {
+    return this.get('subscription') && this.get('account.education') && this.get('subscription.plan_name') === 'Github Student developer pack 1 concurrent plan' && this.get('subscription').isNotEmpty;
+  })
 });
