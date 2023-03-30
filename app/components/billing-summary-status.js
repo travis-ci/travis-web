f1979cabede4ac99600751c24d21d7d74b43fc85
Andrii Mysko <usandrewmisko@gmail.com>
2023-03-30 15:45:43 +0300

    Fix syntax

--- app/components/billing-summary-status.js
+++ app/components/billing-summary-status.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { reads, and, empty } from '@ember/object/computed';
 
 export default Component.extend({
@@ -8,7 +9,8 @@ export default Component.extend({
   isGithubTrial: and('subscription.isGithub', 'trial.hasActiveTrial'),
   hasGithubTrialEnded: and('subscription.isGithub', 'trial.isEnded'),
   noSubscription: empty('subscription'),
-  isDefaultEducationView: computed('subscription', 'account.education', 'subscription.plan_name', function() {
-    return this.get('subscription') && this.get('account.education') && this.get('subscription.plan_name') === 'Github Student developer pack 1 concurrent plan' && this.get('subscription').isNotEmpty;
+  isDefaultEducationView: computed('subscription', 'account.education', 'subscription.plan_name', function () {
+    return this.get('subscription') && this.get('account.education') && this.get('subscription.plan_name') ===
+      'Github Student developer pack 1 concurrent plan' && this.get('subscription').isNotEmpty;
   })
 });
