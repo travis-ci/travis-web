652a59106835da3b9b72157a1e44476651853a05
gbarc80 <gbarc80@gmail.com>
2023-03-31 12:04:42 +0200

    some updates for sub

--- app/components/billing-summary-status.js
+++ app/components/billing-summary-status.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { reads, and, empty } from '@ember/object/computed';
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
   account: null,
@@ -10,6 +11,6 @@ export default Component.extend({
   hasGithubTrialEnded: and('subscription.isGithub', 'trial.isEnded'),
   noSubscription: empty('subscription'),
   isDefaultEducationView: computed('subscription', 'account.education', 'subscription.plan_name', function () {
-    return this.get('subscription') &&  this.get('subscription').isNotEmpty && this.get('account.education');
+    return this.get('subscription') &&  !isEmpty(this.get('subscription')) && this.get('account.education');
   })
 });
