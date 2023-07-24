d99d25726b4ac6ea24792ad8a88afa86f7033ab6
GbArc <gabriel.arczynski@devtactics.net>
2023-07-24 09:12:03 +0200

    ruby up

--- waiter/spec/spec_helper.rb
+++ waiter/spec/spec_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 ENV['RACK_ENV'] = 'test'
 
 require 'sinatra/contrib'
 require 'travis/web'
 
-ru_file = File.expand_path('../../config.ru', __FILE__)
+ru_file = File.expand_path('../config.ru', __dir__)
 web_app = Rack::Builder.parse_file(ru_file).first
 
 RSpec.configure do |config|
