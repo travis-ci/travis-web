4532177cc876419c993a0b8f198dcc2626c79b01
Andrii Mysko <usandrewmisko@gmail.com>
2023-08-02 18:57:34 +0300

    [PRD] TCI/Assembla Handshake with SVN/P4 (#2769)

--- app/models/user.js
+++ app/models/user.js
@@ -78,8 +78,7 @@ export default Owner.extend({
     this.set('applyFilterRepos', !isOrganization);
     return this.api
       .post(`/user/${this.id}/sync`)
-      .then(() => this.poll(),
-        () => this.set('isSyncing', false));
+      .then(() => this.poll());
   },
 
   schedulePoll() {
@@ -91,7 +90,7 @@ export default Owner.extend({
 
   poll() {
     return this.reload().then(() => {
-      if (!this.isSyncing) {
+      if (this.isSyncing) {
         this.schedulePoll();
       } else {
         this.permissionsService.fetchPermissions.perform();
