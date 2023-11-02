effce7d5389ebe296bad1dd77fb989987dbc4bab
Maciej Kempin <maciej.kempin@gmail.com>
2023-11-02 08:25:45 +0000

    fixing problems with authorization

--- mirage/config.js
+++ mirage/config.js
@@ -67,7 +67,7 @@ export default function () {
   });
 
   this.get('/user', function (schema, request) {
-    const authorization = request.requestHeaders['Authorization'];
+    const authorization = request.requestHeaders.Authorization;
     const firstUser = schema.users.first();
 
     if (authorization !== `token ${firstUser.token}`) {
@@ -86,7 +86,7 @@ export default function () {
   });
 
   this.get('/users/permissions', (schema, request) => {
-    const { authorization } = request.requestHeaders;
+    const authorization = request.requestHeaders.Authorization;
 
     if (!authorization) {
       return {};
