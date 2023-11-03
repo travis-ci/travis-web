528077c2fc05d018000ce66611f6848dc1efacdf
Maciej Kempin <maciej.kempin@gmail.com>
2023-11-03 12:30:54 +0000

    bunch of updates

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -1,87 +1,107 @@
 import { scheduleOnce } from '@ember/runloop';
 import Component from '@ember/component';
 import Ember from 'ember';
-import { computed, setProperties, set } from '@ember/object';
+import { computed, setProperties, set, action } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
-import InViewportMixin from 'ember-in-viewport';
+export default class TopBar extends Component {
+  @service auth;
+  @service store;
+  @service externalLinks;
+  @service features;
+  @service flashes;
+  @service router;
+  @service storage;
+  @service inViewport
 
-export default Component.extend(InViewportMixin, {
-  auth: service(),
-  store: service(),
-  externalLinks: service(),
-  features: service(),
-  flashes: service(),
-  router: service(),
-  storage: service(),
+  tagName = 'header';
+  classNames = ['top'];
+  classNameBindings = ['isWhite:top--white'];
+  isWhite = false;
+  landingPage = false;
+  isNavigationOpen = false;
+  isActivation = false;
 
-  tagName: 'header',
-  classNames: ['top'],
-  classNameBindings: ['isWhite:top--white'],
-  isWhite: false,
-  landingPage: false,
-  isNavigationOpen: false,
-  isActivation: false,
+  activeModel = null;
+  @reads('activeModel') model;
 
-  activeModel: null,
-  model: reads('activeModel'),
+  @reads('auth.currentUser') user;
 
-  user: reads('auth.currentUser'),
-  isUnconfirmed: computed('user.confirmedAt', function () {
+  @computed('user.confirmedAt')
+  get isUnconfirmed() {
     if (!this.user ||
-        (this.storage.wizardStep > 0 && this.storage.wizardStep <= 1) ||
-        this.router.currentRouteName == 'first_sync' ||
-        this.router.currentRouteName == 'github_apps_installation')
+      (this.storage.wizardStep > 0 && this.storage.wizardStep <= 1) ||
+      this.router.currentRouteName == 'first_sync' ||
+      this.router.currentRouteName == 'github_apps_installation') {
       return false;
+    }
     return !this.user.confirmedAt;
-  }),
+  }
 
-  userName: computed('user.{login,name}', function () {
-    let login = this.get('user.login');
-    let name = this.get('user.name');
+  @computed('user.{login,name}')
+  get userName() {
+    let login = this.user.login;
+    let name = this.user.name;
     return name || login;
-  }),
+  }
 
-  showCta: computed('auth.signedIn', 'landingPage', 'features.landingPageCta', function () {
-    let signedIn = this.get('auth.signedIn');
+  @computed('auth.signedIn', 'landingPage', 'features.landingPageCta')
+  get showCta() {
+    let signedIn = this.auth.signedIn;
     let landingPage = this.landingPage;
-    let ctaEnabled = this.get('features.landingPageCta');
+    let ctaEnabled = this.features.landingPageCta;
 
     return !signedIn && !landingPage && ctaEnabled;
-  }),
+  }
 
-  hasNoPlan: computed('model.allowance.subscriptionType', 'model.hasV2Subscription', 'model.subscription', function () {
-    return !this.get('model.hasV2Subscription') && this.get('model.subscription') === undefined && this.get('model.allowance.subscriptionType') === 3;
-  }),
+  @computed('model.allowance.subscriptionType', 'model.hasV2Subscription', 'model.subscription')
+  get hasNoPlan() {
+    if(!this.model) return true;
+    return !this.model.hasV2Subscription && this.model.subscription === undefined && this.model.allowance.subscriptionType === 3;
+  }
+
+  @action
+  setupInViewport() {
+    const loader = document.getElementById('loader');
+    const viewportTolerance = { bottom: 200 };
+    const { onEnter, _onExit } = this.inViewport.watchElement(loader, { viewportTolerance });
+    // pass the bound method to `onEnter` or `onExit`
+    onEnter(this.didEnterViewport.bind(this));
+  }
+
+  willDestroy() {
+    // need to manage cache yourself if you don't use the mixin
+    const loader = document.getElementById('loader');
+    this.inViewport.stopWatching(loader);
+
+    super.willDestroy(...arguments);
+  }
 
   didInsertElement() {
     if (Ember.testing) {
-      this._super(...arguments);
+      super.didInsertElement(...arguments);
       return;
     }
 
-    setProperties(this, {
-      viewportSpy: true
-    });
-    this._super(...arguments);
+    set(this, 'viewportSpy', true);
+    super.didInsertElement(...arguments);
     scheduleOnce('afterRender', this, () => {
       const { clientHeight = 76 } = this.element;
       set(this, 'viewportTolerance.top', clientHeight);
     });
-  },
+  }
 
   didEnterViewport() {
     this.flashes.set('topBarVisible', true);
-  },
+  }
 
   didExitViewport() {
     this.flashes.set('topBarVisible', false);
-  },
-
-  actions: {
-    toggleNavigation() {
-      this.toggleProperty('isNavigationOpen');
-    }
   }
-});
+
+  @action
+  toggleNavigation() {
+    this.toggleProperty('isNavigationOpen');
+  }
+}
