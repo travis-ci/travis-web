1f85a1433ad91843113e359a113de4b589fef6c0
Maciej Kempin <maciej.kempin@gmail.com>
2023-11-06 13:10:16 +0000

    skip right

--- tests/acceptance/marketing/travisci-vs-jenkins-test.js
+++ tests/acceptance/marketing/travisci-vs-jenkins-test.js
@@ -9,7 +9,7 @@ import {
   SALES_CONTACT_FORM_CONTAINER,
   SALES_CONTACT_IFRAME
 } from 'travis/tests/helpers/selectors';
-import {skip} from "ember-qunit/addon-test-support/qunit";
+import {skip} from "qunit";
 
 // Main page
 export const PAGE_URL = '/travisci-vs-jenkins';
