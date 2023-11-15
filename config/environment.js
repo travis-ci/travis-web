080145fa8733bcda5322713bac02b2f8208de974
Maciej Kempin <maciej.kempin@gmail.com>
2023-11-15 15:40:12 +0000

    v4.0.1...v4.12.2

--- config/environment.js
+++ config/environment.js
@@ -45,13 +45,10 @@ module.exports = function (environment) {
     screens,
     tailwind,
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false,
       },
     },
 
