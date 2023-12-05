2cee5aca379cf7edbd9d9b64ee77c0f8cbadf0db
Maciej Kempin <maciej.kempin@gmail.com>
2023-12-05 20:56:51 +0000

    push in serializer fix

--- app/serializers/plan.js
+++ app/serializers/plan.js
@@ -2,10 +2,10 @@ import ApplicationSerializer from "./application";
 
 export default ApplicationSerializer.extend({
 
-  pushPayload(store, payloads) {
-    const applicationSerializer = store.serializerFor('application');
-    const json = applicationSerializer.normalizeArrayResponse(store, store.modelFor('plan'), payloads);
-    store.push(json);
-  }
-
 });
+
+export function pushPayload(store, payloads) {
+  const applicationSerializer = store.serializerFor('application');
+  const json = applicationSerializer.normalizeArrayResponse(store, store.modelFor('plan'), payloads);
+  store.push(json);
+}
