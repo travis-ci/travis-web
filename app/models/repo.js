5ce600086afc178f4906fe105789cb69db57969f
AndriiMysko <andrii.mysko@devtactics.net>
2024-05-13 11:16:31 +0500

    Implement release event

--- app/models/repo.js
+++ app/models/repo.js
@@ -147,12 +147,47 @@ const Repo = VcsEntity.extend({
   builds: computed('id', function () {
     let id = this.id;
     const builds = this.store.filter('build', {
-      event_type: ['push', 'api', 'cron'],
+      event_type: ['push', 'api', 'cron', 'release'],
       repository_id: id,
     }, (b) => {
-      let eventTypes = ['push', 'api', 'cron'];
+      let eventTypes = ['push', 'api', 'cron', 'release'];
       return this._buildRepoMatches(b, id) && eventTypes.includes(b.get('eventType'));
     });
+
+    this.store.subscribe(builds, 'build', {
+      event_type: ['push', 'api', 'cron', 'release'],
+      repository_id: id,
+    }, (b) => {
+      let eventTypes = ['push', 'api', 'cron', 'release'];
+      return this._buildRepoMatches(b, id) && eventTypes.includes(b.get('eventType'));
+    });
+
+    return builds;
+    // return this._buildObservableArray(builds);
+  }),
+
+  _requestRepoMatches(request, id) {
+    return `${request.get('repo.id')}` === `${id}`;
+  },
+
+  _requestObservableArray(requests) {
+    const array = ExpandableRecordArray.create({
+      type: 'request',
+      content: []
+    });
+    array.load(requests);
+    return array.observe(requests);
+  },
+
+  requests: computed('id', function () {
+    let id = this.id;
+    const requests = this.store.filter(
+      'request',
+      { repository_id: id },
+      (b) => this._requestRepoMatches(b, id));
+    this.store.subscribe(requests, 'request', {repository_id: id}, (b) => this._requestRepoMatches(b, id));
+
+    return requests; // this._requestObservableArray(requests);
     return this._buildObservableArray(builds);
   }),
 
