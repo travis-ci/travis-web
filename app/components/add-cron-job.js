b05259bf2dedac599765957cba194473e08d7d72
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2024-06-18 14:48:37 +0200

    logs

--- app/components/add-cron-job.js
+++ app/components/add-cron-job.js
@@ -22,6 +22,7 @@ export default Component.extend(BranchSearching, {
   init() {
     this.reset();
     this._super(...arguments);
+    console.log(this.args.repository); // Log the @repository argument
   },
 
   reset() {
