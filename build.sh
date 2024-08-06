90082cd246c9777363b18cbd6aae0bf6466f7485
Dominik <dominik.alberski@gmail.com>
2024-08-06 14:43:05 +0200

    Make small script adjustments

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
-if [ ! -z AIDA_URL ]; then
-  curl -o /app/node_modules/asktravis/dist/aida.js $AIDA_URL
-  curl -o /app/node_modules/asktravis/dist/aida.js.map $AIDA_URL.map
+#!/bin/bash
+
+# Check if AIDA_URL is not empty
+echo "AIDA_URL: $AIDA_URL"
+if [ -n "$AIDA_URL" ]; then
+  curl -o /app/node_modules/asktravis/dist/aida.js "$AIDA_URL"
+  curl -o /app/node_modules/asktravis/dist/aida.js.map "$AIDA_URL.map"
 fi
+
+# Always build the Ember application
 ember build --environment=${ENV:-production}
