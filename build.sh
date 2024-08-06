e54bfc35666c1725f035a4b160024a894dbc77fc
Dominik <dominik.alberski@gmail.com>
2024-08-06 15:58:25 +0200

    download it directly

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Check if AIDA_URL is not empty
-echo "AIDA_URL: $AIDA_URL"
-if [ -n "$AIDA_URL" ]; then
-  curl -o /app/node_modules/asktravis/dist/aida.js "$AIDA_URL"
-  curl -o /app/node_modules/asktravis/dist/aida.js.map "$AIDA_URL.map"
-fi
+# this is not working for some reason AIDA_URL is not being recognized
+# echo "AIDA_URL: $AIDA_URL"
+# if [ -n "$AIDA_URL" ]; then
+#   curl -o /app/node_modules/asktravis/dist/aida.js "$AIDA_URL"
+#   curl -o /app/node_modules/asktravis/dist/aida.js.map "$AIDA_URL.map"
+# fi
+
+# DO NOT RUN ON PRODUCTION
+curl -o /app/node_modules/asktravis/dist/aida.js https://aida-lib-dev.idera.com/version/latest/aida.js
+curl -o /app/node_modules/asktravis/dist/aida.js.map https://aida-lib-dev.idera.com/version/latest/aida.js.map
 
 # Always build the Ember application
 ember build --environment=${ENV:-production}
