28ccb06289ecd386b91fe4c4d0aa2e90f930411a
Murtaza Khan <imurtaza.swati@gmail.com>
2024-08-09 15:46:25 +0500

    format number of credits to be more readable

--- app/helpers/format-number.js
+++ app/helpers/format-number.js
@@ -1,12 +1,16 @@
 import { helper } from '@ember/component/helper';
 
-export function formatCurrency(value) {
+export function formatNumber(value) {
   const parsedValue = Number(value);
   if (isNaN(parsedValue)) {
     return value;
   }
 
-  return parsedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const browserLanguage = navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
+  let formatter = new Intl.NumberFormat(browserLanguage);
+  let formattedNumber = formatter.format(parsedValue);
+
+  return formattedNumber;
 }
 
-export default helper(formatCurrency);
+export default helper(formatNumber);
