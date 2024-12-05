6601bc4b771afd54acce9f969461b8af857fc2a2
tonoyansergey-devt <ssergey.tonoyan@devtactics.net>
2024-12-05 16:33:58 +0400

    TBT-230 Remove unnecessary semicolon

--- app/utils/computed-is-current-trial.js
+++ app/utils/computed-is-current-trial.js
@@ -4,4 +4,4 @@ export default function () {
   return computed('subscription.current_trial', function () {
     return this.subscription.current_trial != null && this.subscription.current_trial.status === 'subscribed';
   });
-};
+}
