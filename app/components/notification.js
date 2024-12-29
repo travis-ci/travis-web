a96e9f65f9dfbfd6814733d25c6dac46b968180e
tonoyansergey-devt <ssergey.tonoyan@devtactics.net>
2024-12-29 21:25:35 +0400

    TBT-140 Banners/Notification System Fixes

---
+++ app/components/notification.js
@@ -0,0 +1,46 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { notEmpty,equal } from '@ember/object/computed';
+
+export default Component.extend({
+  tagName: '',
+
+  type: '',
+  severity: 'error',
+  style: '',
+  icon: '',
+
+  hasIcon: notEmpty('icon'),
+  isGlobal: equal('type', 'global'),
+                                /*
+  isGlobal: computed('type', function() {
+    console.log(`global: ${this.type == 'global'}`)
+    return this.type == 'global';
+  }),*/
+
+  getNotificationClass: computed('type', 'severity', function() {
+    const sev = this.severity == '' ? 'error' : this.severity;
+    const pre = this.type == 'global' ? 'global-' : '';
+    return `${pre}notification-${sev}`;
+  }),
+
+  getIcon: computed('icon', function() {
+    return this.icon;
+  }),
+
+  getIconClass: computed('icon', function() {
+    let iconClass=this.icon;
+    switch(this.severity) {
+    case 'warning':
+      iconClass += ` ${this.icon}-yellow`;
+      break;
+    case 'info':
+      iconClass += ` ${this.icon}-alert-info`;
+      break;
+    default:
+    }
+    return iconClass;
+  }),
+
+
+});
