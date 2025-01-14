ca329f69abe20780725fb1c9f0552735102b6aca
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2025-01-14 08:19:30 +0100

    set key in local storage

--- app/components/temporary-announcement-banner.js
+++ app/components/temporary-announcement-banner.js
@@ -1,17 +1,22 @@
 import Component from '@ember/component';
 import config from 'travis/config/environment';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  storage: service(),
+  bannerKey: 'travis.temporary-announcement-banner',
+
   message: '',
   enabled: false,
 
   init() {
     this._super(...arguments);
-    this.set('enabled', config.tempBanner.tempBannerEnabled === 'true');
+    this.set('enabled', config.tempBanner.tempBannerEnabled === 'true' && !this.storage.getItem(this.bannerKey));
     this.set('message', config.tempBanner.tempBannerMessage || '');
   },
 
   closeBanner() {
+    this.storage.setItem(this.bannerKey, 'true');
     this.set('enabled', false);
   }
 });
