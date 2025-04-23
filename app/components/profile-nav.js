0209fdcd823c6a1e3671e60c05c4d9e8e9391e16
nikolaTrichkovski <nikola.trichkovski@devtactics.net>
2025-04-23 08:59:51 +0200

    typo fix

--- app/components/profile-nav.js
+++ app/components/profile-nav.js
@@ -106,7 +106,7 @@ export default Component.extend({
     }),
 
   isSharePlanTabDisabled: computed('model.v2subscription.isCanceled', 'model.v2subscription.isExpired',
-    'model.v2subscription.current_trial', 'model.v2subscription.plan.isFree', 'model.isUser', 'model.isOrganzaition', function () {
+    'model.v2subscription.current_trial', 'model.v2subscription.plan.isFree', 'model.isUser', 'model.isOrganization', function () {
       const isCanceled = this.model.v2subscription?.isCanceled;
       const isExpired = this.model.v2subscription?.isExpired;
       const isOnTrialOrFree = !!(
